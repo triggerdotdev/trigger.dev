dee0cf71fc0d069ba7d8c77e3af9770c74e113ac
Matt Aitken <matt@mattaitken.com>
2022-12-06 16:16:11 +0000

    Initial Propel auth implementation, but isn’t working server-side which is what I want

--- apps/webapp/app/env.server.ts
+++ apps/webapp/app/env.server.ts
@@ -8,6 +8,7 @@ const EnvironmentSchema = z.object({
       "https://a014169306c748b1adf61875c64b90de:a7fa7bfcc28d43e1bd293e121c677e4a@o4504169280569344.ingest.sentry.io/4504169281880064"
     ),
   POSTHOG_PROJECT_KEY: z.string().optional(),
+  AUTH_URL: z.string(),
 });
 
 export type Environment = z.infer<typeof EnvironmentSchema>;
--- apps/webapp/app/routes/__app.tsx
+++ apps/webapp/app/routes/__app.tsx
@@ -1,28 +1,34 @@
+import {
+  AuthProvider,
+  RedirectToLogin,
+  RequiredAuthProvider,
+} from "@propelauth/react";
 import { Outlet } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
+import { env } from "../env.server";
 import { typedjson } from "remix-typedjson";
-import type { UseDataFunctionReturn } from "remix-typedjson/dist/remix";
-import { Header } from "~/components/Header";
+import { useTypedLoaderData } from "remix-typedjson/dist/remix";
 import { clearRedirectTo, commitSession } from "~/services/redirectTo.server";
 
-export type LoaderData = UseDataFunctionReturn<typeof loader>;
-
-export async function loader({ request }: LoaderArgs) {
+export const loader = async ({ request }: LoaderArgs) => {
   return typedjson(
-    {},
+    { authUrl: env.AUTH_URL },
     {
       headers: {
         "Set-Cookie": await commitSession(await clearRedirectTo(request)),
       },
     }
   );
-}
+};
 
 export default function AppLayout() {
+  const { authUrl } = useTypedLoaderData<typeof loader>();
+
   return (
-    <div className="flex h-screen flex-col overflow-auto">
-      <Header>Home</Header>
-      <Outlet />
-    </div>
+    <AuthProvider authUrl={authUrl}>
+      <div className="flex h-screen flex-col overflow-auto">
+        <Outlet />
+      </div>
+    </AuthProvider>
   );
 }
