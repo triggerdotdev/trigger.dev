d12f6ec386be8ec94caf0e584d4821753e6ab831
Matt Aitken <matt@mattaitken.com>
2022-12-09 19:46:50 +0000

    Having issues with the Pizzly client side package… ESM modules ugh

---
+++ apps/webapp/app/routes/__app/orgs/$organizationSlug/integrations.tsx
@@ -0,0 +1,29 @@
+import { useCallback } from "react";
+import * as Pizzly from "@nangohq/pizzly-frontend";
+
+export function Integrations() {
+  const authenticateWithGitHub = useCallback(async () => {
+    const pizzly = new Pizzly("http://localhost:3004");
+    pizzly
+      .auth("github", "test-connection")
+      .then((result: any) => {
+        console.log(
+          `OAuth flow succeeded for provider "${result.providerConfigKey}" and connection-id "${result.connectionId}"!`
+        );
+      })
+      .catch((error: any) => {
+        console.error(
+          `There was an error in the OAuth flow for integration "${error.providerConfigKey}" and connection-id "${error.connectionId}": ${error.error.type} - ${error.error.message}`
+        );
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Integrations</h1>
+      <button onClick={() => authenticateWithGitHub()}>
+        Connect to GitHub
+      </button>
+    </div>
+  );
+}
---
+++ apps/webapp/types/pizzly.d.ts
@@ -0,0 +1,1 @@
+declare module "@nangohq/pizzly-frontend";
