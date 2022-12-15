f41135fcf730f145bf39883187f7cc4d684e40bd
James Ritchie <james@jamesritchie.co.uk>
2022-12-15 10:52:03 +0000

    Added a new members page

---
+++ apps/webapp/app/routes/__app/orgs/$organizationSlug/members.tsx
@@ -0,0 +1,8 @@
+import { Header1 } from "~/components/primitives/text/Headers";
+export default function Integrations() {
+  return (
+    <div>
+      <Header1 className="text-white">Members</Header1>
+    </div>
+  );
+}
