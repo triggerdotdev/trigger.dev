826d1dfb9d6003e1021e8eb74d77201da57c033f
James Ritchie <james@jamesritchie.co.uk>
2022-12-17 11:58:33 +0000

    Added a back button to the side nav (this is the functionailty i figured makes mode sense but TBD)

--- apps/webapp/app/components/navigation/SideMenu.tsx
+++ apps/webapp/app/components/navigation/SideMenu.tsx
@@ -3,13 +3,14 @@ import {
   ArrowsRightLeftIcon,
   UsersIcon,
   ForwardIcon,
+  ChevronLeftIcon,
 } from "@heroicons/react/24/outline";
 import { Link, NavLink } from "@remix-run/react";
 import {
   useCurrentOrganization,
   useOrganizations,
 } from "~/hooks/useOrganizations";
-import { useCurrentWorkflow, useWorkflows } from "~/hooks/useWorkflows";
+import { useCurrentWorkflow } from "~/hooks/useWorkflows";
 import { Header1 } from "../primitives/text/Headers";
 
 export function SideMenuContainer({ children }: { children: React.ReactNode }) {
@@ -94,9 +95,21 @@ function SideMenu({ title, items }: { title: string; items: SideMenuItem[] }) {
           className="mt-2 flex-1 space-y-1 bg-slate-1000 px-2"
           aria-label="Sidebar"
         >
-          <Header1 size="regular" className="p-2 my-2">
-            <Link to="">{title}</Link>
-          </Header1>
+          <div className="group flex items-center my-2 text-slate-400 rounded hover:bg-slate-900 transition divide-x divide-transparent hover:divide-slate-900">
+            <Link
+              to="/"
+              className="px-2 py-3 hover:bg-slate-800 rounded-l transition"
+            >
+              <ChevronLeftIcon className="h-5 w-5 text-slate-400" />
+            </Link>
+
+            <Header1
+              size="regular"
+              className="pl-2 py-2 w-full text-slate-400 rounded-r hover:bg-slate-800 transition"
+            >
+              <Link to="">{title}</Link>
+            </Header1>
+          </div>
 
           {items.map((item) => (
             <NavLink
