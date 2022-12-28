380bbc4563389fc29c329d36c37f581cd0be044c
Matt Aitken <matt@mattaitken.com>
2022-12-28 15:08:03 +0000

    useWorkflows gets richer object from useMatches

--- apps/webapp/app/hooks/useWorkflows.ts
+++ apps/webapp/app/hooks/useWorkflows.ts
@@ -1,4 +1,6 @@
+import type { UseDataFunctionReturn } from "remix-typedjson/dist/remix";
 import type { Workflow } from "~/models/workflow.server";
+import type { loader } from "~/routes/__app/orgs/$organizationSlug/workflows/$workflowSlug";
 import { hydrateObject, useMatchesData } from "~/utils";
 
 export function useWorkflows(): Workflow[] | undefined {
@@ -13,20 +15,18 @@ export function useWorkflows(): Workflow[] | undefined {
   return workflows;
 }
 
-export function useCurrentWorkflowSlug(): string | undefined {
+export function useCurrentWorkflow(): Workflow | undefined {
   const routeMatch = useMatchesData(
     "routes/__app/orgs/$organizationSlug/workflows/$workflowSlug"
   );
-  return routeMatch?.params?.workflowSlug;
-}
 
-export function useCurrentWorkflow(): Workflow | undefined {
-  const workflows = useWorkflows();
-  const currentWorkflowSlug = useCurrentWorkflowSlug();
+  if (!routeMatch || !routeMatch.data.workflow) {
+    return undefined;
+  }
 
-  const currentWorkflow = workflows?.find(
-    (org) => org.slug === currentWorkflowSlug
-  );
+  const result = hydrateObject<
+    UseDataFunctionReturn<typeof loader>["workflow"]
+  >(routeMatch.data.workflow);
 
-  return currentWorkflow;
+  return result;
 }
