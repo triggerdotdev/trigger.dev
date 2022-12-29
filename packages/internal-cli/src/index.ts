320f5d2aa09d9b757499aa6692abcec7c86f6403
Matt Aitken <matt@mattaitken.com>
2022-12-29 18:19:10 +0000

    Pizzly integrated with the script to create/update configs

--- packages/internal-cli/src/index.ts
+++ packages/internal-cli/src/index.ts
@@ -7,6 +7,7 @@ import {
   SecretsManagerClient,
   GetSecretValueCommand,
 } from "@aws-sdk/client-secrets-manager";
+import { stringify } from "node:querystring";
 
 const client = new SecretsManagerClient({
   region: "us-east-1",
@@ -22,10 +23,16 @@ program
     "The file path to the integration file. Probably ../../apps/webapp/integrations.yml"
   )
   .option("-e, --environment <environment>", "The environment to update")
+  .option("-p, --pizzlyhost <pizzly_host>", "Pizzly host")
+  .option("-s, --pizzlysecretkey <pizzly_secret_key>", "Pizzly secret key")
   .action(
     async (
       integration_file_path: string,
-      options: { environment?: string }
+      options: {
+        environment?: string;
+        pizzly_host?: string;
+        pizzly_secret_key?: string;
+      }
     ) => {
       if (!integration_file_path) {
         console.error(
@@ -36,6 +43,7 @@ program
       }
 
       const environment = options.environment ?? "development";
+      const pizzly_host = options.pizzly_host ?? "http://localhost:3004";
 
       const file = fs.readFileSync(integration_file_path, "utf8");
       const catalog = getCatalog(file);
@@ -57,13 +65,41 @@ program
           const secretData = response.SecretString;
           invariant(secretData, `Secret data is missing: ${secretId}`);
           const secretObject = JSON.parse(secretData);
-          const parsed = z
+          const { client_secret } = z
             .object({
               client_secret: z.string(),
             })
             .parse(secretObject);
 
           console.log(`Found secret for id: ${secretId}`);
+
+          const hasExistingConfig = await hasConfig(
+            pizzly_host,
+            integration.slug,
+            options.pizzly_secret_key
+          );
+
+          if (hasExistingConfig) {
+            const response = await updateConfig(
+              pizzly_host,
+              integration.slug,
+              environmentClientId,
+              client_secret,
+              integration.scopes,
+              options.pizzly_secret_key
+            );
+            console.log(`Updated config for ${integration.slug}`);
+          } else {
+            const response = await createConfig(
+              pizzly_host,
+              integration.slug,
+              environmentClientId,
+              client_secret,
+              integration.scopes,
+              options.pizzly_secret_key
+            );
+            console.log(`Created config for ${integration.slug}`);
+          }
         } catch (error) {
           // For a list of exceptions thrown, see
           // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
@@ -76,6 +112,73 @@ program
     }
   );
 
-async function getConfig() {}
+async function hasConfig(pizzlyHost: string, slug: string, secretKey?: string) {
+  const response = await fetch(`${pizzlyHost}/config/${slug}`, {
+    headers: headers(secretKey),
+  });
+  const config = await response.json();
+
+  if ("error" in config) {
+    return false;
+  }
+
+  return true;
+}
+
+async function updateConfig(
+  pizzlyHost: string,
+  slug: string,
+  client_id: string,
+  client_secret: string,
+  scopes: string[],
+  secretKey?: string
+) {
+  const response = await fetch(`${pizzlyHost}/config`, {
+    method: "PUT",
+    headers: headers(secretKey),
+    body: JSON.stringify({
+      provider_config_key: slug,
+      provider: slug,
+      oauth_client_id: client_id,
+      oauth_client_secret: client_secret,
+      oauth_scopes: scopes,
+    }),
+  });
+  return response.ok;
+}
+
+async function createConfig(
+  pizzlyHost: string,
+  slug: string,
+  client_id: string,
+  client_secret: string,
+  scopes: string[],
+  secretKey?: string
+) {
+  const response = await fetch(`${pizzlyHost}/config`, {
+    method: "POST",
+    headers: headers(secretKey),
+    body: JSON.stringify({
+      provider_config_key: slug,
+      provider: slug,
+      oauth_client_id: client_id,
+      oauth_client_secret: client_secret,
+      oauth_scopes: scopes,
+    }),
+  });
+  return response.ok;
+}
+
+function headers(secretKey?: string) {
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json",
+  };
+  if (secretKey) {
+    headers.Authorization = `Basic ${Buffer.from(secretKey + ":").toString(
+      "base64"
+    )}`;
+  }
+  return headers;
+}
 
 program.parseAsync(process.argv);
