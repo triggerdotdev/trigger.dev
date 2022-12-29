cb2e1b7775266ccd7ca3543d927406a3799cc49d
Matt Aitken <matt@mattaitken.com>
2022-12-29 15:09:45 +0000

    Pass in the path to the integrations file

--- packages/internal-cli/src/index.ts
+++ packages/internal-cli/src/index.ts
@@ -2,9 +2,18 @@ import * as fs from "node:fs";
 import { getCatalog } from "internal-catalog";
 
 async function run() {
-  console.log("Start creating catalog entries...");
+  const args = process.argv.slice(2);
+  const [integrationFilePath] = args;
 
-  const file = fs.readFileSync("../../apps/webapp/integrations.yml", "utf8");
+  if (!integrationFilePath) {
+    console.error(
+      "Missing integration file path.",
+      "You probably want to pass in: ../../apps/webapp/integrations.yml"
+    );
+    return;
+  }
+
+  const file = fs.readFileSync(integrationFilePath, "utf8");
   const catalog = getCatalog(file);
   console.log(catalog);
 }
