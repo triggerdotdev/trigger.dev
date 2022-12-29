f4cfdcc74dec43524ee89aab88de03bb4026d87f
Matt Aitken <matt@mattaitken.com>
2022-12-29 17:10:34 +0000

    Using commander to make it easier to deal with arguments and options

--- packages/internal-cli/src/index.ts
+++ packages/internal-cli/src/index.ts
@@ -1,6 +1,5 @@
+import { Command } from "commander";
 import * as fs from "node:fs";
-import util from "node:util";
-const exec = util.promisify(require("child_process").exec);
 import { z } from "zod";
 import invariant from "tiny-invariant";
 import { getCatalog } from "internal-catalog";
@@ -13,69 +12,70 @@ const client = new SecretsManagerClient({
   region: "us-east-1",
 });
 
-async function run() {
-  const args = process.argv.slice(2);
-  const [integrationFilePath] = args;
+const program = new Command();
 
-  if (!integrationFilePath) {
-    console.error(
-      "Missing integration file path.",
-      "You probably want to pass in: ../../apps/webapp/integrations.yml"
-    );
-    return;
-  }
+program
+  .command("update")
+  .description("Update the catalog")
+  .argument(
+    "<integration_file_path>",
+    "The file path to the integration file. Probably ../../apps/webapp/integrations.yml"
+  )
+  .option("-e, --environment <environment>", "The environment to update")
+  .action(
+    async (
+      integration_file_path: string,
+      options: { environment?: string }
+    ) => {
+      if (!integration_file_path) {
+        console.error(
+          "Missing integration file path.",
+          "You probably want to pass in: ../../apps/webapp/integrations.yml"
+        );
+        return;
+      }
 
-  let environment = args[1];
-  if (!environment) {
-    environment = "development";
-  }
+      const environment = options.environment ?? "development";
 
-  console.log(`Environment: ${environment}`);
+      const file = fs.readFileSync(integration_file_path, "utf8");
+      const catalog = getCatalog(file);
 
-  const file = fs.readFileSync(integrationFilePath, "utf8");
-  const catalog = getCatalog(file);
+      const promises = catalog.map(async (integration) => {
+        const environmentClientId =
+          integration.environments[environment!].oauth.client_id;
+        const secretId = `integrations/${integration.slug}/${environmentClientId}`;
+        try {
+          console.log(`Finding secret for id: ${secretId}`);
 
-  const promises = catalog.map(async (integration) => {
-    const environmentClientId =
-      integration.environments[environment].oauth.client_id;
-    const secretId = `integrations/${integration.slug}/${environmentClientId}`;
-    try {
-      console.log(`Finding secret for id: ${secretId}`);
+          const response = await client.send(
+            new GetSecretValueCommand({
+              SecretId: secretId,
+              VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+            })
+          );
 
-      const response = await client.send(
-        new GetSecretValueCommand({
-          SecretId: secretId,
-          VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-        })
-      );
+          const secretData = response.SecretString;
+          invariant(secretData, `Secret data is missing: ${secretId}`);
+          const secretObject = JSON.parse(secretData);
+          const parsed = z
+            .object({
+              client_secret: z.string(),
+            })
+            .parse(secretObject);
 
-      const secretData = response.SecretString;
-      invariant(secretData, `Secret data is missing: ${secretId}`);
-      const secretObject = JSON.parse(secretData);
-      const parsed = z
-        .object({
-          client_secret: z.string(),
-        })
-        .parse(secretObject);
+          console.log(`Found secret for id: ${secretId}`);
+        } catch (error) {
+          // For a list of exceptions thrown, see
+          // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+          throw error;
+        }
+      });
 
-      const { stdout, stderr } = await exec(
-        `npx pizzly config:create ${integration.slug} ${
-          integration.slug
-        } ${environmentClientId} ${
-          parsed.client_secret
-        } "${integration.scopes.join(",")}"`
-      );
-      console.log("stdout:", stdout);
-      console.log("stderr:", stderr);
-    } catch (error) {
-      // For a list of exceptions thrown, see
-      // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-      throw error;
+      await Promise.all(promises);
+      console.log(`Added ${promises.length} secrets`);
     }
-  });
+  );
 
-  await Promise.all(promises);
-  console.log(`Added ${promises.length} secrets`);
-}
+async function getConfig() {}
 
-run();
+program.parseAsync(process.argv);
