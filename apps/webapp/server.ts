a4e43067bcc5de7c83a35b7f741bc3c07ab82cce
Eric Allam <eallam@icloud.com>
2022-12-30 19:57:25 +0000

    Remove fly specific code from express server

--- apps/webapp/server.ts
+++ apps/webapp/server.ts
@@ -14,7 +14,6 @@ const app = express();
 
 app.use((req, res, next) => {
   // helpful headers:
-  res.set("x-fly-region", process.env.FLY_REGION ?? "unknown");
   res.set("Strict-Transport-Security", `max-age=${60 * 60 * 24 * 365 * 100}`);
 
   // /clean-urls/ -> /clean-urls
@@ -25,33 +24,6 @@ app.use((req, res, next) => {
     return;
   }
   next();
-});
-
-// if we're not in the primary region, then we need to make sure all
-// non-GET/HEAD/OPTIONS requests hit the primary region rather than read-only
-// Postgres DBs.
-// learn more: https://fly.io/docs/getting-started/multi-region-databases/#replay-the-request
-app.all("*", function getReplayResponse(req, res, next) {
-  const { method, path: pathname } = req;
-  const { PRIMARY_REGION, FLY_REGION } = process.env;
-
-  const isMethodReplayable = !["GET", "OPTIONS", "HEAD"].includes(method);
-  const isReadOnlyRegion =
-    FLY_REGION && PRIMARY_REGION && FLY_REGION !== PRIMARY_REGION;
-
-  const shouldReplay = isMethodReplayable && isReadOnlyRegion;
-
-  if (!shouldReplay) return next();
-
-  const logInfo = {
-    pathname,
-    method,
-    PRIMARY_REGION,
-    FLY_REGION,
-  };
-  console.info(`Replaying:`, logInfo);
-  res.set("fly-replay", `region=${PRIMARY_REGION}`);
-  return res.sendStatus(409);
 });
 
 app.use(compression());
