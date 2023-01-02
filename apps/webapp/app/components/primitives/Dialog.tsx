652fe05038382175aa14e9c8f637137b1ca27723
Matt Aitken <matt@mattaitken.com>
2023-01-02 10:47:50 +0000

    Added Styled dialog

---
+++ apps/webapp/app/components/primitives/Dialog.tsx
@@ -0,0 +1,66 @@
+import { Dialog as HeadlessDialog, Transition } from "@headlessui/react";
+import classNames from "classnames";
+import { forwardRef, Fragment } from "react";
+
+type DialogProps = Parameters<typeof HeadlessDialog>[0] & {
+  children: React.ReactNode;
+};
+
+function Dialog({ onClose, children, ...props }: DialogProps) {
+  return (
+    <Transition {...props}>
+      <HeadlessDialog as="div" className="relative z-40" onClose={onClose}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              {children}
+            </Transition.Child>
+          </div>
+        </div>
+      </HeadlessDialog>
+    </Transition>
+  );
+}
+
+type PanelProps = Parameters<typeof HeadlessDialog.Panel>[0];
+const Panel = forwardRef((props: PanelProps, ref) => (
+  <HeadlessDialog.Panel
+    className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all"
+    {...props}
+    ref={ref}
+  />
+));
+Panel.displayName = "Dialog.Panel";
+
+type TitleProps = Parameters<typeof HeadlessDialog.Title>[0];
+const Title = forwardRef((props: TitleProps, ref) => (
+  <HeadlessDialog.Title
+    as="h3"
+    className="text-lg font-medium leading-6 text-gray-900"
+    {...props}
+    ref={ref}
+  />
+));
+Title.displayName = "Dialog.Title";
+
+export const StyledDialog = { Dialog, Panel, Title };
