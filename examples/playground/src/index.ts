cfc451ffaae4e5b07097656d3a38745d804e869d
Eric Allam <eallam@icloud.com>
2023-01-05 16:55:07 +0000

    Added a playground for James and updates react-email

---
+++ examples/playground/src/index.ts
@@ -0,0 +1,40 @@
+import { Trigger, customEvent } from "@trigger.dev/sdk";
+import { slack } from "@trigger.dev/integrations";
+import { z } from "zod";
+
+const trigger = new Trigger({
+  id: "playground",
+  name: "Trigger.dev Playground",
+  apiKey: "trigger_dev_zC25mKNn6c0q",
+  endpoint: "ws://localhost:8889/ws",
+  on: customEvent({
+    name: "playground",
+    schema: z.object({
+      id: z.string(),
+    }),
+  }),
+  run: async (event, ctx) => {
+    await ctx.logger.info(
+      "Hey there! This is the Trigger.dev Playground. You can use this to test your Trigger.dev code."
+    );
+
+    await ctx.waitFor("initial-wait", { minutes: 1 });
+
+    await ctx.logger.error("Error message!", { event });
+
+    await ctx.logger.info("Info message");
+
+    const response = await slack.postMessage("send-to-slack", {
+      channel: "test-integrations",
+      text: `This is a test message from the Trigger.dev Playground ${event.id}`,
+    });
+
+    await ctx.logger.debug("Debug message");
+
+    await ctx.logger.warn("Warning message!");
+
+    return response.message;
+  },
+});
+
+trigger.listen();
