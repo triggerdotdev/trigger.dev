2ef56e4ad048bebfc45d9fed91e5c09d9ee29b23
Eric Allam <eallam@icloud.com>
2023-01-05 11:36:23 +0000

    Sort runs by createdAt instead of startedAt

--- apps/webapp/app/models/workflowRunListPresenter.server.ts
+++ apps/webapp/app/models/workflowRunListPresenter.server.ts
@@ -86,7 +86,7 @@ export class WorkflowRunListPresenter {
         },
       },
       orderBy: {
-        startedAt: "desc",
+        createdAt: "desc",
       },
       skip: offset,
       take: pageSize,
