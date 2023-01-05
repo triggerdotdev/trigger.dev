cdec1263d3209ad3ab1a9521044eae8f9f9ef16a
James Ritchie <james@jamesritchie.co.uk>
2023-01-05 11:38:35 +0000

    Edited the Slack example to include more states

--- examples/send-to-slack/src/index.ts
+++ examples/send-to-slack/src/index.ts
@@ -21,17 +21,22 @@ const trigger = new Trigger({
       "Received domain.created event, waiting for 1 minutes..."
     );
 
-    await ctx.waitFor("initial-wait", { minutes: 5 });
+    await ctx.waitFor("initial-wait", { minutes: 1 });
+
+    await ctx.logger.error("Error message!", { event });
+
     await ctx.waitUntil("initial-wait-until", new Date(Date.now() + 1000 * 60));
 
-    await ctx.logger.info("Posting to Slack...");
+    await ctx.logger.info("Info message");
 
     const response = await slack.postMessage("send-to-slack", {
       channel: "test-integrations",
       text: `New domain created: ${event.domain} by customer ${event.customerId}`,
     });
 
-    await ctx.logger.info("Posted to Slack!");
+    await ctx.logger.debug("Debug message");
+
+    await ctx.logger.warn("Warning message!");
 
     return response.message;
   },
