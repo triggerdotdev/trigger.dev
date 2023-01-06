ea2b76c1c3e5ad57a9c380835686be07b9e3d0c2
James Ritchie <james@jamesritchie.co.uk>
2023-01-06 18:23:04 +0000

    Updated the playground workflows

--- examples/playground/src/index.ts
+++ examples/playground/src/index.ts
@@ -1,8 +1,8 @@
 import { Trigger, customEvent } from "@trigger.dev/sdk";
-import { slack } from "@trigger.dev/integrations";
+import { github, slack } from "@trigger.dev/integrations";
 import { z } from "zod";
 
-const trigger = new Trigger({
+new Trigger({
   id: "playground",
   name: "Trigger.dev Playground",
   apiKey: "trigger_dev_zC25mKNn6c0q",
@@ -35,6 +35,37 @@ const trigger = new Trigger({
 
     return response.message;
   },
-});
+}).listen();
 
-trigger.listen();
+new Trigger({
+  id: "playground-2",
+  name: "Github Issue changes for trigger.dev",
+  apiKey: "trigger_dev_zC25mKNn6c0q",
+  endpoint: "ws://localhost:8889/ws",
+  on: github.triggers.repoIssueEvent({ repo: "triggerdotdev/trigger.dev" }),
+
+  run: async (event, ctx) => {
+    await ctx.logger.info(
+      "Hey there! This is the Trigger.dev Playground. You can use this to test your Trigger.dev code."
+    );
+
+    await ctx.waitFor("initial-wait", { seconds: 30 });
+
+    await ctx.waitUntil("wait-until", new Date(Date.now() + 1000 * 30));
+
+    await ctx.logger.error("Error message!", { event });
+
+    await ctx.logger.info("Info message");
+
+    const response = await slack.postMessage("send-to-slack", {
+      channel: "test-integrations",
+      text: `This is a test message from the Trigger.dev Playground ${event.action}`,
+    });
+
+    await ctx.logger.debug("Debug message");
+
+    await ctx.logger.warn("Warning message!");
+
+    return response.message;
+  },
+}).listen();
