43245ff2013307dfe4ff38681428a9b5b9492f71
James Ritchie <james@jamesritchie.co.uk>
2023-01-06 17:50:10 +0000

    New copy and icons for the side panel

--- apps/webapp/app/components/LoginPromoPanel.tsx
+++ apps/webapp/app/components/LoginPromoPanel.tsx
@@ -2,25 +2,27 @@ import {
   EyeIcon,
   BoltIcon,
   BanknotesIcon,
-  BugAntIcon,
   BellAlertIcon,
+  WrenchScrewdriverIcon,
+  BeakerIcon,
+  CloudArrowUpIcon,
 } from "@heroicons/react/24/outline";
 
 export function LoginPromoPanel() {
   return (
-    <div className="hidden lg:flex flex-col justify-center p-12 min-w-[30vw] h-full bg-slate-950 border-r border-black/20">
+    <div className="hidden lg:flex flex-col justify-center p-12 max-w-[30vw] h-full bg-slate-950 border-r border-black/20">
       <ul>
         <li className="flex gap-2 text-white">
           <div className="flex flex-col items-center gap-2 mt-1.5">
-            <EyeIcon className="h-8 w-8 text-toxic" />
+            <WrenchScrewdriverIcon className="h-8 w-8 text-toxic" />
             <div className="w-0.5 h-full bg-toxic/50"></div>
           </div>
 
           <div className="mb-1">
-            <h2 className="font-semibold text-2xl">Full observability</h2>
-            <p className="mb-10 text-white/80">
-              View every single API request and response in real-time from your
-              dashboard.
+            <h2 className="font-semibold text-2xl mb-2">Create</h2>
+            <p className="mb-10 text-white/60">
+              Write workflows by creating triggers directly in your code. These
+              can be 3rd-party integrations, custom events or on a schedule.
             </p>
           </div>
         </li>
@@ -31,49 +33,37 @@ export function LoginPromoPanel() {
           </div>
 
           <div className="mb-1">
-            <h2 className="font-semibold text-2xl">Faster responses</h2>
-            <p className="mb-10 text-white/80">
-              Get less than 10ms response times.
+            <h2 className="font-semibold text-2xl mb-2">Run</h2>
+            <p className="mb-10 text-white/60">
+              When your server runs, your workflow will be registered and you
+              can authenticate with any APIs you’re using.
             </p>
           </div>
         </li>
         <li className="flex gap-2 text-white">
           <div className="flex flex-col items-center gap-2 mt-1.5">
-            <BanknotesIcon className="h-8 w-8 text-toxic" />
+            <BeakerIcon className="h-8 w-8 text-toxic" />
             <div className="w-0.5 h-full bg-toxic/50"></div>
           </div>
-
           <div className="mb-1">
-            <h2 className="font-semibold text-2xl">Save money</h2>
-            <p className="mb-10 text-white/80">
-              Avoid annoying rate limits by enabling caching – while also saving
-              money.
+            <h2 className="font-semibold text-2xl mb-2">Test</h2>
+            <p className="mb-10 text-white/60">
+              Test your workflow by triggering them manually in your dashboard.
+              Follow it as it runs step-by-step.
             </p>
           </div>
         </li>
-        <li className="flex gap-2 text-white">
-          <div className="flex flex-col items-center gap-2 mt-1.5">
-            <BugAntIcon className="h-8 w-8 text-toxic" />
-            <div className="w-0.5 h-full bg-toxic/50"></div>
-          </div>
 
-          <div className="mb-1">
-            <h2 className="font-semibold text-2xl">Problem solved</h2>
-            <p className="mb-10 text-white/80">
-              We deal with CORS issues for you.
-            </p>
-          </div>
-        </li>
         <li className="flex gap-3 text-white">
           <div className="flex flex-col items-center gap-2 mt-1.5">
-            <BellAlertIcon className="ml-1.5 h-5 w-5 text-toxic" />
+            <CloudArrowUpIcon className="ml-0.5 h-7 w-7 text-toxic" />
           </div>
 
           <div className="mb-1">
-            <h2 className="font-semibold text-2xl">Automatic alerts</h2>
-            <p className="mb-10 text-white/80">
-              Get alerts when issues arise and debug them using our inline help
-              prompts.
+            <h2 className="font-semibold text-2xl mb-2">Deploy</h2>
+            <p className="mb-10 text-white/60">
+              Deploy your new workflow as you would any other code commit and
+              inspect each workflow run in real time.
             </p>
           </div>
         </li>
