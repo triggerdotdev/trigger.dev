61bbe89ec0b9059c2d7111cf409a8d23d78da8b2
James Ritchie <james@jamesritchie.co.uk>
2023-01-08 19:00:10 +0000

    Added sizes to the api logo icons

--- apps/webapp/app/components/code/ApiLogoIcon.tsx
+++ apps/webapp/app/components/code/ApiLogoIcon.tsx
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import type { CatalogIntegration } from "internal-catalog";
 
 type Props = Omit<
@@ -9,18 +8,46 @@ type Props = Omit<
   "src" | "alt"
 > & {
   integration?: CatalogIntegration;
+  size?: Size;
 };
 
-export function ApiLogoIcon({ className, integration, ...props }: Props) {
+type Size = "extra-small" | "small" | "regular" | "large" | "extra-large";
+
+export function ApiLogoIcon({
+  className,
+  integration,
+  size = "extra-large",
+  ...props
+}: Props) {
   if (!integration) {
     return null;
   }
   return (
     <img
-      className={classNames("rounded p-2 h-10 w-10 bg-slate-900/50", className)}
+      className={`
+        rounded p-2 bg-slate-900/50
+        ${getSizeClassName(size)}
+        ${className}
+      `}
       src={integration.icon}
       alt={integration.name}
       {...props}
     />
   );
+}
+
+function getSizeClassName(size: Size) {
+  switch (size) {
+    case "extra-small":
+      return "h-6 w-6";
+    case "small":
+      return "h-8 w-8";
+    case "large":
+      return "h-16 w-16";
+    case "extra-large":
+      return "h-20 w-20";
+    case "regular":
+    default:
+      return "h-10 w-10";
+  }
 }
