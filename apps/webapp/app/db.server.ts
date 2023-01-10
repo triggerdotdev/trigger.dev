59e8b364f648f035b3efd20003bd6f145d2fb7af
Eric Allam <eallam@icloud.com>
2023-01-10 18:10:19 +0000

    Remove the fly stuff from db.server

--- apps/webapp/app/db.server.ts
+++ apps/webapp/app/db.server.ts
@@ -27,32 +27,12 @@ function getClient() {
   const { DATABASE_URL } = env;
   invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
 
-  const databaseUrl = new URL(DATABASE_URL);
+  console.log(`🔌 setting up prisma client to ${DATABASE_URL}`);
 
-  const isLocalHost = databaseUrl.hostname === "localhost";
-
-  const PRIMARY_REGION = isLocalHost ? null : env.PRIMARY_REGION;
-  const FLY_REGION = isLocalHost ? null : env.FLY_REGION;
-
-  const isReadReplicaRegion = !PRIMARY_REGION || PRIMARY_REGION === FLY_REGION;
-
-  if (!isLocalHost) {
-    databaseUrl.host = `${FLY_REGION}.${databaseUrl.host}`;
-    if (!isReadReplicaRegion) {
-      // 5433 is the read-replica port
-      databaseUrl.port = "5433";
-    }
-  }
-
-  console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
-  // NOTE: during development if you change anything in this function, remember
-  // that this only runs once per server restart and won't automatically be
-  // re-run per request like everything else is. So if you need to change
-  // something in this file, you'll need to manually restart the server.
   const client = new PrismaClient({
     datasources: {
       db: {
-        url: databaseUrl.toString(),
+        url: DATABASE_URL,
       },
     },
   });
