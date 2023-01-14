445ba1ba478b08448d82f0e096cc6cf17a86df52
James Ritchie <james@jamesritchie.co.uk>
2023-01-14 14:07:24 +0000

    Made the Connected API list more consistent - now using a List component

--- apps/webapp/app/components/integrations/WorkflowConnections.tsx
+++ apps/webapp/app/components/integrations/WorkflowConnections.tsx
@@ -2,6 +2,7 @@ import invariant from "tiny-invariant";
 import { useConnectionSlots } from "~/hooks/useConnectionSlots";
 import { useCurrentOrganization } from "~/hooks/useOrganizations";
 import { ApiLogoIcon } from "../code/ApiLogoIcon";
+import { List } from "../layout/List";
 import { Panel } from "../layout/Panel";
 import { Body } from "../primitives/text/Body";
 import { Header3 } from "../primitives/text/Headers";
@@ -35,51 +36,52 @@ export function WorkflowConnections() {
           {unconnectedApisCount === 0 ? "" : unconnectedApisCountCopy}
         </>
       </SubTitle>
-      <Panel className="mb-4 py-0">
-        <div className="divide-y divide-slate-700">
-          {connectionSlots.source && (
-            <div className="flex gap-4 w-full py-3">
-              <ApiLogoIcon
+      <List>
+        {connectionSlots.source && (
+          <li className="flex gap-4 w-full px-4 py-4">
+            <ApiLogoIcon
+              integration={connectionSlots.source.integration}
+              size="regular"
+            />
+            <div className="flex items-center justify-between gap-1 w-full">
+              <Body>{connectionSlots.source.integration.name}</Body>
+              <ConnectionSelector
+                type="source"
+                sourceServiceId={connectionSlots.source.id}
+                organizationId={organization.id}
                 integration={connectionSlots.source.integration}
-                size="regular"
+                connections={connectionSlots.source.possibleConnections}
+                selectedConnectionId={connectionSlots.source.connection?.id}
+                className="mr-1"
+                popoverAlign="right"
               />
-              <div className="flex items-center justify-between gap-1 w-full">
-                <Body>{connectionSlots.source.integration.name}</Body>
-                <ConnectionSelector
-                  type="source"
-                  sourceServiceId={connectionSlots.source.id}
-                  organizationId={organization.id}
-                  integration={connectionSlots.source.integration}
-                  connections={connectionSlots.source.possibleConnections}
-                  selectedConnectionId={connectionSlots.source.connection?.id}
-                  className="mr-1"
-                  popoverAlign="right"
-                />
-              </div>
             </div>
-          )}
-          {connectionSlots.services.map((slot) => (
-            <div key={slot.id} className="flex gap-4 items-center w-full py-3">
-              <ApiLogoIcon integration={slot.integration} size="regular" />
-              <div className="flex items-center justify-between w-full">
-                <Header3 size="small" className="truncate text-slate-300">
-                  {slot.integration?.name}
-                </Header3>
-                <ConnectionSelector
-                  type="service"
-                  sourceServiceId={slot.id}
-                  organizationId={organization.id}
-                  integration={slot.integration}
-                  connections={slot.possibleConnections}
-                  selectedConnectionId={slot.connection?.id}
-                  className="mr-1"
-                  popoverAlign="right"
-                />
-              </div>
+          </li>
+        )}
+        {connectionSlots.services.map((slot) => (
+          <li
+            key={slot.id}
+            className="flex gap-4 items-center w-full px-4 py-4"
+          >
+            <ApiLogoIcon integration={slot.integration} size="regular" />
+            <div className="flex items-center justify-between w-full">
+              <Header3 size="small" className="truncate text-slate-300">
+                {slot.integration?.name}
+              </Header3>
+              <ConnectionSelector
+                type="service"
+                sourceServiceId={slot.id}
+                organizationId={organization.id}
+                integration={slot.integration}
+                connections={slot.possibleConnections}
+                selectedConnectionId={slot.connection?.id}
+                className="mr-1"
+                popoverAlign="right"
+              />
             </div>
-          ))}
-        </div>
-      </Panel>
+          </li>
+        ))}
+      </List>
     </>
   );
 }
