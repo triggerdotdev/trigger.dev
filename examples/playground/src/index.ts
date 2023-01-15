5a9641819422c18ebf02032a60b71eaf3ef28f45
James Ritchie <james@jamesritchie.co.uk>
2023-01-14 20:27:50 -0800

    Added new workflow to playground

--- examples/playground/src/index.ts
+++ examples/playground/src/index.ts
@@ -258,3 +258,34 @@ new Trigger({
     return {};
   },
 }).listen();
+
+const postMessage = new Trigger({
+  id: "new-user",
+  name: "New user slack message",
+  apiKey: "<your_api_key>",
+  logLevel: "info",
+  on: customEvent({
+    name: "user.created",
+    schema: z.object({
+      name: z.string(),
+      email: z.string(),
+      paidPlan: z.boolean(),
+    }),
+  }),
+  run: async (event, ctx) => {
+    await ctx.logger.info("This log will appear on the Trigger.dev run page");
+
+    //send a message to the #new-users Slack channel with user details
+    const response = await slack.postMessage("send-to-slack", {
+      channel: "new-users",
+      text: `New user: ${event.name} (${event.email}) signed up. ${
+        event.paidPlan ? "They are paying" : "They are on the free plan"
+      }.`,
+    });
+
+    return response.message;
+  },
+});
+
+//this workflow will now connect and start listening for events
+postMessage.listen();
