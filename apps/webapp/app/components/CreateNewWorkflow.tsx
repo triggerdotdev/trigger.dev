beced3352553da24e29939328d653c08ef4cf478
James Ritchie <james@jamesritchie.co.uk>
2023-01-22 23:21:24 -0800

    WIP adding more info about the integrations

--- apps/webapp/app/components/CreateNewWorkflow.tsx
+++ apps/webapp/app/components/CreateNewWorkflow.tsx
@@ -1,10 +1,10 @@
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import { Panel } from "./layout/Panel";
-import { PrimaryA, SecondaryA, TertiaryA } from "./primitives/Buttons";
+import { PrimaryA, SecondaryA } from "./primitives/Buttons";
 import { Body } from "./primitives/text/Body";
-import onboarding from "../assets/images/onboarding-image.png";
 import { SubTitle } from "./primitives/text/SubTitle";
-import { Header3 } from "./primitives/text/Headers";
+import onboarding from "../assets/images/onboarding-image.png";
+// import airtable from "../integrations/airtable.png";
 
 export default function CreateNewWorkflow() {
   return (
@@ -25,7 +25,7 @@ export default function CreateNewWorkflow() {
           rel="noreferrer"
         >
           <ArrowTopRightOnSquareIcon className="w-4 h-4" />
-          <span>Example workflows</span>
+          <span>Example Workflows</span>
         </SecondaryA>
       </div>
     </>
@@ -37,7 +37,7 @@ export function CreateNewWorkflowNoWorkflows() {
     <>
       <Panel className="flex p-0 overflow-hidden">
         <div className="flex flex-col p-6 w-full xl:w-2/3 text-slate-300 ">
-          <Body className="mb-5 px-3.5 py-2 bg-slate-700 rounded border border-slate-600">
+          <Body className="mb-5 max-w-max px-3.5 py-2 bg-slate-700 rounded border border-slate-600">
             Trigger.dev workflows are written in your own codebase and run in
             your existing infrastructure.
           </Body>
@@ -61,7 +61,7 @@ export function CreateNewWorkflowNoWorkflows() {
               display a prompt to connect.
             </li>
           </ol>
-          <div className="flex gap-2">
+          <div className="flex gap-2 mb-8">
             <PrimaryA
               href="https://docs.trigger.dev"
               target="_blank"
@@ -78,6 +78,43 @@ export function CreateNewWorkflowNoWorkflows() {
               <ArrowTopRightOnSquareIcon className="w-4 h-4" />
               <span>Example workflows</span>
             </SecondaryA>
+          </div>
+          <Body
+            size="small"
+            className="mb-2 uppercase tracking-wide text-slate-400"
+          >
+            API integrations
+          </Body>
+          <Body className="mb-4">
+            Easily authenticate with APIs using the supported integrations
+            below. If there's an integration we don't yet support,{" "}
+            <a
+              href="mailto:help@trigger.dev"
+              className="underline decoration-slate-400 underline-offset-2 hover:decoration-white hover:text-white transition"
+            >
+              let us know
+            </a>{" "}
+            and we'll add it.
+          </Body>
+          <div className="flex gap-2">
+            <div className="flex gap-4 items-center rounded pr-4 border border-slate-700/50">
+              <img src={onboarding} alt="Slack" className="rounded h-10 w-10" />
+              <Body size="small" className="text-slate-300">
+                Slack
+              </Body>
+            </div>
+            <div className="flex gap-4 items-center rounded pr-4 border border-slate-700/50">
+              <img
+                src={onboarding}
+                alt="Airtable"
+                className="rounded h-10 w-10"
+              />
+              <Body size="small" className="text-slate-300">
+                Airtable
+              </Body>
+            </div>
+
+            {/* <ApiLogoIcon integration="Airtable" size="regular" /> */}
           </div>
         </div>
         <div className="hidden xl:flex flex-col w-1/3">
