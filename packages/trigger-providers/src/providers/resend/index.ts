77f323322432808f3a8699e83edffad46430368e
Matt Aitken <matt@mattaitken.com>
2023-01-23 22:40:14 -0800

    Improved the Resend API key instructions

--- packages/trigger-providers/src/providers/resend/index.ts
+++ packages/trigger-providers/src/providers/resend/index.ts
@@ -9,7 +9,10 @@ export const resend = {
     type: "api_key",
     header_name: "Authorization",
     header_type: "access_token",
-    documentation: `1. Login to [Resend](https://resend.com)`,
+    documentation: `1. Login to [Resend](https://resend.com)
+2. Go to the API Keys page
+3. Generate a new API key
+4. Paste it into the field below`,
   },
   schemas,
 };
