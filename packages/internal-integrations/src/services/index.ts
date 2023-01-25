2ab9b5f97b4b5812aee9394c08a894363a286700
Matt Aitken <matt@mattaitken.com>
2023-01-24 20:14:02 -0800

    Build the service request objects ahead of time so we can easily log them if need to

--- packages/internal-integrations/src/services/index.ts
+++ packages/internal-integrations/src/services/index.ts
@@ -32,17 +32,19 @@ export class HttpService {
     endpoint: HttpEndpoint<TResponseSchema, TBodySchema>,
     body?: z.infer<TBodySchema>
   ): Promise<HttpResponse<TResponseSchema>> {
-    const response = await fetch(
-      `${this.options.baseUrl}${endpoint.options.path}`,
-      {
-        method: endpoint.options.method,
-        headers: {
-          Authorization: `Bearer ${this.options.accessToken}`,
-          "Content-Type": "application/json; charset=utf-8",
-        },
-        body: body ? JSON.stringify(body) : undefined,
-      }
-    );
+    const url = `${this.options.baseUrl}${endpoint.options.path}`;
+    const method = endpoint.options.method;
+    const headers = {
+      Authorization: `Bearer ${this.options.accessToken}`,
+      "Content-Type": "application/json; charset=utf-8",
+    };
+    const actualBody = body ? JSON.stringify(body) : undefined;
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body: actualBody,
+    });
 
     log(
       "%s%s response %d",
