972444da16e045f32734ca9b75c1fd69da96490c
Matt Aitken <matt@mattaitken.com>
2023-01-24 20:14:13 -0800

    Slack example with lots of blocks

--- examples/send-to-slack/src/index.ts
+++ examples/send-to-slack/src/index.ts
@@ -24,6 +24,94 @@ new Trigger({
     const response = await slack.postMessage("send-to-slack", {
       channelName: "test-integrations",
       text: `New domain created: ${event.domain} by customer ${event.customerId} cc @Eric #general`,
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Hello, Assistant to the Regional Manager Dwight! *Michael Scott* wants to know where you'd like to take the Paper Company investors to dinner tonight.\n\n *Please select a restaurant:*",
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Farmhouse Thai Cuisine*\n:star::star::star::star: 1528 reviews\n They do have some vegan options, like the roti and curry, plus they have a ton of salad stuff and noodles can be ordered without meat!! They have something for everyone here",
+          },
+          accessory: {
+            type: "image",
+            image_url:
+              "https://s3-media3.fl.yelpcdn.com/bphoto/c7ed05m9lC2EmA3Aruue7A/o.jpg",
+            alt_text: "alt text for image",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Kin Khao*\n:star::star::star::star: 1638 reviews\n The sticky rice also goes wonderfully with the caramelized pork belly, which is absolutely melt-in-your-mouth and so soft.",
+          },
+          accessory: {
+            type: "image",
+            image_url:
+              "https://s3-media2.fl.yelpcdn.com/bphoto/korel-1YjNtFtJlMTaC26A/o.jpg",
+            alt_text: "alt text for image",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Ler Ros*\n:star::star::star::star: 2082 reviews\n I would really recommend the  Yum Koh Moo Yang - Spicy lime dressing and roasted quick marinated pork shoulder, basil leaves, chili & rice powder.",
+          },
+          accessory: {
+            type: "image",
+            image_url:
+              "https://s3-media2.fl.yelpcdn.com/bphoto/DawwNigKJ2ckPeDeDM7jAg/o.jpg",
+            alt_text: "alt text for image",
+          },
+        },
+        {
+          type: "divider",
+        },
+        {
+          type: "actions",
+          elements: [
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Farmhouse",
+                emoji: true,
+              },
+              value: "click_me_123",
+            },
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Kin Khao",
+                emoji: true,
+              },
+              value: "click_me_123",
+              url: "https://google.com",
+            },
+            {
+              type: "button",
+              text: {
+                type: "plain_text",
+                text: "Ler Ros",
+                emoji: true,
+              },
+              value: "click_me_123",
+              url: "https://google.com",
+            },
+          ],
+        },
+      ],
     });
 
     await ctx.waitFor("initial-wait", { seconds: 5 });
@@ -36,4 +124,3 @@ new Trigger({
     return {};
   },
 }).listen();
-
