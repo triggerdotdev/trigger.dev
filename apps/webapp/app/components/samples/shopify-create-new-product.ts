3e53699b05813747ab87ce86392ff8ea36bcb924
D-K-P <dkp.github@pm.me>
2023-02-02 18:11:50 +0000

    Added shopify sample

---
+++ apps/webapp/app/components/samples/shopify-create-new-product.ts
@@ -0,0 +1,58 @@
+export const shopifyCreateNewProducts = `import { Trigger, customEvent } from "@trigger.dev/sdk";
+import { z } from "zod";
+import * as shopify from "@trigger.dev/shopify";
+
+new Trigger({
+  id: "shopify-products",
+  name: "Shopify products",
+  apiKey: "<my_api_key>",
+  on: customEvent({
+    name: "shopify.products",
+    schema: z.object({}),
+  }),
+  run: async (event, ctx) => {
+    const newProduct = await shopify.createProduct("create-product", {
+      descriptionHtml: "This is my brilliant <i>product description</i>.",
+      title: \`Fantastic product \${Math.floor(Math.random() * 1000)}\`,
+      productType: "t-shirt",
+      vendor: "Nike",
+      options: ["Color", "Size"],
+      standardizedProductType: {
+        productTaxonomyNodeId: "gid://shopify/ProductTaxonomyNode/352",
+      },
+      variants: [
+        {
+          price: "99.99",
+          sku: "variant-1",
+          inventoryItem: {
+            tracked: true,
+          },
+          inventoryQuantities: [
+            {
+              availableQuantity: 1,
+              locationId: "gid://shopify/Location/76187369773",
+            },
+          ],
+          options: ["Maroon", "Tiny"],
+        },
+      ],
+    });
+
+    const newImages = await shopify.appendProductImages("append-images", {
+      id: newProduct.id,
+      images: [
+        {
+          src: "https://via.placeholder.com/600/92c952.png",
+          altText: "Image 1",
+        },
+        {
+          src: "https://via.placeholder.com/600/d32776.png",
+          altText: "Image 2",
+        },
+      ],
+    });
+
+    return newProduct;
+  },
+}).listen();
+`;
