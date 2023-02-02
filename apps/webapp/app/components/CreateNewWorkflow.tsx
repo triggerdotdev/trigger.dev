81d3d324e2c6c0d080dcbd0d0bac577ed8cb42d5
James Ritchie <james@jamesritchie.co.uk>
2023-02-02 18:41:12 +0000

    More progress on the onboarding

--- apps/webapp/app/components/CreateNewWorkflow.tsx
+++ apps/webapp/app/components/CreateNewWorkflow.tsx
@@ -2,21 +2,18 @@ import { Tab } from "@headlessui/react";
 import {
   LargeBox,
   LargeBoxList,
-  Segmented,
-  SegmentedList,
   Underlined,
   UnderlinedList,
 } from "~/components/StyledTabs";
-import {
-  ArrowTopRightOnSquareIcon,
-  InformationCircleIcon,
-} from "@heroicons/react/24/outline";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import type { IntegrationMetadata } from "@trigger.dev/integration-sdk";
 import { PrimaryA, SecondaryA } from "./primitives/Buttons";
 import { Body } from "./primitives/text/Body";
 import { SubTitle } from "./primitives/text/SubTitle";
 import { newUserSlackMessage } from "./samples/new-user-slack-message";
-import { link } from "fs";
+import CodeBlock from "./code/CodeBlock";
+import { Header4 } from "./primitives/text/Headers";
+import classNames from "classnames";
 
 export function CreateNewWorkflow() {
   return (
@@ -51,33 +48,49 @@ export function CreateNewWorkflowNoWorkflows({
 }) {
   return (
     <>
-      <div className="mb-5 flex max-w-max items-center gap-2 rounded border border-slate-600 bg-slate-700 px-3.5 py-2">
+      {/* <div className="mb-5 flex max-w-max items-center gap-2 rounded border border-slate-600 bg-slate-700 px-3.5 py-2">
         <InformationCircleIcon className="h-5 w-5 text-slate-300" />
         <Body>
           Trigger.dev workflows are written in your own codebase and run in your
           existing infrastructure.
         </Body>
-      </div>
-      <SubTitle>Step 1. Install the @trigger.dev package</SubTitle>
+      </div> */}
+      <Header4 size="regular" className={subTitle}>
+        1. Install the package
+      </Header4>
       <Tab.Group>
-        <SegmentedList>
-          <Segmented>npm</Segmented>
-          <Segmented>pnpm</Segmented>
-          <Segmented>yarn</Segmented>
-        </SegmentedList>
-        <Tab.Panels className="flex-grow pt-4">
+        <UnderlinedList>
+          <Underlined>npm</Underlined>
+          <Underlined>pnpm</Underlined>
+          <Underlined>yarn</Underlined>
+        </UnderlinedList>
+        <Tab.Panels className="flex-grow">
           <Tab.Panel className="relative h-full">
-            npm install @trigger.dev/sdk
+            <CodeBlock
+              code="npm install @trigger.dev/sdk"
+              align="top"
+              showLineNumbers={false}
+            />
           </Tab.Panel>
           <Tab.Panel className="relative h-full">
-            pnpm install @trigger.dev/sdk
+            <CodeBlock
+              code="pnpm install @trigger.dev/sdk"
+              align="top"
+              showLineNumbers={false}
+            />
           </Tab.Panel>
           <Tab.Panel className="relative h-full">
-            yarn add @trigger.dev/sdk
+            <CodeBlock
+              code="yarn add @trigger.dev/sdk"
+              align="top"
+              showLineNumbers={false}
+            />
           </Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
-      <SubTitle>Step 2. Create your first workflow</SubTitle>
+      <Header4 size="regular" className={classNames("mt-10", subTitle)}>
+        2. Create your workflow
+      </Header4>
       <Tab.Group>
         <UnderlinedList>
           <Underlined>Start from an example</Underlined>
@@ -85,33 +98,46 @@ export function CreateNewWorkflowNoWorkflows({
         </UnderlinedList>
         <Tab.Panels className="flex-grow pt-4">
           <Tab.Panel className="relative h-full">
+            {/* Example projects titles */}
             <Tab.Group>
               <LargeBoxList>
                 {exampleProjects.map((project) => {
                   return <LargeBox key={project.name}>{project.name}</LargeBox>;
                 })}
               </LargeBoxList>
+              {/* Example projects content */}
               <Tab.Panels className="flex-grow pt-4">
-                <Tab.Panel className="relative h-full">
-                  New user slack message
-                </Tab.Panel>
-                <Tab.Panel className="relative h-full">
-                  Welcome email campaign
-                </Tab.Panel>
+                {exampleProjects.map((project) => {
+                  return (
+                    <Tab.Panel key={project.name} className="relative h-full">
+                      <Body size="regular">
+                        Install the extra API integration packages
+                      </Body>
+                      <CodeBlock code={project.requiredPackages} align="top" />
+                      <CodeBlock code={project.code} align="top" />
+                    </Tab.Panel>
+                  );
+                })}
               </Tab.Panels>
             </Tab.Group>
           </Tab.Panel>
           <Tab.Panel className="relative h-full">
             <Tab.Group>
+              {/* From scratch projects titles */}
               <LargeBoxList>
-                <LargeBox>Webhook</LargeBox>
-                <LargeBox>Custom event</LargeBox>
-                <LargeBox>Scheduled (CRON)</LargeBox>
+                {fromScratchProjects.map((project) => {
+                  return <LargeBox key={project.name}>{project.name}</LargeBox>;
+                })}
               </LargeBoxList>
+              {/* From scratch projects content */}
               <Tab.Panels className="flex-grow pt-4">
-                <Tab.Panel className="relative h-full">Webhook</Tab.Panel>
-                <Tab.Panel className="relative h-full">Custom event</Tab.Panel>
-                <Tab.Panel className="relative h-full">Scheduled</Tab.Panel>
+                {fromScratchProjects.map((project) => {
+                  return (
+                    <Tab.Panel key={project.name} className="relative h-full">
+                      <CodeBlock code={project.code} align="top" />
+                    </Tab.Panel>
+                  );
+                })}
               </Tab.Panels>
             </Tab.Group>
           </Tab.Panel>
@@ -135,29 +161,35 @@ export function CreateNewWorkflowNoWorkflows({
   );
 }
 
+const subTitle = "text-slate-200 font-semibold mb-4";
 const allCapsTitleClasses = "mb-2 uppercase tracking-wide text-slate-400";
 
 const exampleProjects = [
   {
-    name: "Send a Slack message when a new user signs up",
+    name: "New user signs up → Post Slack message",
     requiredPackages: "@trigger.dev/slack zod",
     code: newUserSlackMessage,
   },
   {
-    name: "Welcome email campaign",
+    name: "New user signs up → send email campaign",
     requiredPackages: "@trigger.dev/slack zod",
     code: "new Trigger() etc...",
   },
 ];
 
-const scratchProjects = [
+const fromScratchProjects = [
   {
-    name: "Send a Slack message when a new user signs up",
+    name: "Webhook",
     requiredPackages: "@trigger.dev/slack zod",
     code: "code",
   },
   {
-    name: "When a new user signs up, send them a series of emails",
+    name: "Custom event",
+    requiredPackages: "@trigger.dev/slack zod",
+    code: "new Trigger() etc...",
+  },
+  {
+    name: "Scheduled (CRON)",
     requiredPackages: "@trigger.dev/slack zod",
     code: "new Trigger() etc...",
   },
--- apps/webapp/app/components/StyledTabs.tsx
+++ apps/webapp/app/components/StyledTabs.tsx
@@ -34,7 +34,7 @@ export function Classic({ children, ...props }: HeadlessTabProps) {
 export function UnderlinedList({ children, ...props }: HeadlessTabListProps) {
   return (
     <HeadlessTab.List
-      className={"-mb-px flex space-x-4 border-b border-slate-300"}
+      className={"inline-flex space-x-4 border-b border-slate-700"}
       {...props}
     >
       {children}
@@ -49,9 +49,9 @@ export function Underlined({ children, ...props }: HeadlessTabProps) {
         classnames(
           selected
             ? "border-indigo-500 text-slate-300 outline-none"
-            : "border-transparent text-slate-300 hover:border-slate-200 hover:text-slate-200",
+            : "border-transparent text-slate-500 hover:border-slate-200 hover:text-slate-200",
           "disabled:text-slate-300 disabled:hover:border-transparent",
-          "flex whitespace-nowrap border-b-2 py-2 px-4 text-xs font-medium transition"
+          "flex whitespace-nowrap border-b-2 py-2 px-4 text-base font-medium transition"
         )
       }
       {...props}
