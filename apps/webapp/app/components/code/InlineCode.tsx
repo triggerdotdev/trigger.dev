b72d63bdd2cf9e39d6b36ef3c3dcfb6d01d62367
James Ritchie <james@jamesritchie.co.uk>
2023-02-03 13:23:02 +0000

    Moved inline tab style to new component

---
+++ apps/webapp/app/components/code/InlineCode.tsx
@@ -0,0 +1,8 @@
+import type { ReactNode } from "react";
+
+const inlineCode =
+  "px-1 py-0.5 text-sm bg-slate-700 border border-slate-900 rounded text-slate-200";
+
+export function InlineCode({ children }: { children: ReactNode }) {
+  return <code className={inlineCode}>{children}</code>;
+}
