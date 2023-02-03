92a37ae3a5028a5a9dfd9e1a6b88f573cb20396b
Matt Aitken <matt@mattaitken.com>
2023-02-03 16:33:45 +0000

    Fix for Remix hydration issues

--- apps/webapp/app/entry.server.tsx
+++ apps/webapp/app/entry.server.tsx
@@ -1,15 +1,9 @@
-import * as Sentry from "~/services/sentry.server";
-import * as MessageBroker from "~/services/messageBroker.server";
-import { PassThrough } from "stream";
 import type { EntryContext } from "@remix-run/node";
 import { Response } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import isbot from "isbot";
-import { renderToPipeableStream } from "react-dom/server";
-import { Head } from "./root";
-import { renderHeadToString } from "remix-island";
-
-const ABORT_DELAY = 5000;
+import { renderToString } from "react-dom/server";
+import * as MessageBroker from "~/services/messageBroker.server";
+import * as Sentry from "~/services/sentry.server";
 
 export default function handleRequest(
   request: Request,
@@ -17,111 +11,17 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  return isbot(request.headers.get("user-agent"))
-    ? handleBotRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        remixContext
-      )
-    : handleBrowserRequest(
-        request,
-        responseStatusCode,
-        responseHeaders,
-        remixContext
-      );
-}
+  // deepcode ignore Ssti: <This is recommended by Remix>
+  const markup = renderToString(
+    // deepcode ignore OR: <All good in the hood>
+    <RemixServer context={remixContext} url={request.url} />
+  );
 
-function handleBotRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext
-) {
-  return new Promise((resolve, reject) => {
-    let didError = false;
+  responseHeaders.set("Content-Type", "text/html; charset=utf-8");
 
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        onAllReady() {
-          // @ts-ignore
-          const head = renderHeadToString({ request, remixContext, Head });
-          const body = new PassThrough();
-
-          responseHeaders.set("Content-Type", "text/html");
-
-          resolve(
-            new Response(body, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            })
-          );
-          body.write(
-            `<!DOCTYPE html><html><head>${head}</head><body><div id="root">`
-          );
-          pipe(body);
-          body.write(`</div></body></html>`);
-        },
-        onShellError(error: unknown) {
-          reject(error);
-        },
-        onError(error: unknown) {
-          didError = true;
-
-          console.error(error);
-        },
-      }
-    );
-
-    setTimeout(abort, ABORT_DELAY);
-  });
-}
-
-function handleBrowserRequest(
-  request: Request,
-  responseStatusCode: number,
-  responseHeaders: Headers,
-  remixContext: EntryContext
-) {
-  return new Promise((resolve, reject) => {
-    let didError = false;
-
-    const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        onShellReady() {
-          // @ts-ignore
-          const head = renderHeadToString({ request, remixContext, Head });
-          const body = new PassThrough();
-
-          responseHeaders.set("Content-Type", "text/html");
-
-          resolve(
-            new Response(body, {
-              headers: responseHeaders,
-              status: didError ? 500 : responseStatusCode,
-            })
-          );
-
-          body.write(
-            `<!DOCTYPE html><html><head>${head}</head><body><div id="root">`
-          );
-          pipe(body);
-          body.write(`</div></body></html>`);
-        },
-        onShellError(err: unknown) {
-          reject(err);
-        },
-        onError(error: unknown) {
-          didError = true;
-
-          console.error(error);
-        },
-      }
-    );
-
-    setTimeout(abort, ABORT_DELAY);
+  return new Response("<!DOCTYPE html>" + markup, {
+    status: responseStatusCode,
+    headers: responseHeaders,
   });
 }
 
