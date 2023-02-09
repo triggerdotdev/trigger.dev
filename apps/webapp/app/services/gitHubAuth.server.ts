5f649d1b2e23d76e79c33114c3b819254d0d6545
Matt Aitken <matt@mattaitken.com>
2023-02-09 08:48:39 +0000

    Moved postAuthentication actions into a separate file

--- apps/webapp/app/services/gitHubAuth.server.ts
+++ apps/webapp/app/services/gitHubAuth.server.ts
@@ -6,6 +6,7 @@ import { findOrCreateUser } from "~/models/user.server";
 import type { AuthUser } from "./authUser";
 import { scheduleWelcomeEmail } from "./email.server";
 import { taskQueue } from "./messageBroker.server";
+import { postAuthentication } from "./postAuth.server";
 
 const gitHubStrategy = new GitHubStrategy(
   {
@@ -29,20 +30,7 @@ const gitHubStrategy = new GitHubStrategy(
         authenticationExtraParams: extraParams,
       });
 
-      if (isNewUser) {
-        await createFirstOrganization(user);
-        await scheduleWelcomeEmail(user);
-
-        await taskQueue.publish("SEND_INTERNAL_EVENT", {
-          id: user.id,
-          name: "user.created",
-          payload: {
-            id: user.id,
-            source: "GITHUB",
-            admin: user.admin,
-          },
-        });
-      }
+      await postAuthentication({ user, isNewUser, loginMethod: "GITHUB" });
 
       return {
         userId: user.id,
---
+++ apps/webapp/app/services/postAuth.server.ts
@@ -0,0 +1,29 @@
+import { createFirstOrganization } from "~/models/organization.server";
+import type { User } from "~/models/user.server";
+import * as emailProvider from "~/services/email.server";
+import { taskQueue } from "./messageBroker.server";
+
+export async function postAuthentication({
+  user,
+  loginMethod,
+  isNewUser,
+}: {
+  user: User;
+  loginMethod: "MAGIC_LINK" | "GITHUB";
+  isNewUser: boolean;
+}) {
+  if (isNewUser) {
+    await createFirstOrganization(user);
+    await emailProvider.scheduleWelcomeEmail(user);
+
+    await taskQueue.publish("SEND_INTERNAL_EVENT", {
+      id: user.id,
+      name: "user.created",
+      payload: {
+        id: user.id,
+        source: loginMethod,
+        admin: user.admin,
+      },
+    });
+  }
+}
