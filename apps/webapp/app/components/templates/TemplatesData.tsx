038d53c7c5b37be64a3aece9a4ce72f958adff28
James Ritchie <james@jamesritchie.co.uk>
2023-02-10 14:11:21 +0000

    Added templates data file

---
+++ apps/webapp/app/components/templates/TemplatesData.tsx
@@ -0,0 +1,11 @@
+export const TemplateData = [
+  {
+    title: "GitHub stars to Slack",
+    shortTitle: "GitHub stars to Slack",
+    description:
+      "When a GitHub repo is starred, post information about the user to Slack",
+    imageURL: "images/templates/slack-template.png",
+    githubRepoURL: "repo-url",
+    services: ["slack", "github"],
+  },
+];
