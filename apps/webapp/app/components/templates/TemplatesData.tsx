6595535b4075a76dcddb2a6338909789d933f8e3
D-K-P <dkp.github@pm.me>
2023-02-13 11:29:26 +0000

    Added template grid and updated data

--- apps/webapp/app/components/templates/TemplatesData.tsx
+++ apps/webapp/app/components/templates/TemplatesData.tsx
@@ -1,10 +1,32 @@
+// import Slack from "../../../public/integrations/slack.png";
+// import SlackTemplateBg from "../../../public/images/templates/slack-template.png";
+import GitHubStarsTemplateBg from "../../../public/images/templates/github-stars-template-bg.png";
+import ResendSlackTemplateBg from "../../../public/images/templates/resend-slack-template-bg.png";
+import ShopifyTemplateBg from "../../../public/images/templates/shopify-template-bg.png";
+
 export const TemplateData = [
   {
-    title: "GitHub stars to Slack",
+    title: "Slack notifications when a GitHub repo is starred",
     shortTitle: "GitHub stars to Slack",
     description:
       "When a GitHub repo is starred, post information about the user to Slack",
-    imageURL: "images/templates/slack-template.png",
+    imageURL: GitHubStarsTemplateBg,
+    githubRepoURL: "repo-url",
+    services: ["slack", "github"],
+  },
+  {
+    title: "New user welcome email drip campaign",
+    shortTitle: "GitHub stars to Slack",
+    description: "Create a welcome email drip campaign using Slack and Resend",
+    imageURL: ResendSlackTemplateBg,
+    githubRepoURL: "repo-url",
+    services: ["slack", "github"],
+  },
+  {
+    title: "Add a new product to Shopify",
+    shortTitle: "GitHub stars to Slack",
+    description: "Add a new product to Shopify",
+    imageURL: ShopifyTemplateBg,
     githubRepoURL: "repo-url",
     services: ["slack", "github"],
   },
--- apps/webapp/app/components/templates/TemplatesGrid.tsx
+++ apps/webapp/app/components/templates/TemplatesGrid.tsx
@@ -1,49 +1,43 @@
 import { Body } from "../primitives/text/Body";
 import { Header1 } from "../primitives/text/Headers";
+import { TemplateData } from "./TemplatesData";
 import Slack from "../../../public/integrations/slack.png";
-import SlackTemplateBg from "../../../public/images/templates/slack-template.png";
-
 
 
 export function TemplatesGrid() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full flex-wrap mt-8">
-      <TemplateCard />
-      <TemplateCard />
-      <TemplateCard />
-      <TemplateCard />
-      <TemplateCard />
-      <TemplateCard />
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full flex-wrap mt-8">
+      {TemplateData.map((template) => {
+return (
+  <div
+    key={template.title}
+    className=" group w-full items-center overflow-hidden rounded-md border border-slate-700 bg-slate-800 text-sm text-slate-200 shadow-md transition hover:cursor-pointer hover:border-slate-700 hover:bg-slate-800/30 disabled:opacity-50" 
+  >
+    <div className="relative h-32 bg-slate-600  transition group-hover:opacity-90">
+      <img
+        src={template.imageURL}
+        alt=""
+        className="h-full w-full object-cover"
+      />
     </div>
-  );
-}
+    <div className="flex flex-col gap-y-1 p-4">
+      <div
+        key="integration"
+        className="mb-1 flex h-8 w-8 items-center justify-center rounded-lg border-[1px] border-slate-700 bg-slate-900 transition group-hover:border-slate-800 group-hover:bg-slate-900"
+      >
+        <img src={Slack} alt="Slack" className="h-5 w-5" />
+      </div>
+      <Header1 size="small" className="font-semibold">
+        {template.title}
+      </Header1>
+      <Body size="small" className="text-slate-400">
+        {template.description}
+      </Body>
 
-function TemplateCard() {
-  return (
-    <div className=" group w-full items-center overflow-hidden rounded-md border border-slate-900 bg-slate-800 text-sm text-slate-200 shadow-md transition hover:cursor-pointer hover:bg-slate-800/30 disabled:opacity-50">
-      <div className="relative h-36 w-full bg-slate-600 transition group-hover:opacity-90">
-        <img
-          src={SlackTemplateBg}
-          alt="Slack-template"
-          className="object-cover h-full w-auto group-hover:object-scale-down transition"
-        />
-        <div className="absolute bottom-2 left-2">
-          <div className="flex flex-row gap-x-2">
-            <div className="flex h-12 w-12 items-center justify-center rounded-lg border-[1px] border-slate-600 bg-slate-800 transition group-hover:border-slate-700 group-hover:bg-slate-900">
-              <img src={Slack} alt="Slack" className="h-8 w-8" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col gap-y-1 p-4">
-        <Header1 size="small" className="font-semibold">
-          GitHub stars to Slack
-        </Header1>
-        <Body size="small" className="text-slate-400">
-          When a GitHub repo is starred, post information about the user to
-          Slack
-        </Body>
-      </div>
     </div>
+  </div>
+);
+      })}
+       </div>
   );
 }
