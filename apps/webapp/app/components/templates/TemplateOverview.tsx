e1fb69fe441d4e1782713874e3ff064650d1597d
D-K-P <dkp.github@pm.me>
2023-02-13 12:09:55 +0000

    Removed close button

--- apps/webapp/app/components/templates/TemplateOverview.tsx
+++ apps/webapp/app/components/templates/TemplateOverview.tsx
@@ -16,17 +16,15 @@ export function TemplateOverview() {
       </div>
 
       <div className="flex flex-col w-[280px] min-w-[280px] gap-y-2 rounded-r bg-slate-700 p-4">
-        <div className="flex place-content-between">
+
           <div
             key="integration"
             className="mb-1 flex h-8 w-8 items-center justify-center rounded-lg border-[1px] border-slate-700 bg-slate-900 transition group-hover:border-slate-800 group-hover:bg-slate-900"
           >
             <img src={Slack} alt="Slack" className="h-5 w-5" />
           </div>
-          <div className="group flex h-7 w-7 items-center justify-center justify-self-end rounded-full bg-slate-500/80 transition hover:cursor-pointer hover:bg-slate-400/80">
-            <XMarkIcon className="group flex h-6 w-6 text-slate-800 transition group-hover:text-slate-900" />
-          </div>
-        </div>
+          
+  
         <Header2 size="regular" className="font-semibold">
           {TemplateData[0].title}
         </Header2>
