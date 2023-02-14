6a3ffa6395cae1bd513ca307fc2a3a62b9fc42fb
Matt Aitken <matt@mattaitken.com>
2023-02-14 16:53:49 +0000

    Added authentication middleware to the integrations service

--- apps/integrations/src/index.ts
+++ apps/integrations/src/index.ts
@@ -1,13 +1,44 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
 import { handleAction } from "api/v1/action";
 import { handleServices } from "api/v1/services";
+import dotenv from "dotenv";
+import express, { Express, NextFunction, Request, Response } from "express";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT ?? 3006;
 
 app.use(express.json());
+
+const checkAuthentication = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  console.log("authHeader", authHeader);
+  console.log(process.env.API_TOKEN);
+
+  const auth = authHeader.split(" ");
+  if (auth.length !== 2 || auth[0] !== "Bearer") {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const token = auth[1];
+  if (token !== process.env.API_TOKEN) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  next();
+};
+
+app.use(checkAuthentication);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Trigger.dev integrations service");
