60ac029220f6143886e60523a1ba61fa2c48d5c4
James Ritchie <james@jamesritchie.co.uk>
2023-02-14 15:56:29 +0000

    Added a disabled button state

--- apps/webapp/app/components/primitives/Buttons.tsx
+++ apps/webapp/app/components/primitives/Buttons.tsx
@@ -5,7 +5,7 @@ const commonClasses =
   "inline-flex items-center justify-center max-w-max rounded text-sm transition whitespace-nowrap";
 const primaryClasses = classnames(
   commonClasses,
-  "px-4 py-2 bg-indigo-700 text-white hover:bg-indigo-600 focus:ring-indigo-800 gap-2"
+  "px-4 py-2 bg-indigo-700 text-white hover:bg-indigo-600 focus:ring-indigo-800 gap-2 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-slate-700 disabled:text-slate-400"
 );
 const secondaryClasses = classnames(
   commonClasses,
