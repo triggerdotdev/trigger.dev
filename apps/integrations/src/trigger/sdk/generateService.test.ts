2261a769e4ae92a26985b56460cd31658dbabaca
Matt Aitken <matt@mattaitken.com>
2023-02-15 13:49:59 +0000

    Deleted pointless generateService test

--- apps/integrations/src/trigger/sdk/generateService.test.ts
+++
@@ -1,8 +0,0 @@
-import { slackv2 } from "integrations/slack";
-import { expect, test } from "vitest";
-import { generateService } from "./generateService";
-
-test("generate simple service", async () => {
-  generateService(slackv2);
-  expect(1).toEqual(1);
-});
