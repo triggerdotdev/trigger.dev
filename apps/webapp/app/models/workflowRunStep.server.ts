9db77b64786514ddb0b077d2008082efc98eb5eb
Matt Aitken <matt@mattaitken.com>
2023-02-15 10:37:05 +0000

    Reverted workflowRunStep.server.ts

--- apps/webapp/app/models/workflowRunStep.server.ts
+++ apps/webapp/app/models/workflowRunStep.server.ts
@@ -1,29 +1,21 @@
-import { DisplayProperties } from "@trigger.dev/integration-sdk";
 import { z } from "zod";
 import { Prisma } from "~/db.server";
 import { prisma } from "~/db.server";
-import { integrationsClient } from "~/services/integrationsClient.server";
 
 const UniqueConstraintErrorTargetSchema = z.object({
   target: z.array(z.string()),
 });
 
 export async function createStepOnce(
-  config: {
-    runId: string;
-    key: string;
-    service: string;
-    endpoint: string;
-    params?: any;
-    version?: string;
-  },
+  runId: string,
+  key: string,
   data: Omit<Prisma.WorkflowRunStepCreateInput, "run" | "idempotencyKey">
 ) {
   const existingStep = await prisma.workflowRunStep.findUnique({
     where: {
       runId_idempotencyKey: {
-        runId: config.runId,
-        idempotencyKey: config.key,
+        runId,
+        idempotencyKey: key,
       },
     },
   });
@@ -33,21 +25,11 @@ export async function createStepOnce(
   }
 
   try {
-    let displayProperties: DisplayProperties | undefined = undefined;
-    if (config.version === "2") {
-      displayProperties = await integrationsClient.displayProperties({
-        service: config.service,
-        name: config.endpoint,
-        params: config.params,
-      });
-    }
-
     const step = await prisma.workflowRunStep.create({
       data: {
         ...data,
-        displayProperties,
-        idempotencyKey: config.key,
-        runId: config.runId,
+        idempotencyKey: key,
+        runId,
       },
     });
 
