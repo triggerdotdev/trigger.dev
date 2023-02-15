a1535dff70a4e258bf058eee69292216f6131af5
D-K-P <dkp.github@pm.me>
2023-02-15 13:38:29 +0000

    Added title and sub to the templates page

--- apps/webapp/app/routes/__public/templates.tsx
+++ apps/webapp/app/routes/__public/templates.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from "@remix-run/react";
 import { Container } from "~/components/layout/Container";
-import { Header1 } from "~/components/primitives/text/Headers";
+import { Header2 } from "~/components/primitives/text/Headers";
 
 export default function TemplatesLayout() {
   return (
-    <Container className="mx-auto flex w-full max-w-5xl flex-col">
-      <Header1 className="mb-6">Placeholder logged out templates</Header1>
-      <Outlet />
+    <Container className="mx-auto mt-20 lg:mt-24 flex w-full max-w-5xl flex-col items-center justify-center">
+      <h1 className="mb-6 text-center font-title text-5xl font-semibold text-slate-200">
+        Placeholder logged out templates
+      </h1>
+      <Header2 size="small" className="mb-4 text-slate-400">
+        Placeholder template sub-heading
+      </Header2>
+        <Outlet />
     </Container>
   );
 }
