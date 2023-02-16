49068d8db1ee8ec13f9bb650188a74d2d35f1e18
James Ritchie <james@jamesritchie.co.uk>
2023-02-16 09:53:04 +0000

    Fixed missbehaving layout on the examples modal

--- apps/webapp/app/components/templates/ExampleOverview.tsx
+++ apps/webapp/app/components/templates/ExampleOverview.tsx
@@ -4,11 +4,11 @@ import invariant from "tiny-invariant";
 import {
   ExampleProject,
   exampleProjects,
-  fromScratchProjects
+  fromScratchProjects,
 } from "~/components/samples/samplesList";
 import { useCurrentEnvironment } from "~/hooks/useEnvironments";
 import CodeBlock from "../code/CodeBlock";
-import { PrimaryButton, ToxicA } from "../primitives/Buttons";
+import { ToxicA } from "../primitives/Buttons";
 import { StyledDialog } from "../primitives/Dialog";
 import { Body } from "../primitives/text/Body";
 import { Header2 } from "../primitives/text/Headers";
@@ -36,45 +36,43 @@ export function ExampleOverview({
           show={isOpen}
           as={Fragment}
         >
-          <StyledDialog.Panel className="top-0 mx-auto flex max-h-[90vh] max-w-5xl flex-row items-start gap-2 overflow-hidden overflow-y-auto rounded-md">
-            <div className="h-full max-h-[80vh] w-full flex-col overflow-scroll rounded-md bg-slate-800 text-left">
-              <div className="flex flex-row gap-y-4 p-4">
-                <div className="flex flex-col">
-                  <CodeBlock
-                    code={openProject.code(environment.apiKey)}
-                    align="top"
-                    className="flex w-[650px] overflow-scroll"
-                  />
-                </div>
-                <div className="flex w-80 flex-col gap-y-4 pl-4">
-                
-
-                  <ToxicA
-                    className="group flex h-12 min-w-full"
-                    onClick={() => {
-                      setOpenProject(null);
-                      onSelectedProject(openProject);
-                    }}
+          <StyledDialog.Panel className="top-0 mx-auto flex max-h-[80vh] max-w-5xl items-start gap-2 overflow-y-auto ">
+            <div className="grid grid-cols-[minmax(0,_1fr)_20rem] gap-4 rounded-md bg-slate-800 p-4">
+              <CodeBlock
+                code={openProject.code(environment.apiKey)}
+                align="top"
+              />
+              <div className="sticky top-4 flex flex-col gap-y-4">
+                <ToxicA
+                  className="group flex h-12 min-w-full"
+                  onClick={() => {
+                    setOpenProject(null);
+                    onSelectedProject(openProject);
+                  }}
+                >
+                  <span>Use this template</span>
+                  <span
+                    className="ml-1 transition group-hover:translate-x-0.5"
+                    aria-hidden="true"
                   >
-                    <span> Use this template </span>
-                    <span
-                      className="ml-1 transition group-hover:translate-x-0.5"
-                      aria-hidden="true"
-                    >
-                      &rarr;
-                    </span>
-                  </ToxicA>
-                  <div className="flex flex-col rounded items-start justify-start gap-y-4  bg-slate-700/30 px-4 py-4">
-                    {openProject.icon}
-                    <Header2 size="regular" className="font-semibold">
-                      {openProject.name}
-                    </Header2>
-                  </div>
+                    &rarr;
+                  </span>
+                </ToxicA>
+                <div className="flex flex-col items-start justify-start gap-y-4 rounded bg-slate-700/30 px-4 py-4">
+                  {openProject.icon}
                   <Header2 size="regular" className="font-semibold">
-                    {openProject.title}
+                    {openProject.name}
                   </Header2>
-                  <Body>{openProject.description}</Body>
                 </div>
+                <Header2
+                  size="regular"
+                  className="text-left font-semibold text-slate-300"
+                >
+                  {openProject.title}
+                </Header2>
+                <Body className="text-left text-slate-400">
+                  {openProject.description}
+                </Body>
               </div>
             </div>
             <button
@@ -125,20 +123,18 @@ export function FromScratchOverview({
         >
           <div className="fixed inset-0 overflow-y-auto">
             <div className="flex min-h-full items-center justify-center">
-              <StyledDialog.Panel className="top-0 mx-auto flex max-h-[90vh] max-w-5xl flex-row items-start gap-2 overflow-hidden overflow-y-auto rounded-md p-4">
+              <StyledDialog.Panel className="top-0 mx-auto flex max-h-[90vh] max-w-5xl items-start gap-2 overflow-hidden overflow-y-auto rounded-md p-4">
                 <div className="flex h-full w-full flex-col overflow-hidden rounded-md bg-slate-800 text-left">
-                  <div className="flex flex-row items-start justify-start gap-4 border-b border-slate-850/80 bg-slate-700/30 p-4">
+                  <div className="flex items-start justify-start gap-4 border-b border-slate-850/80 bg-slate-700/30 p-4">
                     <div className="flex flex-col">
                       <CodeBlock
                         code={openProject.code(environment.apiKey)}
-                        align="top"                        
+                        align="top"
                         className="flex w-[650px] overflow-scroll"
                       />
                     </div>
 
                     <div className="flex w-80 flex-col items-start justify-start gap-y-4">
-                     
-
                       <ToxicA
                         className="group flex h-12 min-w-full"
                         onClick={() => {
