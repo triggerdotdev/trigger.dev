821af448f34aebfcae6eb32456d99e2cde528035
James Ritchie <james@jamesritchie.co.uk>
2023-02-16 10:13:37 +0000

    Removed unused exports

--- apps/webapp/app/routes/__public/templates.tsx
+++ apps/webapp/app/routes/__public/templates.tsx
@@ -1,7 +1,5 @@
-import { ArrowLeftIcon } from "@heroicons/react/24/outline";
-import { Link, Outlet } from "@remix-run/react";
+import { Outlet } from "@remix-run/react";
 import { Container } from "~/components/layout/Container";
-import { Header2 } from "~/components/primitives/text/Headers";
 
 export default function TemplatesLayout() {
   return (
