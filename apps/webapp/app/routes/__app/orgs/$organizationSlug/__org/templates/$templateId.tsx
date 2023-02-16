19aedf8f73776e1c0be03ce7729af89c1f4d8cb2
James Ritchie <james@jamesritchie.co.uk>
2023-02-16 11:40:50 +0000

    Improved the final connected workfow step

--- apps/webapp/app/routes/__app/orgs/$organizationSlug/__org/templates/$templateId.tsx
+++ apps/webapp/app/routes/__app/orgs/$organizationSlug/__org/templates/$templateId.tsx
@@ -144,7 +144,7 @@ function OrganizationTemplateReady(loaderData: LoaderData) {
         <>
           <ConnectedToGithub />
           {githubConfigured}
-          <div className="mt-4 flex items-center gap-1">
+          <div className="mt-2 flex items-center gap-1">
             <SubTitle className="flex items-center">
               <StepNumber complete />
               Template deployed
@@ -293,9 +293,7 @@ function DeploySection({
   } else {
     return (
       <div className="mt-4 max-w-4xl">
-        <Label className="mb-3 text-sm text-slate-500">
-          View your new workflow:
-        </Label>
+        <SubTitle>View your new workflow</SubTitle>
         <div className="relative mt-1 rounded-lg bg-slate-850 p-4">
           <div className="absolute inset-2.5 z-0 h-[calc(100%-20px)] w-[calc(100%-20px)] animate-pulse rounded-md bg-gradient-to-r from-indigo-500 to-pink-500 blur-sm"></div>
           <WorkflowList
