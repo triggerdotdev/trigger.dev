171d16ef41b4f58177679a05f73e85a73d2065ac
Matt Aitken <matt@mattaitken.com>
2023-02-16 14:29:44 +0000

    If GitHub connect fails, show an error page and log out the params

--- apps/webapp/app/routes/apps/github.callback.ts
+++ apps/webapp/app/routes/apps/github.callback.ts
@@ -1,4 +1,5 @@
-import { LoaderArgs, redirect } from "@remix-run/server-runtime";
+import type { LoaderArgs } from "@remix-run/server-runtime";
+import { redirect } from "@remix-run/server-runtime";
 import { z } from "zod";
 import { AppInstallationCallback } from "~/services/github/appInstallationCallback.server";
 import { requireUserId } from "~/services/session.server";
@@ -17,7 +18,18 @@ export async function loader({ request }: LoaderArgs) {
 
   const service = new AppInstallationCallback();
 
-  const result = await service.call(ParamSchema.parse(params));
+  const parsedParams = ParamSchema.safeParse(params);
+
+  if (!parsedParams.success) {
+    console.error(
+      `[github.callback] Invalid params`,
+      params,
+      parsedParams.error
+    );
+    throw new Response("Failed to connect to GitHub", { status: 400 });
+  }
+
+  const result = await service.call(parsedParams.data);
 
   if (result) {
     const { authorization, templateId } = result;
