ae550df37d952ee690d3c3bedf74036df2a12f25
James Ritchie <james@jamesritchie.co.uk>
2023-02-24 15:25:43 +0000

    Updated the Workflows Overview page if you have no workflows

--- apps/webapp/app/routes/__app/orgs/$organizationSlug/__org/index.tsx
+++ apps/webapp/app/routes/__app/orgs/$organizationSlug/__org/index.tsx
@@ -1,12 +1,13 @@
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import { typedjson, useTypedLoaderData } from "remix-typedjson";
 import invariant from "tiny-invariant";
+import { CopyTextPanel } from "~/components/CopyTextButton";
 import { CreateNewWorkflow } from "~/components/CreateNewWorkflow";
 import { Container } from "~/components/layout/Container";
-import { PanelInfo } from "~/components/layout/PanelInfo";
-import { PrimaryLink } from "~/components/primitives/Buttons";
+import { Panel } from "~/components/layout/Panel";
 import { SubTitle } from "~/components/primitives/text/SubTitle";
 import { Title } from "~/components/primitives/text/Title";
+import { TemplatesGrid } from "~/components/templates/TemplatesGrid";
 import { WorkflowList } from "~/components/workflows/workflowList";
 import { useCurrentOrganization } from "~/hooks/useOrganizations";
 import { getRuntimeEnvironmentFromRequest } from "~/models/runtimeEnvironment.server";
@@ -22,8 +23,11 @@ export const loader = async ({ request, params }: LoaderArgs) => {
   const presenter = new WorkflowListPresenter();
 
   try {
-    const workflows = await presenter.data(params.organizationSlug, currentEnv);
-    return typedjson({ workflows });
+    const { workflows, templates } = await presenter.data(
+      params.organizationSlug,
+      currentEnv
+    );
+    return typedjson({ workflows, templates });
   } catch (error: any) {
     console.error(error);
     throw new Response("Error ", { status: 400 });
@@ -31,7 +35,7 @@ export const loader = async ({ request, params }: LoaderArgs) => {
 };
 
 export default function Page() {
-  const { workflows } = useTypedLoaderData<typeof loader>();
+  const { workflows, templates } = useTypedLoaderData<typeof loader>();
   const currentOrganization = useCurrentOrganization();
   if (currentOrganization === undefined) {
     return <></>;
@@ -39,22 +43,23 @@ export default function Page() {
 
   return (
     <Container>
-      <Title>Workflows</Title>
       {workflows.length === 0 ? (
         <>
-          <SubTitle>0 workflows</SubTitle>
-          <PanelInfo
-            message="You don't have any workflows yet. They will appear here once
-              connected."
-            className="mb-4 max-w-4xl p-4 pr-6"
-          >
-            <PrimaryLink to={`/orgs/${currentOrganization.slug}/workflows/new`}>
-              Create first workflow
-            </PrimaryLink>
-          </PanelInfo>
+          <Title>Create your first workflow</Title>
+          <div className="max-w-5xl">
+            <SubTitle className="">Install the Trigger.dev package</SubTitle>
+            <Panel className="mb-4">
+              <CopyTextPanel value="npm create trigger@latest trigger_development_xxxxxxxxxxxxxxxxx" />
+            </Panel>
+            <SubTitle className="">Or clone a template</SubTitle>
+            <div>
+              <TemplatesGrid openInNewPage={false} templates={templates} />
+            </div>
+          </div>
         </>
       ) : (
         <>
+          <Title>Workflows</Title>
           <SubTitle>
             {workflows.length} active workflow{workflows.length > 1 ? "s" : ""}
           </SubTitle>
