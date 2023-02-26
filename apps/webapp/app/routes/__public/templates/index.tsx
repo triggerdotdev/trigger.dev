8aff958bd3ad469ca6949160e022ede99a5c7e79
James Ritchie <james@jamesritchie.co.uk>
2023-02-26 15:57:06 +0000

    Copy fix

--- apps/webapp/app/routes/__public/templates/index.tsx
+++ apps/webapp/app/routes/__public/templates/index.tsx
@@ -17,8 +17,8 @@ export default function TemplateList() {
       <h1 className="mb-6 text-center font-title text-5xl font-semibold text-slate-200">
         Choose your Template
       </h1>
-      <Header2 size="small" className="mb-8 text-center text-slate-400">
-        Quickly get started with your workflow by using a pre-built example.
+      <Header2 size="small" className="mb-16 text-center text-slate-400">
+        Quickly get started with your workflow by using a pre-built template.
       </Header2>
       <TemplatesGrid templates={templates} openInNewPage={true} />
     </div>
