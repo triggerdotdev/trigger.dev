d5d3786a97ce1b718e45b251a682d8713c60591b
Matt Aitken <matt@mattaitken.com>
2023-02-27 21:38:14 +0000

    Automatically set  "Notion-Version" to "2022-06-28" so users don’t have to

--- apps/integrations/src/integrations/notion/actions/actions.ts
+++ apps/integrations/src/integrations/notion/actions/actions.ts
@@ -1,5 +1,26 @@
 import { makeSimpleActions } from "core/action/makeAction";
 import endpoints from "../endpoints/endpoints";
 
-const actions = makeSimpleActions(endpoints);
+const actions = makeSimpleActions(
+  endpoints,
+  (specs) => {
+    const paramsWithoutVersion = specs.input.parameters?.filter(
+      (p) => p.name !== "Notion-Version"
+    );
+    return {
+      input: {
+        ...specs.input,
+        parameters: paramsWithoutVersion,
+      },
+      output: specs.output,
+    };
+  },
+  (data) => {
+    data.parameters = {
+      ...data.parameters,
+      "Notion-Version": "2022-06-28",
+    };
+    return data;
+  }
+);
 export default actions;
