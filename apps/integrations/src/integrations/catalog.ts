ace099994d9f56a65c9f7028b629512e8be9f058
Matt Aitken <matt@mattaitken.com>
2023-03-03 19:41:21 +0000

    Added Typeform to the catalog

--- apps/integrations/src/integrations/catalog.ts
+++ apps/integrations/src/integrations/catalog.ts
@@ -2,11 +2,13 @@ import { Catalog } from "core/catalog";
 import { airtable } from "./airtable";
 import { notion } from "./notion";
 import { sendgrid } from "./sendgrid";
+import { typeform } from "./typeform";
 
 export const catalog: Catalog = {
   services: {
     airtable,
     notion,
     sendgrid,
+    typeform,
   },
 };
