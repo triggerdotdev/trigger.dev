b43224c353b9c38a2491b2aef707a5181a22337d
Eric Allam <eallam@icloud.com>
2023-03-03 11:26:17 +0000

    Upgrade to remix v1.11.0 for streaming and defer support

--- apps/webapp/app/entry.server.tsx
+++ apps/webapp/app/entry.server.tsx
@@ -1,9 +1,13 @@
-import type { EntryContext } from "@remix-run/node";
-import { Response } from "@remix-run/node";
-import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
 import * as MessageBroker from "~/services/messageBroker.server";
 import * as Sentry from "~/services/sentry.server";
+import { PassThrough } from "stream";
+import { renderToPipeableStream } from "react-dom/server";
+import { RemixServer } from "@remix-run/react";
+import { Response } from "@remix-run/node"; // or cloudflare/deno
+import type { EntryContext, Headers } from "@remix-run/node"; // or cloudflare/deno
+import isbot from "isbot";
+
+const ABORT_DELAY = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -11,17 +15,98 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  // deepcode ignore Ssti: <This is recommended by Remix>
-  const markup = renderToString(
-    // deepcode ignore OR: <All good in the hood>
-    <RemixServer context={remixContext} url={request.url} />
+  // If the request is from a bot, we want to wait for the full
+  // response to render before sending it to the client. This
+  // ensures that bots can see the full page content.
+  if (isbot(request.headers.get("user-agent"))) {
+    return serveTheBots(
+      request,
+      responseStatusCode,
+      responseHeaders,
+      remixContext
+    );
+  }
+
+  return serveBrowsers(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixContext
   );
+}
 
-  responseHeaders.set("Content-Type", "text/html; charset=utf-8");
+function serveTheBots(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+        abortDelay={ABORT_DELAY}
+      />,
+      {
+        // Use onAllReady to wait for the entire document to be ready
+        onAllReady() {
+          responseHeaders.set("Content-Type", "text/html");
+          let body = new PassThrough();
+          pipe(body);
+          resolve(
+            new Response(body, {
+              status: responseStatusCode,
+              headers: responseHeaders,
+            })
+          );
+        },
+        onShellError(err: unknown) {
+          reject(err);
+        },
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+function serveBrowsers(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    let didError = false;
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+        abortDelay={ABORT_DELAY}
+      />,
+      {
+        // use onShellReady to wait until a suspense boundary is triggered
+        onShellReady() {
+          responseHeaders.set("Content-Type", "text/html");
+          let body = new PassThrough();
+          pipe(body);
+          resolve(
+            new Response(body, {
+              status: didError ? 500 : responseStatusCode,
+              headers: responseHeaders,
+            })
+          );
+        },
+        onShellError(err: unknown) {
+          reject(err);
+        },
+        onError(err: unknown) {
+          didError = true;
+          console.error(err);
+        },
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
   });
 }
 
