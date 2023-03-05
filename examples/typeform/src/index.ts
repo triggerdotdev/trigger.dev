2a92afde0f313607b423bd49a8fbb76c96d33c8e
Matt Aitken <matt@mattaitken.com>
2023-03-05 16:19:27 +0000

    Created a typeform example

---
+++ examples/typeform/src/index.ts
@@ -0,0 +1,17 @@
+import { Trigger } from "@trigger.dev/sdk";
+import { events } from "@trigger.dev/typeform";
+
+new Trigger({
+  id: "typeform-webhook",
+  name: "Typeform webhook example",
+  apiKey: "trigger_dev_zC25mKNn6c0q",
+  endpoint: "ws://localhost:8889/ws",
+  logLevel: "debug",
+  on: events.formResponseEvent({
+    form_id: "NclFXN1d",
+  }),
+  run: async (event, ctx) => {
+    await ctx.logger.info(`Action was ${event.event_id}`, event.form_response);
+    return {};
+  },
+}).listen();
