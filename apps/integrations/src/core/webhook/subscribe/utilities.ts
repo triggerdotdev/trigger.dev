22ceeb5e809e9dce22057221609c6c344498ead1
Matt Aitken <matt@mattaitken.com>
2023-03-05 17:33:53 +0000

    Fix for the webhook url

--- apps/integrations/src/core/webhook/subscribe/utilities.ts
+++ apps/integrations/src/core/webhook/subscribe/utilities.ts
@@ -1,3 +1,3 @@
 export function webhookUrl(id: string): string {
-  return `${process.env.WEBHOOKS_ORIGIN}/api/v1/webhooks/${id}/receive`;
+  return `${process.env.WEBHOOKS_ORIGIN}/api/v2/webhooks/${id}/receive`;
 }
