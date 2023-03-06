449c40e261a83fd89c9c7f9918ee04cc4a6c610c
Matt Aitken <matt@mattaitken.com>
2023-03-06 18:07:38 +0000

    Updated the example form

--- examples/typeform/src/index.ts
+++ examples/typeform/src/index.ts
@@ -2,13 +2,13 @@ import { Trigger } from "@trigger.dev/sdk";
 import { events } from "@trigger.dev/typeform";
 
 new Trigger({
-  id: "typeform-webhook",
+  id: "typeform-webhook-3",
   name: "Typeform webhook example",
   apiKey: "trigger_dev_zC25mKNn6c0q",
   endpoint: "ws://localhost:8889/ws",
   logLevel: "debug",
   on: events.formResponseEvent({
-    form_id: "XHfDDtcu",
+    form_id: "Wu9xVIt6",
   }),
   run: async (event, ctx) => {
     await ctx.logger.info(`Action was ${event.event_id}`, event.form_response);
