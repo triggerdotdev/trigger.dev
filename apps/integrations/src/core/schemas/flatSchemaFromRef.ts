84720ce39098949b4315fc15dcf35d2ef293af90
Matt Aitken <matt@mattaitken.com>
2023-03-08 14:39:20 +0000

    Non recursive flat schema generator

--- apps/integrations/src/core/schemas/flatSchemaFromRef.ts
+++ apps/integrations/src/core/schemas/flatSchemaFromRef.ts
@@ -4,49 +4,79 @@ import { JSONSchema } from "./types";
 export function flatSchemaFromRef(spec: any, ref: string): JSONSchema {
   //starting point
   const path = ref.replace("#", "");
-  const object = pointer.get(spec, path);
-  const newSpec = JSON.parse(JSON.stringify(object));
+  try {
+    const object = pointer.get(spec, path);
+    const newSpec = JSON.parse(JSON.stringify(object));
 
-  //walk through the object and deref anything that needs it (then recursively do this)
-  deReffer(spec, newSpec);
+    //walk through the object and deref anything that needs it (then recursively do this)
+    const deReffer = new Dereffer(spec);
+    deReffer.run(newSpec);
 
-  return newSpec;
+    return newSpec;
+  } catch (e) {
+    console.log(spec);
+    console.error("e", e);
+    return spec;
+  }
 }
 
-function deReffer(spec: any, object: any) {
-  //walk through the object. If there is a key called "$ref" then replace it with the referenced object
-  //if the object has anyOf, oneOf or allOf then we need to deReffer each of those
-  //if the object has properties then we need to deReffer each of those
-  //if the object has items then we need to deReffer that
-  //if the object has additionalProperties then we need to deReffer that
-  //if the object has a type of "object" then we need to deReffer that
+class Dereffer {
+  spec: any;
+  stack: any[];
+  refsExplored: Map<string, any>;
 
-  if (object == null) return;
-
-  if (object.$ref) {
-    const path = (object.$ref as string).replace("#", "");
-    const ptr = pointer.get(spec, path);
-    if (ptr === undefined) {
-      throw new Error(`Invalid reference: ${object.$ref}`);
-    }
-
-    Object.assign(object, ptr);
-    delete object.$ref;
+  constructor(spec: any) {
+    this.spec = spec;
+    this.stack = [];
+    this.refsExplored = new Map();
   }
 
-  Object.entries(object).forEach(([key, value]) => {
-    if (key === "$ref") {
-      return;
-    }
+  run(object: any) {
+    this.stack.push(object);
+    this.#walk();
+  }
 
-    if (typeof value === "object") {
-      if (Array.isArray(value)) {
-        value.forEach((item: any) => {
-          deReffer(spec, item);
-        });
-      } else {
-        deReffer(spec, value);
+  #walk() {
+    while (this.stack.length > 0) {
+      const object = this.stack.pop();
+      if (object == null) continue;
+      if (typeof object !== "object") continue;
+
+      if (object.$ref) {
+        const path = (object.$ref as string).replace("#", "");
+
+        if (!this.refsExplored.has(path)) {
+          const ptr = pointer.get(this.spec, path);
+          if (ptr === undefined) {
+            throw new Error(`Invalid reference: ${object.$ref}`);
+          }
+
+          Object.assign(object, ptr);
+          delete object.$ref;
+
+          this.refsExplored.set(path, ptr);
+        } else {
+          const ptr = this.refsExplored.get(path);
+          Object.assign(object, ptr);
+          delete object.$ref;
+        }
       }
+
+      Object.entries(object).forEach(([key, value]) => {
+        if (key === "$ref") {
+          return;
+        }
+
+        if (typeof value === "object") {
+          if (Array.isArray(value)) {
+            value.forEach((item: any) => {
+              this.stack.push(item);
+            });
+          } else {
+            this.stack.push(value);
+          }
+        }
+      });
     }
-  });
+  }
 }
