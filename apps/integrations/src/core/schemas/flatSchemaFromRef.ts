7395d42965a116dff9445055b4ef77a9d63215d3
Matt Aitken <matt@mattaitken.com>
2023-03-08 12:41:57 +0000

    Generate a flat schema from a ref into a spec

--- apps/integrations/src/core/schemas/flatSchemaFromRef.ts
+++ apps/integrations/src/core/schemas/flatSchemaFromRef.ts
@@ -1,16 +1,25 @@
 import pointer from "json-pointer";
+import { JSONSchema } from "./types";
 
-export function deReffer(spec: any, object?: any) {
+export function flatSchemaFromRef(spec: any, ref: string): JSONSchema {
+  //starting point
+  const path = ref.replace("#", "");
+  const object = pointer.get(spec, path);
+  const newSpec = JSON.parse(JSON.stringify(object));
+
+  //walk through the object and deref anything that needs it (then recursively do this)
+  deReffer(spec, newSpec);
+
+  return newSpec;
+}
+
+function deReffer(spec: any, object: any) {
   //walk through the object. If there is a key called "$ref" then replace it with the referenced object
   //if the object has anyOf, oneOf or allOf then we need to deReffer each of those
   //if the object has properties then we need to deReffer each of those
   //if the object has items then we need to deReffer that
   //if the object has additionalProperties then we need to deReffer that
   //if the object has a type of "object" then we need to deReffer that
-
-  if (object === undefined) {
-    object = spec;
-  }
 
   Object.entries(object).forEach(([key, value]) => {
     if (key === "$ref") {
@@ -19,30 +28,21 @@ export function deReffer(spec: any, object?: any) {
       if (ptr === undefined) {
         throw new Error(`Invalid reference: ${value}`);
       }
-      object = {
-        ...object,
-        ...ptr,
-      };
-      delete object?.$ref;
+
+      Object.assign(object, ptr);
+      delete object.$ref;
+
+      //because we've modified the object, we need to re-run the loop
+      deReffer(spec, object);
     }
 
-    recursive(object, "properties");
-    recursive(object, "additionalproperties");
-    recursive(object, "allOf");
-    recursive(object, "oneOf");
-    recursive(object, "anyOf");
-
-    if (object?.items) {
-      object.items.forEach((item: any) => {
-        deReffer(spec, item);
-      });
-    }
-
-    function recursive(object: any, key: string) {
-      if (object[key] && typeof object[key] === "object") {
-        Object.entries(object[key]).forEach(([key, value]) => {
-          deReffer(spec, value);
+    if (typeof value === "object") {
+      if (Array.isArray(value)) {
+        value.forEach((item: any) => {
+          deReffer(spec, item);
         });
+      } else {
+        deReffer(spec, value);
       }
     }
   });
