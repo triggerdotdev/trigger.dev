1f34a22bd8d76d4684a7574654e39727ec10dd15
Eric Allam <eallam@icloud.com>
2023-03-09 10:27:37 +0000

    Update how we poll for logs and add build logs back in

--- apps/webapp/app/features/ee/projects/services/deploymentLogPoll.server.ts
+++ apps/webapp/app/features/ee/projects/services/deploymentLogPoll.server.ts
@@ -3,8 +3,13 @@ import { createHash } from "node:crypto";
 import type { PrismaClient } from "~/db.server";
 import { prisma } from "~/db.server";
 import { taskQueue } from "~/services/messageBroker.server";
-import type { LogLine } from "../cakework.server";
-import { getBuildLogs, getVmLogs } from "../cakework.server";
+import type {
+  GetLogsOptions,
+  GetLogsResponse,
+  LogLine,
+} from "../cakework.server";
+import { getBuildLogs } from "../cakework.server";
+import { getVmLogs } from "../cakework.server";
 
 export class DeploymentLogPoll {
   #prismaClient: PrismaClient;
@@ -29,7 +34,6 @@ export class DeploymentLogPoll {
 
     switch (deployment.status) {
       case "STOPPED":
-      case "STOPPING":
       case "ERROR":
       case "CANCELLED": {
         return true;
@@ -37,27 +41,14 @@ export class DeploymentLogPoll {
       case "PENDING":
       case "BUILDING":
       case "DEPLOYING": {
-        // Turning off build logs for now
-        // const newLogCount = await this.#pollBuildLogs(deployment);
-
-        // If count === 720, we've been polling for 1 hour
-
-        if (count === 720) {
-          // Stop polling after 1 hour
-          return true;
-        }
-
-        await taskQueue.publish(
-          "DEPLOYMENT_LOG_POLL",
-          { id, count: count + 1 },
-          {},
-          { deliverAfter: 5000 }
-        );
+        await this.#pollLogs(deployment, "BUILD");
+        await this.#scheduleNextPoll(deployment, "BUILD", count);
         break;
       }
+      case "STOPPING":
       case "DEPLOYED": {
-        await this.#pollMachineLogs(deployment);
-        await this.#scheduleNextPoll(deployment, count);
+        await this.#pollLogs(deployment, "MACHINE");
+        await this.#scheduleNextPoll(deployment, "MACHINE", count);
 
         break;
       }
@@ -67,11 +58,16 @@ export class DeploymentLogPoll {
     }
   }
 
-  async #scheduleNextPoll(deployment: ProjectDeployment, count: number) {
-    // Find the latest log
+  async #scheduleNextPoll(
+    deployment: ProjectDeployment,
+    logType: "BUILD" | "MACHINE",
+    count: number
+  ) {
+    // Find the latest actual log
     const latestLog = await this.#prismaClient.deploymentLog.findFirst({
       where: {
         deploymentId: deployment.id,
+        logType,
       },
       orderBy: {
         createdAt: "desc",
@@ -80,6 +76,9 @@ export class DeploymentLogPoll {
     });
 
     if (!latestLog) {
+      console.log(
+        `[${logType}] No logs for ${deployment.id} yet. Trying again in 3 seconds.`
+      );
       // If there are no logs, schedule again in 3 seconds
       await taskQueue.publish(
         "DEPLOYMENT_LOG_POLL",
@@ -95,6 +94,10 @@ export class DeploymentLogPoll {
     if (latestLog) {
       // and it's less than 10 seconds old, schedule again in 3 seconds
       if (latestLog.createdAt.getTime() > Date.now() - 10000) {
+        console.log(
+          `[${logType}] Latest log for ${deployment.id} is less than 10 seconds old. Trying again in 3 seconds.`
+        );
+
         await taskQueue.publish(
           "DEPLOYMENT_LOG_POLL",
           { id: deployment.id, count: count + 1 },
@@ -106,6 +109,10 @@ export class DeploymentLogPoll {
 
       // and it's less than 60 seconds old, schedule again in 5 seconds
       if (latestLog.createdAt.getTime() > Date.now() - 60 * 1000) {
+        console.log(
+          `[${logType}] Latest log for ${deployment.id} is less than 60 seconds old. Trying again in 5 seconds.`
+        );
+
         await taskQueue.publish(
           "DEPLOYMENT_LOG_POLL",
           { id: deployment.id, count: count + 1 },
@@ -117,6 +124,10 @@ export class DeploymentLogPoll {
 
       // and it's less than 5 minutes old, schedule again in 10 seconds
       if (latestLog.createdAt.getTime() > Date.now() - 5 * 60 * 1000) {
+        console.log(
+          `[${logType}] Latest log for ${deployment.id} is less than 5 minutes old. Trying again in 10 seconds.`
+        );
+
         await taskQueue.publish(
           "DEPLOYMENT_LOG_POLL",
           { id: deployment.id, count: count + 1 },
@@ -128,6 +139,10 @@ export class DeploymentLogPoll {
 
       // And it's less than 60 minutes old, schedule again in 30 seconds
       if (latestLog.createdAt.getTime() > Date.now() - 60 * 60 * 1000) {
+        console.log(
+          `[${logType}] Latest log for ${deployment.id} is less than 60 minutes old. Trying again in 30 seconds.`
+        );
+
         await taskQueue.publish(
           "DEPLOYMENT_LOG_POLL",
           { id: deployment.id, count: count + 1 },
@@ -139,6 +154,10 @@ export class DeploymentLogPoll {
 
       // And it's less than 24 hours old, schedule again in 5 minutes
       if (latestLog.createdAt.getTime() > Date.now() - 24 * 60 * 60 * 1000) {
+        console.log(
+          `[${logType}] Latest log for ${deployment.id} is less than 24 hours old. Trying again in 5 minutes.`
+        );
+
         await taskQueue.publish(
           "DEPLOYMENT_LOG_POLL",
           { id: deployment.id, count: count + 1 },
@@ -153,6 +172,10 @@ export class DeploymentLogPoll {
         latestLog.createdAt.getTime() >
         Date.now() - 7 * 24 * 60 * 60 * 1000
       ) {
+        console.log(
+          `[${logType}] Latest log for ${deployment.id} is less than 7 days old. Trying again in 30 minutes.`
+        );
+
         await taskQueue.publish(
           "DEPLOYMENT_LOG_POLL",
           { id: deployment.id, count: count + 1 },
@@ -167,6 +190,10 @@ export class DeploymentLogPoll {
         latestLog.createdAt.getTime() >
         Date.now() - 30 * 24 * 60 * 60 * 1000
       ) {
+        console.log(
+          `[${logType}] Latest log for ${deployment.id} is less than 30 days old. Trying again in 2 hours.`
+        );
+
         await taskQueue.publish(
           "DEPLOYMENT_LOG_POLL",
           { id: deployment.id, count: count + 1 },
@@ -177,91 +204,36 @@ export class DeploymentLogPoll {
       }
 
       // Else stop scheduling
-    }
-  }
-
-  async #pollBuildLogs(deployment: ProjectDeployment) {
-    const latestBuildLog = await this.#prismaClient.deploymentLog.findFirst({
-      where: {
-        deploymentId: deployment.id,
-        logType: "BUILD",
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
-
-    const fromDate =
-      latestBuildLog?.createdAt ??
-      deployment.buildStartedAt ??
-      new Date(24 * 60 * 60 * 1000);
-
-    const toDate = new Date();
-
-    const logs = await this.#gatherBuildLogs(deployment, fromDate, toDate);
-
-    const filteredLogs = logs.filter((log) => filterLine(log.message));
-
-    // Create a new log for each line
-    await this.#prismaClient.deploymentLog.createMany({
-      data: filteredLogs.map((log, index) =>
-        createLogRecord(deployment.id, index, log, "BUILD")
-      ),
-      skipDuplicates: true,
-    });
-
-    return filteredLogs.length;
-  }
-
-  async #gatherBuildLogs(
-    deployment: ProjectDeployment,
-    fromDate: Date,
-    toDate: Date,
-    paginationToken?: string,
-    logs: LogLine[] = []
-  ): Promise<Array<LogLine>> {
-    const logsResponse = await getBuildLogs(deployment.buildId, {
-      from: fromDate,
-      to: toDate,
-      batch: 100,
-      pagination: paginationToken,
-    });
-
-    if (logsResponse.pagination) {
-      return this.#gatherBuildLogs(
-        deployment,
-        fromDate,
-        toDate,
-        logsResponse.pagination,
-        logs.concat(logsResponse.lines)
+      console.log(
+        `[${logType}] Latest log for ${deployment.id} is more than 30 days old. Stopping polling.`
       );
     }
-
-    return logs.concat(logsResponse.lines);
   }
 
-  async #pollMachineLogs(deployment: ProjectDeployment) {
-    const latestBuildLog = await this.#prismaClient.deploymentLog.findFirst({
-      where: {
-        deploymentId: deployment.id,
-        logType: "MACHINE",
-      },
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+  async #pollLogs(deployment: ProjectDeployment, logType: "BUILD" | "MACHINE") {
+    const { machineLogLatestLog, buildLogLatestLog } =
+      await this.#prismaClient.projectDeployment.findUniqueOrThrow({
+        where: {
+          id: deployment.id,
+        },
+        select: {
+          machineLogLatestLog: true,
+          buildLogLatestLog: true,
+        },
+      });
+
+    const latestLog =
+      logType === "BUILD" ? buildLogLatestLog : machineLogLatestLog;
 
     let fromDate =
-      latestBuildLog?.createdAt ??
-      deployment.buildFinishedAt ??
-      new Date(24 * 60 * 60 * 1000);
+      latestLog ?? deployment.buildFinishedAt ?? new Date(24 * 60 * 60 * 1000);
 
     // Add 1 millisecond to fromDate to avoid duplicate logs
     fromDate = new Date(fromDate.getTime() + 1);
 
     const toDate = new Date();
 
-    const logs = await this.#gatherMachineLogs(deployment, fromDate, toDate);
+    const logs = await this.#gatherLogs(deployment, logType, fromDate, toDate);
 
     const filteredLogs = logs.filter((log) => filterLine(log.message));
 
@@ -273,11 +245,22 @@ export class DeploymentLogPoll {
       skipDuplicates: true,
     });
 
+    // Update the machineLogLatestLog
+    await this.#prismaClient.projectDeployment.update({
+      where: {
+        id: deployment.id,
+      },
+      data: {
+        machineLogLatestLog: toDate,
+      },
+    });
+
     return filteredLogs.length;
   }
 
-  async #gatherMachineLogs(
+  async #gatherLogs(
     deployment: ProjectDeployment,
+    logType: "BUILD" | "MACHINE",
     fromDate: Date,
     toDate: Date,
     paginationToken?: string,
@@ -287,7 +270,7 @@ export class DeploymentLogPoll {
       return [];
     }
 
-    const logsResponse = await getVmLogs(deployment.vmIdentifier, {
+    const logsResponse = await this.#getLogs(deployment, logType, {
       from: fromDate,
       to: toDate,
       batch: 100,
@@ -295,8 +278,9 @@ export class DeploymentLogPoll {
     });
 
     if (logsResponse.pagination) {
-      return this.#gatherMachineLogs(
+      return this.#gatherLogs(
         deployment,
+        logType,
         fromDate,
         toDate,
         logsResponse.pagination,
@@ -305,6 +289,25 @@ export class DeploymentLogPoll {
     }
 
     return logs.concat(logsResponse.lines);
+  }
+
+  async #getLogs(
+    deployment: ProjectDeployment,
+    logType: "BUILD" | "MACHINE",
+    options: GetLogsOptions = {}
+  ): Promise<GetLogsResponse> {
+    switch (logType) {
+      case "BUILD":
+        return getBuildLogs(deployment.buildId, options);
+      case "MACHINE":
+        if (!deployment.vmIdentifier) {
+          return { lines: [] };
+        }
+
+        return getVmLogs(deployment.vmIdentifier, options);
+      default:
+        throw new Error(`Unknown log type: ${logType}`);
+    }
   }
 }
 
