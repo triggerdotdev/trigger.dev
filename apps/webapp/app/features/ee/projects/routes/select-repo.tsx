0f9c0a8d06e3b315b68c3a8b5dcb003624167359
Eric Allam <eallam@icloud.com>
2023-03-10 17:58:20 +0000

    Improve the select repo experience a little bit

--- apps/webapp/app/features/ee/projects/routes/select-repo.tsx
+++ apps/webapp/app/features/ee/projects/routes/select-repo.tsx
@@ -94,169 +94,158 @@ export default function Page() {
   return (
     <Container>
       <Title>Deploy a GitHub repository</Title>
-      <Form method="post" className="w-full">
-        {appAuthorizations.length === 0 ? (
-          <>
-            <ConnectToGithub redirectTo={redirectTo} />
-            <ConfigureGithub />
-          </>
-        ) : (
-          <>
-            {(!isSubmittingOrLoading &&
-              actionData?.type === "serviceDefinitionError") ||
-            actionData?.type === "serviceError" ? (
-              <PanelWarning message={actionData.message} className="mb-4" />
-            ) : !isSubmittingOrLoading &&
-              actionData?.type === "payloadError" ? (
-              <PanelWarning
-                message="Something went wrong with your request. Please try again."
-                className="mb-4"
-              />
-            ) : (
-              <></>
-            )}
-            <Suspense
-              fallback={
-                <Panel className="mb-6 flex h-10 items-center justify-center gap-2 py-10">
-                  <Spinner />
-                  <Body>Loading repositories…</Body>
-                </Panel>
-              }
+      {appAuthorizations.length === 0 ? (
+        <>
+          <ConnectToGithub redirectTo={redirectTo} />
+          <ConfigureGithub />
+        </>
+      ) : (
+        <>
+          {(!isSubmittingOrLoading &&
+            actionData?.type === "serviceDefinitionError") ||
+          actionData?.type === "serviceError" ? (
+            <PanelWarning message={actionData.message} className="mb-4" />
+          ) : !isSubmittingOrLoading && actionData?.type === "payloadError" ? (
+            <PanelWarning
+              message="Something went wrong with your request. Please try again."
+              className="mb-4"
+            />
+          ) : (
+            <></>
+          )}
+          <Suspense
+            fallback={
+              <Panel className="mb-6 flex h-10 items-center justify-center gap-2 py-10">
+                <Spinner />
+                <Body>Loading repositories…</Body>
+              </Panel>
+            }
+          >
+            <Await
+              errorElement={<p>Error loading repositories</p>}
+              resolve={repositories}
             >
-              <Await
-                errorElement={<p>Error loading repositories</p>}
-                resolve={repositories}
-              >
-                {(resolvedPromise) => (
-                  <>
-                    {resolvedPromise.map((reposWithAuth) => (
-                      <Fragment key={reposWithAuth.authorization.id}>
-                        <div className="flex w-full items-center justify-between">
-                          <div className="mb-2 flex items-center gap-2">
-                            <OctoKitty className="h-5 w-5" />
-                            <SubTitle className="mb-0">
-                              {reposWithAuth.authorization.accountName}
-                            </SubTitle>
-                          </div>
-                          <TertiaryLink
-                            to={`/apps/github?redirectTo=${encodeURIComponent(
-                              redirectTo
-                            )}&authorizationId=${
-                              reposWithAuth.authorization.id
-                            }`}
-                            reloadDocument
-                          >
-                            Configure {reposWithAuth.authorization.accountName}
-                            <ArrowTopRightOnSquareIcon className="h-4 w-4 text-slate-500" />
-                          </TertiaryLink>
+              {(resolvedPromise) => (
+                <>
+                  {resolvedPromise.map((reposWithAuth) => (
+                    <Fragment key={reposWithAuth.authorization.id}>
+                      <div className="flex w-full items-center justify-between">
+                        <div className="mb-2 flex items-center gap-2">
+                          <OctoKitty className="h-5 w-5" />
+                          <SubTitle className="mb-0">
+                            {reposWithAuth.authorization.accountName}
+                          </SubTitle>
                         </div>
-                        <List className="mb-6">
-                          {reposWithAuth.repositories.map((repo) => (
-                            <li
-                              key={repo.repository.id}
-                              className="flex items-center justify-between gap-2 py-2.5 pl-4 pr-2.5 first:rounded-t-md"
-                            >
-                              <div className="flex items-center gap-2.5">
-                                {repo.repository.private ? (
-                                  <LockClosedIcon className="h-4 w-4 text-slate-400" />
-                                ) : (
-                                  <LockOpenIcon className="h-4 w-4 text-slate-400" />
-                                )}
-
-                                <a
-                                  className="group flex items-center gap-2 text-slate-300 transition hover:text-slate-100"
-                                  href={repo.repository.html_url}
-                                  target="_blank"
-                                  rel="noreferrer"
-                                >
-                                  {repo.repository.full_name}#
-                                  {repo.repository.default_branch}
-                                  <ArrowTopRightOnSquareIcon className="h-4 w-4 text-slate-400 opacity-0 transition group-hover:opacity-100" />
-                                </a>
-                              </div>
-                              {repo.projectId ? (
-                                <div className="flex items-center gap-6">
-                                  {repo.status === "relevant" ? (
-                                    <Tooltip text="This repo is configured for deploying a workflow">
-                                      <div className="flex items-center gap-1 text-slate-400">
-                                        <StarIcon className="h-4 w-4 text-yellow-500" />
-                                        <Body size="small" className="">
-                                          This repo is configured
-                                        </Body>
-                                        <InformationCircleIcon className="h-4 w-4" />
-                                      </div>
-                                    </Tooltip>
-                                  ) : (
-                                    <></>
-                                  )}
-                                  <SecondaryLink
-                                    to={`../projects/${repo.projectId}`}
-                                  >
-                                    View Project
-                                  </SecondaryLink>
-                                </div>
+                        <TertiaryLink
+                          to={`/apps/github?redirectTo=${encodeURIComponent(
+                            redirectTo
+                          )}&authorizationId=${reposWithAuth.authorization.id}`}
+                          reloadDocument
+                        >
+                          Configure {reposWithAuth.authorization.accountName}
+                          <ArrowTopRightOnSquareIcon className="h-4 w-4 text-slate-500" />
+                        </TertiaryLink>
+                      </div>
+                      <List className="mb-6">
+                        {reposWithAuth.repositories.map((repo) => (
+                          <li
+                            key={repo.repository.id}
+                            className="flex items-center justify-between gap-2 py-2.5 pl-4 pr-2.5 first:rounded-t-md"
+                          >
+                            <div className="flex items-center gap-2.5">
+                              {repo.repository.private ? (
+                                <LockClosedIcon className="h-4 w-4 text-slate-400" />
                               ) : (
-                                <div className="flex items-center gap-6">
-                                  {repo.status === "relevant" ? (
-                                    <Tooltip text="This repo is configured for deploying a workflow">
-                                      <div className="flex items-center gap-1 text-slate-400">
-                                        <StarIcon className="h-4 w-4 text-yellow-500" />
-                                        <Body size="small" className="">
-                                          This repo is configured
-                                        </Body>
-                                        <InformationCircleIcon className="h-4 w-4" />
-                                      </div>
-                                    </Tooltip>
-                                  ) : (
-                                    <></>
-                                  )}
-                                  <Form method="post">
-                                    <input
-                                      type="hidden"
-                                      name="repoId"
-                                      value={repo.repository.id}
-                                    />
-
-                                    <input
-                                      type="hidden"
-                                      name="repoName"
-                                      value={repo.repository.full_name}
-                                    />
-
-                                    <input
-                                      type="hidden"
-                                      name="appAuthorizationId"
-                                      value={repo.appAuthorizationId}
-                                    />
-
-                                    <PrimaryButton type="submit" size="regular">
-                                      Select
-                                    </PrimaryButton>
-                                  </Form>
-                                </div>
+                                <LockOpenIcon className="h-4 w-4 text-slate-400" />
                               )}
-                            </li>
-                          ))}
-                        </List>
-                      </Fragment>
-                    ))}
-                  </>
-                )}
-              </Await>
-            </Suspense>
 
-            <SubTitle>Connect another GitHub account</SubTitle>
-            <div className="mb-3 grid grid-cols-2 gap-4">
-              <PrimaryLink
-                to={`/apps/github?redirectTo=${encodeURIComponent(redirectTo)}`}
-              >
-                <OctoKitty className="-ml-1 h-5 w-5" />
-                Add another account
-              </PrimaryLink>
-            </div>
-          </>
-        )}
-      </Form>
+                              <a
+                                className="group flex items-center gap-2 text-slate-300 transition hover:text-slate-100"
+                                href={repo.repository.html_url}
+                                target="_blank"
+                                rel="noreferrer"
+                              >
+                                {repo.repository.full_name}#
+                                {repo.repository.default_branch}
+                                <ArrowTopRightOnSquareIcon className="h-4 w-4 text-slate-400 opacity-0 transition group-hover:opacity-100" />
+                              </a>
+                            </div>
+                            {repo.projectId ? (
+                              <div className="flex items-center gap-6">
+                                <SecondaryLink
+                                  to={`../projects/${repo.projectId}`}
+                                >
+                                  View Project
+                                </SecondaryLink>
+                              </div>
+                            ) : (
+                              <div className="flex items-center gap-6">
+                                {repo.status === "relevant" ? (
+                                  <Tooltip text="This repo is configured for deploying a workflow">
+                                    <div className="flex items-center gap-1 text-slate-400">
+                                      <StarIcon className="h-4 w-4 text-yellow-500" />
+                                      <Body size="small" className="">
+                                        This repo contains a workflow
+                                      </Body>
+                                      <InformationCircleIcon className="h-4 w-4" />
+                                    </div>
+                                  </Tooltip>
+                                ) : (
+                                  <></>
+                                )}
+                                <Form
+                                  method="post"
+                                  onSubmit={(e) =>
+                                    !confirm(
+                                      "This will deploy this repository and automatically run your workflows in the live environment. Are you sure?"
+                                    ) && e.preventDefault()
+                                  }
+                                >
+                                  <input
+                                    type="hidden"
+                                    name="repoId"
+                                    value={repo.repository.id}
+                                  />
+
+                                  <input
+                                    type="hidden"
+                                    name="repoName"
+                                    value={repo.repository.full_name}
+                                  />
+
+                                  <input
+                                    type="hidden"
+                                    name="appAuthorizationId"
+                                    value={repo.appAuthorizationId}
+                                  />
+
+                                  <PrimaryButton type="submit" size="regular">
+                                    Deploy
+                                  </PrimaryButton>
+                                </Form>
+                              </div>
+                            )}
+                          </li>
+                        ))}
+                      </List>
+                    </Fragment>
+                  ))}
+                </>
+              )}
+            </Await>
+          </Suspense>
+
+          <SubTitle>Connect another GitHub account</SubTitle>
+          <div className="mb-3 grid grid-cols-2 gap-4">
+            <PrimaryLink
+              to={`/apps/github?redirectTo=${encodeURIComponent(redirectTo)}`}
+            >
+              <OctoKitty className="-ml-1 h-5 w-5" />
+              Add another account
+            </PrimaryLink>
+          </div>
+        </>
+      )}
     </Container>
   );
 }
