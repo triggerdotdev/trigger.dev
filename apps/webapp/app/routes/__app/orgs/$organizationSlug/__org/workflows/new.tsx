9b7102605fc61dce6a92e62aefc6a537031417af
James Ritchie <james@jamesritchie.co.uk>
2023-03-10 17:10:12 +0000

    Fixed the create new workflow page layout

--- apps/webapp/app/routes/__app/orgs/$organizationSlug/__org/workflows/new.tsx
+++ apps/webapp/app/routes/__app/orgs/$organizationSlug/__org/workflows/new.tsx
@@ -1,12 +1,17 @@
 import { Outlet } from "@remix-run/react";
+import { AppBody } from "~/components/layout/AppLayout";
 import { Container } from "~/components/layout/Container";
+import { Header } from "~/components/layout/Header";
 import { Title } from "~/components/primitives/text/Title";
 
 export default function NewWorkflowPage() {
   return (
-    <Container>
-      <Title>Create a new workflow</Title>
-      <Outlet />
-    </Container>
+    <AppBody>
+      <Header />
+      <Container>
+        <Title>Create a new workflow</Title>
+        <Outlet />
+      </Container>
+    </AppBody>
   );
 }
