d6d8ca67a879e1d9ab9c4d603d36586a5079ad51
James Ritchie <james@jamesritchie.co.uk>
2023-03-13 18:42:24 +0000

    Tooltip modal colour fix

--- apps/webapp/app/components/primitives/MenuTitleToolTip.tsx
+++ apps/webapp/app/components/primitives/MenuTitleToolTip.tsx
@@ -8,7 +8,7 @@ export type TooltipProps = {
 export const MenuTitleToolTip: React.FC<TooltipProps> = memo((props) => {
   return (
     <span className="group relative flex">
-      <span className="pointer-events-none absolute top-0.5 left-12 flex translate-x-0 items-center justify-center whitespace-nowrap rounded bg-slate-700 px-3 py-2 text-sm text-slate-300 opacity-0 shadow transition before:absolute before:-left-2 before:top-full before:-translate-y-[22px] before:border-4 before:border-transparent before:border-r-slate-800 before:shadow before:content-[''] group-hover:opacity-100">
+      <span className="pointer-events-none absolute top-0.5 left-12 flex translate-x-0 items-center justify-center whitespace-nowrap rounded bg-slate-700 px-3 py-2 text-sm text-slate-300 opacity-0 shadow transition before:absolute before:-left-2 before:top-full before:-translate-y-[22px] before:border-4 before:border-transparent before:border-r-slate-700 before:shadow before:content-[''] group-hover:opacity-100">
         {props.text}
       </span>
       {props.children}
