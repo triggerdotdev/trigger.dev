4803ad3c9a9d5522de6e43be4207cf1e06e646df
James Ritchie <james@jamesritchie.co.uk>
2023-03-14 14:18:21 +0000

    Small UI update to the Build logs layout

--- apps/webapp/app/features/ee/projects/routes/projects/$projectP/deploys/$deployId.build-logs.tsx
+++ apps/webapp/app/features/ee/projects/routes/projects/$projectP/deploys/$deployId.build-logs.tsx
@@ -1,9 +1,10 @@
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import type { LoaderArgs } from "@remix-run/server-runtime";
 import { typedjson, useTypedLoaderData } from "remix-typedjson";
 import { z } from "zod";
 import { SecondaryLink } from "~/components/primitives/Buttons";
-import { Body } from "~/components/primitives/text/Body";
 import { Header1 } from "~/components/primitives/text/Headers";
+import { SubTitle } from "~/components/primitives/text/SubTitle";
 import { DeploymentBuildLogsPresenter } from "~/features/ee/projects/presenters/deploymentBuildLogsPresenter.server";
 import { LogOutput } from "../../../../components/LogOutput";
 
@@ -25,14 +26,15 @@ export default function DeploymentBuildLogsPage() {
   return (
     <div className="flex h-full flex-col">
       <div className="flex items-start justify-between">
-        <Header1 className="mb-6">{deployment.version} Build Logs</Header1>
+        <Header1 className="mb-6">Build Logs</Header1>
         <div className="flex gap-2">
           <SecondaryLink to={`../deploys/${deployment.id}`}>
-            Back to Deploy
+            <ArrowLeftIcon className="-ml-1 h-4 w-4" />
+            Back to Deployment
           </SecondaryLink>
         </div>
       </div>
-
+      <SubTitle>Build: {deployment.version}</SubTitle>
       <div className="flex flex-auto overflow-auto rounded-md bg-slate-950 p-4">
         <LogOutput logs={logs} />
       </div>
