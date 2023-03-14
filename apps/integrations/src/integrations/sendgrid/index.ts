e7b9fffa6edf027b160869b0e556e4622dcd38cc
Matt Aitken <matt@mattaitken.com>
2023-03-14 16:52:34 +0000

    SendGrid schemas

--- apps/integrations/src/integrations/sendgrid/index.ts
+++ apps/integrations/src/integrations/sendgrid/index.ts
@@ -1,6 +1,7 @@
 import { Service } from "core/service/types";
 import { authentication } from "./authentication";
 import actions from "./actions/actions";
+import { spec } from "./common/schemas";
 
 export const sendgrid: Service = {
   name: "SendGrid",
@@ -8,6 +9,7 @@ export const sendgrid: Service = {
   version: "0.1.21",
   baseUrl: "https://api.sendgrid.com",
   live: true,
+  schema: spec,
   authentication,
   actions,
   retryableStatusCodes: [408, 429, 500, 502, 503, 504],
