b37d2a0ea24e150a2f2969ee30150b7e85713b2f
Matt Aitken <matt@mattaitken.com>
2023-04-28 18:27:51 +0100

    Improved the NamedIcon to use an object and not use the API Store

--- apps/webapp/app/components/Icon.tsx
+++ apps/webapp/app/components/Icon.tsx
@@ -5,71 +5,50 @@ import {
   StopIcon,
 } from "@heroicons/react/20/solid";
 import classNames from "classnames";
-import { apiStore } from "~/services/externalApis/apiStore";
 
-const iconNames = ["warning", "error", "info"] as const;
-export type IconName = (typeof iconNames)[number];
+const icons = {
+  warning: (className: string) => (
+    <ExclamationTriangleIcon
+      className={classNames(className, "text-yellow-500")}
+    />
+  ),
+  error: (className: string) => (
+    <ExclamationCircleIcon className={classNames(className, "text-rose-500")} />
+  ),
+  info: (className: string) => (
+    <InformationCircleIcon className={classNames(className, "text-blue-500")} />
+  ),
+  slack: (className: string) => (
+    <img src={`/integrations/slack.png`} className={className} alt="Slack" />
+  ),
+  airtable: (className: string) => (
+    <img
+      src={`/integrations/airtable.png`}
+      className={className}
+      alt="Airtable"
+    />
+  ),
+};
+
+export type IconNames = keyof typeof icons;
 
 export function NamedIcon({
   name,
   className,
-}: {
-  name: IconName | string;
-  className: string;
-}) {
-  if (iconNames.includes(name as IconName)) {
-    return <PredeterminedIcon name={name as IconName} className={className} />;
-  }
-
-  return <IntegrationIcon name={name as string} className={className} />;
-}
-
-function PredeterminedIcon({
-  name,
-  className,
-}: {
-  name: IconName;
-  className: string;
-}) {
-  switch (name) {
-    case "warning":
-      return (
-        <ExclamationTriangleIcon
-          className={classNames(className, "text-yellow-500")}
-        />
-      );
-    case "error":
-      return (
-        <ExclamationCircleIcon
-          className={classNames(className, "text-rose-500")}
-        />
-      );
-    case "info":
-      return (
-        <InformationCircleIcon
-          className={classNames(className, "text-blue-500")}
-        />
-      );
-  }
-}
-
-function IntegrationIcon({
-  name,
-  className,
+  fallback,
 }: {
   name: string;
   className: string;
+  fallback?: JSX.Element;
 }) {
-  const api = apiStore.getApi(name);
-  if (api) {
-    return (
-      <img
-        src={`/integrations/${name}.png`}
-        className={className}
-        alt={api.name}
-      />
-    );
+  if (Object.keys(icons).includes(name)) {
+    return icons[name as IconNames](className);
   }
 
-  return <StopIcon className={classNames(className, "text-white/20")} />;
+  if (fallback) {
+    return fallback;
+  }
+
+  //default fallback icon
+  return <StopIcon className={className} />;
 }
