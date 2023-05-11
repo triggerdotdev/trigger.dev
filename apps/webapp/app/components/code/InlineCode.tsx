7a4742a314fd948ec9b1e20c7bb6052c6838586d
Matt Aitken <matt@mattaitken.com>
2023-05-11 09:47:54 +0100

    Made inline code sky blue

--- apps/webapp/app/components/code/InlineCode.tsx
+++ apps/webapp/app/components/code/InlineCode.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 
 const inlineCode =
-  "px-1 py-0.5 text-sm rounded-md border border-slate-800 bg-slate-950 text-slate-400 font-mono text-sm";
+  "px-1 py-0.5 text-sm rounded-md border border-slate-800 bg-slate-950 text-sky-200 font-mono text-sm";
 
 export function InlineCode({ children }: { children: ReactNode }) {
   return <code className={inlineCode}>{children}</code>;
