789fab5b9697fffb973aff5d07bed69a1d157e13
Matt Aitken <matt@mattaitken.com>
2023-05-11 09:46:44 +0100

    Added extension to the Tailwind merge plugin

--- apps/webapp/app/utils/cn.ts
+++ apps/webapp/app/utils/cn.ts
@@ -1,5 +1,11 @@
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { twMerge, extendTailwindMerge } from "tailwind-merge";
+
+const customTwMerge = extendTailwindMerge({
+  theme: {
+    fontSize: ["xxs"],
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
