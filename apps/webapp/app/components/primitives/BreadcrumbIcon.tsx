722b4b8e2da293b8ab277a3df6f9434265e4abdf
Matt Aitken <matt@mattaitken.com>
2023-05-12 14:10:27 +0100

    BreadcrumbIcon

---
+++ apps/webapp/app/components/primitives/BreadcrumbIcon.tsx
@@ -0,0 +1,21 @@
+export function BreadcrumbIcon() {
+  return (
+    <svg
+      className="text-slate-650"
+      width="9"
+      height="20"
+      viewBox="0 0 9 26"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        x1="8.32382"
+        y1="0.6286"
+        x2="0.6286"
+        y2="24.6762"
+        stroke="currentColor"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
