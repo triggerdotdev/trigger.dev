f08c4e2354ebd7798e7660ba74f44f98b97c1d9a
Matt Aitken <matt@mattaitken.com>
2023-05-12 13:17:21 +0100

    Move path building to a file so we don’t have lots of them floating around

---
+++ apps/webapp/app/models/project.server.ts
@@ -0,0 +1,1 @@
+export type { Project } from ".prisma/client";
---
+++ apps/webapp/app/utils/pathBuilder.ts
@@ -0,0 +1,35 @@
+import type { Organization } from "~/models/organization.server";
+import type { Project } from "~/models/project.server";
+
+type OrgForPath = Pick<Organization, "slug">;
+type ProjectForPath = Pick<Project, "id">;
+
+export function organizationPath(organization: OrgForPath) {
+  return `/orgs/${organizationParam(organization)}`;
+}
+
+export function newOrganizationPath() {
+  return `/orgs/new`;
+}
+
+export function organizationIntegrationsPath(organization: OrgForPath) {
+  return `${organizationPath(organization)}/integrations`;
+}
+
+export function projectPath(organization: OrgForPath, project: ProjectForPath) {
+  return `/orgs/${organizationParam(organization)}/projects/${projectParam(
+    project
+  )}`;
+}
+
+export function newProjectPath(organization: OrgForPath) {
+  return `${organizationPath(organization)}/projects/new`;
+}
+
+export function organizationParam(organization: OrgForPath) {
+  return organization.slug;
+}
+
+export function projectParam(project: ProjectForPath) {
+  return project.id;
+}
