6c3cb7f5d189f14cb427e0f4eb15e15adbb24db2
Matt Aitken <matt@mattaitken.com>
2023-05-12 17:25:48 +0100

    Early org grid

---
+++ apps/webapp/app/routes/_app._index/OrganizationGrid.tsx
@@ -0,0 +1,49 @@
+import { BuildingOffice2Icon } from "@heroicons/react/24/solid";
+import { Link } from "@remix-run/react";
+import { LinkButton } from "~/components/primitives/Buttons";
+import { Header2 } from "~/components/primitives/Headers";
+import { Paragraph } from "~/components/primitives/Paragraph";
+import type { MatchedOrganization } from "~/hooks/useOrganizations";
+import { cn } from "~/utils/cn";
+import { projectPath } from "~/utils/pathBuilder";
+
+export function OrganizationGridItem({
+  organization,
+}: {
+  organization: MatchedOrganization;
+}) {
+  return (
+    <li key={organization.id}>
+      <div
+        className={cn(
+          "to-50% block rounded-md border border-slate-850 bg-gradient-to-b from-indigo-900/50 to-slate-950 p-2 "
+        )}
+      >
+        <div className="flex gap-4 border-b border-slate-850 px-2 pb-4 pt-2">
+          <BuildingOffice2Icon
+            className="h-10 w-10 flex-none text-fuchsia-600"
+            aria-hidden="true"
+          />
+          <div className="flex-1">
+            <Header2 className="">{organization.title}</Header2>
+            <Paragraph variant="extra-small">1 team member</Paragraph>
+          </div>
+        </div>
+        <div className="pt-4">
+          <Paragraph variant="extra-small/bright/caps">Projects</Paragraph>
+          <div>
+            {organization.projects.map((project) => (
+              <LinkButton
+                variant="menuItem"
+                to={projectPath(organization, project)}
+                key={project.id}
+              >
+                {project.name}
+              </LinkButton>
+            ))}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
--- apps/webapp/app/routes/_app._index/route.tsx
+++ apps/webapp/app/routes/_app._index/route.tsx
@@ -1,12 +1,5 @@
-import {
-  BuildingOffice2Icon,
-  PlusIcon,
-  UserIcon,
-} from "@heroicons/react/24/outline";
-import { Link } from "@remix-run/react";
 import { MainContainer } from "~/components/layout/AppLayout";
-import { Button, LinkButton } from "~/components/primitives/Buttons";
-import { Header3 } from "~/components/primitives/Headers";
+import { LinkButton } from "~/components/primitives/Buttons";
 import { PageBody } from "~/components/primitives/PageBody";
 import {
   PageButtons,
@@ -15,12 +8,10 @@ import {
   PageTitle,
   PageTitleRow,
 } from "~/components/primitives/PageHeader";
-import { Paragraph } from "~/components/primitives/Paragraph";
-import type { MatchedOrganization } from "~/hooks/useOrganizations";
 import { useOrganizations } from "~/hooks/useOrganizations";
 import { useOptionalUser } from "~/hooks/useUser";
-import { cn } from "~/utils/cn";
-import { newOrganizationPath, organizationPath } from "~/utils/pathBuilder";
+import { newOrganizationPath } from "~/utils/pathBuilder";
+import { OrganizationGridItem } from "./OrganizationGrid";
 
 export default function AppLayout() {
   const organizations = useOrganizations();
@@ -46,78 +37,23 @@ export default function AppLayout() {
         </PageDescription>
       </PageHeader>
       <PageBody>
-        <div className="z-10 mb-12 flex items-center justify-center">
-          <ul className="grid max-w-7xl grid-cols-2 gap-2 lg:grid-cols-3">
-            {organizations ? (
-              <OrganizationGrid organizations={organizations} />
-            ) : (
-              <li>
-                <Paragraph>No organizations</Paragraph>
-              </li>
-            )}
+        <ul className="grid grid-flow-row auto-rows-max">
+          {organizations ? (
+            organizations.map((organization) => (
+              <OrganizationGridItem
+                key={organization.id}
+                organization={organization}
+              />
+            ))
+          ) : (
             <li>
-              <Link
-                to={newOrganizationPath()}
-                className={cn(
-                  "h-full border border-slate-700 hover:border-transparent hover:bg-[rgb(38,51,71)] hover:shadow-md",
-                  boxClasses
-                )}
-              >
-                <PlusIcon className="h-10 w-10 text-green-500" />
-                <Header3 className="mb-10">New Organization</Header3>
-              </Link>
+              <LinkButton to={newOrganizationPath()} variant="primary/small">
+                Create your first organization
+              </LinkButton>
             </li>
-          </ul>
-        </div>
+          )}
+        </ul>
       </PageBody>
     </MainContainer>
   );
 }
-
-function OrganizationGrid({
-  organizations,
-}: {
-  organizations: MatchedOrganization[];
-}) {
-  return (
-    <>
-      {organizations.map((organization) => (
-        <OrganizationGridItem
-          key={organization.id}
-          organization={organization}
-        />
-      ))}
-    </>
-  );
-}
-
-function OrganizationGridItem({
-  organization,
-}: {
-  organization: MatchedOrganization;
-}) {
-  return (
-    <li key={organization.id} className="h-full w-full">
-      <Link
-        to={organizationPath(organization)}
-        className={cn(
-          "border border-slate-700 bg-slate-800 hover:bg-[rgb(38,51,71)]",
-          boxClasses
-        )}
-      >
-        {organization.title === "Personal Workspace" ? (
-          <UserIcon className="h-10 w-10 text-slate-300" aria-hidden="true" />
-        ) : (
-          <BuildingOffice2Icon
-            className="h-10 w-10 text-blue-500"
-            aria-hidden="true"
-          />
-        )}
-        <Header3 className="mb-16 text-slate-300">{organization.title}</Header3>
-      </Link>
-    </li>
-  );
-}
-
-const boxClasses =
-  "flex flex-col gap-4 w-80 text-center shadow-md items-center justify-center rounded-lg px-2 pb-2 pt-16 min-h-full transition";
