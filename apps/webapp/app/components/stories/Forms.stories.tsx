00c9da8de99d5e7d56dff8e635665c88e761c786
Matt Aitken <matt@mattaitken.com>
2023-05-15 14:20:18 +0100

    Form story updated

--- apps/webapp/app/components/stories/Forms.stories.tsx
+++ apps/webapp/app/components/stories/Forms.stories.tsx
@@ -10,6 +10,7 @@ import { FormButtons } from "../primitives/FormButtons";
 import { Button } from "../primitives/Buttons";
 import { MainCenteredContainer } from "../layout/AppLayout";
 import { FormTitle } from "../primitives/FormTitle";
+import { Form } from "@remix-run/react";
 
 const meta: Meta<typeof Forms> = {
   title: "Primitives/Forms",
@@ -43,37 +44,39 @@ function Forms() {
         <FormTitle LeadingIcon="organization">
           Create a new Organization
         </FormTitle>
-        <Fieldset>
-          <InputGroup>
-            <Label>Organization name</Label>
-            <Input
-              placeholder="Your org name"
-              required={true}
-              defaultValue="Acme Inc."
+        <Form>
+          <Fieldset>
+            <InputGroup>
+              <Label>Organization name</Label>
+              <Input
+                placeholder="Your org name"
+                required={true}
+                defaultValue="Acme Inc."
+              />
+              <Hint>E.g. your company name or your workspace name.</Hint>
+            </InputGroup>
+
+            <InputGroup>
+              <Label>Project name</Label>
+              <Input placeholder="Your Project name" required={true} />
+              <Hint>Your Jobs will live inside this Project.</Hint>
+              <FormError>You must enter a project name</FormError>
+            </InputGroup>
+
+            <FormButtons
+              confirmButton={
+                <Button
+                  type="submit"
+                  variant={"primary/small"}
+                  TrailingIcon="arrow-right"
+                >
+                  Create
+                </Button>
+              }
+              cancelButton={<Button variant={"tertiary/small"}>Cancel</Button>}
             />
-            <Hint>E.g. your company name or your workspace name.</Hint>
-          </InputGroup>
-
-          <InputGroup>
-            <Label>Project name</Label>
-            <Input placeholder="Your Project name" required={true} />
-            <Hint>Your Jobs will live inside this Project.</Hint>
-            <FormError>You must enter a project name</FormError>
-          </InputGroup>
-
-          <FormButtons
-            confirmButton={
-              <Button
-                type="submit"
-                variant={"primary/small"}
-                TrailingIcon="arrow-right"
-              >
-                Create
-              </Button>
-            }
-            cancelButton={<Button variant={"tertiary/small"}>Cancel</Button>}
-          />
-        </Fieldset>
+          </Fieldset>
+        </Form>
       </div>
     </MainCenteredContainer>
   );
