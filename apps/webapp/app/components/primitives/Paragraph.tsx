4a15d0e8221f8a0788d17639ad1d9163abaf5cbf
James Ritchie <james@jamesritchie.co.uk>
2023-05-18 14:27:04 +0100

    Removed leading overrides from paragraph component

--- apps/webapp/app/components/primitives/Paragraph.tsx
+++ apps/webapp/app/components/primitives/Paragraph.tsx
@@ -1,28 +1,24 @@
 import { cn } from "~/utils/cn";
 
 const paragraphVariants = {
-  base: "font-sans text-base font-normal leading-normal text-dimmed",
-  "base/bright": "font-sans text-base font-normal leading-normal text-bright",
-  small: "font-sans text-sm font-normal leading-normal text-dimmed",
-  "small/bright": "font-sans text-sm font-normal leading-normal text-bright",
-  "extra-small": "font-sans text-xs font-normal leading-normal text-dimmed",
-  "extra-small/bright":
-    "font-sans text-xs font-normal leading-normal text-bright",
-  "extra-small/mono":
-    "font-mono text-xs font-normal leading-normal text-dimmed",
-  "extra-small/bright/mono": "font-mono text-xs leading-normal text-bright",
+  base: "font-sans text-base font-normal text-dimmed",
+  "base/bright": "font-sans text-base font-normal text-bright",
+  small: "font-sans text-sm font-normal text-dimmed",
+  "small/bright": "font-sans text-sm font-normal text-bright",
+  "extra-small": "font-sans text-xs font-normal text-dimmed",
+  "extra-small/bright": "font-sans text-xs font-normal text-bright",
+  "extra-small/mono": "font-mono text-xs font-normal text-dimmed",
+  "extra-small/bright/mono": "font-mono text-xs text-bright",
   "extra-small/caps":
-    "font-sans text-xs uppercase tracking-wider font-normal leading-normal text-dimmed",
+    "font-sans text-xs uppercase tracking-wider font-normal text-dimmed",
   "extra-small/bright/caps":
-    "font-sans text-xs uppercase tracking-wider font-normal leading-normal text-bright",
-  "extra-extra-small":
-    "font-sans text-xxs font-normal leading-normal text-dimmed",
-  "extra-extra-small/bright":
-    "font-sans text-xxs font-normal leading-normal text-bright",
+    "font-sans text-xs uppercase tracking-wider font-normal text-bright",
+  "extra-extra-small": "font-sans text-xxs font-normal text-dimmed",
+  "extra-extra-small/bright": "font-sans text-xxs font-normal text-bright",
   "extra-extra-small/caps":
-    "font-sans text-xxs uppercase tracking-wider font-normal leading-normal text-dimmed",
+    "font-sans text-xxs uppercase tracking-wider font-normal text-dimmed",
   "extra-extra-small/bright/caps":
-    "font-sans text-xxs uppercase tracking-wider font-normal leading-normal text-bright",
+    "font-sans text-xxs uppercase tracking-wider font-normal text-bright",
 };
 
 type ParagraphProps = {
