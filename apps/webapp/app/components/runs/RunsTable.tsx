bfdd3bc361fbbac123cb6de2014eaaa6b97a2dcc
Matt Aitken <matt@mattaitken.com>
2023-05-26 16:15:01 +0100

    Oops I’d flipped isTest while… testing. Irony

--- apps/webapp/app/components/runs/RunsTable.tsx
+++ apps/webapp/app/components/runs/RunsTable.tsx
@@ -79,7 +79,7 @@ export function RunsTable({
                   })}
                 </Cell>
                 <Cell to={path}>
-                  {!run.isTest ? (
+                  {run.isTest ? (
                     <CheckIcon className="h-4 w-4 text-slate-400" />
                   ) : (
                     <StopIcon className="h-4 w-4 text-slate-850" />
