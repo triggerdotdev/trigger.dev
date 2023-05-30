b35d5e35fec4facec20dd6f1793c3d5af9297426
James Ritchie <james@jamesritchie.co.uk>
2023-05-30 16:05:11 +0100

    Improved Sheet background visibility

--- apps/webapp/app/components/primitives/Sheet.tsx
+++ apps/webapp/app/components/primitives/Sheet.tsx
@@ -47,7 +47,7 @@ const SheetOverlay = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
-      "fixed inset-0 z-50 bg-background/50 backdrop-blur-sm transition-all duration-100 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in",
+      "fixed inset-0 z-50 bg-slate-900/50 transition-all duration-100 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in",
       className
     )}
     {...props}
