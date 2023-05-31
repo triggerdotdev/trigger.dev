4abdc5193412eb8b0375a608c6b08a67b35432f4
James Ritchie <james@jamesritchie.co.uk>
2023-05-31 15:37:17 +0100

    Improved the badge

--- apps/webapp/app/components/primitives/Badge.tsx
+++ apps/webapp/app/components/primitives/Badge.tsx
@@ -3,15 +3,17 @@ import { cn } from "~/utils/cn";
 
 const variants = {
   default:
-    "inline-flex items-center border rounded-full px-2.5 py-1 text-xxs transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-slate-700 text-slate-200 uppercase",
+    "grid place-items-center rounded-full px-2.5 h-6 tracking-wider text-xxs bg-slate-700 text-bright uppercase",
 };
 
 type BadgeProps = React.HTMLAttributes<HTMLDivElement> & {
   variant?: keyof typeof variants;
 };
 
-function Badge({ className, variant = "default", ...props }: BadgeProps) {
+export function Badge({
+  className,
+  variant = "default",
+  ...props
+}: BadgeProps) {
   return <div className={cn(variants[variant], className)} {...props} />;
 }
-
-export { Badge };
