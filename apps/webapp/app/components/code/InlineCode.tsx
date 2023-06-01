6947292adff45e195c32e3310a89814a80af7ba7
James Ritchie <james@jamesritchie.co.uk>
2023-06-01 10:55:18 +0100

    Inline code component now has size variants to match the paragraph it’s inlined with

--- apps/webapp/app/components/code/InlineCode.tsx
+++ apps/webapp/app/components/code/InlineCode.tsx
@@ -1,8 +1,23 @@
-import type { ReactNode } from "react";
+import { cn } from "~/utils/cn";
 
 const inlineCode =
-  "px-1 py-0.5 text-sm rounded-md border border-slate-800 bg-slate-950 text-sky-200 font-mono text-sm";
+  "px-1 py-0.5 rounded border border-slate-800 bg-slate-950 text-sky-200 font-mono";
 
-export function InlineCode({ children }: { children: ReactNode }) {
-  return <code className={inlineCode}>{children}</code>;
+const variants = {
+  "extra-extra-small": "text-xxs",
+  "extra-small": "text-xs",
+  small: "text-sm",
+  base: "text-base",
+};
+
+type InlineCodeProps = {
+  children: React.ReactNode;
+};
+
+type VariantProps = InlineCodeProps & {
+  variant?: keyof typeof variants;
+};
+
+export function InlineCode({ variant = "small", children }: VariantProps) {
+  return <code className={cn(inlineCode, variants[variant])}>{children}</code>;
 }
