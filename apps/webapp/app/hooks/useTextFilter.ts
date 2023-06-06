0da4b43d257fb27f23748a000eb3947f401d6c0b
Matt Aitken <matt@mattaitken.com>
2023-06-06 16:48:25 +0100

    Created reusable useTextFilter hook

---
+++ apps/webapp/app/hooks/useTextFilter.ts
@@ -0,0 +1,31 @@
+import { fi } from "date-fns/locale";
+import { useMemo, useState } from "react";
+
+type TextFilterProps<T> = {
+  defaultValue?: string;
+  items: T[];
+  filter: (item: T, filterText: string) => boolean;
+};
+
+export function useTextFilter<T>({
+  defaultValue = "",
+  items,
+  filter,
+}: TextFilterProps<T>) {
+  const [filterText, setFilterText] = useState(defaultValue);
+
+  const filteredItems = useMemo<T[]>(() => {
+    if (filterText === "") {
+      return items;
+    }
+    return items.filter((item) => {
+      return filter(item, filterText);
+    });
+  }, [items, filterText]);
+
+  return {
+    filterText,
+    setFilterText,
+    filteredItems,
+  };
+}
