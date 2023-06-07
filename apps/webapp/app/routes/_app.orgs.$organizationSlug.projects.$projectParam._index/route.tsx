a47cdb5a156ed8f117c989ef060c297e5b982393
Matt Aitken <matt@mattaitken.com>
2023-06-07 15:01:07 +0100

    Improved the filtered empty state for the Jobs table

--- apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam._index/route.tsx
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam._index/route.tsx
@@ -1,5 +1,6 @@
 import { JobSkeleton } from "~/components/jobs/JobSkeleton";
 import { PageBody, PageContainer } from "~/components/layout/AppLayout";
+import { Callout } from "~/components/primitives/Callout";
 import { DateTime } from "~/components/primitives/DateTime";
 import { Header2 } from "~/components/primitives/Headers";
 import { Help, HelpContent, HelpTrigger } from "~/components/primitives/Help";
@@ -17,6 +18,7 @@ import {
 import { Paragraph } from "~/components/primitives/Paragraph";
 import {
   Table,
+  TableBlankRow,
   TableBody,
   TableCell,
   TableCellChevron,
@@ -105,129 +107,131 @@ export default function Page() {
                 <HelpTrigger title="How do I create a Job?" />
               </div>
               {project.jobs.length > 0 ? (
-                filteredItems.length > 0 ? (
-                  <div className="flex flex-col gap-10">
-                    <Table>
-                      <TableHeader>
-                        <TableRow>
-                          <TableHeaderCell>Job</TableHeaderCell>
-                          <TableHeaderCell>ID</TableHeaderCell>
-                          <TableHeaderCell>Integrations</TableHeaderCell>
-                          <TableHeaderCell>Properties</TableHeaderCell>
-                          <TableHeaderCell>Last run</TableHeaderCell>
-                          <TableHeaderCell hiddenLabel>
-                            Go to page
-                          </TableHeaderCell>
-                        </TableRow>
-                      </TableHeader>
-                      <TableBody>
-                        {filteredItems.map((job) => {
-                          const path = jobPath(organization, project, job);
-                          return (
-                            <TableRow key={job.id}>
-                              <TableCell to={path}>
-                                <span className="flex items-center gap-2">
-                                  <NamedIcon
-                                    name={job.event.icon}
-                                    className="h-8 w-8"
-                                  />
-                                  <LabelValueStack
-                                    label={job.title}
-                                    value={job.event.title}
-                                    variant="primary"
-                                  />
-                                </span>
-                              </TableCell>
-                              <TableCell to={path}>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHeaderCell>Job</TableHeaderCell>
+                      <TableHeaderCell>ID</TableHeaderCell>
+                      <TableHeaderCell>Integrations</TableHeaderCell>
+                      <TableHeaderCell>Properties</TableHeaderCell>
+                      <TableHeaderCell>Last run</TableHeaderCell>
+                      <TableHeaderCell hiddenLabel>Go to page</TableHeaderCell>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {filteredItems.length > 0 ? (
+                      filteredItems.map((job) => {
+                        const path = jobPath(organization, project, job);
+                        return (
+                          <TableRow key={job.id}>
+                            <TableCell to={path}>
+                              <span className="flex items-center gap-2">
+                                <NamedIcon
+                                  name={job.event.icon}
+                                  className="h-8 w-8"
+                                />
                                 <LabelValueStack
-                                  label={job.slug}
-                                  value={`v${job.version}`}
+                                  label={job.title}
+                                  value={job.event.title}
                                   variant="primary"
                                 />
-                              </TableCell>
-                              <TableCell to={path}>
-                                {job.integrations.map((integration) => (
-                                  <SimpleTooltip
-                                    key={integration.key}
-                                    button={
-                                      <NamedIcon
-                                        name={integration.icon}
-                                        className="h-6 w-6"
-                                      />
-                                    }
-                                    content={`${integration.title}: ${integration.key}`}
-                                  />
-                                ))}
-                              </TableCell>
-                              <TableCell to={path}>
-                                {job.elements && (
-                                  <SimpleTooltip
-                                    button={
-                                      <div className="flex max-w-[200px] items-start justify-start gap-5 truncate">
-                                        {job.elements.map((element, index) => (
-                                          <LabelValueStack
-                                            key={index}
-                                            label={element.label}
-                                            value={element.text}
-                                            className=" last:truncate"
-                                          />
-                                        ))}
-                                      </div>
-                                    }
-                                    content={
-                                      <div className="flex flex-col gap-2">
-                                        {job.elements.map((element, index) => (
-                                          <LabelValueStack
-                                            key={index}
-                                            label={element.label}
-                                            value={element.text}
-                                          />
-                                        ))}
-                                      </div>
-                                    }
-                                  />
-                                )}
-                              </TableCell>
-                              <TableCell to={path}>
-                                {job.lastRun ? (
-                                  <LabelValueStack
-                                    label={
-                                      <span
-                                        className={classForJobStatus(
-                                          job.lastRun.status
-                                        )}
-                                      >
-                                        {runStatusTitle(job.lastRun.status)}
-                                      </span>
-                                    }
-                                    value={
-                                      <DateTime
-                                        date={job.lastRun.createdAt}
-                                        className={classForJobStatus(
-                                          job.lastRun.status
-                                        )}
-                                      />
-                                    }
-                                  />
-                                ) : (
-                                  <LabelValueStack
-                                    label={"Never run"}
-                                    value={"–"}
-                                  />
-                                )}
-                              </TableCell>
-                              <TableCellChevron to={path} />
-                            </TableRow>
-                          );
-                        })}
-                      </TableBody>
-                    </Table>
-                  </div>
-                ) : (
-                  <Paragraph variant="small" className="p-4">
-                    No Jobs match {filterText}. Try a different search query.
-                  </Paragraph>
-                )
+                              </span>
+                            </TableCell>
+                            <TableCell to={path}>
+                              <LabelValueStack
+                                label={job.slug}
+                                value={`v${job.version}`}
+                                variant="primary"
+                              />
+                            </TableCell>
+                            <TableCell to={path}>
+                              {job.integrations.map((integration) => (
+                                <SimpleTooltip
+                                  key={integration.key}
+                                  button={
+                                    <NamedIcon
+                                      name={integration.icon}
+                                      className="h-6 w-6"
+                                    />
+                                  }
+                                  content={`${integration.title}: ${integration.key}`}
+                                />
+                              ))}
+                            </TableCell>
+                            <TableCell to={path}>
+                              {job.elements && (
+                                <SimpleTooltip
+                                  button={
+                                    <div className="flex max-w-[200px] items-start justify-start gap-5 truncate">
+                                      {job.elements.map((element, index) => (
+                                        <LabelValueStack
+                                          key={index}
+                                          label={element.label}
+                                          value={element.text}
+                                          className=" last:truncate"
+                                        />
+                                      ))}
+                                    </div>
+                                  }
+                                  content={
+                                    <div className="flex flex-col gap-2">
+                                      {job.elements.map((element, index) => (
+                                        <LabelValueStack
+                                          key={index}
+                                          label={element.label}
+                                          value={element.text}
+                                        />
+                                      ))}
+                                    </div>
+                                  }
+                                />
+                              )}
+                            </TableCell>
+                            <TableCell to={path}>
+                              {job.lastRun ? (
+                                <LabelValueStack
+                                  label={
+                                    <span
+                                      className={classForJobStatus(
+                                        job.lastRun.status
+                                      )}
+                                    >
+                                      {runStatusTitle(job.lastRun.status)}
+                                    </span>
+                                  }
+                                  value={
+                                    <DateTime
+                                      date={job.lastRun.createdAt}
+                                      className={classForJobStatus(
+                                        job.lastRun.status
+                                      )}
+                                    />
+                                  }
+                                />
+                              ) : (
+                                <LabelValueStack
+                                  label={"Never run"}
+                                  value={"–"}
+                                />
+                              )}
+                            </TableCell>
+                            <TableCellChevron to={path} />
+                          </TableRow>
+                        );
+                      })
+                    ) : (
+                      <TableBlankRow colSpan={6}>
+                        <Paragraph
+                          variant="small"
+                          className="flex items-center justify-center"
+                        >
+                          No Jobs match {filterText}. Try a different search
+                          query.
+                        </Paragraph>
+                      </TableBlankRow>
+                    )}
+                  </TableBody>
+                </Table>
               ) : (
                 <>
                   <JobSkeleton />
