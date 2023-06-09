618bc927e9579ac21499184529f4e66c3204fcb2
Matt Aitken <matt@mattaitken.com>
2023-06-09 10:10:35 +0100

    Export ErrorWIthStack type

--- packages/internal/src/schemas/errors.ts
+++ packages/internal/src/schemas/errors.ts
@@ -7,3 +7,5 @@ export const ErrorWithMessage = z.object({
 export const ErrorWithStackSchema = ErrorWithMessage.extend({
   stack: z.string().optional(),
 });
+
+export type ErrorWithStack = z.infer<typeof ErrorWithStackSchema>;
