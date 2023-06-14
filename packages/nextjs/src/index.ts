98fcc5d2d4baef3b43b2da4591a9599e99f44dbb
Eric Allam <eallam@icloud.com>
2023-06-14 08:50:39 +0100

    Make sure nextjs app routes have the correct types

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -60,7 +60,7 @@ export function createAppRoute(
     dynamic: "force-dynamic",
     runtime: "nodejs",
     preferredRegion: "auto",
-  };
+  } as const;
 }
 
 async function convertToStandardRequest(
