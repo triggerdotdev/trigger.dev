4903cfc68923fbb92b2e8e58efc62c06a2872a9a
Matt Aitken <matt@mattaitken.com>
2023-06-14 12:45:56 +0100

    Don’t let Callout icons shrink when the space is small

--- apps/webapp/app/components/primitives/Callout.tsx
+++ apps/webapp/app/components/primitives/Callout.tsx
@@ -10,22 +10,26 @@ import { Paragraph } from "./Paragraph";
 const variantClasses = {
   info: {
     className: "border-blue-400/20 bg-blue-400/30",
-    icon: <InformationCircleIcon className="h-5 w-5 text-blue-400" />,
+    icon: <InformationCircleIcon className="h-5 w-5 shrink-0 text-blue-400" />,
     text: "text-blue-200",
   },
   warning: {
     className: "border-yellow-400/20 bg-yellow-400/30",
-    icon: <ExclamationCircleIcon className="h-5 w-5 text-yellow-400" />,
+    icon: (
+      <ExclamationCircleIcon className="h-5 w-5 shrink-0 text-yellow-400" />
+    ),
     text: "text-yellow-200",
   },
   error: {
     className: "border-[hsl(347,77%,50%)]/30 bg-[hsl(347,77%,50%)]/20",
-    icon: <ExclamationTriangleIcon className="h-5 w-5 text-rose-400" />,
+    icon: (
+      <ExclamationTriangleIcon className="h-5 w-5 shrink-0 text-rose-400" />
+    ),
     text: "text-rose-200",
   },
   idea: {
     className: "border-green-400/20 bg-green-400/30",
-    icon: <LightBulbIcon className="h-5 w-5 text-green-400" />,
+    icon: <LightBulbIcon className="h-5 w-5 shrink-0 text-green-400" />,
     text: "text-green-200",
   },
 } as const;
