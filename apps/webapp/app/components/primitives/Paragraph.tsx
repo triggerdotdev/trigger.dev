2b5b8d7931c02856a8cb294b6d0f1c372832c79f
Matt Aitken <matt@mattaitken.com>
2023-06-15 14:05:13 +0100

    Added Paragraph spacing

--- apps/webapp/app/components/primitives/Paragraph.tsx
+++ apps/webapp/app/components/primitives/Paragraph.tsx
@@ -1,24 +1,62 @@
 import { cn } from "~/utils/cn";
 
 const paragraphVariants = {
-  base: "font-sans text-base font-normal text-dimmed",
-  "base/bright": "font-sans text-base font-normal text-bright",
-  small: "font-sans text-sm font-normal text-dimmed",
-  "small/bright": "font-sans text-sm font-normal text-bright",
-  "extra-small": "font-sans text-xs font-normal text-dimmed",
-  "extra-small/bright": "font-sans text-xs font-normal text-bright",
-  "extra-small/mono": "font-mono text-xs font-normal text-dimmed",
-  "extra-small/bright/mono": "font-mono text-xs text-bright",
-  "extra-small/caps":
-    "font-sans text-xs uppercase tracking-wider font-normal text-dimmed",
-  "extra-small/bright/caps":
-    "font-sans text-xs uppercase tracking-wider font-normal text-bright",
-  "extra-extra-small": "font-sans text-xxs font-normal text-dimmed",
-  "extra-extra-small/bright": "font-sans text-xxs font-normal text-bright",
-  "extra-extra-small/caps":
-    "font-sans text-xxs uppercase tracking-wider font-normal text-dimmed",
-  "extra-extra-small/bright/caps":
-    "font-sans text-xxs uppercase tracking-wider font-normal text-bright",
+  base: {
+    text: "font-sans text-base font-normal text-dimmed",
+    spacing: "mb-3",
+  },
+  "base/bright": {
+    text: "font-sans text-base font-normal text-bright",
+    spacing: "mb-3",
+  },
+  small: {
+    text: "font-sans text-sm font-normal text-dimmed",
+    spacing: "mb-2",
+  },
+  "small/bright": {
+    text: "font-sans text-sm font-normal text-bright",
+    spacing: "mb-2",
+  },
+  "extra-small": {
+    text: "font-sans text-xs font-normal text-dimmed",
+    spacing: "mb-1.5",
+  },
+  "extra-small/bright": {
+    text: "font-sans text-xs font-normal text-bright",
+    spacing: "mb-1.5",
+  },
+  "extra-small/mono": {
+    text: "font-mono text-xs font-normal text-dimmed",
+    spacing: "mb-1.5",
+  },
+  "extra-small/bright/mono": {
+    text: "font-mono text-xs text-bright",
+    spacing: "mb-1.5",
+  },
+  "extra-small/caps": {
+    text: "font-sans text-xs uppercase tracking-wider font-normal text-dimmed",
+    spacing: "mb-1.5",
+  },
+  "extra-small/bright/caps": {
+    text: "font-sans text-xs uppercase tracking-wider font-normal text-bright",
+    spacing: "mb-1.5",
+  },
+  "extra-extra-small": {
+    text: "font-sans text-xxs font-normal text-dimmed",
+    spacing: "mb-1",
+  },
+  "extra-extra-small/bright": {
+    text: "font-sans text-xxs font-normal text-bright",
+    spacing: "mb-1",
+  },
+  "extra-extra-small/caps": {
+    text: "font-sans text-xxs uppercase tracking-wider font-normal text-dimmed",
+    spacing: "mb-1",
+  },
+  "extra-extra-small/bright/caps": {
+    text: "font-sans text-xxs uppercase tracking-wider font-normal text-bright",
+    spacing: "mb-1",
+  },
 };
 
 export type ParagraphVariant = keyof typeof paragraphVariants;
@@ -26,17 +64,26 @@ export type ParagraphVariant = keyof typeof paragraphVariants;
 type ParagraphProps = {
   variant?: ParagraphVariant;
   className?: string;
+  spacing?: boolean;
   children: React.ReactNode;
 } & React.HTMLAttributes<HTMLParagraphElement>;
 
 export function Paragraph({
   variant = "base",
   className,
+  spacing = false,
   children,
   ...props
 }: ParagraphProps) {
   return (
-    <p className={cn(paragraphVariants[variant], className)} {...props}>
+    <p
+      className={cn(
+        paragraphVariants[variant].text,
+        spacing === true && paragraphVariants[variant].spacing,
+        className
+      )}
+      {...props}
+    >
       {children}
     </p>
   );
