014a97fcdca21a0ce9efd3726c3af6ca11ea1373
James Ritchie <james@jamesritchie.co.uk>
2023-06-16 15:03:17 +0100

    improved the env labels

--- apps/webapp/app/components/environments/EnvironmentLabel.tsx
+++ apps/webapp/app/components/environments/EnvironmentLabel.tsx
@@ -13,7 +13,7 @@ export function EnvironmentLabel({
   return (
     <span
       className={cn(
-        "flex h-4 items-center justify-center rounded-sm px-1 text-xxs font-medium uppercase text-midnight-900",
+        "flex h-4 items-center justify-center rounded-[2px] px-1 text-xxs font-medium uppercase tracking-wider text-midnight-900",
         environmentColorClassName(environment),
         className
       )}
