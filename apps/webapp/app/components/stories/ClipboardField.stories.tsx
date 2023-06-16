27c1d2516bd470de69d480c5bd04973bb1c91845
James Ritchie <james@jamesritchie.co.uk>
2023-06-16 15:03:33 +0100

    More variants to the clipboard field

--- apps/webapp/app/components/stories/ClipboardField.stories.tsx
+++ apps/webapp/app/components/stories/ClipboardField.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { withDesign } from "storybook-addon-designs";
 import { ClipboardField } from "../ClipboardField";
+import { EnvironmentLabel } from "../environments/EnvironmentLabel";
 
 const meta: Meta = {
   title: "Primitives/ClipboardField",
@@ -22,8 +23,37 @@ function ClipboardFieldExample() {
         <ClipboardField value="copy paste me" variant="primary/small" />
         <ClipboardField value="copy paste me" variant="secondary/small" />
         <ClipboardField value="copy paste me" variant="tertiary/small" />
+        <ClipboardField
+          value="with leadingIcon"
+          variant="tertiary/small"
+          icon={
+            <EnvironmentLabel
+              environment={{ type: "PRODUCTION", slug: "PROD" }}
+            />
+          }
+        />
+        <ClipboardField
+          value="with leadingIcon"
+          variant="tertiary/small"
+          icon="search"
+        />
         <ClipboardField value="copy paste me" variant="primary/medium" />
         <ClipboardField value="copy paste me" variant="secondary/medium" />
+        <ClipboardField value="copy paste me" variant="tertiary/medium" />
+        <ClipboardField
+          value="with leadingIcon"
+          variant="tertiary/medium"
+          icon={
+            <EnvironmentLabel
+              environment={{ type: "PRODUCTION", slug: "PROD" }}
+            />
+          }
+        />
+        <ClipboardField
+          value="with leadingIcon"
+          variant="tertiary/medium"
+          icon="search"
+        />
       </div>
       <div className="flex flex-col items-start gap-y-8 p-8">
         <ClipboardField
@@ -38,12 +68,54 @@ function ClipboardFieldExample() {
         />
         <ClipboardField
           value="copy paste me"
+          variant="tertiary/small"
+          secure={true}
+        />
+        <ClipboardField
+          value="with leadingIcon"
+          variant="tertiary/small"
+          icon={
+            <EnvironmentLabel
+              environment={{ type: "PRODUCTION", slug: "PROD" }}
+            />
+          }
+          secure={true}
+        />
+        <ClipboardField
+          value="with leadingIcon"
+          variant="tertiary/small"
+          icon="search"
+          secure={true}
+        />
+        <ClipboardField
+          value="copy paste me"
           variant="primary/medium"
           secure={true}
         />
         <ClipboardField
           value="copy paste me"
           variant="secondary/medium"
+          secure={true}
+        />
+        <ClipboardField
+          value="copy paste me"
+          variant="tertiary/medium"
+          secure={true}
+        />
+        <ClipboardField
+          value="with leadingIcon"
+          variant="tertiary/medium"
+          icon={
+            <EnvironmentLabel
+              environment={{ type: "PRODUCTION", slug: "PROD" }}
+            />
+          }
+          secure={true}
+        />
+        <ClipboardField
+          value="with leadingIcon"
+          variant="tertiary/medium"
+          icon="search"
           secure={true}
         />
       </div>
