eb85cc5c14812b1af06d33ecff3de0dbd4176a35
James Ritchie <james@jamesritchie.co.uk>
2023-06-16 13:50:31 +0100

    Adding new variants to the clipboard component

--- apps/webapp/app/components/ClipboardField.tsx
+++ apps/webapp/app/components/ClipboardField.tsx
@@ -1,16 +1,39 @@
 import { cn } from "~/utils/cn";
 import { Button } from "./primitives/Buttons";
 import { useCallback, useState } from "react";
+import { CheckIcon } from "@heroicons/react/20/solid";
 
 const variations = {
-  primary: {
+  "primary/small": {
     container:
       "flex text-dimmed font-mono rounded border border-slate-800 bg-slate-850 text-xs ring-offset-background transition hover:border-slate-750 hover:bg-slate-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 focus:border-ring focus:outline-none focus:ring-2 focus:ring-ring",
     input: "bg-slate-850 border-0 text-xs px-2 w-auto rounded-l h-6 leading-6",
     buttonVariant: "primary/small" as const,
     button: "rounded-l-none",
   },
-  secondary: {
+  "secondary/small": {
+    container:
+      "flex text-dimmed font-mono rounded border border-slate-750 bg-slate-850 text-xs ring-offset-background transition hover:border-slate-750 hover:bg-slate-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 focus:border-ring focus:outline-none focus:ring-2 focus:ring-ring",
+    input: "bg-slate-850 border-0 text-xs px-2 w-auto rounded-l h-6 leading-6",
+    buttonVariant: "secondary/small" as const,
+    button: "rounded-l-none border-l border-slate-750",
+  },
+  "tertiary/small": {
+    container:
+      "flex text-dimmed font-mono rounded border border-slate-850 bg-transparent text-xs ring-offset-background transition hover:border-slate-800 hover:bg-slate-950 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 focus:border-ring focus:outline-none focus:ring-2 focus:ring-ring",
+    input:
+      "bg-transparent border-0 text-xs px-2 w-auto rounded-l h-6 leading-6",
+    buttonVariant: "tertiary/small" as const,
+    button: "rounded-l-none border-l border-slate-850",
+  },
+  "primary/medium": {
+    container:
+      "flex text-dimmed font-mono rounded border border-slate-800 bg-slate-850 text-xs ring-offset-background transition hover:border-slate-750 hover:bg-slate-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 focus:border-ring focus:outline-none focus:ring-2 focus:ring-ring",
+    input: "bg-slate-850 border-0 text-xs px-2 w-auto rounded-l h-6 leading-6",
+    buttonVariant: "primary/small" as const,
+    button: "rounded-l-none",
+  },
+  "secondary/medium": {
     container:
       "flex text-dimmed font-mono rounded border border-slate-750 bg-slate-850 text-xs ring-offset-background transition hover:border-slate-750 hover:bg-slate-800 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 focus:border-ring focus:outline-none focus:ring-2 focus:ring-ring",
     input: "bg-slate-850 border-0 text-xs px-2 w-auto rounded-l h-6 leading-6",
@@ -70,8 +93,12 @@ export function ClipboardField({
           }
         }}
       />
-      <Button variant={buttonVariant} onClick={copy} className={button}>
-        {copied ? "Copied" : "Copy"}
+      <Button
+        variant={buttonVariant}
+        onClick={copy}
+        className={cn("min-w-[3rem]", button)}
+      >
+        {copied ? <CheckIcon className="h-4 w-4 text-green-500" /> : "Copy"}
       </Button>
     </div>
   );
