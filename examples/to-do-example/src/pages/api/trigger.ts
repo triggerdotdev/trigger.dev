f2b190b21a5aac453481960c56990497ef037a2d
D-K-P <dkp.github@pm.me>
2023-06-19 16:38:30 +0100

    Replaced to-do folder with a blank nextjs project

--- examples/to-do-example/src/pages/api/trigger.ts
+++
@@ -1,8 +0,0 @@
-import { client } from "@/trigger";
-import { createPagesRoute } from "@trigger.dev/nextjs";
-
-const { handler, config } = createPagesRoute(client, { path: "/api/trigger" });
-
-export { config };
-
-export default handler;
