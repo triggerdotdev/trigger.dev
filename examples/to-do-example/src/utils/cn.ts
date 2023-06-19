4b56671d2d047903b2ec03d8a1ce452d427dd9e3
D-K-P <dkp.github@pm.me>
2023-06-19 17:18:35 +0100

    Added tailwind config / clsx / tailwind-merge

---
+++ examples/to-do-example/src/utils/cn.ts
@@ -0,0 +1,31 @@
+import { clsx, type ClassValue } from "clsx";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const customTwMerge = extendTailwindMerge({
+  classGroups: {
+    text: [
+      {
+        text: [
+          "xxs",
+          "xs",
+          "sm",
+          "md",
+          "lg",
+          "xl",
+          "2xl",
+          "3xl",
+          "4xl",
+          "5xl",
+          "6xl",
+          "7xl",
+          "8xl",
+          "9xl",
+        ],
+      },
+    ],
+  },
+});
+
+export function cn(...inputs: ClassValue[]) {
+  return customTwMerge(clsx(inputs));
+}
--- examples/to-do-example/tailwind.config.js
+++ examples/to-do-example/tailwind.config.js
@@ -1,18 +1,80 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+const slate = {
+  ...colors.slate,
+  450: "#7E8FA6",
+  500: "#6B7C95",
+  550: "#586981",
+  600: "#45566D",
+  650: "#3C4B62",
+  750: "#293649",
+  850: "#1A2434",
+  900: "#131B2B",
+};
+
+/** Trigger.dev custom palette */
+const midnight = {
+  ...colors.slate,
+  450: colors.slate[850],
+  500: colors.slate[650],
+  550: colors.slate[700],
+  600: colors.slate[750],
+  650: colors.slate[800],
+  750: colors.slate[850],
+  800: colors.slate[900],
+  850: "#0E1521",
+  900: "#0B1018",
+};
+
+/** Text colors */
+const bright = colors.slate[200];
+const dimmed = colors.slate[400];
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        mono: ["Roboto Mono", "monospace"],
       },
+      fontSize: {
+        xxs: [
+          "0.65rem",
+          {
+            lineHeight: "0.75rem",
+            letterSpacing: "-0.01em",
+            fontWeight: "500",
+          },
+        ],
+      },
+      colors: {
+        midnight,
+        bright,
+        dimmed,
+        slate,
+        acid: {
+          400: "#F1FF98",
+          500: "#E7FF52",
+          600: "#FFF067",
+        },
+        toxic: {
+          400: "#8EFF9A",
+          500: "#41FF54",
+          600: "#00FFA3",
+        },
+      },
+    },
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      "gradient-conic":
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
     },
   },
   plugins: [],
-}
+};
