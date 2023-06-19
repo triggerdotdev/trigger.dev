68beefdaf2e503be5b98e1046f1c29754cc8a9e0
D-K-P <dkp.github@pm.me>
2023-06-19 15:05:15 +0100

    Added new integration logos and upped the companyicons package number

--- apps/webapp/app/services/externalApis/apis.ts
+++ apps/webapp/app/services/externalApis/apis.ts
@@ -4,84 +4,149 @@ export type Api = {
 };
 
 export const apisList = [
-  {
+ {
     identifier: "airtable",
     name: "Airtable",
-  },
-  {
+},
+{
     identifier: "asana",
     name: "Asana",
-  },
-  {
+},
+{
+    identifier: "aws",
+    name: "AWS",
+},
+{
     identifier: "discord",
     name: "Discord",
-  },
-  {
+},
+{
+    identifier: "dropbox",
+    name: "Dropbox",
+},
+{
     identifier: "facebook",
     name: "Facebook",
-  },
-  {
+},
+{
+    identifier: "flickr",
+    name: "Flickr",
+},
+{
     identifier: "github",
     name: "GitHub",
-  },
-  {
+},
+{
+    identifier: "giphy",
+    name: "Giphy",
+},
+{
     identifier: "gmail",
     name: "Gmail",
-  },
-  {
+},
+{
     identifier: "googlecalendar",
     name: "Google Calendar",
-  },
-  {
+},
+{
+    identifier: "googlecloudplatform",
+    name: "Google Cloud Platform",
+},
+{
     identifier: "googledocs",
     name: "Google Docs",
-  },
-  {
+},
+{
+    identifier: "googledrive",
+    name: "Google Drive",
+},
+{
+    identifier: "googlemaps",
+    name: "Google Maps",
+},
+{
     identifier: "hubspot",
     name: "HubSpot",
-  },
-  {
+},
+{
+    identifier: "instagram",
+    name: "Instagram",
+},
+{
+    identifier: "linkedin",
+    name: "LinkedIn",
+},
+{
     identifier: "linear",
     name: "Linear",
-  },
-  {
+},
+{
+    identifier: "mailchimp",
+    name: "Mailchimp",
+},
+{
     identifier: "mailgun",
     name: "Mailgun",
-  },
-  {
+},
+{
+    identifier: "microsoftazure",
+    name: "Microsoft Azure",
+},
+{
     identifier: "notion",
     name: "Notion",
-  },
-  {
+},
+{
     identifier: "openai",
     name: "OpenAI",
-  },
-  {
+},
+{
+    identifier: "reddit",
+    name: "Reddit",
+},
+{
     identifier: "salesforce",
     name: "Salesforce",
-  },
-  {
+},
+{
     identifier: "sendgrid",
     name: "SendGrid",
-  },
-  {
+},
+{
     identifier: "shopify",
     name: "Shopify",
-  },
-  {
+},
+{
     identifier: "slack",
     name: "Slack",
-  },
-  {
+},
+{
+    identifier: "spotify",
+    name: "Spotify",
+},
+{
     identifier: "stripe",
     name: "Stripe",
-  },
-  {
+},
+{
+    identifier: "trello",
+    name: "Trello",
+},
+{
     identifier: "twilio",
     name: "Twilio",
-  },
-  {
+},
+{
     identifier: "twitter",
     name: "Twitter",
-  },
+},
+{
+    identifier: "whatsapp",
+    name: "WhatsApp",
+},
+{
+    identifier: "youtube",
+    name: "YouTube",
+},
+
 ];
