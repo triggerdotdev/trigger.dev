fb308e8038c710b5158d4fad755a0a20e96e3634
Matt Aitken <matt@mattaitken.com>
2023-06-21 13:04:38 +0100

    Sent a notification when the feedback form is submitted, even if that custoemr is already active

--- apps/webapp/app/routes/resources.feedback.ts
+++ apps/webapp/app/routes/resources.feedback.ts
@@ -148,6 +148,7 @@ export async function action({ request }: ActionArgs) {
         },
       ],
       changeCustomerStatusToActive: true,
+      sendCustomTimelineEntryCreatedNotification: true,
     });
 
     if (upsertTimelineEntryRes.error) {
