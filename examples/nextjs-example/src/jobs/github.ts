fff2f409246daa577d1e044a574b0ddd00cb42df
Matt Aitken <matt@mattaitken.com>
2023-06-21 17:55:10 +0100

    Added issue commented example job

--- examples/nextjs-example/src/jobs/github.ts
+++ examples/nextjs-example/src/jobs/github.ts
@@ -49,3 +49,17 @@ new Job(client, {
     return { payload, ctx };
   },
 });
+
+new Job(client, {
+  id: "github-integration-on-issue-commented",
+  name: "GitHub Integration - On Issue commented",
+  version: "0.1.0",
+  trigger: githubApiKey.triggers.repo({
+    event: events.onIssueComment,
+    repo: "triggerdotdev/empty",
+  }),
+  run: async (payload, io, ctx) => {
+    await io.logger.info("This is a simple log info message");
+    return { payload, ctx };
+  },
+});
