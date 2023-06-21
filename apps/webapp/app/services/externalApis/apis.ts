2cf253a91476fb70fa0645c5e59264497362b6a8
D-K-P <dkp.github@pm.me>
2023-06-21 10:18:59 +0100

    Added new integrations / icons

--- apps/webapp/app/services/externalApis/apis.ts
+++ apps/webapp/app/services/externalApis/apis.ts
@@ -4,149 +4,208 @@ export type Api = {
 };
 
 export const apisList = [
- {
+  {
     identifier: "airtable",
     name: "Airtable",
-},
-{
+  },
+  {
+    identifier: "algolia",
+    name: "Algolia",
+  },
+  {
+    identifier: "anthropic",
+    name: "Anthropic",
+  },
+  {
+    identifier: "appsmith",
+    name: "Appsmith",
+  },
+  {
     identifier: "asana",
     name: "Asana",
-},
-{
+  },
+  {
     identifier: "aws",
     name: "AWS",
-},
-{
+  },
+  {
+    identifier: "caldotcom",
+    name: "Cal.com",
+  },
+  {
+    identifier: "clickup",
+    name: "ClickUp",
+  },
+  {
+    identifier: "coda",
+    name: "Coda",
+  },
+  {
     identifier: "discord",
     name: "Discord",
-},
-{
+  },
+  {
     identifier: "dropbox",
     name: "Dropbox",
-},
-{
+  },
+  {
     identifier: "facebook",
     name: "Facebook",
-},
-{
+  },
+  {
     identifier: "flickr",
     name: "Flickr",
-},
-{
+  },
+  {
     identifier: "github",
     name: "GitHub",
-},
-{
+  },
+  {
     identifier: "giphy",
     name: "Giphy",
-},
-{
+  },
+  {
     identifier: "gmail",
     name: "Gmail",
-},
-{
+  },
+  {
     identifier: "googlecalendar",
     name: "Google Calendar",
-},
-{
+  },
+  {
     identifier: "googlecloudplatform",
     name: "Google Cloud Platform",
-},
-{
+  },
+  {
     identifier: "googledocs",
     name: "Google Docs",
-},
-{
+  },
+  {
     identifier: "googledrive",
     name: "Google Drive",
-},
-{
+  },
+  {
     identifier: "googlemaps",
     name: "Google Maps",
-},
-{
+  },
+  {
     identifier: "hubspot",
     name: "HubSpot",
-},
-{
+  },
+  {
     identifier: "instagram",
     name: "Instagram",
-},
-{
+  },
+  {
+    identifier: "infisical",
+    name: "Infisical",
+  },
+  {
     identifier: "linkedin",
     name: "LinkedIn",
-},
-{
+  },
+  {
     identifier: "linear",
     name: "Linear",
-},
-{
+  },
+  {
+    identifier: "loops",
+    name: "Loops",
+  },
+  {
     identifier: "mailchimp",
     name: "Mailchimp",
-},
-{
+  },
+  {
     identifier: "mailgun",
     name: "Mailgun",
-},
-{
+  },
+  {
     identifier: "microsoftazure",
     name: "Microsoft Azure",
-},
-{
+  },
+  {
+    identifier: "mux",
+    name: "Mux",
+  },
+  {
     identifier: "notion",
     name: "Notion",
-},
-{
+  },
+  {
     identifier: "openai",
     name: "OpenAI",
-},
-{
+  },
+  {
+    identifier: "plain",
+    name: "Plain",
+  },
+  {
+    identifier: "posthog",
+    name: "Posthog",
+  },
+  {
     identifier: "reddit",
     name: "Reddit",
-},
-{
+  },
+  {
+    identifier: "replicate",
+    name: "Replicate",
+  },
+  {
     identifier: "salesforce",
     name: "Salesforce",
-},
-{
+  },
+  {
+    identifier: "segment",
+    name: "Segment",
+  },
+  {
     identifier: "sendgrid",
     name: "SendGrid",
-},
-{
+  },
+  {
     identifier: "shopify",
     name: "Shopify",
-},
-{
+  },
+  {
     identifier: "slack",
     name: "Slack",
-},
-{
+  },
+  {
     identifier: "spotify",
     name: "Spotify",
-},
-{
+  },
+  {
     identifier: "stripe",
     name: "Stripe",
-},
-{
+  },
+  {
+    identifier: "supabase",
+    name: "Supabase",
+  },
+  {
+    identifier: "svix",
+    name: "Svix",
+  },
+  {
     identifier: "trello",
     name: "Trello",
-},
-{
+  },
+  {
     identifier: "twilio",
     name: "Twilio",
-},
-{
+  },
+  {
     identifier: "twitter",
     name: "Twitter",
-},
-{
+  },
+  {
     identifier: "whatsapp",
     name: "WhatsApp",
-},
-{
+  },
+  {
     identifier: "youtube",
     name: "YouTube",
-},
-
+  },
 ];
