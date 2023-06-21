21d7ff2f748934576bb88643013a2958dfde78fe
Matt Aitken <matt@mattaitken.com>
2023-06-21 17:53:56 +0100

    Added a function to truncate a string with an ellipsis

--- packages/integration-kit/src/index.ts
+++ packages/integration-kit/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./json";
 export * from "./omit";
+export * from "./properties";
---
+++ packages/integration-kit/src/properties.ts
@@ -0,0 +1,3 @@
+export function truncate(str: string, maxLength: number) {
+  return str.length > maxLength ? str.slice(0, maxLength - 1) + "…" : str;
+}
