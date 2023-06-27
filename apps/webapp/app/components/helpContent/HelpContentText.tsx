cfe0a6cbb8ca95ee78f3874f4057d3b28d6965e1
James Ritchie <james@jamesritchie.co.uk>
2023-06-27 15:44:43 +0100

    Copy changes

--- apps/webapp/app/components/helpContent/HelpContentText.tsx
+++ apps/webapp/app/components/helpContent/HelpContentText.tsx
@@ -1,5 +1,8 @@
 import { Paragraph } from "~/components/primitives/Paragraph";
 import { StepNumber } from "~/components/primitives/StepNumber";
+import { IntegrationIcon } from "~/routes/_app.orgs.$organizationSlug.projects.$projectParam.integrations/route";
+import { Callout } from "../primitives/Callout";
+import integrationButton from "./integration-button.png";
 
 export function HowToCreateAJob() {
   return (
@@ -26,10 +29,48 @@ export function HowToRunATest() {
 export function HowToConnectAnIntegration() {
   return (
     <>
-      <StepNumber stepNumber="1" title="Select an Integration from the list" />
+      <StepNumber stepNumber="1" title="Select an API from the list" />
       <StepContentContainer>
-        <Paragraph variant="small">Content</Paragraph>
+        <Paragraph>
+          APIs marked with a
+          <span
+            className="mx-2 -mt-1 inline-flex"
+            aria-label="Trigger.dev Integration icon"
+          >
+            <IntegrationIcon />
+          </span>
+          are Trigger.dev Integrations. These Integrations make connecting to
+          the API easier by offering OAuth or API key authentication. All APIs
+          can also be used with generic fetch or an SDK.
+        </Paragraph>
+        <img src={integrationButton} className="mt-2 h-10" />
       </StepContentContainer>
+      <StepNumber stepNumber="2" title="Choose how you want to connect" />
+      <StepContentContainer>
+        <Paragraph>
+          Follow the instructions for your chosen connection method in the
+          popover form. If no Integration exists yet, you can request one by
+          clicking the "I want an Integration" button.
+        </Paragraph>
+      </StepContentContainer>
+      <StepNumber
+        stepNumber="3"
+        title="Your connection will appear in the list"
+      />
+      <StepContentContainer>
+        <Paragraph>
+          Once you've connected your API, it will appear in the list of
+          Integrations below. You can view details and manage your connection by
+          selecting it from the table.
+        </Paragraph>
+      </StepContentContainer>
+      <Callout
+        variant={"docs"}
+        href="https://trigger.dev/docs/integrations/introduction"
+      >
+        View the Integration docs page for more information on connecting an API
+        using an Integration or other method.
+      </Callout>
     </>
   );
 }
@@ -46,5 +87,5 @@ export function HowToUseThisIntegration() {
 }
 
 function StepContentContainer({ children }: { children: React.ReactNode }) {
-  return <div className="ml-9 mt-1">{children}</div>;
+  return <div className="mb-6 ml-9 mt-1">{children}</div>;
 }
