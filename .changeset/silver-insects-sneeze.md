f160b34b344f0fabf28229f4ae5a3ff0e4adb3c7
Matt Aitken <matt@mattaitken.com>
2023-07-04 16:28:58 +0100

    isTriggerError jsdocs

---
+++ .changeset/silver-insects-sneeze.md
@@ -0,0 +1,5 @@
+---
+"@trigger.dev/sdk": patch
+---
+
+isTriggerError jsdocs
--- packages/trigger-sdk/src/errors.ts
+++ packages/trigger-sdk/src/errors.ts
@@ -12,6 +12,12 @@ export class RetryWithTaskError {
   ) {}
 }
 
+/** Use this function if you're using a `try/catch` block to catch errors.
+ * It checks if a thrown error is a special internal error that you should ignore.
+ * If this returns `true` then you must rethrow the error: `throw err;`
+ * @param err The error to check
+ * @returns `true` if the error is a Trigger Error, `false` otherwise.
+ */
 export function isTriggerError(
   err: unknown
 ): err is ResumeWithTaskError | RetryWithTaskError {
