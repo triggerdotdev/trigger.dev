bae1b0be72fb27e02f2bec0b17d5cee107c268f8
Matt Aitken <matt@mattaitken.com>
2023-07-11 18:06:42 +0100

    Added action to retry registration

--- apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam.triggers_.external.$triggerParam/route.tsx
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam.triggers_.external.$triggerParam/route.tsx
@@ -1,12 +1,12 @@
-import { Response } from "@remix-run/node";
-import type { LoaderArgs } from "@remix-run/server-runtime";
+import { Response, json } from "@remix-run/node";
+import type { ActionFunction, LoaderArgs } from "@remix-run/server-runtime";
 import { Fragment } from "react";
 import { typedjson, useTypedLoaderData } from "remix-typedjson";
 import { EnvironmentLabel } from "~/components/environments/EnvironmentLabel";
 import { PageBody, PageContainer } from "~/components/layout/AppLayout";
 import { BreadcrumbLink } from "~/components/navigation/NavBar";
 import { BreadcrumbIcon } from "~/components/primitives/BreadcrumbIcon";
-import { Callout } from "~/components/primitives/Callout";
+import { Callout, variantClasses } from "~/components/primitives/Callout";
 import { Header2 } from "~/components/primitives/Headers";
 import { NamedIcon } from "~/components/primitives/NamedIcon";
 import {
@@ -23,16 +23,26 @@ import { useOrganization } from "~/hooks/useOrganizations";
 import { useProject } from "~/hooks/useProject";
 import { useTypedMatchData } from "~/hooks/useTypedMatchData";
 import { TriggerSourcePresenter } from "~/presenters/TriggerSourcePresenter.server";
-import { requireUser } from "~/services/session.server";
+import { requireUser, requireUserId } from "~/services/session.server";
 import { Handle } from "~/utils/handle";
 import {
   TriggerSourceParamSchema,
+  organizationsPath,
   projectTriggersPath,
-  triggerSourceRunsPath,
+  externalTriggerPath,
+  externalTriggerRunsParentPath,
   trimTrailingSlash,
 } from "~/utils/pathBuilder";
 import { ListPagination } from "../_app.orgs.$organizationSlug.projects.$projectParam.jobs.$jobParam._index/ListPagination";
 import { RunListSearchSchema } from "../_app.orgs.$organizationSlug.projects.$projectParam.jobs.$jobParam._index/route";
+import { Button } from "~/components/primitives/Buttons";
+import { Form, useActionData, useNavigation } from "@remix-run/react";
+import { cn } from "~/utils/cn";
+import { conform, useForm } from "@conform-to/react";
+import { parse } from "@conform-to/zod";
+import { z } from "zod";
+import { ActivateSourceService } from "~/services/sources/activateSource.server";
+import { redirectWithSuccessMessage } from "~/models/message.server";
 
 export const loader = async ({ request, params }: LoaderArgs) => {
   const user = await requireUser(request);
@@ -63,6 +73,40 @@ export const loader = async ({ request, params }: LoaderArgs) => {
   return typedjson({ trigger });
 };
 
+const schema = z.object({
+  jobId: z.string(),
+});
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const userId = await requireUserId(request);
+  const { organizationSlug, projectParam, triggerParam } =
+    TriggerSourceParamSchema.parse(params);
+
+  const formData = await request.formData();
+  const submission = parse(formData, { schema });
+
+  if (!submission.value) {
+    return json(submission);
+  }
+
+  try {
+    const service = new ActivateSourceService();
+
+    const result = await service.call(triggerParam, submission.value.jobId);
+
+    return redirectWithSuccessMessage(
+      externalTriggerPath(
+        { slug: organizationSlug },
+        { slug: projectParam },
+        { id: triggerParam }
+      ),
+      request,
+      `Retrying registration now`
+    );
+  } catch (error: any) {
+    return json({ errors: { body: error.message } }, { status: 400 });
+  }
+};
 export const handle: Handle = {
   //this one is complicated because we render outside the parent route (using triggers_ in the path)
   breadcrumb: (match, matches) => {
@@ -97,6 +141,19 @@ export default function Page() {
   const { trigger } = useTypedLoaderData<typeof loader>();
   const organization = useOrganization();
   const project = useProject();
+  const navigation = useNavigation();
+  const lastSubmission = useActionData();
+
+  const [form, { jobId }] = useForm({
+    id: "trigger-registration-retry",
+    lastSubmission,
+    onValidate({ formData }) {
+      return parse(formData, { schema });
+    },
+  });
+
+  const isLoading =
+    navigation.state === "submitting" && navigation.formData !== undefined;
 
   return (
     <PageContainer>
@@ -141,6 +198,46 @@ export default function Page() {
             External Triggers need to be registered with the external service.
             You can see the list of attempted registrations below.
           </Paragraph>
+
+          {!trigger.active &&
+            (trigger.registrationJob ? (
+              <Form method="post" {...form.props}>
+                <Callout
+                  variant="error"
+                  className="justiy-between mb-4 items-center"
+                >
+                  <Paragraph
+                    variant="small"
+                    className={cn(variantClasses.error.textColor, "grow")}
+                  >
+                    Registration hasn't succeeded yet, check the runs below.
+                  </Paragraph>
+                  <input
+                    {...conform.input(jobId, { type: "hidden" })}
+                    defaultValue={trigger.registrationJob?.id}
+                  />
+                  <Button
+                    variant="danger/small"
+                    type="submit"
+                    name={conform.INTENT}
+                    value="retry"
+                    disabled={isLoading}
+                    LeadingIcon={isLoading ? "spinner-white" : undefined}
+                  >
+                    {isLoading ? "Retrying…" : "Retry now"}
+                  </Button>
+                </Callout>
+              </Form>
+            ) : (
+              <Callout
+                variant="error"
+                className="justiy-between mb-4 items-center"
+              >
+                This External Trigger hasn't registered successfully. Contact
+                support for help: {trigger.id}
+              </Callout>
+            ))}
+
           {trigger.runList ? (
             <>
               <ListPagination
@@ -151,7 +248,7 @@ export default function Page() {
                 runs={trigger.runList.runs}
                 total={trigger.runList.runs.length}
                 hasFilters={false}
-                runsParentPath={triggerSourceRunsPath(
+                runsParentPath={externalTriggerRunsParentPath(
                   organization,
                   project,
                   trigger
