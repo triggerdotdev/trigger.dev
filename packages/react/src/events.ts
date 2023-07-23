b04852c98c6a865dfe8f55ca1742ee1656e6ea2a
Matt Aitken <matt@mattaitken.com>
2023-07-23 17:51:03 +0100

    useQueryEvent

---
+++ packages/react/src/events.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { GetEventSchema } from "@trigger.dev/internal";
+import { zodfetch } from "./fetch";
+import { useTriggerProvider } from "./TriggerProvider";
+
+export function useQueryEvent(eventId: string) {
+  const { apiUrl, publicApiKey } = useTriggerProvider();
+
+  return useQuery(
+    [`event-${eventId}`],
+    async () => {
+      return await zodfetch(
+        GetEventSchema,
+        `${apiUrl}/api/v1/events/${eventId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${publicApiKey}`,
+          },
+        }
+      );
+    },
+    {
+      refetchInterval: 6000,
+    }
+  );
+}
