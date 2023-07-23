e135b06e2cffcf1ee9f8df7b07619f15ad8c5998
Matt Aitken <matt@mattaitken.com>
2023-07-23 18:42:36 +0100

    Show errors, not working properly yet

--- examples/nextjs-example/src/components/EventTest.tsx
+++ examples/nextjs-example/src/components/EventTest.tsx
@@ -3,12 +3,14 @@
 import { useQueryEvent } from "@trigger.dev/react";
 
 export function EventTest() {
-  const { isLoading, data } = useQueryEvent("clj73e44q00947c60kp0dpc37");
+  const { isLoading, data, error } = useQueryEvent("clj73e44q00947c60kp0dpc37");
 
   return (
     <>
       {isLoading ? (
         <p>Loading</p>
+      ) : error ? (
+        error
       ) : (
         <code>
           <pre>{JSON.stringify(data, null, 2)}</pre>
