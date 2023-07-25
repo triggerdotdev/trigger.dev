4dce703b20ea2fc001f4e14c629e6803be018e6b
D-K-P <dkp.github@pm.me>
2023-07-25 16:17:57 +0100

    Changed the create a job in code callout from Quick start to Create a Job

--- apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam._index/route.tsx
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam._index/route.tsx
@@ -170,9 +170,7 @@ export default function Page() {
                       <>
                         <Callout
                           variant="docs"
-                          to={docsPath(
-                            "documentation/quickstart#your-first-job"
-                          )}
+                          to={docsPath("documentation/guides/create-a-job")}
                           className="my-3"
                         >
                           Create your first Job in code
