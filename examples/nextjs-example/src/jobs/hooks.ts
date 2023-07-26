981f4010a96d11376d6e8bba0b77325a4aeed2b4
Matt Aitken <matt@mattaitken.com>
2023-07-26 14:38:05 +0100

    Shortened the delays in the hooks example to make it less annoying when testing

--- examples/nextjs-example/src/jobs/hooks.ts
+++ examples/nextjs-example/src/jobs/hooks.ts
@@ -10,9 +10,9 @@ client.defineJob({
   }),
   run: async (payload, io, ctx) => {
     await io.logger.log("This Job is triggered from a button in the frontend");
-    await io.wait("wait", 20);
+    await io.wait("wait", 5);
     await io.logger.log("It runs for a while to test the React hooks");
-    await io.wait("wait 2", 10);
+    await io.wait("wait 2", 5);
     await io.logger.log("This is the end of the job");
 
     return {
