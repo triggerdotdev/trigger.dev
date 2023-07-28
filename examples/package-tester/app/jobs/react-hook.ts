a642fb9159be97f416120b35811803f27e6980e1
Matt Aitken <matt@mattaitken.com>
2023-07-28 16:17:42 +0100

    Added OpenAI step to the job to check types are still ok in integrations

--- examples/package-tester/app/jobs/react-hook.ts
+++ examples/package-tester/app/jobs/react-hook.ts
@@ -1,5 +1,11 @@
 import { eventTrigger } from "@trigger.dev/sdk";
 import { client } from "../trigger";
+import { OpenAI } from "@trigger.dev/openai";
+
+const openai = new OpenAI({
+  id: "openai",
+  apiKey: process.env["OPENAI_API_KEY"]!,
+});
 
 // use Open AI to summarize text from the form
 client.defineJob({
@@ -9,12 +15,28 @@ client.defineJob({
   trigger: eventTrigger({
     name: "react-hook",
   }),
+  integrations: {
+    openai,
+  },
   run: async (_payload, io) => {
     await io.wait("Wait 2 seconds", 2);
     await io.wait("Wait 1 second", 1);
 
+    const result = await io.openai.backgroundCreateChatCompletion(
+      "Tell me a joke",
+      {
+        model: "gpt-3.5-turbo-16k",
+        messages: [
+          {
+            role: "user",
+            content: `Tell me a joke please`,
+          },
+        ],
+      }
+    );
+
     return {
-      summary: "This is the output value from the Job",
+      summary: result?.choices[0]?.message?.content,
     };
   },
 });
