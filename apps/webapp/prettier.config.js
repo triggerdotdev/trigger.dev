5b0666960a2fa21d8c9dd9ba5c17a9b205318fe3
Matt Aitken <matt@mattaitken.com>
2023-07-29 19:24:42 +0100

    Setup project-wide prettier

--- apps/webapp/prettier.config.js
+++ apps/webapp/prettier.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  ...require('../../prettier.config.js'),
   plugins: [require('prettier-plugin-tailwindcss')],
-}
+};
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+};
