a3d4576e710aa0948fe8d9e16bcdef8d29cfff67
Matt Aitken <matt@mattaitken.com>
2023-07-30 12:11:39 +0100

    Updated Prettier config settings

--- prettier.config.js
+++ prettier.config.js
@@ -2,10 +2,10 @@ module.exports = {
   semi: true,
   singleQuote: false,
   jsxSingleQuote: false,
-  trailingComma: "all",
+  trailingComma: "es5",
   bracketSpacing: true,
   bracketSameLine: false,
-  printWidth: 100,
+  printWidth: 80,
   tabWidth: 2,
   useTabs: false,
 };
