3fe872658ccaba0f3999a47073c3d74ebbae373f
Jemilu Mohammed <jemiluv8@gmail.com>
2023-07-31 17:38:12 +0000

    import jobs module in routeContent for createTriggerPageRoute just as we did for createTriggerAppRoute

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -587,6 +587,8 @@ async function createTriggerPageRoute(
 import { createPagesRoute } from "@trigger.dev/nextjs";
 import { client } from "${routePathPrefix}trigger";
 
+import "${routePathPrefix}jobs";
+
 //this route is used to send and receive data with Trigger.dev
 const { handler, config } = createPagesRoute(client);
 export { config };
