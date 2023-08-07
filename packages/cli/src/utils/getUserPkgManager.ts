ff04bf44ee6f7bde958a318b099b9d8c3577092e
Matt Aitken <matt@mattaitken.com>
2023-08-07 13:01:22 +0100

    CLI: detect package manager using artifacts if they exist (#261)
    * Detect package manager from artifacts (e.g. package-lock.json) and fallback to process.env.npm_config_user_agent
    
    * Added a changeset

--- packages/cli/src/utils/getUserPkgManager.ts
+++ packages/cli/src/utils/getUserPkgManager.ts
@@ -1,6 +1,17 @@
+import pathModule from "path";
+import { pathExists } from "./fileSystem.js";
+
 export type PackageManager = "npm" | "pnpm" | "yarn";
 
-export const getUserPkgManager: () => PackageManager = () => {
+export async function getUserPackageManager(path: string): Promise<PackageManager> {
+  try {
+    return detectPackageManagerFromArtifacts(path);
+  } catch (error) {
+    return detectPackageManagerFromCurrentCommand();
+  }
+}
+
+function detectPackageManagerFromCurrentCommand(): PackageManager {
   // This environment variable is set by npm and yarn but pnpm seems less consistent
   const userAgent = process.env.npm_config_user_agent;
 
@@ -16,4 +27,22 @@ export const getUserPkgManager: () => PackageManager = () => {
     // If no user agent is set, assume npm
     return "npm";
   }
-};
+}
+
+async function detectPackageManagerFromArtifacts(path: string): Promise<PackageManager> {
+  const packageFiles = [
+    { name: "yarn.lock", pm: "yarn" } as const,
+    { name: "pnpm-lock.yaml", pm: "pnpm" } as const,
+    { name: "package-lock.json", pm: "npm" } as const,
+    { name: "npm-shrinkwrap.json", pm: "npm" } as const,
+  ];
+
+  for (const { name, pm } of packageFiles) {
+    const exists = await pathExists(pathModule.join(path, name));
+    if (exists) {
+      return pm;
+    }
+  }
+
+  throw new Error("Could not detect package manager from artifacts");
+}
