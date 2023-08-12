2ee4a47dafb616e4a0b0b9186fe12d77bc7ab3be
Wesley Matos <wm4tos.777@gmail.com>
2023-08-12 20:19:07 -0300

    test: create `jest.config.js`

---
+++ packages/cli/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ["<rootDir>/test/**/*.ts?(x)", "<rootDir>/test/**/?(*.)+(spec|test).ts?(x)"],
+};
