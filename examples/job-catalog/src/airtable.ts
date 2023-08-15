54bfbe6a4392766be92b29ad7fe3b5cf17ec882e
Matt Aitken <matt@mattaitken.com>
2023-08-15 15:43:47 +0100

    First Airtable job

--- examples/job-catalog/src/airtable.ts
+++ examples/job-catalog/src/airtable.ts
@@ -1,1 +1,41 @@
-export {}
+import { TriggerClient, eventTrigger } from "@trigger.dev/sdk";
+import { createExpressServer } from "@trigger.dev/express";
+import { z } from "zod";
+import { Airtable } from "@trigger.dev/airtable";
+
+export const client = new TriggerClient({
+  id: "job-catalog",
+  apiKey: process.env["TRIGGER_API_KEY"],
+  apiUrl: process.env["TRIGGER_API_URL"],
+  verbose: false,
+  ioLogLocalEnabled: true,
+});
+
+const airtable = new Airtable({
+  id: "airtable",
+  token: process.env["AIRTABLE_TOKEN"],
+});
+
+client.defineJob({
+  id: "airtable-example-1",
+  name: "Airtable Example 1: getRecords",
+  version: "0.1.0",
+  trigger: eventTrigger({
+    name: "airtable.example",
+    schema: z.object({
+      baseId: z.string(),
+      tableName: z.string(),
+    }),
+  }),
+  integrations: {
+    airtable,
+  },
+  run: async (payload, io, ctx) => {
+    await io.airtable.getRecords("get records", {
+      baseId: payload.baseId,
+      tableName: payload.tableName,
+    });
+  },
+});
+
+createExpressServer(client);
