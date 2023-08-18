9e79284d637f46c5abf2ab35d6685a7bf6bf6607
Eric Allam <eallam@icloud.com>
2023-08-18 14:57:52 +0100

    Added slack to job catalog

---
+++ examples/job-catalog/src/slack.ts
@@ -0,0 +1,33 @@
+import { createExpressServer } from "@trigger.dev/express";
+import { TriggerClient, eventTrigger } from "@trigger.dev/sdk";
+import { Slack } from "@trigger.dev/slack";
+
+export const slack = new Slack({ id: "slack" });
+
+export const client = new TriggerClient({
+  id: "job-catalog",
+  apiKey: process.env["TRIGGER_API_KEY"],
+  apiUrl: process.env["TRIGGER_API_URL"],
+  verbose: false,
+  ioLogLocalEnabled: true,
+});
+
+client.defineJob({
+  id: "slack-example-1",
+  name: "Slack Example 1",
+  version: "1.0.0",
+  trigger: eventTrigger({
+    name: "slack.example",
+  }),
+  integrations: {
+    slack,
+  },
+  run: async (payload, io, ctx) => {
+    await io.slack.postMessage("Slack 📝", {
+      channel: "C04GWUTDC3W",
+      text: "Welcome to the team, Eric!",
+    });
+  },
+});
+
+createExpressServer(client);
