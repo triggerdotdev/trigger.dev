74686c00cbe9c613abc0877277785551b1a355fa
Wesley <100464352+ologbonowiwi@users.noreply.github.com>
2023-08-18 08:57:12 -0300

    feature/TRI 712/create eslint plugin to ensure uniqueness on task keys (#324)
    * test: add test to no-duplicated-task-keys rule
    
    * feat: implement no-duplicated-task-keys
    
    * chore: create testing scripts
    
    * refactor: fill in rule meta
    
    * feat: export no-duplicated-task-keys rule
    
    * chore: bump package version to 0.0.1
    
    * chore: create eslint plugin
    
    * feat: create no-duplicated-task-keys rule
    
    * refactor: delete no-duplicated-task-keys from config-custom
    
    * refactor: rename eslint-plugin folder
    
    * feat: cover case from nextjs-example
    
    * feat: cover additional cases from examples/package-tester
    
    * chore: add eslint-plugin on nextjs-example
    
    * feat: add more cases from `nextjs-example`
    
    * revert: rollback changes on eslint-config-custom
    
    * Set version to 2.0.9, inline with other packages
    
    * Create chilly-pianos-try.md
    
    ---------
    
    Co-authored-by: Matt Aitken <matt@mattaitken.com>

---
+++ packages/eslint-plugin/.eslintrc.js
@@ -0,0 +1,23 @@
+"use strict";
+
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:eslint-plugin/recommended",
+    "plugin:node/recommended",
+  ],
+  env: {
+    node: true,
+  },
+  overrides: [
+    {
+      files: ["tests/**/*.js"],
+      env: { mocha: true },
+    },
+  ],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+};
---
+++ packages/eslint-plugin/lib/index.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview ESLint plugin with trigger.dev best practices
+ * @author 
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const requireIndex = require("requireindex");
+
+//------------------------------------------------------------------------------
+// Plugin Definition
+//------------------------------------------------------------------------------
+
+
+// import all rules in lib/rules
+module.exports.rules = requireIndex(__dirname + "/rules");
+
+
+
