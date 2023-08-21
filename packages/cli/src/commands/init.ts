6cbc4f11904250eca8e913dd6a53c21ddd86c2c6
Chigala <92148630+Chigala@users.noreply.github.com>
2023-08-21 18:59:31 +0100

    fix: CLI init now logs the correct app route path.

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -526,7 +526,11 @@ export * from "./examples"
 
   await fs.writeFile(pathModule.join(directories, routeFileName), routeContent);
 
-  logger.success(`✅ Created app route at ${usesSrcDir ? "src/" : ""}app/api/trigger.ts`);
+  logger.success(
+    `✅ Created app route at ${usesSrcDir ? "src/" : ""}app/api/${removeFileExtension(
+      triggerFileName
+    )}/${routeFileName}`
+  );
 
   const triggerFileExists = await pathExists(pathModule.join(path, triggerFileName));
 
@@ -733,4 +737,8 @@ async function setupEnvironmentVariable(
 
     logger.success(`✅ Added ${variableName}=${renderer(value)} to ${fileName}`);
   }
+}
+
+function removeFileExtension(filename: string) {
+  return filename.replace(/\.[^.]+$/, "");
 }
