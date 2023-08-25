bf32ddea7c3edff9825d8757fb5fae99296f717d
Matt Aitken <matt@mattaitken.com>
2023-08-25 19:00:45 +0100

    Resend switched to new integration

--- integrations/resend/src/index.ts
+++ integrations/resend/src/index.ts
@@ -1,74 +1,48 @@
-import type { IntegrationClient, TriggerIntegration } from "@trigger.dev/sdk";
+import type {
+  ConnectionAuth,
+  IO,
+  IOTask,
+  IntegrationTaskKey,
+  RunTaskErrorCallback,
+  RunTaskOptions,
+  RunTaskResult,
+  TriggerIntegration,
+} from "@trigger.dev/sdk";
 import { Resend as ResendClient } from "resend";
-
-import type { AuthenticatedTask } from "@trigger.dev/sdk";
 
 type SendEmailData = Parameters<InstanceType<typeof ResendClient>["sendEmail"]>[0];
 
 type SendEmailResponse = { id: string };
-
-export const sendEmail: AuthenticatedTask<
-  InstanceType<typeof ResendClient>,
-  SendEmailData,
-  SendEmailResponse
-> = {
-  run: async (params, client) => {
-    return client.sendEmail(params) as Promise<SendEmailResponse>;
-  },
-  init: (params) => {
-    const subjectProperty = params.subject ? [{ label: "Subject", text: params.subject }] : [];
-
-    return {
-      name: "Send Email",
-      params,
-      icon: "resend",
-      properties: [
-        {
-          label: "From",
-          text: params.from,
-        },
-        {
-          label: "To",
-          text: Array.isArray(params.to) ? params.to.join(", ") : params.to,
-        },
-        ...subjectProperty,
-      ],
-      retry: {
-        limit: 8,
-        factor: 1.8,
-        minTimeoutInMs: 500,
-        maxTimeoutInMs: 30000,
-        randomize: true,
-      },
-    };
-  },
-};
-
-const tasks = {
-  sendEmail,
-};
 
 export type ResendIntegrationOptions = {
   id: string;
   apiKey: string;
 };
 
-export class Resend implements TriggerIntegration<IntegrationClient<ResendClient, typeof tasks>> {
-  client: IntegrationClient<ResendClient, typeof tasks>;
+export class Resend implements TriggerIntegration {
+  private _options: ResendIntegrationOptions;
+  private _client?: ResendClient;
+  private _io?: IO;
+  private _connectionKey?: string;
 
   constructor(private options: ResendIntegrationOptions) {
     if (Object.keys(options).includes("apiKey") && !options.apiKey) {
       throw `Can't create Resend integration (${options.id}) as apiKey was undefined`;
     }
 
-    this.client = {
-      tasks,
-      usesLocalAuth: true,
-      client: new ResendClient(options.apiKey),
-      auth: {
-        apiKey: options.apiKey,
-      },
-    };
+    this._options = options;
+  }
+
+  get authSource() {
+    return this._options.apiKey ? ("LOCAL" as const) : ("HOSTED" as const);
+  }
+
+  cloneForRun(io: IO, connectionKey: string, auth?: ConnectionAuth) {
+    const resend = new Resend(this._options);
+    resend._io = io;
+    resend._connectionKey = connectionKey;
+    resend._client = new ResendClient(this._options.apiKey);
+    return resend;
   }
 
   get id() {
@@ -77,5 +51,49 @@ export class Resend implements TriggerIntegration<IntegrationClient<ResendClient
 
   get metadata() {
     return { id: "resend", name: "Resend.com" };
+  }
+
+  runTask<TResult extends RunTaskResult = void>(
+    key: IntegrationTaskKey,
+    callback: (client: ResendClient, task: IOTask, io: IO) => Promise<TResult>,
+    options?: RunTaskOptions,
+    errorCallback?: RunTaskErrorCallback
+  ) {
+    if (!this._io) throw new Error("No IO");
+    if (!this._connectionKey) throw new Error("No connection key");
+
+    return this._io.runTask<TResult>(
+      key,
+      (task, io) => {
+        if (!this._client) throw new Error("No client");
+        return callback(this._client, task, io);
+      },
+      { icon: "resend", ...(options ?? {}), connectionKey: this._connectionKey },
+      errorCallback
+    );
+  }
+
+  sendEmail(key: IntegrationTaskKey, params: SendEmailData): Promise<SendEmailResponse> {
+    return this.runTask(
+      key,
+      async (client) => {
+        return client.sendEmail(params) as Promise<SendEmailResponse>;
+      },
+      {
+        name: "Send Email",
+        params,
+        properties: [
+          {
+            label: "From",
+            text: params.from,
+          },
+          {
+            label: "To",
+            text: Array.isArray(params.to) ? params.to.join(", ") : params.to,
+          },
+          ...(params.subject ? [{ label: "Subject", text: params.subject }] : []),
+        ],
+      }
+    );
   }
 }
