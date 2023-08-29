5dc5056d0b0c53566cca4ddf43497bbe95977938
Matt Aitken <matt@mattaitken.com>
2023-08-29 11:42:48 +0100

    WIP on Slack, there are compile errors

--- integrations/slack/src/index.ts
+++ integrations/slack/src/index.ts
@@ -1,27 +1,61 @@
 import { WebClient } from "@slack/web-api";
-import type { IntegrationClient, TriggerIntegration } from "@trigger.dev/sdk";
-import { joinConversation, postMessage } from "./tasks";
-
-const tasks = {
-  postMessage,
-  joinConversation,
-};
+import type {
+  Block,
+  KnownBlock,
+  MessageAttachment,
+  MessageMetadata,
+  WebAPIPlatformError,
+} from "@slack/web-api";
+import type {
+  ConnectionAuth,
+  IO,
+  IOTask,
+  IntegrationTaskKey,
+  RunTaskErrorCallback,
+  RunTaskOptions,
+  RunTaskResult,
+  TriggerIntegration,
+} from "@trigger.dev/sdk";
 
 export type SlackIntegrationOptions = {
   id: string;
 };
 
-export class Slack implements TriggerIntegration<IntegrationClient<WebClient, typeof tasks>> {
-  client: IntegrationClient<WebClient, typeof tasks>;
+type ConversationsJoinResponse = Awaited<ReturnType<SlackClientType["conversations"]["join"]>>;
+
+type SlackClientType = InstanceType<typeof WebClient>;
+
+export type ChatPostMessageArguments = {
+  channel: string;
+  text?: string;
+  as_user?: boolean;
+  attachments?: MessageAttachment[];
+  blocks?: (KnownBlock | Block)[];
+  icon_emoji?: string;
+  icon_url?: string;
+  metadata?: MessageMetadata;
+  link_names?: boolean;
+  mrkdwn?: boolean;
+  parse?: "full" | "none";
+  reply_broadcast?: boolean;
+  thread_ts?: string;
+  unfurl_links?: boolean;
+  unfurl_media?: boolean;
+  username?: string;
+};
+
+export class Slack implements TriggerIntegration {
+  private _options: SlackIntegrationOptions;
+  private _client?: WebClient;
+  private _io?: IO;
+  private _connectionKey?: string;
 
   constructor(private options: SlackIntegrationOptions) {
-    this.client = {
-      tasks,
-      usesLocalAuth: false,
-      clientFactory: (auth) => {
-        return new WebClient(auth.accessToken);
-      },
-    };
+    this._options = options;
+  }
+
+  get authSource() {
+    return "HOSTED" as const;
   }
 
   get id() {
@@ -31,4 +65,105 @@ export class Slack implements TriggerIntegration<IntegrationClient<WebClient, ty
   get metadata() {
     return { id: "slack", name: "Slack.com" };
   }
+
+  cloneForRun(io: IO, connectionKey: string, auth?: ConnectionAuth) {
+    const slack = new Slack(this._options);
+    slack._io = io;
+    slack._connectionKey = connectionKey;
+    if (!auth) {
+      throw new Error("No auth");
+    }
+    slack._client = new WebClient(auth.accessToken);
+    return slack;
+  }
+
+  runTask<TResult extends RunTaskResult = void>(
+    key: IntegrationTaskKey,
+    callback: (client: WebClient, task: IOTask, io: IO) => Promise<TResult>,
+    options?: RunTaskOptions,
+    errorCallback?: RunTaskErrorCallback
+  ) {
+    if (!this._io) throw new Error("No IO");
+    if (!this._connectionKey) throw new Error("No connection key");
+
+    return this._io.runTask<TResult>(
+      key,
+      (task, io) => {
+        if (!this._client) throw new Error("No client");
+        return callback(this._client, task, io);
+      },
+      { icon: "slack", ...(options ?? {}), connectionKey: this._connectionKey },
+      errorCallback
+    );
+  }
+
+  postMessage(
+    key: IntegrationTaskKey,
+    params: ChatPostMessageArguments
+  ) {
+    return this.runTask(
+      key,
+      async (client) => {
+        try {
+          return client.chat.postMessage(params);
+        } catch (error) {
+          if (isPlatformError(error)) {
+            if (error.data.error === "not_in_channel") {
+              const joinResponse = await this.joinConversation(`Join ${params.channel}`, { channel: params.channel });
+
+              if (joinResponse.ok) {
+                return await client.chat.postMessage(params);
+              }
+            }
+          }
+
+          throw error;
+        }
+      },
+      {
+        name: "Post Message",
+        params,
+        icon: "slack",
+        properties: [
+          {
+            label: "Channel ID",
+            text: params.channel,
+          },
+          ...(params.text ? [{ label: "Message", text: params.text }] : []),
+        ],
+      }
+    );
+  }
+
+  joinConversation(key: IntegrationTaskKey, params: { channel: string} ) {
+    return this.runTask(
+      key,
+      async (client) => {
+        const result = await client.conversations.join(params);
+        return result;
+      }, {
+      name: "Join Channel",
+      params,
+      icon: "slack",
+      properties: [
+        {
+          label: "Channel ID",
+          text: params.channel,
+        },
+      ],
+    }),
+  
 }
+
+}
+
+function isPlatformError(error: unknown): error is WebAPIPlatformError {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "code" in error &&
+    typeof error.code === "string" &&
+    error.code === "slack_webapi_platform_error"
+  );
+}
+
