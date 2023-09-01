f8c578d88ba6900c96e3694c4882cfa3eb3570f9
Matt Aitken <matt@mattaitken.com>
2023-09-01 13:35:40 +0100

    Converted Stripe over

---
+++ integrations/stripe/src/subscriptions.ts
@@ -0,0 +1,49 @@
+import { IntegrationTaskKey } from "@trigger.dev/sdk";
+import { RetrieveSubscriptionParams, RetrieveSubscriptionResponse, StripeRunTask } from "./index";
+import { omit } from "./utils";
+
+export class Subscriptions {
+  runTask: StripeRunTask;
+
+  constructor(runTask: StripeRunTask) {
+    this.runTask = runTask;
+  }
+
+  /**
+   * Retrieves the subscription with the given ID.
+   */
+  retrieve(
+    key: IntegrationTaskKey,
+    params: RetrieveSubscriptionParams
+  ): Promise<RetrieveSubscriptionResponse> {
+    return this.runTask(
+      key,
+      async (client, task) => {
+        const response = await client.subscriptions.retrieve(params.id, omit(params, "id"), {
+          stripeAccount: params.stripeAccount,
+        });
+
+        return response;
+      },
+      {
+        name: "Retrieve Subscription",
+        params,
+        icon: "stripe",
+        properties: [
+          {
+            label: "Subscription ID",
+            text: params.id,
+          },
+          ...(params.stripeAccount
+            ? [
+                {
+                  label: "Stripe Account",
+                  text: params.stripeAccount,
+                },
+              ]
+            : []),
+        ],
+      }
+    );
+  }
+}
