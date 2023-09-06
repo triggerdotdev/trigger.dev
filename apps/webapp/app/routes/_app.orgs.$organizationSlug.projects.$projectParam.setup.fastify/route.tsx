968376a1174d3cf43f41e9fe2841e396744b251e
James Ritchie <james@jamesritchie.co.uk>
2023-09-06 18:30:04 +0100

    New routes for the new frameworks

---
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam.setup.fastify/route.tsx
@@ -0,0 +1,9 @@
+import { SetupNextjs } from "~/components/SetupNextjs";
+
+export default function Page() {
+  return (
+    <>
+      <SetupNextjs />
+    </>
+  );
+}
---
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam.setup.nuxt/route.tsx
@@ -0,0 +1,9 @@
+import { SetupNuxt } from "~/components/SetupNuxt";
+
+export default function Page() {
+  return (
+    <>
+      <SetupNuxt />
+    </>
+  );
+}
