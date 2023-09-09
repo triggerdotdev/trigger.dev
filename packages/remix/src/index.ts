c747dd7e082696029f30a201049df012b68589d6
Chigala <92148630+Chigala@users.noreply.github.com>
2023-09-09 12:05:30 +0100

    updated the remix package to return the action variable

--- packages/remix/src/index.ts
+++ packages/remix/src/index.ts
@@ -1,9 +1,9 @@
-import type { ActionArgs } from "@remix-run/server-runtime";
+import type { ActionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import type { TriggerClient } from "@trigger.dev/sdk";
 
-export function createRemixAdapter(client: TriggerClient) {
-  const Post = async ({ request }: ActionArgs) => {
+export function createRemixRoute(client: TriggerClient) {
+  const action = async ({ request }: ActionArgs) => {
     const response = await client.handleRequest(request);
 
     if (!response) {
@@ -12,4 +12,6 @@ export function createRemixAdapter(client: TriggerClient) {
 
     return json(response.body, { status: response.status });
   };
+  return { action };
+  
 }
