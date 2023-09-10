cc88a1d6c89fa99b9b102fc0f32ad0b78b502bf9
Chigala <92148630+Chigala@users.noreply.github.com>
2023-09-11 00:02:23 +0100

    added the convertToStandardRequest function in the sveltekit package

--- packages/sveltekit/src/index.ts
+++ packages/sveltekit/src/index.ts
@@ -5,13 +5,38 @@ import type { RequestHandler } from "@sveltejs/kit";
 
 export function createSvelteRoute(client: TriggerClient) {
   const POST: RequestHandler = async ({ request }) => {
-    	const response = await client.handleRequest(request);
+    const standardizedRequest = await convertToStandardRequest(request);
+    const response = await client.handleRequest(standardizedRequest);
 
-      if (!response) {
-        return json({ error: "Resource not found" }, { status: 404 });
-      }
+    if (!response) {
+      return json({ error: "Resource not found" }, { status: 404 });
+    }
 
-      return json(response.body, { status: response.status });
+    return json(response.body, { status: response.status });
   };
   return { POST };
+}
+
+async function convertToStandardRequest(req: Request): Promise<Request> {
+  // Prepare the request to be a fetch-compatible Request object:
+  const requestHeaders = req.headers;
+  const requestMethod = req.method;
+  const responseHeaders = Object.create(null);
+
+  for (const [headerName, headerValue] of requestHeaders.entries()) {
+    responseHeaders[headerName] = headerValue;
+  }
+
+  // Create a new Request object to be passed to the TriggerClient
+  // where we pass the clone the incoming request metadata such as
+  // headers, method, body.
+  const request = new Request("https://svelte/api/trigger", {
+    headers: responseHeaders,
+    method: requestMethod,
+    // @ts-ignore
+    body: req.body ? req.body : req,
+    duplex: "half",
+  });
+
+  return request;
 }
