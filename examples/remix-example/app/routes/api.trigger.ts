4b18ba357f62b72ac6adf9c698ab644927cd03ab
Matt Aitken <matt@mattaitken.com>
2023-09-11 17:51:19 +0100

    Update api.trigger.ts

--- examples/remix-example/app/routes/api.trigger.ts
+++ examples/remix-example/app/routes/api.trigger.ts
@@ -2,10 +2,8 @@ import { createRemixRoute } from "@trigger.dev/remix";
 
 import { client } from "~/trigger";
 
-//TODO: look for a better way to run side effects
-//import your jobs
-import { configureJob } from "~/jobs/example";
-
-configureJob();
+// Remix will automatically strip files with side effects
+// So you need to *export* your Job definitions like this:
+export * from "~/jobs/example.server";
 
 export const { action } = createRemixRoute(client);
