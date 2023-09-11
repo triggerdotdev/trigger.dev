951b19243d1bdf79348e0a0dfa760736fc0dfd85
Chigala <92148630+Chigala@users.noreply.github.com>
2023-09-11 06:40:16 +0100

    added a wait function to the example job

--- examples/remix-example/app/jobs/example.ts
+++ examples/remix-example/app/jobs/example.ts
@@ -4,13 +4,14 @@ import { client } from "~/trigger";
 // your first job
 export function configureJob () {
 client.defineJob({
-  id: "remix-test-job",
-  name: "remix test job",
+  id: "remix-job",
+  name: "remix-wait-job",
   version: "0.0.1",
   trigger: eventTrigger({
     name: "remix.test",
   }),
   run: async (payload, io, ctx) => {
+    await io.wait("waiting", 5)
     await io.logger.info("Hello world!", { payload });
 
     return {
