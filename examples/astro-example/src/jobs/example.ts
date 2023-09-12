bd19e87d62b4ff367436e5ea267988e1624dfbdf
Chigala <92148630+Chigala@users.noreply.github.com>
2023-09-12 09:52:10 +0100

    moved trigger.ts file and job folder to src directory

--- examples/astro-example/src/jobs/example.ts
+++ examples/astro-example/src/jobs/example.ts
@@ -3,8 +3,8 @@ import { client } from "../trigger";
 
 // your first job
   client.defineJob({
-    id: "astro-test-2",
-    name: "astro Job two",
+    id: "astro-test",
+    name: "astro test Job",
     version: "0.0.1",
     trigger: eventTrigger({
       name: "test.event",
