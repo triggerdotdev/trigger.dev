c85f9539f4f58051e0a3dc12cdaf0941bcda9205
Chigala <92148630+Chigala@users.noreply.github.com>
2023-09-12 09:54:32 +0100

    updated to use aliases

--- examples/astro-example/src/pages/api/trigger.ts
+++ examples/astro-example/src/pages/api/trigger.ts
@@ -1,5 +1,7 @@
 import { createAstroRoute } from "@trigger.dev/astro";
-import { client } from "../../../trigger";
-import "../../../jobs/example";
+import {client} from "@/trigger"
+
+//import your jobs
+import "@/jobs/example"
 
 export const { POST } = createAstroRoute(client);
