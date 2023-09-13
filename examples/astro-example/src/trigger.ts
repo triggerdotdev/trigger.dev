2780a2b71e8bb79481dd20b352f145b2887dbfdd
Matt Aitken <matt@mattaitken.com>
2023-09-13 17:17:31 +0100

    Added the TRIGGER_API_URL to the trigger client in the example

--- examples/astro-example/src/trigger.ts
+++ examples/astro-example/src/trigger.ts
@@ -2,5 +2,6 @@ import { TriggerClient } from "@trigger.dev/sdk";
 
 export const client = new TriggerClient({
   id: "astro-test",
-  apiKey: import.meta.env.TRIGGER_API_KEY
+  apiUrl: import.meta.env.TRIGGER_API_URL,
+  apiKey: import.meta.env.TRIGGER_API_KEY,
 });
