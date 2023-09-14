98d76cef359017b80b9fdfa46de028adbd7b8096
Ram <ramk97095@gmail.com>
2023-09-14 10:46:42 +0530

    feat: Added example for openAI function call

--- examples/job-catalog/src/openai.ts
+++ examples/job-catalog/src/openai.ts
@@ -10,6 +10,59 @@ export const client = new TriggerClient({
   ioLogLocalEnabled: true,
 });
 
+const functions = [
+  {
+    "name": "get_current_affairs",
+    "description": "Get the current affairs from google",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "query": {
+          "type": "string",
+          "description": "query to search for google result",
+        }
+      },
+      "required": ["query"],
+    },
+  }
+]
+
+type ChatMessage = {
+  role: 'system' | 'user' | 'assistant' | 'function';
+  content: string;
+  name?: string;
+  function_call?: () => void
+};
+
+const chatMessages: ChatMessage[] = [
+  {
+    role: "user",
+    content: "who won fifa world cup 2023 with some related links to read more about it",
+  }
+]
+
+const get_current_affairs = async (query: string) => {
+  const params = {
+    key: process.env["GOOGLE_CUSTOM_SEARCH_API_KEY"] || '',
+    cx: process.env["googleCSEId"] || '',
+    q: query
+  }
+  const queryString = new URLSearchParams(params).toString();
+  const apiUrl = `https://www.googleapis.com/customsearch/v1?${queryString}`;
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const results = data?.items?.slice(0, 5)?.map((item: any) => ({
+    title: item.title,
+    link: item.link,
+    snippet: item.snippet,
+  })) || []
+  return JSON.stringify(results)
+}
+
+const available_functions: { [key: string]: (query: string) => Promise<string> } = {
+  get_current_affairs: get_current_affairs,
+};
+
 const openai = new OpenAI({
   id: "openai",
   apiKey: process.env["OPENAI_API_KEY"]!,
@@ -74,6 +127,38 @@ client.defineJob({
       model: "text-embedding-ada-002",
       input: "The food was delicious and the waiter...",
     });
+
+    //Function call example for OpenAI
+    const createCompletion = async () => {
+      const chatCompletion = await io.openai.createChatCompletion("chat-completion", {
+        model: "gpt-3.5-turbo",
+        messages: chatMessages,
+        functions,
+        function_call: "auto"
+      });
+      return chatCompletion
+    }
+
+    const chatCompletion = await createCompletion();
+
+    // @ts-ignore
+    const response = chatCompletion?.data?.choices[0].message;
+
+    if ('function_call' in response) {
+      const function_name: string = "get_current_affairs";
+      const function_call = available_functions[function_name];
+      const args = JSON.parse(response['function_call']['arguments'])
+      // @ts-ignore
+      const fn_response = await function_call(...Object.values(args));
+      chatMessages.push({
+        role: 'function',
+        name: function_name,
+        content: fn_response
+      })
+      const finalCompletion = await createCompletion();
+      // @ts-ignore
+      await io.logger.info(finalCompletion?.data?.choices[0].message);
+    }
   },
 });
 
