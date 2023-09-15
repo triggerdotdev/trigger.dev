a08522fc114757fef3528a62f14fb0221d25ca12
Chigala <92148630+Chigala@users.noreply.github.com>
2023-09-15 09:03:55 +0100

    updated the tsconfig and svelte.config file

--- examples/svelte-example/svelte.config.js
+++ examples/svelte-example/svelte.config.js
@@ -13,6 +13,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
+			$trigger: 'src/trigger',
+			'$jobs/*': 'src/jobs/*',
 			'@trigger.dev/sveltekit': '../../packages/sveltekit/src/index',
 			'@trigger.dev/sveltekit/*': '../../packages/sveltekit/src/*',
 			'@trigger.dev/sdk': '../../packages/trigger-sdk/src/index',
