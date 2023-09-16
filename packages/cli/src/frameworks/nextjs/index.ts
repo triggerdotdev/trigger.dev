8ddf9118930f2aa38c607adef3e4c796f335cd9c
Matt Aitken <matt@mattaitken.com>
2023-09-16 17:23:15 +0100

    Next.js App routing support, with common files logic shared

--- packages/cli/src/frameworks/nextjs/index.ts
+++ packages/cli/src/frameworks/nextjs/index.ts
@@ -58,13 +58,7 @@ export class NextJs implements Framework {
     if (nextJsDir === "pages") {
       await createTriggerPageRoute(routeDir, options.endpointSlug, options.typescript, pathAlias);
     } else {
-      await createTriggerAppRoute(
-        path,
-        routeDir,
-        options.endpointSlug,
-        options.typescript,
-        usesSrcDir
-      );
+      await createTriggerAppRoute(routeDir, options.endpointSlug, options.typescript, pathAlias);
     }
   }
 
@@ -136,6 +130,42 @@ async function createTriggerPageRoute(
   }
   logger.success(`✅ Created API route at ${apiRoutePath}`);
 
+  await createJobsAndTriggerFile(path, endpointSlug, fileExtension, pathAlias, templatesDir);
+}
+
+async function createTriggerAppRoute(
+  path: string,
+  endpointSlug: string,
+  isTypescriptProject: boolean,
+  pathAlias: string | undefined
+) {
+  const templatesDir = pathModule.join(templatesPath(), "nextjs");
+  const fileExtension = isTypescriptProject ? ".ts" : ".js";
+
+  //app/api/trigger/route.js or src/app/api/trigger/route.js
+  const apiRoutePath = pathModule.join(path, "app", "api", "trigger", `route${fileExtension}`);
+  const apiRouteResult = await createFileFromTemplate({
+    templatePath: pathModule.join(templatesDir, "appApiRoute.js"),
+    replacements: {
+      routePathPrefix: pathAlias ? pathAlias + "/" : "../../",
+    },
+    outputPath: apiRoutePath,
+  });
+  if (!apiRouteResult.success) {
+    throw new Error("Failed to create API route file");
+  }
+  logger.success(`✅ Created API route at ${apiRoutePath}`);
+
+  await createJobsAndTriggerFile(path, endpointSlug, fileExtension, pathAlias, templatesDir);
+}
+
+async function createJobsAndTriggerFile(
+  path: string,
+  endpointSlug: string,
+  fileExtension: string,
+  pathAlias: string | undefined,
+  templatesDir: string
+) {
   //trigger.js or src/trigger.js
   const triggerFilePath = pathModule.join(path, `trigger${fileExtension}`);
   const triggerResult = await createFileFromTemplate({
@@ -180,124 +210,4 @@ async function createTriggerPageRoute(
     throw new Error("Failed to create jobs index file");
   }
   logger.success(`✅ Created jobs index at ${jobsIndexFilePath}`);
-}
-
-async function createTriggerAppRoute(
-  projectPath: string,
-  path: string,
-  endpointSlug: string,
-  isTypescriptProject: boolean,
-  usesSrcDir = false
-) {
-  const pathAlias = getPathAlias({ projectPath, isTypescriptProject, usesSrcDir });
-
-  const extension = isTypescriptProject ? ".ts" : ".js";
-  const triggerFileName = `trigger${extension}`;
-  const examplesFileName = `examples${extension}`;
-  const examplesIndexFileName = `index${extension}`;
-  const routeFileName = `route${extension}`;
-
-  const routePathPrefix = pathAlias ? pathAlias + "/" : "../../../";
-
-  const routeContent = `
-import { createAppRoute } from "@trigger.dev/nextjs";
-import { client } from "${routePathPrefix}trigger";
-
-
-import "${routePathPrefix}jobs";
-
-//this route is used to send and receive data with Trigger.dev
-export const { POST, dynamic } = createAppRoute(client);
-`;
-
-  const triggerContent = `
-import { TriggerClient } from "@trigger.dev/sdk";
-
-export const client = new TriggerClient({
-  id: "${endpointSlug}",
-  apiKey: process.env.TRIGGER_API_KEY,
-  apiUrl: process.env.TRIGGER_API_URL,
-});
-  `;
-
-  const jobsPathPrefix = pathAlias ? pathAlias + "/" : "../";
-
-  const jobsContent = `
-import { eventTrigger } from "@trigger.dev/sdk";
-import { client } from "${jobsPathPrefix}trigger";
-
-// Your first job
-// This Job will be triggered by an event, log a joke to the console, and then wait 5 seconds before logging the punchline
-client.defineJob({
-  // This is the unique identifier for your Job, it must be unique across all Jobs in your project
-  id: "example-job",
-  name: "Example Job: a joke with a delay",
-  version: "0.0.1",
-  // This is triggered by an event using eventTrigger. You can also trigger Jobs with webhooks, on schedules, and more: https://trigger.dev/docs/documentation/concepts/triggers/introduction
-  trigger: eventTrigger({
-    name: "example.event",
-  }),
-  run: async (payload, io, ctx) => {
-    // This logs a message to the console
-    await io.logger.info("🧪 Example Job: a joke with a delay");
-    await io.logger.info("How do you comfort a JavaScript bug?");
-    // This waits for 5 seconds, the second parameter is the number of seconds to wait, you can add delays of up to a year
-    await io.wait("Wait 5 seconds for the punchline...", 5);
-    await io.logger.info("You console it! 🤦");
-    await io.logger.info(
-      "✨ Congratulations, You just ran your first successful Trigger.dev Job! ✨"
-    );
-    // To learn how to write much more complex (and probably funnier) Jobs, check out our docs: https://trigger.dev/docs/documentation/guides/create-a-job
-  },
-});
-
-`;
-
-  const examplesIndexContent = `
-// import all your job files here
-
-export * from "./examples"
-`;
-
-  const directories = pathModule.join(path, "app", "api", "trigger");
-  await fs.mkdir(directories, { recursive: true });
-
-  const fileExists = await pathExists(pathModule.join(directories, routeFileName));
-
-  if (fileExists) {
-    logger.info("Skipping creation of app route because it already exists");
-    return;
-  }
-
-  await fs.writeFile(pathModule.join(directories, routeFileName), routeContent);
-
-  logger.success(
-    `✅ Created app route at ${usesSrcDir ? "src/" : ""}app/api/${removeFileExtension(
-      triggerFileName
-    )}/${routeFileName}`
-  );
-
-  const triggerFileExists = await pathExists(pathModule.join(path, triggerFileName));
-
-  if (!triggerFileExists) {
-    await fs.writeFile(pathModule.join(path, triggerFileName), triggerContent);
-
-    logger.success(`✅ Created trigger client at ${usesSrcDir ? "src/" : ""}${triggerFileName}`);
-  }
-
-  const exampleDirectories = pathModule.join(path, "jobs");
-  await fs.mkdir(exampleDirectories, { recursive: true });
-
-  const exampleFileExists = await pathExists(pathModule.join(exampleDirectories, examplesFileName));
-
-  if (!exampleFileExists) {
-    await fs.writeFile(pathModule.join(exampleDirectories, examplesFileName), jobsContent);
-
-    await fs.writeFile(
-      pathModule.join(exampleDirectories, examplesIndexFileName),
-      examplesIndexContent
-    );
-
-    logger.success(`✅ Created example job at ${usesSrcDir ? "src/" : ""}jobs/examples.ts`);
-  }
 }
