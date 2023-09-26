a196a86a5b830820f02d8cd8cc49d215a4f6388c
nicktrn <55853254+nicktrn@users.noreply.github.com>
2023-09-26 09:48:52 +0000

    Basic job catalog example

---
+++ references/job-catalog/src/replicate.ts
@@ -0,0 +1,39 @@
+import { createExpressServer } from "@trigger.dev/express";
+import { TriggerClient, eventTrigger } from "@trigger.dev/sdk";
+import { Replicate } from "@trigger.dev/replicate";
+import { z } from "zod";
+
+export const client = new TriggerClient({
+  id: "job-catalog",
+  apiKey: process.env["TRIGGER_API_KEY"],
+  apiUrl: process.env["TRIGGER_API_URL"],
+  verbose: false,
+  ioLogLocalEnabled: true,
+});
+
+const replicate = new Replicate({
+  id: "replicate",
+  apiKey: process.env["REPLICATE_API_KEY"]!,
+});
+
+client.defineJob({
+  id: "replicate-create-prediction",
+  name: "Replicate - Create Prediction",
+  version: "0.1.0",
+  integrations: { replicate },
+  trigger: eventTrigger({
+    name: "replicate.predict",
+    schema: z.object({
+      prompt: z.string(),
+      version: z.string(),
+    }),
+  }),
+  run: async (payload, io, ctx) => {
+    return io.replicate.predictions.createAndWaitForCompletion("await-prediction", {
+      version: payload.version,
+      input: { prompt: payload.prompt },
+    });
+  },
+});
+
+createExpressServer(client);
