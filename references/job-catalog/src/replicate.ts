6f739cac6a3f176363608e7597107a0228770310
nicktrn <55853254+nicktrn@users.noreply.github.com>
2023-09-29 07:20:46 +0000

    Image processing jobs

--- references/job-catalog/src/replicate.ts
+++ references/job-catalog/src/replicate.ts
@@ -17,22 +17,103 @@ const replicate = new Replicate({
 });
 
 client.defineJob({
-  id: "replicate-create-prediction",
-  name: "Replicate - Create Prediction",
+  id: "replicate-forge-image",
+  name: "Replicate - Forge Image",
   version: "0.1.0",
   integrations: { replicate },
   trigger: eventTrigger({
-    name: "replicate.predict",
+    name: "replicate.bad.forgery",
     schema: z.object({
-      prompt: z.string(),
-      version: z.string(),
+      imageUrl: z
+        .string()
+        .url()
+        .default("https://trigger.dev/blog/supabase-integration/postgres-meme.png"),
     }),
   }),
   run: async (payload, io, ctx) => {
-    return io.replicate.predictions.createAndAwait("await-prediction", {
-      version: payload.version,
-      input: { prompt: payload.prompt },
+    const blipVersion = "2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746";
+    const sdVersion = "ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4";
+
+    const blipPrediction = await io.replicate.run("caption-image", {
+      identifier: `salesforce/blip:${blipVersion}`,
+      input: {
+        image: payload.imageUrl,
+      },
     });
+
+    if (typeof blipPrediction.output !== "string") {
+      throw new Error(`Expected string output, got ${typeof blipPrediction.output}`);
+    }
+
+    const caption = blipPrediction.output.replace("Caption: ", "");
+
+    const sdPrediction = await io.replicate.predictions.createAndAwait("draw-image", {
+      version: sdVersion,
+      input: {
+        prompt: caption,
+      },
+    });
+
+    return {
+      caption,
+      output: sdPrediction.output,
+    };
+  },
+});
+
+client.defineJob({
+  id: "replicate-cinematic-prompt",
+  name: "Replicate - Cinematic Prompt",
+  version: "0.1.0",
+  integrations: { replicate },
+  trigger: eventTrigger({
+    name: "replicate.cinematic",
+    schema: z.object({
+      prompt: z.string().default("rick astley riding a harley through post-apocalyptic miami"),
+      version: z
+        .string()
+        .default("af1a68a271597604546c09c64aabcd7782c114a63539a4a8d14d1eeda5630c33"),
+    }),
+  }),
+  run: async (payload, io, ctx) => {
+    const prediction = await io.replicate.predictions.createAndAwait("await-prediction", {
+      version: payload.version,
+      input: {
+        prompt: `${payload.prompt}, cinematic, 70mm, anamorphic, bokeh`,
+        width: 1280,
+        height: 720,
+      },
+    });
+    return prediction.output;
+  },
+});
+
+client.defineJob({
+  id: "replicate-pagination",
+  name: "Replicate - Pagination",
+  version: "0.1.0",
+  integrations: {
+    replicate,
+  },
+  trigger: eventTrigger({
+    name: "replicate.paginate",
+  }),
+  run: async (payload, io, ctx) => {
+    // getAll - returns an array of all results (uses paginate internally)
+    const all = await io.replicate.getAll(io.replicate.predictions.list, "get-all");
+
+    // paginate - returns an async generator, useful to process one page at a time
+    for await (const predictions of io.replicate.paginate(
+      io.replicate.predictions.list,
+      "paginate-all"
+    )) {
+      await io.logger.info("stats", {
+        total: predictions.length,
+        versions: predictions.map((p) => p.version),
+      });
+    }
+
+    return { count: all.length };
   },
 });
 
