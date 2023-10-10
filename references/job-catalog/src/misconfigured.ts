5ed4ba15540e465d54f1f8b0d530356388f6177d
Matt Aitken <matt@mattaitken.com>
2023-10-10 13:02:07 +0100

    Created a job-catalog file for misconfigured Jobs that should error when running the CLI

---
+++ references/job-catalog/src/misconfigured.ts
@@ -0,0 +1,24 @@
+import { createExpressServer } from "@trigger.dev/express";
+import { TriggerClient, eventTrigger, intervalTrigger } from "@trigger.dev/sdk";
+import { z } from "zod";
+
+export const client = new TriggerClient({
+  id: "job-catalog",
+  apiKey: process.env["TRIGGER_API_KEY"],
+  apiUrl: process.env["TRIGGER_API_URL"],
+  verbose: false,
+  ioLogLocalEnabled: true,
+});
+
+// This job is misconfigured because it has an interval trigger with a 1 second interval, minimum is 60 seconds
+client.defineJob({
+  id: "disallowed-interval",
+  name: "Disallowed Interval",
+  version: "0.0.2",
+  trigger: intervalTrigger({
+    seconds: 1,
+  }),
+  run: async (payload, io, ctx) => {},
+});
+
+createExpressServer(client);
