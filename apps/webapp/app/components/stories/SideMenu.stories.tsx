6889f0410560e3f76f5157d17f31d6a6922a9348
James Ritchie <james@jamesritchie.co.uk>
2023-10-12 11:55:16 +0100

    Added a storybook story for the side menu

--- apps/webapp/app/components/stories/SideMenu.stories.tsx
+++ apps/webapp/app/components/stories/SideMenu.stories.tsx
@@ -1,21 +1,28 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { withDesign } from "storybook-addon-designs";
+import { SideMenu } from "../navigation/SideMenu";
 
-const meta: Meta<typeof SelectMenu> = {
-  title: "Primitives/Menus",
-  component: SelectMenu,
+const meta: Meta<typeof SideMenuV2> = {
+  title: "Compound/SideMenu",
+  component: SideMenuV2,
   decorators: [withDesign],
-  tags: ["autodocs"],
 };
 
 export default meta;
 
-type Story = StoryObj<typeof SelectMenu>;
+type Story = StoryObj<typeof SideMenuV2>;
 
 export const Selects: Story = {
-  render: (args) => <SelectMenu />,
+  render: (args) => <SideMenuV2 />,
 };
 
-function SelectMenu() {
-  return <div>sdfsdf</div>;
+function SideMenuV2() {
+  return (
+    <div className="h-screen w-full">
+      <div className="grid h-full grid-cols-[220px_auto]">
+        <SideMenu />
+        <div className="h-full w-full bg-midnight-950"></div>
+      </div>
+    </div>
+  );
 }
