cbd871a57630b72375fafdea71e794d6f13597cc
James Ritchie <james@jamesritchie.co.uk>
2023-10-13 14:29:01 +0100

    Added a side menu header and main org header

--- apps/webapp/app/components/navigation/SideMenu.tsx
+++ apps/webapp/app/components/navigation/SideMenu.tsx
@@ -1,14 +1,12 @@
+import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
 import { cn } from "~/utils/cn";
-import { accountPath } from "~/utils/pathBuilder";
-import { UserProfilePhoto } from "../UserProfilePhoto";
+import { LogoIcon } from "../LogoIcon";
 import { NavLinkButton } from "../primitives/Buttons";
-import { NamedIcon, type IconNames } from "../primitives/NamedIcon";
-import { SimpleTooltip } from "../primitives/Tooltip";
 import { Icon } from "../primitives/Icon";
-
-export function SideMenuContainer({ children }: { children: React.ReactNode }) {
-  return <div className="flex h-full w-full justify-stretch overflow-hidden">{children}</div>;
-}
+import { type IconNames } from "../primitives/NamedIcon";
+import { Paragraph } from "../primitives/Paragraph";
+import { SimpleTooltip } from "../primitives/Tooltip";
+import { UserAvatar } from "../UserProfilePhoto";
 
 export function SideMenu() {
   return (
@@ -18,8 +16,10 @@ export function SideMenu() {
       )}
     >
       <div className="flex h-full flex-col justify-between">
-        <div className="space-y-8 p-1">
-          <div className="flex flex-col gap-y-1">
+        <div className="overflow-y-auto px-1 pb-0">
+          <SideMenuOrgHeader />
+          <div className={cn("mb-8 mt-2 flex flex-col gap-y-1")}>
+            <SideMenuHeader title="My Project 1" />
             <SideMenuItem name="Jobs" icon="job" count={33} to="" data-action="jobs" />
             <SideMenuItem name="Runs" icon="integration" to="" data-action="runs" hasWarning />
             <SideMenuItem name="Events" icon="trigger" to="" data-action="events" />
@@ -34,6 +34,7 @@ export function SideMenu() {
             <SideMenuItem name="API Keys" icon="environment" to="" data-action="api keys" />
           </div>
           <div className="flex flex-col gap-1">
+            <SideMenuHeader title="My Org 1" />
             <SideMenuItem
               name="Integrations"
               icon="integration"
@@ -69,6 +70,33 @@ export function SideMenu() {
           />
           <SideMenuItem name="Help & Feedback" icon="log" to="" data-action="help & feedback" />
         </div>
+      </div>
+    </div>
+  );
+}
+
+function SideMenuOrgHeader() {
+  return (
+    <div className="sticky top-0 flex items-center justify-between bg-background px-2 py-3">
+      <div className="flex items-center gap-2">
+        <LogoIcon className="h-4 w-4" />
+        <Paragraph variant="extra-small">My Org 1</Paragraph>
+      </div>
+      <div>
+        <UserAvatar className="h-5 w-5 text-slate-600" />
+      </div>
+    </div>
+  );
+}
+
+function SideMenuHeader({ title, menuItems }: { title: string; menuItems?: React.ReactNode }) {
+  return (
+    <div className="group flex items-center justify-between px-1.5">
+      <Paragraph variant="extra-extra-small/caps" className="cursor-default text-slate-500">
+        {title}
+      </Paragraph>
+      <div>
+        <EllipsisHorizontalIcon className="trasition h-4 w-4 text-slate-500 group-hover:text-bright" />
       </div>
     </div>
   );
