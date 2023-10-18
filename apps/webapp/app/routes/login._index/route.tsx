dafbc230a713c470956f4772613a5c424a79701f
James Ritchie <james@jamesritchie.co.uk>
2023-10-18 10:22:22 +0100

    Improved the layout and style of the page

--- apps/webapp/app/routes/login._index/route.tsx
+++ apps/webapp/app/routes/login._index/route.tsx
@@ -11,11 +11,12 @@ import {
   useTypedLoaderData,
 } from "remix-typedjson";
 import { LogoIcon } from "~/components/LogoIcon";
+import { LogoType } from "~/components/LogoType";
 import { AppContainer, MainCenteredContainer } from "~/components/layout/AppLayout";
 import { Button, LinkButton } from "~/components/primitives/Buttons";
 import { Fieldset } from "~/components/primitives/Fieldset";
 import { FormTitle } from "~/components/primitives/FormTitle";
-import { Header3 } from "~/components/primitives/Headers";
+import { Header1, Header3 } from "~/components/primitives/Headers";
 import { Icon } from "~/components/primitives/Icon";
 import { NamedIcon } from "~/components/primitives/NamedIcon";
 import { Paragraph } from "~/components/primitives/Paragraph";
@@ -74,10 +75,10 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 const layout =
-  "group grid place-items-center p-4 text-center overflow-hidden grayscale hover:opacity-100 hover:grayscale-0 transition";
-const border = "border border-border/50 hover:border-border rounded-lg transition";
-const login = "col-span-3 row-span-4";
-const logos = "h-20 w-20 opacity-20 group-hover:opacity-100 transition";
+  "group grid place-items-center p-4 text-center overflow-hidden hover:opacity-100 hover:grayscale-0 transition";
+const gridCell = "hover:bg-midnight-850 rounded-lg transition bg-midnight-850/40";
+const logos =
+  "h-20 w-20 opacity-20 group-hover:opacity-100 transition grayscale group-hover:grayscale-0";
 const tall = "row-span-2";
 const wide = "col-span-2";
 const wider = "col-span-3 row-span-2";
@@ -87,58 +88,52 @@ export default function LoginPage() {
   const data = useTypedLoaderData<typeof loader>();
 
   return (
-    <main className="grid h-full w-full grid-flow-row grid-cols-8 grid-rows-6 gap-4 p-4">
-      <div className={cn(layout, border, wide)}>1</div>
-      <div className={cn(layout, border)}>
-        <Icon icon="linear" className={logos} />
-      </div>
-      <div className={cn(layout, border, mediumSquare)}>3</div>
-      <LoginTooltip
-        side="bottom"
-        content="Use our Supabase Integration in your Job to react to changes in your database."
-      >
-        <div className={cn(layout, border)}>
-          <Icon icon="supabase" className={logos} />
-        </div>
-      </LoginTooltip>
-      <div className={cn(layout, border, mediumSquare)}>5</div>
-      <div className={cn(login)}>
+    <main className="grid h-full w-full grid-cols-12">
+      <div className="border-midnight-750 z-10 col-span-5 border-r bg-midnight-850">
         <LoginForm />
       </div>
-      <LoginTooltip
-        side="bottom"
-        content="Trigger payments, emails, subscription upgrades and more with our Stripe Integration."
-      >
-        <div className={cn("", layout, border)}>
-          <Icon icon="stripe" className={logos} />
-        </div>
-      </LoginTooltip>
-      <LoginTooltip side="top" content="❤️ Loved by developers">
-        <div className={cn(layout, border, wider)}>
-          <div>
-            <Header3 className="relative text-2xl font-normal leading-8 text-dimmed before:absolute before:-top-10 before:left-2 before:-z-10 before:text-8xl before:text-indigo-500 before:opacity-20 before:content-['❝']">
-              Trigger.dev is redefining background jobs for modern developers.
-            </Header3>
-            <Paragraph variant="small" className="mt-4 text-slate-600">
-              Paul Copplestone, Supabase
-            </Paragraph>
+      <div className="col-span-7 grid h-full w-full grid-flow-row grid-cols-5 grid-rows-6 gap-4 p-4">
+        <div className={cn(layout, gridCell, mediumSquare)}>1</div>
+        <LoginTooltip
+          side="bottom"
+          content="Use our Supabase Integration in your Job to react to changes in your database."
+        >
+          <div className={cn(layout, gridCell)}>
+            <Icon icon="supabase" className={logos} />
           </div>
+        </LoginTooltip>
+        <div className={cn(layout, gridCell, mediumSquare)}>4</div>
+        <LoginTooltip
+          side="bottom"
+          content="Trigger payments, emails, subscription upgrades and more with our Stripe Integration."
+        >
+          <div className={cn("", layout, gridCell)}>
+            <Icon icon="stripe" className={logos} />
+          </div>
+        </LoginTooltip>
+        <LoginTooltip side="top" content="❤️ Loved by developers">
+          <div className={cn(layout, gridCell, wider)}>
+            <div className="p-4">
+              <Header3 className="relative text-2xl font-normal leading-8 text-dimmed before:absolute before:-top-10 before:left-2 before:-z-10 before:text-8xl before:text-indigo-500 before:opacity-20 before:content-['❝']">
+                Trigger.dev is redefining background jobs for modern developers.
+              </Header3>
+              <Paragraph variant="small" className="mt-4 text-slate-600">
+                Paul Copplestone, Supabase
+              </Paragraph>
+            </div>
+          </div>
+        </LoginTooltip>
+        <div className={cn(layout, gridCell)}>
+          <Icon icon="airtable" className={logos} />
         </div>
-      </LoginTooltip>
-      <div className={cn(layout, border)}>
-        <Icon icon="airtable" className={logos} />
+        <div className={cn(layout, gridCell)}>
+          <Icon icon="typeform" className={logos} />
+        </div>
+        <div className={cn(layout, gridCell, mediumSquare)}>8</div>
+        <div className={cn(layout, gridCell, tall)}>9</div>
+        <div className={cn(layout, gridCell, mediumSquare)}>10</div>
+        <div className={cn(layout, gridCell, wide)}>11</div>
       </div>
-      <div className={cn(layout, border)}>
-        <Icon icon="typeform" className={logos} />
-      </div>
-      <div className={cn(layout, border, mediumSquare)}>11</div>
-      <div className={cn(layout, border, tall)}>12</div>
-      <div className={cn(layout, border, mediumSquare)}>13</div>
-      <div className={cn(layout, border)}>
-        <Icon icon="resend" className={logos} />
-      </div>
-      <div className={cn(layout, border, wide)}>15</div>
-      <div className={cn(layout, border, wide)}>16</div>
     </main>
   );
 }
@@ -167,59 +162,68 @@ function LoginTooltip({
 function LoginForm() {
   const data = useTypedLoaderData<typeof loader>();
   return (
-    <div className="flex h-full items-center justify-center">
-      <Form
-        action={`/auth/github${data.redirectTo ? `?redirectTo=${data.redirectTo}` : ""}`}
-        method="post"
-      >
-        <div className="flex flex-col items-center gap-y-6">
-          <a href="https://trigger.dev">
-            <LogoIcon className="mb-4 h-16 w-16" />
-          </a>
-          <FormTitle divide={false} title="Welcome to Trigger.dev" className="mb-2 pb-0" />
-          <Paragraph variant="small" className="mb-4">
-            Create an account or login
-          </Paragraph>
-          <Fieldset>
-            <div className="flex flex-col gap-y-2">
-              {data.showGithubAuth && (
-                <Button
-                  type="submit"
-                  variant="primary/large"
-                  fullWidth
-                  data-action="continue with github"
-                >
-                  <NamedIcon name={"github"} className={"mr-1.5 h-4 w-4"} />
-                  Continue with GitHub
-                </Button>
-              )}
-              <LinkButton
-                to="/login/magic"
-                variant="secondary/large"
-                fullWidth
-                data-action="continue with email"
-              >
-                <NamedIcon
-                  name={"envelope"}
-                  className={"mr-1.5 h-4 w-4 text-dimmed transition group-hover:text-bright"}
-                />
-                Continue with Email
-              </LinkButton>
+    <div className="h-full p-6">
+      <div className="flex items-center justify-between">
+        <a href="https://trigger.dev">
+          <LogoType className="w-36" />
+        </a>
+        <LinkButton to="https://trigger.dev/docs" variant={"secondary/small"} LeadingIcon="docs">
+          Documentation
+        </LinkButton>
+      </div>
+      <div className="flex h-full items-center justify-center">
+        <Form
+          action={`/auth/github${data.redirectTo ? `?redirectTo=${data.redirectTo}` : ""}`}
+          method="post"
+        >
+          <div className="flex flex-col items-center gap-y-6">
+            <div>
+              <Header1 className="pb-4 font-normal lg:text-3xl">Welcome</Header1>
+              <Paragraph variant="small" className="mb-6">
+                Create an account or login
+              </Paragraph>
+              <Fieldset>
+                <div className="flex flex-col gap-y-2">
+                  {data.showGithubAuth && (
+                    <Button
+                      type="submit"
+                      variant="primary/large"
+                      fullWidth
+                      data-action="continue with github"
+                    >
+                      <NamedIcon name={"github"} className={"mr-1.5 h-4 w-4"} />
+                      Continue with GitHub
+                    </Button>
+                  )}
+                  <LinkButton
+                    to="/login/magic"
+                    variant="secondary/large"
+                    fullWidth
+                    data-action="continue with email"
+                  >
+                    <NamedIcon
+                      name={"envelope"}
+                      className={"mr-1.5 h-4 w-4 text-dimmed transition group-hover:text-bright"}
+                    />
+                    Continue with Email
+                  </LinkButton>
+                </div>
+                <Paragraph variant="extra-small" className="mt-2 text-center">
+                  By signing up you agree to our{" "}
+                  <TextLink href="https://trigger.dev/legal" target="_blank">
+                    terms
+                  </TextLink>
+                  {" "}and{" "}
+                  <TextLink href="https://trigger.dev/legal/privacy" target="_blank">
+                    privacy
+                  </TextLink>
+                  {" "}policy.
+                </Paragraph>
+              </Fieldset>
             </div>
-            <Paragraph variant="extra-small" className="mt-2 text-center">
-              By signing up you agree to our{" "}
-              <TextLink href="https://trigger.dev/legal" target="_blank">
-                terms
-              </TextLink>
-              {" "}and{" "}
-              <TextLink href="https://trigger.dev/legal/privacy" target="_blank">
-                privacy
-              </TextLink>
-              {" "}policy.
-            </Paragraph>
-          </Fieldset>
-        </div>
-      </Form>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
