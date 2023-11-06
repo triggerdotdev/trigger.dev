ed214b4d0ac34004b04a26ca974318f46b6cd3e5
Matt Aitken <matt@mattaitken.com>
2023-11-06 14:40:19 +0000

    Always try get the arrayBuffer for an HTTP request, catch and use undefined as the default

--- apps/webapp/app/utils/createHttpSourceRequest.ts
+++ apps/webapp/app/utils/createHttpSourceRequest.ts
@@ -3,12 +3,15 @@ import { requestUrl } from "./requestUrl.server";
 
 export async function createHttpSourceRequest(request: Request): Promise<HttpSourceRequest> {
   const url = requestUrl(request);
+  let arrayBuffer: ArrayBuffer | undefined;
+  try {
+    arrayBuffer = await request.arrayBuffer();
+  } catch (e) {}
+
   return {
     headers: Object.fromEntries(request.headers) as Record<string, string>,
     url: url.href,
     method: request.method,
-    rawBody: ["POST", "PUT", "PATCH"].includes(request.method)
-      ? Buffer.from(await request.arrayBuffer())
-      : undefined,
+    rawBody: arrayBuffer ? Buffer.from(arrayBuffer) : undefined,
   };
 }
