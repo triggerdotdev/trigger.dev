69f7ba6c33033a0387c1cf1b691e598f7e2f3056
Matt Aitken <matt@mattaitken.com>
2023-11-06 14:53:22 +0000

    verifyRequestSignature docs page and JSDocs

--- packages/trigger-sdk/src/security.ts
+++ packages/trigger-sdk/src/security.ts
@@ -1,15 +1,22 @@
 import crypto from "crypto";
 import { VerifyResult } from "./types";
 
+/** Easily verify webhook payloads when they're using common signing methods. */
 export async function verifyRequestSignature({
   request,
   headerName,
   secret,
   algorithm,
 }: {
+  /** The web request that you want to verify. */
   request: Request;
+  /** The name of the header that contains the signature. E.g. `X-Cal-Signature-256`. */
   headerName: string;
+  /** The secret that you use to hash the payload. For HttpEndpoints this will usually originally
+      come from the Trigger.dev dashboard and should be stored in an environment variable. */
   secret: string;
+  /** The hashing algorithm that was used to create the signature. Currently only `sha256` is
+      supported. */
   algorithm: "sha256";
 }): Promise<VerifyResult> {
   const headerValue = request.headers.get(headerName);
