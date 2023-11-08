9dc73a0fcce94d9afc2ce00905d686d528d1f29b
Matt Aitken <matt@mattaitken.com>
2023-11-08 15:45:28 +0000

    Removed some boilerplate and use new function for sendEvent

--- apps/proxy/src/index.ts
+++ apps/proxy/src/index.ts
@@ -1,23 +1,13 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { queueEvent } from "./queueEvent";
 
 export interface Env {}
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response(`Hello World! ${request.url}`);
-
     const url = new URL(request.url);
 
     if (url.pathname === "/api/v1/events" && request.method === "POST") {
-      return new Response("Hello worker!");
+      return queueEvent(request);
     }
 
     return fetch(request);
