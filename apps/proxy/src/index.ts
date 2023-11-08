05eb552f969aeb7905f6a9a1d1904a0e8b186702
Matt Aitken <matt@mattaitken.com>
2023-11-08 16:41:35 +0000

    If the env vars aren’t set, log the request and pass it through

--- apps/proxy/src/index.ts
+++ apps/proxy/src/index.ts
@@ -1,13 +1,21 @@
 import { queueEvent } from "./queueEvent";
 
-export interface Env {}
+export interface Env {
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  AWS_SQS_QUEUE_URL: string;
+}
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     const url = new URL(request.url);
 
     if (url.pathname === "/api/v1/events" && request.method === "POST") {
-      return queueEvent(request);
+      if (env.AWS_ACCESS_KEY_ID && env.AWS_SECRET_ACCESS_KEY && env.AWS_SQS_QUEUE_URL) {
+        return queueEvent(request);
+      } else {
+        console.log("/api/v1/events. Missing AWS credentials", request);
+      }
     }
 
     return fetch(request);
