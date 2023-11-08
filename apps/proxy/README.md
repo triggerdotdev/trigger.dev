5a07c05c9c312b3e2bad5a4bdf081901b6e84254
Matt Aitken <matt@mattaitken.com>
2023-11-08 17:57:30 +0000

    Progress with sendEvent proxy

--- apps/proxy/README.md
+++ apps/proxy/README.md
@@ -6,7 +6,21 @@ This is an optional module that can be used to proxy and queue requests to the T
 
 The Trigger.dev API is designed to be fast and reliable. However, if you have a lot of traffic, you may want to use this proxy to queue requests to the API. It intercepts some requests to the API and adds them to an AWS SQS queue.
 
-## How to use
+## Setup
+
+### 1. Create an AWS SQS queue
+
+In AWS you should create a new AWS SQS queue with appropriate security settings. You will need the queue URL for the next step.
+
+### 2. Add Cloudflare secrets
+
+You use `wrangler` (the Cloudflare CLI tool) to set secrets.
+
+```bash
+wrangler secret put AWS_ACCESS_KEY_ID
+wrangler secret put AWS_SECRET_ACCESS_KEY
+wrangler secret put AWS_SQS_QUEUE_URL
+```
 
 ## Development
 
--- apps/proxy/src/queueEvent.ts
+++ apps/proxy/src/queueEvent.ts
@@ -1,9 +1,11 @@
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
 import { ApiEventLog, SendEventBodySchema, SendEventOptions } from "@trigger.dev/core";
-import { getApiKeyFromRequest } from "./apikey";
 import { generateErrorMessage } from "zod-error";
+import { getApiKeyFromRequest } from "./apikey";
+import { Env } from ".";
 
 /** Adds the event to an AWS SQS queue, so it can be consumed from the main Trigger.dev API */
-export async function queueEvent(request: Request): Promise<Response> {
+export async function queueEvent(request: Request, env: Env): Promise<Response> {
   //check there's a private API key
   const apiKeyResult = getApiKeyFromRequest(request);
   if (!apiKeyResult || apiKeyResult.type !== "PRIVATE") {
@@ -14,31 +16,72 @@ export async function queueEvent(request: Request): Promise<Response> {
   }
 
   //parse the request body
-  const anyBody = await request.json();
-  const body = SendEventBodySchema.safeParse(anyBody);
-  if (!body.success) {
-    return new Response(JSON.stringify({ message: generateErrorMessage(body.error.issues) }), {
-      status: 422,
+  try {
+    const anyBody = await request.json();
+    const body = SendEventBodySchema.safeParse(anyBody);
+    if (!body.success) {
+      return new Response(JSON.stringify({ message: generateErrorMessage(body.error.issues) }), {
+        status: 422,
+        headers: { "content-type": "application/json" },
+      });
+    }
+
+    // The AWS SDK tries to use crypto from off of the window,
+    // so we need to trick it into finding it where it expects it
+    //@ts-ignore
+    global.window = {};
+    //@ts-ignore
+    window.crypto = crypto;
+
+    const client = new SQSClient({
+      region: env.AWS_REGION,
+      credentials: {
+        accessKeyId: env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+      },
+    });
+
+    //add the event to the queue
+    const send = new SendMessageCommand({
+      // use wrangler secrets to provide this global variable
+      QueueUrl: env.AWS_SQS_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        event: body.data.event,
+        options: body.data.options,
+        apiKey: apiKeyResult.apiKey,
+      }),
+    });
+
+    const queuedEvent = await client.send(send);
+    console.log("Queued event", queuedEvent);
+
+    //respond with the event
+    const event: ApiEventLog = {
+      id: body.data.event.id,
+      name: body.data.event.name,
+      payload: body.data.event.payload,
+      context: body.data.event.context,
+      timestamp: body.data.event.timestamp ?? new Date(),
+      deliverAt: calculateDeliverAt(body.data.options),
+    };
+
+    return new Response(JSON.stringify(event), {
+      status: 200,
       headers: { "content-type": "application/json" },
     });
+  } catch (e) {
+    return new Response(
+      JSON.stringify({
+        message: `Failed to parse event body: ${
+          e instanceof Error ? e.message : JSON.stringify(e)
+        }`,
+      }),
+      {
+        status: 422,
+        headers: { "content-type": "application/json" },
+      }
+    );
   }
-
-  //add the event to the queue
-
-  //respond with the event
-  const event: ApiEventLog = {
-    id: body.data.event.id,
-    name: body.data.event.name,
-    payload: body.data.event.payload,
-    context: body.data.event.context,
-    timestamp: body.data.event.timestamp ?? new Date(),
-    deliverAt: calculateDeliverAt(body.data.options),
-  };
-
-  return new Response(JSON.stringify(event), {
-    status: 200,
-    headers: { "content-type": "application/json" },
-  });
 }
 
 function calculateDeliverAt(options?: SendEventOptions) {
