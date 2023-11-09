ec6a8ae47cd6ed8e27798f7e947248709e2899e2
Matt Aitken <matt@mattaitken.com>
2023-11-09 17:10:29 +0000

    Reworked the proxy to make it easier to add more endpoints

--- apps/proxy/src/index.ts
+++ apps/proxy/src/index.ts
@@ -13,30 +13,49 @@ export interface Env {
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
     if (!env.REWRITE_HOSTNAME) throw new Error("Missing REWRITE_HOSTNAME");
+    console.log("url", request.url);
+
+    if (!queueingIsEnabled(env)) {
+      console.log("Missing AWS credentials. Passing through to the origin.");
+      return redirectToOrigin(request, env);
+    }
 
     const url = new URL(request.url);
-    console.log("url", url.toString());
-
-    if (url.pathname === "/api/v1/events" && request.method === "POST") {
-      if (env.AWS_SQS_ACCESS_KEY_ID && env.AWS_SQS_SECRET_ACCESS_KEY && env.AWS_SQS_QUEUE_URL) {
-        return queueEvent(request, env);
-      } else {
-        console.log("/api/v1/events. Missing AWS credentials. Passing through to the origin.");
+    switch (url.pathname) {
+      case "/api/v1/events": {
+        if (request.method === "POST") {
+          return queueEvent(request, env);
+        }
+        break;
+      }
+      case "/api/v1/events/bulk": {
+        if (request.method === "POST") {
+          return queueEvent(request, env);
+        }
+        break;
       }
     }
 
     //the same request but with the hostname (and port) changed
-    const newUrl = new URL(request.url);
-    newUrl.hostname = env.REWRITE_HOSTNAME;
-    newUrl.port = env.REWRITE_PORT || newUrl.port;
-
-    const requestInit: RequestInit = {
-      method: request.method,
-      headers: request.headers,
-      body: request.body,
-    };
-
-    console.log("rewritten url", newUrl.toString());
-    return fetch(newUrl.toString(), requestInit);
+    return redirectToOrigin(request, env);
   },
 };
+
+function redirectToOrigin(request: Request, env: Env) {
+  const newUrl = new URL(request.url);
+  newUrl.hostname = env.REWRITE_HOSTNAME;
+  newUrl.port = env.REWRITE_PORT || newUrl.port;
+
+  const requestInit: RequestInit = {
+    method: request.method,
+    headers: request.headers,
+    body: request.body,
+  };
+
+  console.log("rewritten url", newUrl.toString());
+  return fetch(newUrl.toString(), requestInit);
+}
+
+function queueingIsEnabled(env: Env) {
+  return env.AWS_SQS_ACCESS_KEY_ID && env.AWS_SQS_SECRET_ACCESS_KEY && env.AWS_SQS_QUEUE_URL;
+}
