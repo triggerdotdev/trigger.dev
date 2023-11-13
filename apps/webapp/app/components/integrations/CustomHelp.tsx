ade08bc519c7804949a06afb58812f9b7caf2126
D-K-P <8297864+D-K-P@users.noreply.github.com>
2023-11-13 17:09:34 +0000

    Code changes onSubmit

--- apps/webapp/app/components/integrations/CustomHelp.tsx
+++ apps/webapp/app/components/integrations/CustomHelp.tsx
@@ -1,10 +1,18 @@
 import { QueryClientProvider } from "react-query";
 import { CodeExample } from "~/routes/resources.codeexample";
-import { Api } from "~/services/externalApis/apis.server";
+import { Api, ApiExample } from "~/services/externalApis/apis.server";
 import { Header1, Header2 } from "../primitives/Headers";
 import { Paragraph } from "../primitives/Paragraph";
+import { cn } from "~/utils/cn";
+import { useState } from "react";
 
 export function CustomHelp({ api }: { api: Api }) {
+  const [selectedExample, setSelectedExample] = useState(api.examples?.[0]);
+
+  const changeCodeExample = (example: ApiExample) => {
+    setSelectedExample(example);
+  };
+
   return (
     <div className="mt-4">
       <Header1 className="mb-2">You can use any API with requests or an SDK</Header1>
@@ -14,18 +22,25 @@ export function CustomHelp({ api }: { api: Api }) {
         make sure they're resumable.
       </Paragraph>
       <Paragraph spacing>Here's an example with the official GitHub SDK</Paragraph>
-
       {api.examples && api.examples.length > 0 && (
         <>
-          <CodeExample example={api.examples[0]} />
+          <div className=" flex w-full flex-row gap-4 overflow-x-scroll	scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-700 sm:h-full  lg:w-[22rem] lg:flex-col">
+            {api.examples?.map((example, index) => (
+              <button
+                onClick={() => changeCodeExample(example)}
+                key={example.codeUrl}
+                className={cn(
+                  "w-64 min-w-[16rem] p-4 transition-colors duration-300 sm:w-full sm:rounded",
+                  "border-px border border-slate-900 bg-slate-900 text-slate-300 transition duration-300 hover:bg-slate-800"
+                )}
+              >
+                {example.title}
+              </button>
+            ))}
+          </div>
+          {selectedExample && <CodeExample example={selectedExample} />}
         </>
       )}
-
-      <Header2 className="mb-2">How to use fetch</Header2>
-      <Paragraph spacing>
-        You can use the fetch API to make requests to any API. Or a different request library like
-        axios if you'd prefer. Again wrapping the request in a Task will make sure it's resumable.
-      </Paragraph>
     </div>
   );
 }
