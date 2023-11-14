0f7cf6e836f3ef3e9c446b1376f24955e9aac964
D-K-P <8297864+D-K-P@users.noreply.github.com>
2023-11-14 14:54:08 +0000

    Added example code to the apis without examples, added a focus state

--- apps/webapp/app/components/integrations/CustomHelp.tsx
+++ apps/webapp/app/components/integrations/CustomHelp.tsx
@@ -6,16 +6,43 @@ import { Header1, Header2 } from "../primitives/Headers";
 import { Paragraph } from "../primitives/Paragraph";
 import { TextLink } from "../primitives/TextLink";
 
-export function CustomHelp({ api }: { api: Api }) {
-  const [selectedExample, setSelectedExample] = useState(api.examples?.[0]);
+const fallbackExamples = [
+  {
+    title: "Post to Slack when meetings are booked or cancelled.",
+    slug: "cal-slack-meeting-alert",
+    version: "1.0.0",
+    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/cal-http-endpoint.ts",
+  },
+  {
+    title: "Translate some text with DeepL.",
+    slug: "translate-text-with-deepl",
+    version: "1.0.0",
+    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/deepl.ts",
+  },
+  {
+    title: "Create a Discord bot and send a message to a channel.",
+    slug: "discord-bot-send-message",
+    version: "1.0.0",
+    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/discord.ts",
+  },
+  {
+    title: "Retrieve a Notion page by ID.",
+    slug: "retrieve-notion-page",
+    version: "1.0.0",
+    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/notion.ts",
+  },
+];
 
-  const changeCodeExample = (example: ApiExample) => {
-    setSelectedExample(example);
+export function CustomHelp({ api }: { api: Api }) {
+  const [selectedExample, setSelectedExample] = useState(0);
+
+  const changeCodeExample = (index: number) => {
+    setSelectedExample(index);
   };
 
   return (
     <div className="mt-4">
-      <Header1 className="mb-2">You can use any API with requests or an SDK</Header1>
+      <Header1 className="mb-2">Using an API with an SDK or requests</Header1>
       <Paragraph spacing>
         You can use Trigger.dev with any existing Node SDK or even just using fetch. You can
         subscribe to any API with{" "}
@@ -32,10 +59,10 @@ export function CustomHelp({ api }: { api: Api }) {
         . This makes your background job resumable and appear in our dashboard.
       </Paragraph>
 
-      {api.examples && api.examples.length > 0 && (
+      {api.examples && api.examples.length > 0 ? (
         <>
           <Header2 className="mb-2">Example code</Header2>
-          <Paragraph spacing>
+          <Paragraph spacing className="mb-4">
             This is how you can use {api.name} with Trigger.dev. This code can be copied and
             modified to suit your use-case.
           </Paragraph>
@@ -43,44 +70,42 @@ export function CustomHelp({ api }: { api: Api }) {
             {api.examples.length > 1 &&
               api.examples?.map((example, index) => (
                 <button
-                  onClick={() => changeCodeExample(example)}
+                  onClick={() => changeCodeExample(index)}
                   key={example.codeUrl}
                   className={cn(
-                    "w-64 min-w-[16rem] p-4 transition-colors duration-300 sm:w-full sm:rounded",
-                    "border-px border border-slate-900 bg-slate-900 text-slate-300 transition duration-300 hover:bg-slate-800"
+                    "w-64 min-w-[16rem] p-2 transition-colors duration-300 sm:w-full sm:rounded",
+                    "border-px focus:border-px border border-slate-900 bg-slate-900 text-slate-300 transition duration-300 hover:bg-slate-800  focus:border focus:border-indigo-600"
                   )}
                 >
                   {example.title}
                 </button>
               ))}
           </div>
-          {selectedExample && <CodeExample example={selectedExample} />}
+          <CodeExample example={api.examples[selectedExample]} />
         </>
-      )}
-
-      {!api.examples && (
+      ) : (
         <>
-          {/* <Header2 className="mb-2">Example code</Header2>
-          <Paragraph spacing>
-            We are currently working on {api.name} examples with Trigger.dev. However, here are some
-            other code examples for you to look at to get an idea of how Trigger.dev works with
-            other APIs.
+          <Header2 className="mb-2">Example code</Header2>
+          <Paragraph spacing className="mb-4">
+            We are currently working on code samples for {api.name}. In the meantime, you can use
+            one of our examples below as a starting point if you want to use {api.name} in your
+            project. Please reach out to us if you're having any issues.
           </Paragraph>
           <div className=" flex w-full flex-row gap-4 overflow-x-scroll	scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-700 sm:h-full">
-            {apisList[0].examples?.map((example, index) => (
+            {fallbackExamples.map((example, index) => (
               <button
-                onClick={() => changeCodeExample(example)}
+                onClick={() => changeCodeExample(index)}
                 key={example.codeUrl}
                 className={cn(
-                  "w-64 min-w-[16rem] p-4 transition-colors duration-300 sm:w-full sm:rounded",
-                  "border-px border border-slate-900 bg-slate-900 text-slate-300 transition duration-300 hover:bg-slate-800"
+                  "w-64 min-w-[16rem] p-2 transition-colors duration-300 sm:w-full sm:rounded",
+                  "border-px focus:border-px border border-slate-900 bg-slate-900 text-slate-300 transition duration-300 hover:bg-slate-800 focus:border focus:border-indigo-600"
                 )}
               >
                 {example.title}
               </button>
             ))}
           </div>
-          {selectedExample && <CodeExample example={selectedExample} />} */}
+          <CodeExample example={fallbackExamples[selectedExample]} />
         </>
       )}
     </div>
