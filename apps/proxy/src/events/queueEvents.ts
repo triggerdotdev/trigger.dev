5dd1bcc589f95790e16b0859db6c694e454c2a82
Matt Aitken <matt@mattaitken.com>
2023-11-14 21:34:08 +0000

    sendEvents proxy uses batches of 10 (max for SQS)

--- apps/proxy/src/events/queueEvents.ts
+++ apps/proxy/src/events/queueEvents.ts
@@ -1,15 +1,10 @@
 import { SQSClient, SendMessageBatchCommand } from "@aws-sdk/client-sqs";
-import {
-  ApiEventLog,
-  SendBulkEventsBodySchema,
-  SendEventBodySchema,
-  SendEventOptions,
-} from "@trigger.dev/core";
+import { ApiEventLog, SendBulkEventsBodySchema } from "@trigger.dev/core";
 import { generateErrorMessage } from "zod-error";
-import { getApiKeyFromRequest } from "../apikey";
 import { Env } from "..";
-import { calculateDeliverAt } from "./utils";
+import { getApiKeyFromRequest } from "../apikey";
 import { json } from "../json";
+import { calculateDeliverAt } from "./utils";
 
 /** Adds the event to an AWS SQS queue, so it can be consumed from the main Trigger.dev API */
 export async function queueEvents(request: Request, env: Env): Promise<Response> {
@@ -30,7 +25,7 @@ export async function queueEvents(request: Request, env: Env): Promise<Response>
     const body = SendBulkEventsBodySchema.safeParse(anyBody);
     if (!body.success) {
       return json(
-        { message: generateErrorMessage(body.error.issues) },
+        { error: generateErrorMessage(body.error.issues) },
         {
           status: 422,
         }
@@ -49,30 +44,49 @@ export async function queueEvents(request: Request, env: Env): Promise<Response>
       },
     });
 
-    const updatedEvents = body.data.events.map((event) => {
+    const updatedEvents: ApiEventLog[] = body.data.events.map((event) => {
       const timestamp = event.timestamp ?? new Date();
       return {
         ...event,
+        payload: event.payload,
         timestamp,
       };
     });
 
-    //add the event to the queue
-    const send = new SendMessageBatchCommand({
-      // use wrangler secrets to provide this global variable
-      QueueUrl: env.AWS_SQS_QUEUE_URL,
-      Entries: updatedEvents.map((event) => ({
-        Id: event.id,
-        MessageBody: JSON.stringify({
-          event,
-          options: body.data.options,
-          apiKey: apiKeyResult.apiKey,
-        }),
-      })),
-    });
+    //divide updatedEvents into multiple batches of 10 (max size SQS accepts)
+    const batches: ApiEventLog[][] = [];
+    let currentBatch: ApiEventLog[] = [];
+    for (let i = 0; i < updatedEvents.length; i++) {
+      currentBatch.push(updatedEvents[i]);
+      if (currentBatch.length === 10) {
+        batches.push(currentBatch);
+        currentBatch = [];
+      }
+    }
+    if (currentBatch.length > 0) {
+      batches.push(currentBatch);
+    }
 
-    const queuedEvent = await client.send(send);
-    console.log("Queued event", queuedEvent);
+    //loop through the batches and send them
+    for (let i = 0; i < batches.length; i++) {
+      const batch = batches[i];
+      //add the event to the queue
+      const send = new SendMessageBatchCommand({
+        // use wrangler secrets to provide this global variable
+        QueueUrl: env.AWS_SQS_QUEUE_URL,
+        Entries: batch.map((event, index) => ({
+          Id: `event-${index}`,
+          MessageBody: JSON.stringify({
+            event,
+            options: body.data.options,
+            apiKey: apiKeyResult.apiKey,
+          }),
+        })),
+      });
+
+      const queuedEvent = await client.send(send);
+      console.log("Queued events", queuedEvent);
+    }
 
     //respond with the events
     const events: ApiEventLog[] = updatedEvents.map((event) => ({
@@ -85,11 +99,10 @@ export async function queueEvents(request: Request, env: Env): Promise<Response>
       status: 200,
     });
   } catch (e) {
+    console.error("queueEvents error", e);
     return json(
       {
-        message: `Failed to parse event body: ${
-          e instanceof Error ? e.message : JSON.stringify(e)
-        }`,
+        error: `Failed to send events: ${e instanceof Error ? e.message : JSON.stringify(e)}`,
       },
       {
         status: 422,
