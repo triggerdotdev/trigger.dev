888344797ece7accd92ab3708681e7d958159250
Dan <8297864+D-K-P@users.noreply.github.com>
2023-11-15 17:38:24 +0000

    Added code examples and modified blank state on the integrations & APIs page (#736)
    * Added more API icons
    
    * Updated the title
    
    * Added resources.codeexample.ts to pull in example code from github
    
    * Added regex to remove redundant express code from the examples
    
    * Added cal examples
    
    * Code changes onSubmit
    
    * Improved the code loading state
    
    * Removed request integration button
    
    * Improved the Customhelp copy
    
    * Fixed styling issues and removed imports
    
    * Added all the code examples
    
    * Don’t show title if there’s 1 or less examples
    
    * Updated copy for the non example version
    
    * Added example code to the apis without examples, added a focus state
    
    * Fixed titles
    
    * Got rid of the phantom gap
    
    * Made the copy more generic so it works on the integration pages as well
    
    * Removed import
    
    * Added view examples button to the integration pages
    
    * Removed fetch/sdk option from integration sheet
    
    * Apply code example regex on the server
    
    ---------
    
    Co-authored-by: Matt Aitken <matt@mattaitken.com>

--- apps/webapp/app/utils/pathBuilder.ts
+++ apps/webapp/app/utils/pathBuilder.ts
@@ -369,3 +369,8 @@ export function docsIntegrationPath(api: string) {
 export function docsCreateIntegration() {
   return `${docsRoot()}/integrations/create`;
 }
+
+//api
+export function apiReferencePath(apiSlug: string) {
+  return `https://trigger.dev/apis/${apiSlug}`;
+}
