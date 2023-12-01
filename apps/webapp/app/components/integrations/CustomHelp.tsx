0cb652b138fcddfdc97479ec98c24a731c05b4b6
D-K-P <8297864+D-K-P@users.noreply.github.com>
2023-12-01 15:42:08 +0000

    Updated the blank state copy

--- apps/webapp/app/components/integrations/CustomHelp.tsx
+++ apps/webapp/app/components/integrations/CustomHelp.tsx
@@ -18,8 +18,12 @@ export function CustomHelp({ api }: { api: Api }) {
     <div className="mt-4">
       <Header1 className="mb-2">Using {api.name} with an SDK or requests</Header1>
       <Paragraph spacing>
-        You can use Trigger.dev with any existing Node SDK or even just using fetch. You can
-        subscribe to any API with{" "}
+        To get started we recommend searching for the official Node SDK for {api.name}, you can then
+        use their API documentation to get started and create tasks. If they don't have an official
+        SDK there are often third parties you can use instead.
+      </Paragraph>
+      <Paragraph spacing className="mb-4">
+        You can subscribe to any API with{" "}
         <TextLink href="https://trigger.dev/docs/documentation/concepts/http-endpoints">
           HTTP endpoints
         </TextLink>{" "}
@@ -31,10 +35,6 @@ export function CustomHelp({ api }: { api: Api }) {
           io.runTask
         </TextLink>
         . This makes your background job resumable and appear in our dashboard.
-      </Paragraph>
-      <Paragraph spacing className="mb-4">
-        To get started we recommend search for an official Node SDK for {api.name}, you can then use
-        their API documentation to get started and create tasks.
       </Paragraph>
 
       {api.examples && api.examples.length > 0 ? (
@@ -64,7 +64,7 @@ export function CustomHelp({ api }: { api: Api }) {
         </>
       ) : (
         <>
-          <Header3 className="mb-2">We're happy to help</Header3>
+          <Header2 className="mb-2">We're happy to help</Header2>
           <Paragraph spacing className="mb-4">
             Please{" "}
             <Feedback
@@ -75,25 +75,9 @@ export function CustomHelp({ api }: { api: Api }) {
               }
               defaultValue="help"
             />{" "}
-            if you're having any issues connecting your API, we'll help you get set up as quickly as
-            possible.
+            if you're having any issues connecting to {api.name}, we'll help you get set up as
+            quickly as possible.
           </Paragraph>
-
-          {/* <div className=" flex w-full flex-row gap-4 overflow-x-scroll	scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-700 sm:h-full">
-            {fallbackExamples.map((example, index) => (
-              <button
-                onClick={() => changeCodeExample(index)}
-                key={example.codeUrl}
-                className={cn(
-                  "w-64 min-w-[16rem] p-2 transition-colors duration-300 sm:w-full sm:rounded",
-                  "border-px focus:border-px cursor-pointer border border-slate-900 bg-slate-900 text-slate-300 transition duration-300 hover:bg-slate-800 focus:border focus:border-indigo-600"
-                )}
-              >
-                {example.title}
-              </button>
-            ))}
-          </div>
-          <CodeExample example={fallbackExamples[selectedExample]} /> */}
         </>
       )}
     </div>
