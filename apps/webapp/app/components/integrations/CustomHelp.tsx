4c61557aa04e11ac73763389883babc5ef6385ff
D-K-P <8297864+D-K-P@users.noreply.github.com>
2023-12-01 14:20:54 +0000

    Changed custom help so it doesn’t show code examples

--- apps/webapp/app/components/integrations/CustomHelp.tsx
+++ apps/webapp/app/components/integrations/CustomHelp.tsx
@@ -3,36 +3,9 @@ import { CodeExample } from "~/routes/resources.codeexample";
 import { Api } from "~/services/externalApis/apis.server";
 import { cn } from "~/utils/cn";
 import { Feedback } from "../Feedback";
-import { Header1, Header2 } from "../primitives/Headers";
+import { Header1, Header2, Header3 } from "../primitives/Headers";
 import { Paragraph } from "../primitives/Paragraph";
 import { TextLink } from "../primitives/TextLink";
-
-const fallbackExamples = [
-  {
-    title: "Post to Slack when meetings are booked or cancelled.",
-    slug: "cal-slack-meeting-alert",
-    version: "1.0.0",
-    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/cal-http-endpoint.ts",
-  },
-  {
-    title: "Translate some text with DeepL.",
-    slug: "translate-text-with-deepl",
-    version: "1.0.0",
-    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/deepl.ts",
-  },
-  {
-    title: "Create a Discord bot and send a message to a channel.",
-    slug: "discord-bot-send-message",
-    version: "1.0.0",
-    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/discord.ts",
-  },
-  {
-    title: "Retrieve a Notion page by ID.",
-    slug: "retrieve-notion-page",
-    version: "1.0.0",
-    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/notion.ts",
-  },
-];
 
 export function CustomHelp({ api }: { api: Api }) {
   const [selectedExample, setSelectedExample] = useState(0);
@@ -43,7 +16,7 @@ export function CustomHelp({ api }: { api: Api }) {
 
   return (
     <div className="mt-4">
-      <Header1 className="mb-2">Using an API with an SDK or requests</Header1>
+      <Header1 className="mb-2">Using {api.name} with an SDK or requests</Header1>
       <Paragraph spacing>
         You can use Trigger.dev with any existing Node SDK or even just using fetch. You can
         subscribe to any API with{" "}
@@ -58,6 +31,10 @@ export function CustomHelp({ api }: { api: Api }) {
           io.runTask
         </TextLink>
         . This makes your background job resumable and appear in our dashboard.
+      </Paragraph>
+      <Paragraph spacing className="mb-4">
+        To get started we recommend search for an official Node SDK for {api.name}, you can then use
+        their API documentation to get started and create tasks.
       </Paragraph>
 
       {api.examples && api.examples.length > 0 ? (
@@ -87,9 +64,8 @@ export function CustomHelp({ api }: { api: Api }) {
         </>
       ) : (
         <>
-          <Header2 className="mb-2">Example code using fetch / an existing SDK</Header2>
+          <Header3 className="mb-2">We're happy to help</Header3>
           <Paragraph spacing className="mb-4">
-            You can use one of our examples below as a starting point / reference for your projects.
             Please{" "}
             <Feedback
               button={
@@ -99,10 +75,11 @@ export function CustomHelp({ api }: { api: Api }) {
               }
               defaultValue="help"
             />{" "}
-            if you're having any issues.
+            if you're having any issues connecting your API, we'll help you get set up as quickly as
+            possible.
           </Paragraph>
 
-          <div className=" flex w-full flex-row gap-4 overflow-x-scroll	scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-700 sm:h-full">
+          {/* <div className=" flex w-full flex-row gap-4 overflow-x-scroll	scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-700 sm:h-full">
             {fallbackExamples.map((example, index) => (
               <button
                 onClick={() => changeCodeExample(index)}
@@ -116,7 +93,7 @@ export function CustomHelp({ api }: { api: Api }) {
               </button>
             ))}
           </div>
-          <CodeExample example={fallbackExamples[selectedExample]} />
+          <CodeExample example={fallbackExamples[selectedExample]} /> */}
         </>
       )}
     </div>
