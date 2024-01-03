390ac6101eafe95437962d5d1b1809928dc151bc
Dan <8297864+D-K-P@users.noreply.github.com>
2024-01-03 11:18:44 +0000

    Added new http-endpoint examples, changed examples links in the docs (#775)
    * updated companyicons package
    
    * Changed custom help so it doesn’t show code examples
    
    * Added Asana / Instagram / Salesforce / Segment / Square / Svix / Snyk / Todoist & Zapier http endpoint examples to the integration page
    
    * Commented out some of the apis we dont have code examples for
    
    * Uncommented raycast and reddit
    
    * Updated the blank state copy
    
    * Changed the copy so it works better on examples with and without code
    
    * Added Brex / Discoed / Google Sheets, Docs, Drive / Gmail / PagerDuty / YouTube endpoint examples
    
    * Added more examples
    
    * Change the top tab to ‘Examples’ and updated /examples links to point to trigger.dev/apis
    
    * Update package.json
    
    * Updated pnpm-lock file
    
    * Updated pnpm-lock file
    
    ---------
    
    Co-authored-by: Matt Aitken <matt@mattaitken.com>

--- apps/webapp/app/components/integrations/CustomHelp.tsx
+++ apps/webapp/app/components/integrations/CustomHelp.tsx
@@ -3,36 +3,9 @@ import { CodeExample } from "~/routes/resources.codeexample";
 import { Api } from "~/services/externalApis/apis.server";
 import { cn } from "~/utils/cn";
 import { Feedback } from "../Feedback";
-import { Header1, Header2 } from "../primitives/Headers";
+import { Header1, Header2, Header3 } from "../primitives/Headers";
 import { Paragraph } from "../primitives/Paragraph";
 import { TextLink } from "../primitives/TextLink";
-
-const fallbackExamples = [
-  {
-    title: "Post to Slack when meetings are booked or cancelled.",
-    slug: "cal-slack-meeting-alert",
-    version: "1.0.0",
-    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/cal-http-endpoint.ts",
-  },
-  {
-    title: "Translate some text with DeepL.",
-    slug: "translate-text-with-deepl",
-    version: "1.0.0",
-    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/deepl.ts",
-  },
-  {
-    title: "Create a Discord bot and send a message to a channel.",
-    slug: "discord-bot-send-message",
-    version: "1.0.0",
-    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/discord.ts",
-  },
-  {
-    title: "Retrieve a Notion page by ID.",
-    slug: "retrieve-notion-page",
-    version: "1.0.0",
-    codeUrl: "https://github.com/triggerdotdev/api-reference/raw/main/src/notion.ts",
-  },
-];
 
 export function CustomHelp({ api }: { api: Api }) {
   const [selectedExample, setSelectedExample] = useState(0);
@@ -43,7 +16,7 @@ export function CustomHelp({ api }: { api: Api }) {
 
   return (
     <div className="mt-4">
-      <Header1 className="mb-2">Using an API with an SDK or requests</Header1>
+      <Header1 className="mb-2">Using {api.name} with an SDK or requests</Header1>
       <Paragraph spacing>
         You can use Trigger.dev with any existing Node SDK or even just using fetch. You can
         subscribe to any API with{" "}
@@ -87,9 +60,13 @@ export function CustomHelp({ api }: { api: Api }) {
         </>
       ) : (
         <>
-          <Header2 className="mb-2">Example code using fetch / an existing SDK</Header2>
+          <Header2 className="mb-2">Getting started with {api.name}</Header2>
           <Paragraph spacing className="mb-4">
-            You can use one of our examples below as a starting point / reference for your projects.
+            We recommend searching for the official {api.name} Node SDK. If they have one, you can
+            install it and then use their API documentation to get started and create tasks. If they
+            don't, there are often third party SDKs you can use instead.
+          </Paragraph>
+          <Paragraph spacing className="mb-4">
             Please{" "}
             <Feedback
               button={
@@ -99,24 +76,9 @@ export function CustomHelp({ api }: { api: Api }) {
               }
               defaultValue="help"
             />{" "}
-            if you're having any issues.
+            if you're having any issues connecting to {api.name}, we'll help you get set up as
+            quickly as possible.
           </Paragraph>
-
-          <div className=" flex w-full flex-row gap-4 overflow-x-scroll	scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-700 sm:h-full">
-            {fallbackExamples.map((example, index) => (
-              <button
-                onClick={() => changeCodeExample(index)}
-                key={example.codeUrl}
-                className={cn(
-                  "w-64 min-w-[16rem] p-2 transition-colors duration-300 sm:w-full sm:rounded",
-                  "border-px focus:border-px cursor-pointer border border-slate-900 bg-slate-900 text-slate-300 transition duration-300 hover:bg-slate-800 focus:border focus:border-indigo-600"
-                )}
-              >
-                {example.title}
-              </button>
-            ))}
-          </div>
-          <CodeExample example={fallbackExamples[selectedExample]} />
         </>
       )}
     </div>
