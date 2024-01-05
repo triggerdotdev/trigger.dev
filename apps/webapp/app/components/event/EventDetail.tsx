27a10aee6811b6a6f706c2a67db8199b0c593ad7
Matt Aitken <matt@mattaitken.com>
2024-01-05 12:10:35 +0000

    Changed some wording on the event detail page

--- apps/webapp/app/components/event/EventDetail.tsx
+++ apps/webapp/app/components/event/EventDetail.tsx
@@ -25,7 +25,7 @@ export function EventDetail({ event }: { event: Event }) {
           {deliveredAt && (
             <RunPanelIconProperty
               icon="flag"
-              label="Finished at"
+              label="Delivered"
               value={<DateTime date={deliveredAt} />}
             />
           )}
