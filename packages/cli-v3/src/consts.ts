86468d61ab32388a23e088497b1d8bfcde72f46d
Matt Aitken <matt@mattaitken.com>
2024-01-14 17:24:48 +0000

    Removed some unused things

--- packages/cli-v3/src/consts.ts
+++ packages/cli-v3/src/consts.ts
@@ -5,16 +5,8 @@ import { fileURLToPath } from "url";
 // Path is in relation to a single index.js file inside ./dist
 const __filename = fileURLToPath(import.meta.url);
 const distPath = path.dirname(__filename);
+
 export const PKG_ROOT = path.join(distPath, "../");
-
-export const TITLE_TEXT = `                                               
- _____       _                           _           
-|_   _| ___ |_| ___  ___  ___  ___     _| | ___  _ _ 
-  | |  |  _|| || . || . || -_||  _| _ | . || -_|| | |
-  |_|  |_|  |_||_  ||_  ||___||_|  |_||___||___| \\_/ 
-               |___||___|                            
-`;
-
 export const COMMAND_NAME = "trigger.dev";
 export const CLOUD_WEB_URL = "https://cloud.trigger.dev";
 export const CLOUD_API_URL = "https://api.trigger.dev";
