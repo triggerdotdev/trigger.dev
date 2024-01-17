f08d11f4aac9ab7766298a6d76b869cf9bb5ff38
Eric Allam <eallam@icloud.com>
2024-01-17 18:08:41 +0000

    More websocket connection stuff

--- apps/webapp/app/v3/handleWebsockets.server.ts
+++ apps/webapp/app/v3/handleWebsockets.server.ts
@@ -1,23 +1,87 @@
-import { singleton } from "../utils/singleton";
+import { IncomingMessage } from "node:http";
 import { WebSocketServer } from "ws";
+import { AuthenticatedEnvironment, authenticateApiKey } from "~/services/apiAuth.server";
+import { singleton } from "../utils/singleton";
+import { randomUUID } from "node:crypto";
+import { Evt } from "evt";
+import { logger } from "~/services/logger.server";
 
 export const wss = singleton("wss", initalizeWebSocketServer);
+
+let handlers: Map<string, WebsocketHandlers>;
 
 function initalizeWebSocketServer() {
   const server = new WebSocketServer({ noServer: true });
 
-  server.on("connection", (ws, req) => {
-    const apiKey = req.headers.authorization;
+  server.on("connection", handleWebSocketConnection);
 
-    if (!apiKey || typeof apiKey !== "string") {
-      console.log("Invalid API Key, closing the connection");
-
-      ws.close(1008, "Invalid API Key");
-      return;
-    }
-
-    console.log(`[${process.pid}] Client connected with API Key: ${apiKey}`);
-  });
+  handlers = new Map();
 
   return server;
+}
+
+async function handleWebSocketConnection(ws: WebSocket, req: IncomingMessage) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || typeof authHeader !== "string") {
+    ws.close(1008, "Missing Authorization header");
+    return;
+  }
+
+  const [authType, apiKey] = authHeader.split(" ");
+
+  if (authType !== "Bearer" || !apiKey) {
+    ws.close(1008, "Invalid Authorization header");
+    return;
+  }
+
+  const authenticationResult = await authenticateApiKey(apiKey);
+
+  if (!authenticationResult) {
+    ws.close(1008, "Invalid API key");
+    return;
+  }
+
+  const authenticatedEnv = authenticationResult.environment;
+
+  const handler = new WebsocketHandlers(ws, authenticatedEnv);
+
+  handlers.set(handler.id, handler);
+
+  handler.onClose.attach((closeEvent) => {
+    logger.debug("Websocket closed", { closeEvent });
+
+    handlers.delete(handler.id);
+  });
+
+  await handler.start();
+}
+
+class WebsocketHandlers {
+  public id: string;
+  public onClose: Evt<CloseEvent> = new Evt();
+
+  constructor(public ws: WebSocket, public authenticatedEnv: AuthenticatedEnvironment) {
+    this.id = randomUUID();
+
+    ws.onclose = this.#handleClose.bind(this);
+    ws.onmessage = this.#handleMessage.bind(this);
+    ws.onerror = this.#handleError.bind(this);
+  }
+
+  async start() {}
+
+  async #handleMessage(ev: MessageEvent) {
+    const data = JSON.parse(ev.data.toString());
+
+    logger.debug("Websocket message received", { data });
+  }
+
+  async #handleClose(ev: CloseEvent) {
+    this.onClose.post(ev);
+  }
+
+  async #handleError(ev: Event) {
+    logger.error("Websocket error", { ev });
+  }
 }
