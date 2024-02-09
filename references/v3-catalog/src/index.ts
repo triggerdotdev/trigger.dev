1a083ca1cff65a4a15c05552236c0a8e18899a6e
Eric Allam <eallam@icloud.com>
2024-02-09 15:28:45 +0000

    Remove tracing stuff from the v3-catalog

--- references/v3-catalog/src/index.ts
+++ references/v3-catalog/src/index.ts
@@ -1,32 +1,11 @@
-import { SpanStatusCode } from "@opentelemetry/api";
-import { flushOtel, getTracer } from "./tracer";
-
-const tracer = getTracer("v3-catalog", "0.0.1");
-
 import { parentTask } from "./trigger/simple";
 
 export async function main() {
-  const result = await tracer.startActiveSpan("main", async (span) => {
-    try {
-      const handle = await parentTask.trigger({
-        payload: { message: "This is a message from the trigger-dev CLI" },
-      });
-
-      return handle;
-    } catch (e) {
-      if (e instanceof Error) {
-        span.recordException(e);
-      }
-
-      span.setStatus({
-        code: SpanStatusCode.ERROR,
-      });
-    } finally {
-      span.end();
-    }
+  const handle = await parentTask.trigger({
+    payload: { message: "This is a message from the trigger-dev CLI" },
   });
 
-  await flushOtel();
+  return handle;
 }
 
 main().then(console.log).catch(console.error);
