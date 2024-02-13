75f3aa8b9242e4ec4d3d134a973df25dffbda316
Matt Aitken <matt@mattaitken.com>
2024-02-13 09:17:59 +0000

    Allow people to choose v3 when creating a new project

--- apps/webapp/app/models/project.server.ts
+++ apps/webapp/app/models/project.server.ts
@@ -8,7 +8,12 @@ export type { Project } from "@trigger.dev/database";
 const externalRefGenerator = customAlphabet("abcdefghijklmnopqrstuvwxyz", 20);
 
 export async function createProject(
-  { organizationSlug, name, userId }: { organizationSlug: string; name: string; userId: string },
+  {
+    organizationSlug,
+    name,
+    userId,
+    version,
+  }: { organizationSlug: string; name: string; userId: string; version: "v2" | "v3" },
   attemptCount = 0
 ): Promise<Project & { organization: Organization }> {
   //check the user has permissions to do this
@@ -41,6 +46,7 @@ export async function createProject(
         organizationSlug,
         name,
         userId,
+        version,
       },
       attemptCount + 1
     );
@@ -56,6 +62,7 @@ export async function createProject(
         },
       },
       externalRef: externalRefGenerator(),
+      version: version === "v3" ? "V3" : "V2",
     },
     include: {
       organization: {
