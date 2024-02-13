cba4bc918fa6ed313df51e0c7f52421a5f00dfd5
Matt Aitken <matt@mattaitken.com>
2024-02-13 09:17:09 +0000

    Moved v3Enabled to the existing feature flags

--- apps/webapp/app/features.server.ts
+++ apps/webapp/app/features.server.ts
@@ -1,7 +1,9 @@
+import { env } from "./env.server";
 import { requestUrl } from "./utils/requestUrl.server";
 
 export type TriggerFeatures = {
   isManagedCloud: boolean;
+  v3Enabled: boolean;
 };
 
 // If the request host is cloud.trigger.dev then we are on the managed cloud
@@ -17,5 +19,6 @@ export function featuresForRequest(request: Request): TriggerFeatures {
 
   return {
     isManagedCloud,
+    v3Enabled: env.V3_ENABLED === "true",
   };
 }
