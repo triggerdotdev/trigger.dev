91134a212100d2fa6cf1daf1a0b77ad7283ce617
nicktrn <55853254+nicktrn@users.noreply.github.com>
2024-02-29 18:59:13 +0000

    fix v3 ref catalog entry

--- references/v3-catalog/src/index.ts
+++ references/v3-catalog/src/index.ts
@@ -1,7 +1,7 @@
-import { parentTask, simpleParentTask } from "./trigger/simple";
+import { parentTask } from "./trigger/simple";
 
 export async function main() {
-  const handle = await simpleParentTask.trigger({
+  const handle = await parentTask.trigger({
     payload: { message: "This is a message from the trigger-dev CLI" },
   });
 
