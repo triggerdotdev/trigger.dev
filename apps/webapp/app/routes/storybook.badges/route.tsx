dbac8cdbc521b3ee44404540885fb539e3d66c49
James Ritchie <james@jamesritchie.co.uk>
2024-03-01 17:15:25 +0000

    Added new badge variants

--- apps/webapp/app/routes/storybook.badges/route.tsx
+++ apps/webapp/app/routes/storybook.badges/route.tsx
@@ -4,7 +4,12 @@ export default function Story() {
   return (
     <div className="flex flex-col items-start gap-y-8 p-8">
       <Badge>Default</Badge>
+      <div className="bg-charcoal-1000 p-4">
+        <Badge variant="small">Small</Badge>
+      </div>
       <Badge variant="outline">Outline</Badge>
+      <Badge variant="v3">v3</Badge>
+      <Badge variant="outline-rounded">Outline rounded</Badge>
     </div>
   );
 }
