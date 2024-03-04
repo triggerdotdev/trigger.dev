f25eef2f9bbcb964449ac11031d4b99d980645f3
Matt Aitken <matt@mattaitken.com>
2024-03-04 13:39:43 +0000

    Partials now render to the end

--- apps/webapp/app/presenters/v3/RunPresenter.server.ts
+++ apps/webapp/app/presenters/v3/RunPresenter.server.ts
@@ -72,16 +72,26 @@ export class RunPresenter {
 
     //we need the start offset for each item, and the total duration of the entire tree
     const treeRootStartTimeMs = tree ? tree?.data.startTime.getTime() : 0;
-    let duration = tree?.data.duration ?? 0;
+    let totalDuration = tree?.data.duration ?? 0;
     const events = tree
       ? flattenTree(tree).map((n) => {
           const offset = millisecondsToNanoseconds(
             n.data.startTime.getTime() - treeRootStartTimeMs
           );
-          duration = Math.max(duration, offset + n.data.duration);
+          totalDuration = Math.max(totalDuration, offset + n.data.duration);
           return { ...n, data: { ...n.data, offset, isRoot: n.id === traceSummary.rootSpan.id } };
         })
       : [];
+
+    //total duration should be a minimum of 5ms
+    totalDuration = Math.max(totalDuration, millisecondsToNanoseconds(1));
+
+    //we need to adjust any partial nodes so they run the full duration
+    for (const event of events) {
+      if (event.data.isPartial) {
+        event.data.duration = totalDuration - event.data.offset;
+      }
+    }
 
     return {
       run: {
@@ -97,7 +107,7 @@ export class RunPresenter {
       events: events,
       parentRunFriendlyId:
         tree?.id === traceSummary.rootSpan.id ? undefined : traceSummary.rootSpan.runId,
-      duration,
+      duration: totalDuration,
     };
   }
 }
