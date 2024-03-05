8ff8241811f5bb1d5ad7d60b923d1801bec6cb15
James Ritchie <james@jamesritchie.co.uk>
2024-03-05 18:13:48 +0000

    Added a new blank state to the runs page

--- apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.v3.$projectParam.runs._index/route.tsx
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.v3.$projectParam.runs._index/route.tsx
@@ -1,17 +1,24 @@
+import { BeakerIcon, BookOpenIcon } from "@heroicons/react/24/solid";
 import { useNavigation } from "@remix-run/react";
 import { LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { typedjson, useTypedLoaderData } from "remix-typedjson";
+import { StepContentContainer } from "~/components/StepContentContainer";
+import { MainCenteredContainer, PageBody } from "~/components/layout/AppLayout";
+import { LinkButton } from "~/components/primitives/Buttons";
+import { Header1 } from "~/components/primitives/Headers";
+import { NavBar, PageTitle } from "~/components/primitives/PageHeader";
+import { Paragraph } from "~/components/primitives/Paragraph";
+import { StepNumber } from "~/components/primitives/StepNumber";
 import { RunsFilters, TaskRunListSearchFilters } from "~/components/runs/v3/RunFilters";
 import { TaskRunsTable } from "~/components/runs/v3/TaskRunsTable";
+import { useOrganization } from "~/hooks/useOrganizations";
 import { useProject } from "~/hooks/useProject";
 import { useUser } from "~/hooks/useUser";
 import { RunListPresenter } from "~/presenters/v3/RunListPresenter.server";
 import { requireUserId } from "~/services/session.server";
 import { cn } from "~/utils/cn";
-import { ProjectParamSchema } from "~/utils/pathBuilder";
+import { ProjectParamSchema, v3TestPath } from "~/utils/pathBuilder";
 import { ListPagination } from "../../components/ListPagination";
-import { PageBody } from "~/components/layout/AppLayout";
-import { NavBar, PageTitle } from "~/components/primitives/PageHeader";
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const userId = await requireUserId(request);
@@ -75,8 +82,54 @@ export default function Page() {
             />
             <ListPagination list={list} className="mt-2 justify-end" />
           </div>
+          <RunTaskInstructions />
         </div>
       </PageBody>
     </>
+  );
+}
+
+function RunTaskInstructions() {
+  const organization = useOrganization();
+  const project = useProject();
+  return (
+    <MainCenteredContainer className="max-w-prose">
+      <Header1 className="mb-4 border-b py-4">How to run a task</Header1>
+      <StepNumber stepNumber="A" title="Trigger a test run" />
+      <StepContentContainer>
+        <Paragraph spacing>
+          You can perform a Run with any payload you want, or use one of our examples, on the test
+          page.
+        </Paragraph>
+        <LinkButton
+          to={v3TestPath(organization, project)}
+          variant="primary/medium"
+          LeadingIcon={BeakerIcon}
+          className="inline-flex"
+        >
+          Test
+        </LinkButton>
+        <div className="mt-5 flex items-center gap-2">
+          <hr className="w-full" />
+          <Paragraph variant="extra-extra-small/dimmed/caps">OR</Paragraph>
+          <hr className="w-full" />
+        </div>
+      </StepContentContainer>
+
+      <StepNumber stepNumber="B" title="Trigger your task for real" />
+      <StepContentContainer>
+        <Paragraph spacing>
+          Performing a real run depends on the type of Trigger you Task is using.
+        </Paragraph>
+        <LinkButton
+          to={v3TestPath(organization, project)}
+          variant="primary/medium"
+          LeadingIcon={BookOpenIcon}
+          className="inline-flex"
+        >
+          How to run a task
+        </LinkButton>
+      </StepContentContainer>
+    </MainCenteredContainer>
   );
 }
