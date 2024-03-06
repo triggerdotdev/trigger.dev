d304f83ffec7dd223cca40427c78653623dfbe4b
Matt Aitken <matt@mattaitken.com>
2024-03-06 17:52:38 +0000

    Fix for typecheck error

--- apps/webapp/app/routes/storybook.free-plan-usage/route.tsx
+++ apps/webapp/app/routes/storybook.free-plan-usage/route.tsx
@@ -1,37 +1,19 @@
 import { FreePlanUsage } from "~/components/billing/FreePlanUsage";
-import { MatchedOrganization } from "~/hooks/useOrganizations";
-import { organizationBillingPath } from "~/utils/pathBuilder";
 
-const mockOrganization: MatchedOrganization = {
-  id: "mockID",
-  title: "mockTitle",
-  slug: "mockSlug",
-  projects: [
-    { id: "mockId1", slug: "mockSlug1", name: "mockName1", jobCount: 1 },
-    { id: "mockId2", slug: "mockSlug2", name: "mockName2", jobCount: 2 },
-  ],
-  hasUnconfiguredIntegrations: false,
-  runsEnabled: true,
-};
-
-type FreePlanUsageBarProps = {
-  organization: MatchedOrganization;
-};
-
-export default function Story({ organization = mockOrganization }: FreePlanUsageBarProps) {
+export default function Story() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-4 p-12">
       <div className="w-fit">
-        <FreePlanUsage to={organizationBillingPath(organization)} percentage={0.1} />
+        <FreePlanUsage to={""} percentage={0.1} />
       </div>
       <div className="w-fit">
-        <FreePlanUsage to={organizationBillingPath(organization)} percentage={0.5} />
+        <FreePlanUsage to={""} percentage={0.5} />
       </div>
       <div className="w-fit">
-        <FreePlanUsage to={organizationBillingPath(organization)} percentage={0.75} />
+        <FreePlanUsage to={""} percentage={0.75} />
       </div>
       <div className="w-fit">
-        <FreePlanUsage to={organizationBillingPath(organization)} percentage={1} />
+        <FreePlanUsage to={""} percentage={1} />
       </div>
     </div>
   );
