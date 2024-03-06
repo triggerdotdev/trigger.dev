4e516f77e4573ced33f786c9088144d32a4c22e0
James Ritchie <james@jamesritchie.co.uk>
2024-03-06 17:14:49 +0000

    improved the Sheet and feedback form

--- apps/webapp/app/components/Feedback.tsx
+++ apps/webapp/app/components/Feedback.tsx
@@ -9,7 +9,7 @@ import { Button } from "./primitives/Buttons";
 import { Fieldset } from "./primitives/Fieldset";
 import { FormButtons } from "./primitives/FormButtons";
 import { FormError } from "./primitives/FormError";
-import { Header2 } from "./primitives/Headers";
+import { Header1, Header2 } from "./primitives/Headers";
 import { InputGroup } from "./primitives/InputGroup";
 import { Label } from "./primitives/Label";
 import { Paragraph } from "./primitives/Paragraph";
@@ -57,11 +57,9 @@ export function Feedback({ button, defaultValue = "bug" }: FeedbackProps) {
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild={true}>{button}</SheetTrigger>
       <SheetContent>
-        <SheetHeader className="justify-between">How can we help?</SheetHeader>
         <SheetBody className="flex h-full flex-col justify-between">
           <DiscordBanner />
-          <hr className="mb-3" />
-          <Header2 className="mb-4">Send us a message</Header2>
+          <Header2 className="mb-4">How can we help?</Header2>
           <Form method="post" action="/resources/feedback" {...form.props}>
             <Fieldset className="max-w-full">
               <input value={location.pathname} {...conform.input(path, { type: "hidden" })} />
@@ -88,14 +86,14 @@ export function Feedback({ button, defaultValue = "bug" }: FeedbackProps) {
                 <FormError id={message.errorId}>{message.error}</FormError>
               </InputGroup>
               <FormError>{form.error}</FormError>
-              <div className="flex w-full items-center justify-between">
+              <div className="flex w-full items-start justify-between">
                 <Paragraph variant="small" className="w-full">
                   We read every message and respond quickly.
                 </Paragraph>
                 <FormButtons
                   className="m-0 w-max"
                   confirmButton={
-                    <Button type="submit" variant="primary/medium">
+                    <Button type="submit" variant="primary/small">
                       Send
                     </Button>
                   }
@@ -114,20 +112,20 @@ function DiscordBanner() {
     <a
       href="https://trigger.dev/discord"
       target="_blank"
-      className="group mb-4 flex w-full items-center justify-between rounded-md border border-charcoal-600 bg-gradient-to-br from-blue-400/30 to-indigo-400/50 p-4 transition hover:border-indigo-400"
+      className="group mb-4 flex w-full items-center justify-between rounded-md border border-grid-bright p-4 transition hover:border-text-link"
     >
       <div className="flex flex-col gap-y-2">
         <DiscordIcon className="h-8 w-8" />
-        <h2 className="font-title text-2xl text-text-bright transition group-hover:text-white">
+        <Header1 className="text-2xl font-semibold text-text-bright transition group-hover:text-white">
           Join the Trigger.dev
           <br />
           Discord community
-        </h2>
+        </Header1>
         <Paragraph variant="small/bright">
           Get help or answer questions from the Trigger.dev community.
         </Paragraph>
       </div>
-      <ChevronRightIcon className="h-5 w-5 text-charcoal-400 transition group-hover:translate-x-1 group-hover:text-indigo-400" />
+      <ChevronRightIcon className="size-5 text-grid-bright transition group-hover:translate-x-1 group-hover:text-text-link" />
     </a>
   );
 }
