11820ea24a43b8cbd1ef440dee554ac478081759
nicktrn <55853254+nicktrn@users.noreply.github.com>
2024-03-16 20:36:38 +0000

    add checkpoint requirement checks to docker provider

--- apps/docker-provider/src/index.ts
+++ apps/docker-provider/src/index.ts
@@ -10,8 +10,63 @@ const OTEL_EXPORTER_OTLP_ENDPOINT =
 
 const logger = new SimpleLogger(`[${MACHINE_NAME}]`);
 
+type InitializeReturn = {
+  canCheckpoint: boolean;
+  willSimulate: boolean;
+};
+
 class DockerTaskOperations implements TaskOperations {
+  #initialized = false;
+  #canCheckpoint = false;
+
   constructor(private opts = { forceSimulate: false }) {}
+
+  async #initialize(): Promise<InitializeReturn> {
+    if (this.#initialized) {
+      return this.#getInitializeReturn();
+    }
+
+    logger.log("Initializing task operations");
+
+    if (this.opts.forceSimulate) {
+      logger.log("Forced simulation enabled. Will simulate regardless of checkpoint support.");
+    }
+
+    try {
+      await $`criu --version`;
+    } catch (error) {
+      logger.error("No checkpoint support: Missing CRIU binary. Will simulate instead.");
+      this.#canCheckpoint = false;
+      this.#initialized = true;
+
+      return this.#getInitializeReturn();
+    }
+
+    try {
+      await $`docker checkpoint`;
+    } catch (error) {
+      logger.error("No checkpoint support: Docker needs to have experimental features enabled");
+      logger.error("Will simulate instead");
+      this.#canCheckpoint = false;
+      this.#initialized = true;
+
+      return this.#getInitializeReturn();
+    }
+
+    logger.log("Full checkpoint support!");
+
+    this.#initialized = true;
+    this.#canCheckpoint = true;
+
+    return this.#getInitializeReturn();
+  }
+
+  #getInitializeReturn(): InitializeReturn {
+    return {
+      canCheckpoint: this.#canCheckpoint,
+      willSimulate: !this.#canCheckpoint || this.opts.forceSimulate,
+    };
+  }
 
   async index(opts: {
     contentHash: string;
@@ -20,6 +75,8 @@ class DockerTaskOperations implements TaskOperations {
     apiKey: string;
     apiUrl: string;
   }) {
+    await this.#initialize();
+
     const containerName = this.#getIndexContainerName(opts.contentHash);
 
     logger.log(`Indexing task ${opts.imageTag}`, {
@@ -43,6 +100,8 @@ class DockerTaskOperations implements TaskOperations {
     machine: Machine;
     envId: string;
   }) {
+    await this.#initialize();
+
     const containerName = this.#getRunContainerName(opts.attemptId);
 
     const { exitCode } = logger.debug(
@@ -60,9 +119,11 @@ class DockerTaskOperations implements TaskOperations {
     checkpointRef: string;
     machine: Machine;
   }) {
+    await this.#initialize();
+
     const containerName = this.#getRunContainerName(opts.attemptId);
 
-    if (this.opts.forceSimulate) {
+    if (!this.#canCheckpoint || this.opts.forceSimulate) {
       logger.log("Simulating restore");
 
       const { exitCode } = logger.debug(await $`docker unpause ${containerName}`);
@@ -84,10 +145,14 @@ class DockerTaskOperations implements TaskOperations {
   }
 
   async delete(opts: { runId: string }) {
+    await this.#initialize();
+
     logger.log("noop: delete");
   }
 
   async get(opts: { runId: string }) {
+    await this.#initialize();
+
     logger.log("noop: get");
   }
 
