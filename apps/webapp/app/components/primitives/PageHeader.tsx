6bd00cd2a2e7fa3e8194602db6dfafb5ea9d9366
James Ritchie <james@jamesritchie.co.uk>
2024-03-20 12:08:38 +0000

    Removed feedback button from PageHeader

--- apps/webapp/app/components/primitives/PageHeader.tsx
+++ apps/webapp/app/components/primitives/PageHeader.tsx
@@ -1,18 +1,17 @@
 import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
 import { Link, useNavigation } from "@remix-run/react";
 import { useOptionalOrganization } from "~/hooks/useOrganizations";
+import { useOptionalProject } from "~/hooks/useProject";
 import { cn } from "~/utils/cn";
 import { plansPath } from "~/utils/pathBuilder";
 import { UpgradePrompt, useShowUpgradePrompt } from "../billing/UpgradePrompt";
 import { BreadcrumbIcon } from "./BreadcrumbIcon";
-import { Button, LinkButton } from "./Buttons";
+import { LinkButton } from "./Buttons";
 import { Header2 } from "./Headers";
 import { LoadingBarDivider } from "./LoadingBarDivider";
 import { NamedIcon } from "./NamedIcon";
 import { Paragraph } from "./Paragraph";
 import { Tabs, TabsProps } from "./Tabs";
-import { useOptionalProject } from "~/hooks/useProject";
-import { Feedback } from "../Feedback";
 
 type WithChildren = {
   children: React.ReactNode;
@@ -24,29 +23,11 @@ export function NavBar({ children }: WithChildren) {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading" || navigation.state === "submitting";
   const project = useOptionalProject();
-  const isV3Project = project?.version === "V3";
 
   return (
     <div>
       <div className="grid h-10 w-full grid-rows-[auto_1px] bg-background-bright">
-        <div className="flex w-full items-center justify-between pl-3 pr-1">
-          {children}
-          {isV3Project && (
-            <Feedback
-              defaultValue="developer preview"
-              button={
-                <Button
-                  variant="secondary/small"
-                  LeadingIcon="log"
-                  leadingIconClassName="text-primary"
-                  data-action="help & feedback"
-                >
-                  Developer preview feedback
-                </Button>
-              }
-            />
-          )}
-        </div>
+        <div className="flex w-full items-center justify-between pl-3 pr-1">{children}</div>
         <LoadingBarDivider isLoading={isLoading} />
       </div>
       {showUpgradePrompt.shouldShow && organization && (
