f0e4630975c78321119b94eb240dda967ba9dbe1
Eric Allam <eallam@icloud.com>
2024-03-21 16:48:51 +0000

    Added logging around resolving dependency paths

--- packages/cli-v3/src/utilities/build.ts
+++ packages/cli-v3/src/utilities/build.ts
@@ -1,6 +1,7 @@
 import { Config } from "@trigger.dev/core/v3";
 import type { Plugin } from "esbuild";
 import { logger } from "./logger";
+import { createRequire } from "node:module";
 
 export function bundleDependenciesPlugin(config: Config): Plugin {
   return {
@@ -14,14 +15,20 @@ export function bundleDependenciesPlugin(config: Config): Plugin {
         for (let pattern of config.dependenciesToBundle ?? []) {
           // bundle it if the path matches the pattern
           if (typeof pattern === "string" ? args.path === pattern : pattern.test(args.path)) {
-            const resolvedPath = require.resolve(args.path);
+            try {
+              const resolvedPath = resolvePath(args.path);
 
-            logger.debug(`Bundling ${args.path} as ${resolvedPath}`);
+              logger.debug(`Bundling ${args.path} as ${resolvedPath}`);
 
-            return {
-              path: resolvedPath,
-              external: false,
-            };
+              return {
+                path: resolvedPath,
+                external: false,
+              };
+            } catch (error) {
+              logger.error(`Failed to resolve path ${args.path}`, error);
+
+              return undefined;
+            }
           }
         }
 
@@ -29,4 +36,22 @@ export function bundleDependenciesPlugin(config: Config): Plugin {
       });
     },
   };
+}
+
+function resolvePath(path: string): string {
+  try {
+    logger.debug("[bundle-dependencies] Attempting to resolve path using ESM resolver", {
+      path,
+      importMetaUrl: import.meta.url,
+    });
+
+    return createRequire(import.meta.url).resolve(path);
+  } catch (error) {
+    logger.debug("[bundle-dependencies] Failed. Falling back to require.resolve", {
+      path,
+      error,
+    });
+
+    return require.resolve(path);
+  }
 }
