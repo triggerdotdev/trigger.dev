4f9b8d6721c450e30f6b1e6aee10265ac9f59ec2
Matt Aitken <matt@mattaitken.com>
2024-03-27 16:22:29 +0000

    Task that logs a lot

--- references/v3-catalog/src/trigger/logging.ts
+++ references/v3-catalog/src/trigger/logging.ts
@@ -1,4 +1,4 @@
-import { task } from "@trigger.dev/sdk/v3";
+import { logger, task, wait } from "@trigger.dev/sdk/v3";
 import slugify from "@sindresorhus/slugify";
 
 export const loggingTask = task({
@@ -7,5 +7,15 @@ export const loggingTask = task({
     console.log(`Hello world 9 ${slugify("foo bar")}`);
 
     return null;
+  },
+});
+
+export const lotsOfLogs = task({
+  id: "lots-of-logs",
+  run: async (payload: { count: number }) => {
+    for (let i = 0; i < payload.count; i++) {
+      logger.info(`Hello world ${i} ${slugify("foo bar")}`);
+      await wait.for({ seconds: 0.1 });
+    }
   },
 });
