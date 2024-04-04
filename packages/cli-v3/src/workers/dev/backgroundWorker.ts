75397aea81b5bf4e7d318e69de85f0f74d45a2db
Eric Allam <eallam@icloud.com>
2024-04-04 12:05:25 +0100

    Pass (and log) cwd during background worker initialization

--- packages/cli-v3/src/workers/dev/backgroundWorker.ts
+++ packages/cli-v3/src/workers/dev/backgroundWorker.ts
@@ -319,9 +319,14 @@ export class BackgroundWorker {
 
     let resolved = false;
 
+    const cwd = dirname(this.path);
+
+    logger.debug("Initializing worker", { path: this.path, cwd });
+
     this.tasks = await new Promise<Array<TaskMetadataWithFilePath>>((resolve, reject) => {
       const child = fork(this.path, {
         stdio: [/*stdin*/ "ignore", /*stdout*/ "pipe", /*stderr*/ "pipe", "ipc"],
+        cwd,
         env: {
           ...this.params.env,
           ...this.#readEnvVars(),
@@ -580,14 +585,17 @@ class TaskRunProcess {
       ...(this.worker.debugOtel ? { OTEL_LOG_LEVEL: "debug" } : {}),
     };
 
+    const cwd = dirname(this.path);
+
     logger.debug(`[${this.execution.run.id}] initializing task run process`, {
       env: fullEnv,
       path: this.path,
+      cwd,
     });
 
     this._child = fork(this.path, {
       stdio: [/*stdin*/ "ignore", /*stdout*/ "pipe", /*stderr*/ "pipe", "ipc"],
-      cwd: dirname(this.path),
+      cwd,
       env: fullEnv,
       execArgv: this.worker.debuggerOn
         ? ["--inspect-brk", "--trace-uncaught", "--no-warnings=ExperimentalWarning"]
