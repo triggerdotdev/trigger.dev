397391a92ede08bfa8f10ec39c8d7d4bcbeccbb6
Matt Aitken <matt@mattaitken.com>
2024-04-15 19:09:10 +0100

    Tidied imports and fixed name of options object

--- apps/webapp/app/v3/services/deleteTaskSchedule.server.ts
+++ apps/webapp/app/v3/services/deleteTaskSchedule.server.ts
@@ -1,19 +1,13 @@
-import { Prisma, TaskSchedule } from "@trigger.dev/database";
-import { generateFriendlyId } from "../friendlyIdentifiers";
 import { BaseService } from "./baseService.server";
-import { $transaction, PrismaClientOrTransaction } from "~/db.server";
-import { nanoid } from "nanoid";
-import { RegisterNextTaskScheduleInstanceService } from "./registerNextTaskScheduleInstance.server";
-import { CreateSchedule, CronPattern } from "../schedules";
 
-export type UpsertTaskScheduleServiceOptions = {
+type Options = {
   projectId: string;
   userId: string;
   friendlyId: string;
 };
 
 export class DeleteTaskScheduleService extends BaseService {
-  public async call({ projectId, userId, friendlyId }: UpsertTaskScheduleServiceOptions) {
+  public async call({ projectId, userId, friendlyId }: Options) {
     //first check that the user has access to the project
     const project = await this._prisma.project.findFirst({
       where: {
