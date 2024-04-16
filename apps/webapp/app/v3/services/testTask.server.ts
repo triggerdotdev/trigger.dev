0cd28171c5ab38364f4296de9b6758575f61a760
Matt Aitken <matt@mattaitken.com>
2024-04-16 16:35:05 +0100

    Test runs are working for schedules

--- apps/webapp/app/v3/services/testTask.server.ts
+++ apps/webapp/app/v3/services/testTask.server.ts
@@ -2,6 +2,8 @@ import { findEnvironmentById } from "~/models/runtimeEnvironment.server";
 import { BaseService } from "./baseService.server";
 import { TriggerTaskService } from "./triggerTask.server";
 import { TestTaskData } from "../testTask";
+import { nextScheduledTimestamps } from "../utils/calculateNextSchedule.server";
+import { stringifyIO } from "@trigger.dev/core/v3";
 
 export class TestTaskService extends BaseService {
   public async call(userId: string, data: TestTaskData) {
@@ -16,15 +18,29 @@ export class TestTaskService extends BaseService {
       case "STANDARD":
         return await triggerTaskService.call(data.taskIdentifier, authenticatedEnvironment, {
           payload: data.payload,
+          options: {
+            test: true,
+          },
         });
       case "SCHEDULED": {
-        //todo put data into the correct format
-        // return await triggerTaskService.call(data.taskIdentifier, authenticatedEnvironment, {
-        //   payload: data.payload,
-        //   options: {
-        //     scheduled: true,
-        //   },
-        // });
+        const payload = {
+          scheduleId: "sched_1234",
+          timestamp: data.timestamp,
+          lastTimestamp: data.lastTimestamp,
+          externalId: data.externalId,
+          upcoming: [],
+        };
+        const payloadPacket = await stringifyIO(payload);
+
+        return await triggerTaskService.call(
+          data.taskIdentifier,
+          authenticatedEnvironment,
+          {
+            payload: payloadPacket.data,
+            options: { payloadType: payloadPacket.dataType, test: true },
+          },
+          { customIcon: "scheduled" }
+        );
       }
       default:
         throw new Error("Invalid trigger source");
