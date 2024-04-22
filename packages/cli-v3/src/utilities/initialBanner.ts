2e406e9ad73a100da81aab45eb21ea85d6b7abfa
nicktrn <55853254+nicktrn@users.noreply.github.com>
2024-04-22 12:12:40 +0100

    fix cli update check while in beta

--- packages/cli-v3/src/utilities/initialBanner.ts
+++ packages/cli-v3/src/utilities/initialBanner.ts
@@ -68,7 +68,7 @@ async function doUpdateCheck(): Promise<string | undefined> {
   try {
     // default cache for update check is 1 day
     update = await checkForUpdate(pkg, {
-      distTag: pkg.version.startsWith("0.0.0") ? "beta" : "latest",
+      distTag: pkg.version.startsWith("3.0.0-beta") ? "beta" : "latest",
     });
   } catch (err) {
     // ignore error
