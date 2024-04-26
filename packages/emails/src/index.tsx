d7c35cde918b8ea577b1c2064d7ec537cfe8acd2
Eric Allam <eallam@icloud.com>
2024-04-26 16:39:54 +0100

    Remove unnecessary emails

--- packages/emails/src/index.tsx
+++ packages/emails/src/index.tsx
@@ -1,23 +1,19 @@
+import { render } from "@react-email/render";
 import { ReactElement } from "react";
-import { setGlobalBasePath } from "../emails/components/BasePath";
-import WelcomeEmail from "../emails/welcome";
-import MagicLinkEmail from "../emails/magic-link";
-import ConnectIntegration from "../emails/connect-integration";
-import WorkflowFailed from "../emails/workflow-failed";
-import WorkflowIntegration from "../emails/workflow-integration";
-import InviteEmail, { InviteEmailSchema } from "../emails/invite";
 import AlertAttemptFailureEmail, { AlertAttemptEmailSchema } from "../emails/alert-attempt-failure";
+import { setGlobalBasePath } from "../emails/components/BasePath";
 import AlertDeploymentFailureEmail, {
   AlertDeploymentFailureEmailSchema,
 } from "../emails/deployment-failure";
 import AlertDeploymentSuccessEmail, {
   AlertDeploymentSuccessEmailSchema,
 } from "../emails/deployment-success";
-import { render } from "@react-email/render";
+import InviteEmail, { InviteEmailSchema } from "../emails/invite";
+import MagicLinkEmail from "../emails/magic-link";
+import WelcomeEmail from "../emails/welcome";
 
 import { Resend } from "resend";
 import { z } from "zod";
-import React from "react";
 
 export const DeliverEmailSchema = z
   .discriminatedUnion("email", [
@@ -33,20 +29,6 @@ export const DeliverEmailSchema = z
     AlertAttemptEmailSchema,
     AlertDeploymentFailureEmailSchema,
     AlertDeploymentSuccessEmailSchema,
-    z.object({
-      email: z.literal("connect_integration"),
-      workflowId: z.string(),
-      integration: z.string(),
-    }),
-    z.object({
-      email: z.literal("workflow_failed"),
-      workflowId: z.string(),
-    }),
-    z.object({
-      email: z.literal("workflow_integration"),
-      workflowId: z.string(),
-      integration: z.string(),
-    }),
   ])
   .and(z.object({ to: z.string() }));
 
@@ -113,25 +95,6 @@ export class EmailClient {
         return {
           subject: `You've been invited to join ${data.orgName} on Trigger.dev`,
           component: <InviteEmail {...data} />,
-        };
-      case "connect_integration":
-        return {
-          subject: `Action required: you need to connect to ${data.integration}`,
-          component: (
-            <ConnectIntegration workflowId={data.workflowId} integration={data.integration} />
-          ),
-        };
-      case "workflow_failed":
-        return {
-          subject: "Action required: your workflow has stopped running!",
-          component: <WorkflowFailed workflowId={data.workflowId} />,
-        };
-      case "workflow_integration":
-        return {
-          subject: `Action required: connect ${data.integration} to start your workflow`,
-          component: (
-            <WorkflowIntegration workflowId={data.workflowId} integration={data.integration} />
-          ),
         };
       case "alert-attempt": {
         return {
