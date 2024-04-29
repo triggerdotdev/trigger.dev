99e94b17d87d743aca0656f51cfdd7ab7036cee1
Matt Aitken <matt@mattaitken.com>
2024-04-29 18:20:55 +0100

    Fix for up/down navigation when filtered

--- apps/webapp/app/components/primitives/TreeView/reducer.ts
+++ apps/webapp/app/components/primitives/TreeView/reducer.ts
@@ -421,7 +421,7 @@ export function reducer(state: TreeState, action: Action): TreeState {
       return state;
     }
     case "SELECT_LAST_VISIBLE_NODE": {
-      const node = lastVisibleNode(state.tree, state.nodes);
+      const node = lastVisibleNode(state.tree, state.filteredNodes);
       if (node) {
         return reducer(state, {
           type: "SELECT_NODE",
@@ -447,7 +447,7 @@ export function reducer(state: TreeState, action: Action): TreeState {
         });
       }
 
-      const visible = visibleNodes(state.tree, state.nodes);
+      const visible = visibleNodes(state.tree, state.filteredNodes);
       const selectedIndex = visible.findIndex((node) => node.id === selected);
       const nextNode = visible[selectedIndex + 1];
       if (nextNode) {
@@ -476,9 +476,9 @@ export function reducer(state: TreeState, action: Action): TreeState {
         });
       }
 
-      const visible = visibleNodes(state.tree, state.nodes);
+      const visible = visibleNodes(state.tree, state.filteredNodes);
       const selectedIndex = visible.findIndex((node) => node.id === selected);
-      const previousNode = visible[selectedIndex - 1];
+      const previousNode = visible[Math.max(0, selectedIndex - 1)];
       if (previousNode) {
         return reducer(state, {
           type: "SELECT_NODE",
