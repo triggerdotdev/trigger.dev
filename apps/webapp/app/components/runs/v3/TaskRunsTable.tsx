8bd83bea988dd66d4751116164258bd600ebcfe5
Matt Aitken <matt@mattaitken.com>
2024-05-17 11:26:33 +0100

    Don’t focus the checkbox

--- apps/webapp/app/components/runs/v3/TaskRunsTable.tsx
+++ apps/webapp/app/components/runs/v3/TaskRunsTable.tsx
@@ -89,7 +89,6 @@ export function TaskRunsTable({
           {allowSelection && (
             <TableHeaderCell>
               <Checkbox
-                autoFocus
                 checked={hasAll(runs.map((r) => r.id))}
                 onChange={(element) => {
                   const ids = runs.map((r) => r.id);
