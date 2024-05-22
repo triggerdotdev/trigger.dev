14843e1d113f226f0f9c0f2e034715b7c2209904
Émile Ré <nemile.re@gmail.com>
2024-05-22 18:29:08 +0200

    Boilerplate server-only use case

---
+++ packages/cli-v3/test/fixtures/server-only/trigger/helloWorld.js
@@ -0,0 +1,9 @@
+import "server-only";
+import { task } from "@trigger.dev/sdk/v3";
+
+export const helloWorldTask = task({
+  id: "hello-world",
+  run: async (payload) => {
+    console.log("Hello, World!", payload);
+  },
+});
