cf560aef7f28fc770a7ac23c4cc73d939f8e5760
Émile Ré <nemile.re@gmail.com>
2024-05-28 17:44:07 +0200

    Add pnpm script to run e2e tests only

--- packages/cli-v3/e2e/vite.config.js
+++ packages/cli-v3/e2e/vite.config.js
@@ -3,6 +3,6 @@ import { configDefaults, defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    exclude: [...configDefaults.exclude, "e2e/**/*"],
+    exclude: [...configDefaults.exclude, "src/**/*"],
   },
 });
