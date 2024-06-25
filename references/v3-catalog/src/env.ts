77ad4127cb7c4c15a339b3597af15400c2efaad9
Eric Allam <eallam@icloud.com>
2024-06-25 09:08:43 +0100

    v3: Improved ESM module require error detection logic

---
+++ references/v3-catalog/src/env.ts
@@ -0,0 +1,32 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    TRIGGER_SECRET_KEY: z.string(),
+    OPENAI_API_KEY: z.string().min(1),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {
+    NEXT_PUBLIC_TEST: z.string().min(1).optional(),
+  },
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    TRIGGER_SECRET_KEY: process.env.TRIGGER_SECRET_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    NEXT_PUBLIC_TEST: process.env.NEXT_PUBLIC_TEST,
+  },
+});
