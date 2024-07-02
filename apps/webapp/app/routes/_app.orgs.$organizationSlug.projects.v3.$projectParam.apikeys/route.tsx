57fec71fd92777751adcb78b203499b9e3bb61c4
James Ritchie <james@jamesritchie.co.uk>
2024-07-02 14:41:08 +0100

    better panel width

--- apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.v3.$projectParam.apikeys/route.tsx
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.v3.$projectParam.apikeys/route.tsx
@@ -125,7 +125,7 @@ export default function Page() {
           </Table>
 
           <div className="flex gap-3">
-            <InfoPanel icon={InformationCircleIcon} title="Secret keys">
+            <InfoPanel icon={InformationCircleIcon} title="Secret keys" panelClassName="max-w-md">
               <Paragraph variant="small">
                 Secret keys should be used on your server. They give full API access and allow you
                 to <TextLink to={docsPath("v3/triggering")}>trigger tasks</TextLink> from your
