d505ff09ce70882747d63650d5f061f30bd71c4c
Eric Allam <eallam@icloud.com>
2024-07-04 14:19:24 +0100

    Remove logger

--- apps/webapp/app/routes/metrics.ts
+++ apps/webapp/app/routes/metrics.ts
@@ -1,10 +1,7 @@
 import { LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { metricsRegister } from "~/metrics.server";
-import { logger } from "~/services/logger.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  logger.debug("Getting metrics from the metrics register");
-
   return new Response(await metricsRegister.metrics(), {
     headers: {
       "Content-Type": metricsRegister.contentType,
