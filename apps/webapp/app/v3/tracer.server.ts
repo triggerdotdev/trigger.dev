8b9947516aa598eead83e024ccf712b1afebe1a5
Jacob Paris <jacob@jacobparis.com>
2024-07-05 19:17:57 +0300

    fix: manual require prisma instrumentation

--- apps/webapp/app/v3/tracer.server.ts
+++ apps/webapp/app/v3/tracer.server.ts
@@ -33,7 +33,8 @@ import { env } from "~/env.server";
 import { AuthenticatedEnvironment } from "~/services/apiAuth.server";
 import { singleton } from "~/utils/singleton";
 import { LoggerSpanExporter } from "./telemetry/loggerExporter.server";
-
+import { createRequire } from "module";
+const require = createRequire(import.meta.url ?? __filename);
 export const SEMINTATTRS_FORCE_RECORDING = "forceRecording";
 
 class CustomWebappSampler implements Sampler {
@@ -171,6 +172,7 @@ function getTracer() {
   ];
 
   if (env.INTERNAL_OTEL_TRACE_INSTRUMENT_PRISMA_ENABLED === "1") {
+    const { PrismaInstrumentation } = require("@prisma/instrumentation");
     instrumentations.push(new PrismaInstrumentation());
   }
 
