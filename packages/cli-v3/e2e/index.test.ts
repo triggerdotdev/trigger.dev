3e46665c3b31857ab4f2d9ba0a931d294c1ead1b
Émile Ré <nemile.re@gmail.com>
2024-07-09 12:23:10 +0400

    Fix setup/teardown

--- packages/cli-v3/e2e/index.test.ts
+++ packages/cli-v3/e2e/index.test.ts
@@ -49,8 +49,8 @@ logger.loggerLevel = options.logLevel;
 
 if (testCases.length > 0) {
   describe.concurrent("bundling", async () => {
-    beforeEach<E2EFixtureTest>(async ({ dir, packageManager }) => {
-      await rimraf(join(dir, "**/node_modules"), {
+    beforeEach<E2EFixtureTest>(async ({ dir, packageManager, skip }) => {
+      await rimraf(join(dir, "**/node_modules/**"), {
         glob: true,
       });
       await rimraf(join(dir, ".yarn"), { glob: true });
