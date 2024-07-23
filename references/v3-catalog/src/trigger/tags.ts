4c6ad3b4d597a526090dceecb2aacdaac5b615e7
Matt Aitken <matt@mattaitken.com>
2024-07-23 12:05:29 +0100

    More comprehensive test of tags with all triggering functions

--- references/v3-catalog/src/trigger/tags.ts
+++ references/v3-catalog/src/trigger/tags.ts
@@ -1,5 +1,5 @@
 import { RunTags } from "@trigger.dev/core/v3";
-import { logger, runs, tags, task } from "@trigger.dev/sdk/v3";
+import { logger, runs, tags, task, tasks } from "@trigger.dev/sdk/v3";
 import { simpleChildTask } from "./subtasks";
 
 type Payload = {
@@ -10,9 +10,52 @@ export const triggerRunsWithTags = task({
   id: "trigger-runs-with-tags",
   run: async (payload: Payload, { ctx }) => {
     const { id } = await simpleChildTask.trigger(
-      { message: "Hello from triggerRunsWithTags" },
+      { message: "trigger from triggerRunsWithTags" },
       { tags: payload.tags }
     );
+
+    await simpleChildTask.triggerAndWait(
+      { message: "triggerAndWait from triggerRunsWithTags" },
+      { tags: payload.tags }
+    );
+
+    await simpleChildTask.batchTrigger([
+      {
+        payload: { message: "batchTrigger 1 from triggerRunsWithTags" },
+        options: { tags: payload.tags },
+      },
+      {
+        payload: { message: "batchTrigger 2 from triggerRunsWithTags" },
+        options: { tags: payload.tags },
+      },
+    ]);
+
+    const results = await simpleChildTask.batchTriggerAndWait([
+      {
+        payload: { message: "batchTriggerAndWait 1 from triggerRunsWithTags" },
+        options: { tags: payload.tags },
+      },
+      {
+        payload: { message: "batchTriggerAndWait 2 from triggerRunsWithTags" },
+        options: { tags: payload.tags },
+      },
+    ]);
+
+    await tasks.trigger<typeof simpleChildTask>(
+      "simple-child-task",
+      { message: "tasks.trigger from triggerRunsWithTags" },
+      { tags: payload.tags }
+    );
+    await tasks.batchTrigger<typeof simpleChildTask>("simple-child-task", [
+      {
+        payload: { message: "tasks.batchTrigger 1 from triggerRunsWithTags" },
+        options: { tags: payload.tags },
+      },
+      {
+        payload: { message: "tasks.batchTrigger 2 from triggerRunsWithTags" },
+        options: { tags: payload.tags },
+      },
+    ]);
 
     const run = await runs.retrieve(id);
     logger.log("run", run);
