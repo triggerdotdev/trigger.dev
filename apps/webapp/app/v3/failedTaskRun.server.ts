ccea150396684932dd15daa4f122495419d6b6e2
Matt Aitken <matt@mattaitken.com>
2024-08-08 12:38:54 +0100

    Import tidy

--- apps/webapp/app/v3/failedTaskRun.server.ts
+++ apps/webapp/app/v3/failedTaskRun.server.ts
@@ -1,8 +1,6 @@
 import { TaskRunFailedExecutionResult } from "@trigger.dev/core/v3";
-import { logger } from "~/services/logger.server";
-import { marqs } from "~/v3/marqs/index.server";
-
 import { TaskRunStatus } from "@trigger.dev/database";
+import { logger } from "~/services/logger.server";
 import { createExceptionPropertiesFromError, eventRepository } from "./eventRepository.server";
 import { BaseService } from "./services/baseService.server";
 import { FinalizeTaskRunService } from "./services/finalizeTaskRun.server";
