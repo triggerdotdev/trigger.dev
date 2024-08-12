1d12f8a619dc048574ef32740719b64b99da0721
Matt Aitken <matt@mattaitken.com>
2024-08-12 15:15:23 +0100

    Use the cookie package

--- apps/webapp/app/services/resizablePanel.server.ts
+++ apps/webapp/app/services/resizablePanel.server.ts
@@ -1,28 +1,39 @@
-import { createCookie, createCookieSessionStorage } from "@remix-run/node";
+import { parse } from "cookie";
 import { type ResizableSnapshot } from "~/components/primitives/Resizable";
 import { logger } from "./logger.server";
 
 export async function getResizableSnapshot(
   request: Request,
   id: string
-): Promise<ResizableSnapshot> {
-  const cookie = createCookie(id, {
-    httpOnly: false,
-    secure: false,
-    path: "/",
-  });
-
+): Promise<ResizableSnapshot | undefined> {
   try {
-    const header = request.headers.get("Cookie");
-    const value = await cookie.parse(header);
-    logger.info("getResizableSnapshot", { id, value, cookie });
-    if (value != null && "status" in value) {
-      return value as ResizableSnapshot;
-    } else {
+    const cookieHeader = request.headers.get("Cookie");
+    if (!cookieHeader) {
       return undefined;
     }
+
+    const cookies = parse(cookieHeader);
+    const cookieValue = cookies[id];
+
+    if (cookieValue) {
+      try {
+        const parsedValue = JSON.parse(cookieValue) as any;
+        if (typeof parsedValue === "object" && "status" in parsedValue) {
+          return parsedValue as ResizableSnapshot;
+        }
+      } catch (parseError) {
+        logger.error("getResizableSnapshot() error parsing cookie value:", {
+          parseError,
+          cookieValue,
+        });
+      }
+    }
+
+    return undefined;
   } catch (error) {
-    console.error(error);
+    logger.error("getResizableSnapshot() error:", {
+      error,
+    });
     return undefined;
   }
 }
