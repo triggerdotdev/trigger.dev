c82a3ad5078bda86c4911c463564fa0340924020
Eric Allam <eallam@icloud.com>
2024-09-05 22:07:42 +0100

    projects now have a dedicated build server

--- apps/webapp/app/v3/remoteImageBuilder.server.ts
+++ apps/webapp/app/v3/remoteImageBuilder.server.ts
@@ -1,13 +1,17 @@
 import { depot } from "@depot/sdk-node";
+import { Project } from "@trigger.dev/database";
+import { prisma } from "~/db.server";
 import { env } from "~/env.server";
 
-export async function createRemoteImageBuild() {
+export async function createRemoteImageBuild(project: Project) {
   if (!env.DEPOT_TOKEN || !env.DEPOT_PROJECT_ID) {
     return;
   }
 
+  const builderProjectId = await createBuilderProjectIfNotExists(project);
+
   const result = await depot.build.v1.BuildService.createBuild(
-    { projectId: env.DEPOT_PROJECT_ID },
+    { projectId: builderProjectId },
     {
       headers: {
         Authorization: `Bearer ${env.DEPOT_TOKEN}`,
@@ -16,8 +20,40 @@ export async function createRemoteImageBuild() {
   );
 
   return {
-    projectId: env.DEPOT_PROJECT_ID,
+    projectId: builderProjectId,
     buildToken: result.buildToken,
     buildId: result.buildId,
   };
+}
+
+async function createBuilderProjectIfNotExists(project: Project) {
+  if (project.builderProjectId) {
+    return project.builderProjectId;
+  }
+
+  const result = await depot.core.v1.ProjectService.createProject(
+    {
+      name: `${env.APP_ENV} ${project.externalRef}`,
+      organizationId: env.DEPOT_ORG_ID,
+      regionId: env.DEPOT_REGION,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${env.DEPOT_TOKEN}`,
+      },
+    }
+  );
+
+  if (!result.project) {
+    throw new Error("Failed to create builder project");
+  }
+
+  await prisma.project.update({
+    where: { id: project.id },
+    data: {
+      builderProjectId: result.project.projectId,
+    },
+  });
+
+  return result.project.projectId;
 }
