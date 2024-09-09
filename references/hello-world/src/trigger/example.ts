cc5f0e18c9701013c967fae5e462452ef02dc374
Eric Allam <eallam@icloud.com>
2024-09-09 16:58:15 +0100

    Fixing the flushing/killing process with the new build system

--- references/hello-world/src/trigger/example.ts
+++ references/hello-world/src/trigger/example.ts
@@ -1,4 +1,5 @@
 import { logger, task, wait } from "@trigger.dev/sdk/v3";
+import { setTimeout } from "timers/promises";
 
 export const helloWorldTask = task({
   id: "hello-world",
@@ -20,8 +21,8 @@ export const helloWorldTask = task({
 export const parentTask = task({
   id: "parent",
   run: async (payload: any, { ctx }) => {
-    await childTask.triggerAndWait({ message: "Hello, world!" });
     logger.log("Hello, world from the parent", { payload });
+    await childTask.triggerAndWait({ message: "Hello, world!" });
   },
 });
 
@@ -29,5 +30,15 @@ export const childTask = task({
   id: "child",
   run: async (payload: any, { ctx }) => {
     logger.info("Hello, world from the child", { payload });
+
+    if (Math.random() > 0.5) {
+      throw new Error("Random error at start");
+    }
+
+    await setTimeout(10000);
+
+    if (Math.random() > 0.5) {
+      throw new Error("Random error at end");
+    }
   },
 });
