4b92ce906136761a58207fe9d1379fb3a6ad7874
Matt Aitken <matt@mattaitken.com>
2024-09-13 19:40:01 +0100

    Unsubscribe from Electric when leaving the run page

--- apps/webapp/app/hooks/useSyncTrace.ts
+++ apps/webapp/app/hooks/useSyncTrace.ts
@@ -1,5 +1,6 @@
 import { useShape } from "@electric-sql/react";
 import { TaskEvent } from "@trigger.dev/database";
+import { useEffect, useRef } from "react";
 import { createTraceTreeFromEvents } from "~/utils/taskEvent";
 
 type Params = {
@@ -11,9 +12,19 @@ export type Trace = ReturnType<typeof createTraceTreeFromEvents>;
 export type TraceEvent = Trace["events"][number];
 
 export function useSyncTrace({ origin, traceId }: Params) {
+  const aborter = useRef(new AbortController());
+
   const { data, error, isError } = useShape({
     url: `${origin}/sync/traces/${traceId}`,
+    signal: aborter.current.signal,
   });
+
+  useEffect(() => {
+    return () => {
+      aborter.current.abort();
+      aborter.current = new AbortController();
+    };
+  }, [origin, traceId]);
 
   return { error, isError, events: data ? (data as any as TaskEvent[]) : undefined };
 }
--- apps/webapp/app/hooks/useSyncTraceRuns.ts
+++ apps/webapp/app/hooks/useSyncTraceRuns.ts
@@ -1,6 +1,7 @@
 import { Prettify } from "@trigger.dev/core";
 import { TaskRun } from "@trigger.dev/database";
 import { SyncedShapeData, useSyncedShape } from "./useSyncedShape";
+import { useEffect, useRef } from "react";
 
 type Params = {
   origin: string;
@@ -10,9 +11,19 @@ type Params = {
 export type RawRun = Prettify<SyncedShapeData<TaskRun>>;
 
 export function useSyncTraceRuns({ origin, traceId }: Params) {
+  const aborter = useRef(new AbortController());
+
   const { data, error, isError } = useSyncedShape<TaskRun>({
     url: `${origin}/sync/traces/runs/${traceId}`,
+    signal: aborter.current.signal,
   });
+
+  useEffect(() => {
+    return () => {
+      aborter.current.abort();
+      aborter.current = new AbortController();
+    };
+  }, [origin, traceId]);
 
   return { runs: data, error, isError };
 }
