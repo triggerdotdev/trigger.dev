eef34a15703e4fea823d195a98bae1141380556a
Matt Aitken <matt@mattaitken.com>
2024-09-20 13:26:20 +0100

    Started scaffolding the RunEngine

--- packages/run-engine/src/index.ts
+++ packages/run-engine/src/index.ts
@@ -15,11 +15,23 @@ export class RunEngine {
     this.prisma = options.prisma;
   }
 
-  /** Creates a new run with the options, returns metadata */
+  /** Triggers one run.
+   *  This doesn't start execution, but it will schedule it for execution.
+   */
   async trigger() {
     // const result = await this.options.prisma.taskRun.create({});
     // return result;
   }
 
+  /** Triggers multiple runs.
+   * This doesn't start execution, but it will create a batch and schedule them for execution.
+   */
+  async batchTrigger() {}
+
+  /** The run can be added to the queue. When it's pulled from the queue it will be executed. */
+  async readyToExecute(runId: string) {}
+
+  /** We want to actually execute the run, this could be a continuation of a previous execution.
+   * This is called from the queue, when the run has been pulled. */
   async execute(runId: string) {}
 }
