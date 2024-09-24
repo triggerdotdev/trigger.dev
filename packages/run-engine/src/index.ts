5d63c91663ae34fa54020b603dd6307f12fa15e0
Matt Aitken <matt@mattaitken.com>
2024-09-24 15:39:55 +0100

    Notes on the flow for an execution starting

--- packages/run-engine/src/index.ts
+++ packages/run-engine/src/index.ts
@@ -29,9 +29,35 @@ export class RunEngine {
   async batchTrigger() {}
 
   /** The run can be added to the queue. When it's pulled from the queue it will be executed. */
-  async readyToExecute(runId: string) {}
+  async prepareForQueue(runId: string) {}
 
   /** We want to actually execute the run, this could be a continuation of a previous execution.
    * This is called from the queue, when the run has been pulled. */
-  async execute(runId: string) {}
+  //todo think more about this, when do we create the attempt?
+  //todo what does this actually do?
+  //todo how does it get sent to the worker? DEV and PROD
+  async prepareForExecution(runId: string) {}
+
+  async prepareForAttempt(runId: string) {}
+
+  async complete(runId: string, completion: any) {}
 }
+
+/* 
+Starting execution flow:
+
+1. Run id is pulled from a queue
+2. Prepare the run for an attempt (returns data to send to the worker)
+  a. The run is marked as "waiting to start"?
+  b. Create a TaskRunState with the run id, and the state "waiting to start".
+  c. Start a heartbeat with the TaskRunState id, in case it never starts.
+3. The run is sent to the worker
+4. When the worker has received the run, it ask the platform for an attempt
+5. The attempt is created
+  a. The attempt is created
+  b. The TaskRunState is updated to "EXECUTING"
+  c. Start a heartbeat with the TaskRunState id.
+  c. The TaskRun is updated to "EXECUTING"
+6. A response is sent back to the worker with the attempt data
+7. The code executes...
+*/
