ff89c9d09afbe45265b7d41cae98dd204fe9f34f
Eric Allam <eallam@icloud.com>
2024-10-01 10:35:20 -0700

    WIP rate limt per org

--- references/v3-catalog/src/clientUsage.ts
+++ references/v3-catalog/src/clientUsage.ts
@@ -1,14 +1,21 @@
 import { runs, tasks } from "@trigger.dev/sdk/v3";
 import type { runMetadataChildTask } from "./trigger/runMetadata.js";
+import { setTimeout } from "timers/promises";
 
 async function main() {
-  const anyHandle = await tasks.trigger<typeof runMetadataChildTask>("run-metadata-child-task", {});
-
-  const subscription = await runs.subscribe(anyHandle);
-
-  for await (const run of subscription) {
-    console.log("Received run update:", run);
+  for await (const run of runs.list({ taskIdentifier: "import-artwork", status: "QUEUED" })) {
+    console.log("Cancelling run:", run.id);
+    await runs.cancel(run.id);
+    console.log("Cancelled run:", run.id);
   }
+
+  // const anyHandle = await tasks.trigger<typeof runMetadataChildTask>("run-metadata-child-task", {});
+
+  // const subscription = await runs.subscribe(anyHandle);
+
+  // for await (const run of subscription) {
+  //   console.log("Received run update:", run);
+  // }
 }
 
 main().catch(console.error);
