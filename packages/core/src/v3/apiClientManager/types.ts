971d42d4b52c36c349b03dd86c97b0c4d9022cf7
Eric Allam <eallam@icloud.com>
2024-10-09 10:33:27 +0100

    Authenticating to the realtime API with JWTs are working

--- packages/core/src/v3/apiClientManager/types.ts
+++ packages/core/src/v3/apiClientManager/types.ts
@@ -2,6 +2,13 @@ import { type ApiRequestOptions } from "../apiClient/index.js";
 
 export type ApiClientConfiguration = {
   baseURL?: string;
+  /**
+   * @deprecated Use `accessToken` instead.
+   */
   secretKey?: string;
+  /**
+   * The access token to authenticate with the Trigger API.
+   */
+  accessToken?: string;
   requestOptions?: ApiRequestOptions;
 };
