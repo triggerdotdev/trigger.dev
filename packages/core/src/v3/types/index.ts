bd2b9a03eafe506dd759696317d16febd1d050ce
Eric Allam <eallam@icloud.com>
2024-10-10 14:08:22 +0100

    schema tasks

--- packages/core/src/v3/types/index.ts
+++ packages/core/src/v3/types/index.ts
@@ -1,81 +1,6 @@
-import { RetryOptions, TaskMetadata, TaskRunContext } from "../schemas/index.js";
-import { Prettify } from "./utils.js";
-
 export * from "./utils.js";
 export * from "./tasks.js";
 export * from "./idempotencyKeys.js";
-
-export type InitOutput = Record<string, any> | void | undefined;
-
-export type RunFnParams<TInitOutput extends InitOutput> = Prettify<{
-  /** Metadata about the task, run, attempt, queue, environment, organization, project and batch.  */
-  ctx: Context;
-  /** If you use the `init` function, this will be whatever you returned. */
-  init?: TInitOutput;
-  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
-  signal?: AbortSignal;
-}>;
-
-export type MiddlewareFnParams = Prettify<{
-  ctx: Context;
-  next: () => Promise<void>;
-  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
-  signal?: AbortSignal;
-}>;
-
-export type InitFnParams = Prettify<{
-  ctx: Context;
-  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
-  signal?: AbortSignal;
-}>;
-
-export type StartFnParams = Prettify<{
-  ctx: Context;
-  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
-  signal?: AbortSignal;
-}>;
-
-export type Context = TaskRunContext;
-
-export type SuccessFnParams<TInitOutput extends InitOutput> = RunFnParams<TInitOutput>;
-
-export type FailureFnParams<TInitOutput extends InitOutput> = RunFnParams<TInitOutput>;
-
-export type HandleErrorFnParams<TInitOutput extends InitOutput> = RunFnParams<TInitOutput> &
-  Prettify<{
-    retry?: RetryOptions;
-    retryAt?: Date;
-    retryDelayInMs?: number;
-  }>;
-
-export type HandleErrorModificationOptions = {
-  skipRetrying?: boolean | undefined;
-  retryAt?: Date | undefined;
-  retryDelayInMs?: number | undefined;
-  retry?: RetryOptions | undefined;
-  error?: unknown;
-};
-
-export type HandleErrorResult =
-  | undefined
-  | void
-  | HandleErrorModificationOptions
-  | Promise<undefined | void | HandleErrorModificationOptions>;
-
-export type HandleErrorArgs = {
-  ctx: Context;
-  retry?: RetryOptions;
-  retryAt?: Date;
-  retryDelayInMs?: number;
-  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
-  signal?: AbortSignal;
-};
-
-export type HandleErrorFunction = (
-  payload: any,
-  error: unknown,
-  params: HandleErrorArgs
-) => HandleErrorResult;
 
 type ResolveEnvironmentVariablesOptions = {
   variables: Record<string, string> | Array<{ name: string; value: string }>;
@@ -97,20 +22,3 @@ export type ResolveEnvironmentVariablesParams = {
 export type ResolveEnvironmentVariablesFunction = (
   params: ResolveEnvironmentVariablesParams
 ) => ResolveEnvironmentVariablesResult;
-
-export type TaskMetadataWithFunctions = TaskMetadata & {
-  fns: {
-    run: (payload: any, params: RunFnParams<any>) => Promise<any>;
-    init?: (payload: any, params: InitFnParams) => Promise<InitOutput>;
-    cleanup?: (payload: any, params: RunFnParams<any>) => Promise<void>;
-    middleware?: (payload: any, params: MiddlewareFnParams) => Promise<void>;
-    handleError?: (
-      payload: any,
-      error: unknown,
-      params: HandleErrorFnParams<any>
-    ) => HandleErrorResult;
-    onSuccess?: (payload: any, output: any, params: SuccessFnParams<any>) => Promise<void>;
-    onFailure?: (payload: any, error: unknown, params: FailureFnParams<any>) => Promise<void>;
-    onStart?: (payload: any, params: StartFnParams) => Promise<void>;
-  };
-};
--- packages/core/src/v3/types/tasks.ts
+++ packages/core/src/v3/types/tasks.ts
@@ -2,6 +2,15 @@ import { SerializableJson } from "../../schemas/json.js";
 import { RunTags } from "../schemas/api.js";
 import { QueueOptions } from "../schemas/schemas.js";
 import { IdempotencyKey } from "./idempotencyKeys.js";
+import {
+  MachineCpu,
+  MachineMemory,
+  RetryOptions,
+  TaskMetadata,
+  TaskRunContext,
+} from "../schemas/index.js";
+import { Prettify } from "./utils.js";
+import { AnySchemaParseFn, inferSchemaOut, Schema } from "./schemas.js";
 
 type RequireOne<T, K extends keyof T> = {
   [X in Exclude<keyof T, K>]?: T[X];
@@ -10,6 +19,8 @@ type RequireOne<T, K extends keyof T> = {
 };
 
 export type Queue = RequireOne<QueueOptions, "name">;
+export type TaskSchema = Schema;
+export type { inferSchemaIn } from "./schemas.js";
 
 type TaskRunConcurrencyOptions = Queue;
 
@@ -53,6 +64,274 @@ export class TaskRunPromise<T> extends Promise<TaskRunResult<T>> {
     });
   }
 }
+
+export type InitOutput = Record<string, any> | void | undefined;
+
+export type RunFnParams<TInitOutput extends InitOutput> = Prettify<{
+  /** Metadata about the task, run, attempt, queue, environment, organization, project and batch.  */
+  ctx: Context;
+  /** If you use the `init` function, this will be whatever you returned. */
+  init?: TInitOutput;
+  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
+  signal?: AbortSignal;
+}>;
+
+export type MiddlewareFnParams = Prettify<{
+  ctx: Context;
+  next: () => Promise<void>;
+  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
+  signal?: AbortSignal;
+}>;
+
+export type InitFnParams = Prettify<{
+  ctx: Context;
+  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
+  signal?: AbortSignal;
+}>;
+
+export type StartFnParams = Prettify<{
+  ctx: Context;
+  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
+  signal?: AbortSignal;
+}>;
+
+export type Context = TaskRunContext;
+
+export type SuccessFnParams<TInitOutput extends InitOutput> = RunFnParams<TInitOutput>;
+
+export type FailureFnParams<TInitOutput extends InitOutput> = RunFnParams<TInitOutput>;
+
+export type HandleErrorFnParams<TInitOutput extends InitOutput> = RunFnParams<TInitOutput> &
+  Prettify<{
+    retry?: RetryOptions;
+    retryAt?: Date;
+    retryDelayInMs?: number;
+  }>;
+
+export type HandleErrorModificationOptions = {
+  skipRetrying?: boolean | undefined;
+  retryAt?: Date | undefined;
+  retryDelayInMs?: number | undefined;
+  retry?: RetryOptions | undefined;
+  error?: unknown;
+};
+
+export type HandleErrorResult =
+  | undefined
+  | void
+  | HandleErrorModificationOptions
+  | Promise<undefined | void | HandleErrorModificationOptions>;
+
+export type HandleErrorArgs = {
+  ctx: Context;
+  retry?: RetryOptions;
+  retryAt?: Date;
+  retryDelayInMs?: number;
+  /** Abort signal that is aborted when a task run exceeds it's maxDuration. Can be used to automatically cancel downstream requests */
+  signal?: AbortSignal;
+};
+
+export type HandleErrorFunction = (
+  payload: any,
+  error: unknown,
+  params: HandleErrorArgs
+) => HandleErrorResult;
+
+type CommonTaskOptions<
+  TIdentifier extends string,
+  TPayload = void,
+  TOutput = unknown,
+  TInitOutput extends InitOutput = any,
+> = {
+  /** An id for your task. This must be unique inside your project and not change between versions.  */
+  id: TIdentifier;
+
+  /** The retry settings when an uncaught error is thrown.
+       *
+       * If omitted it will use the values in your `trigger.config.ts` file.
+       * 
+       * @example
+       * 
+       * ```
+       * export const taskWithRetries = task({
+          id: "task-with-retries",
+          retry: {
+            maxAttempts: 10,
+            factor: 1.8,
+            minTimeoutInMs: 500,
+            maxTimeoutInMs: 30_000,
+            randomize: false,
+          },
+          run: async ({ payload, ctx }) => {
+            //...
+          },
+        });
+      * ```
+      * */
+  retry?: RetryOptions;
+
+  /** Used to configure what should happen when more than one run is triggered at the same time.
+   * 
+   * @example 
+   * one at a time execution
+   * 
+   * ```ts
+   * export const oneAtATime = task({
+      id: "one-at-a-time",
+      queue: {
+        concurrencyLimit: 1,
+      },
+      run: async ({ payload, ctx }) => {
+        //...
+      },
+    });
+   * ```
+   */
+  queue?: QueueOptions;
+  /** Configure the spec of the machine you want your task to run on.
+   * 
+   * @example
+   * 
+   * ```ts
+   * export const heavyTask = task({
+      id: "heavy-task",
+      machine: {
+        cpu: 2,
+        memory: 4,
+      },
+      run: async ({ payload, ctx }) => {
+        //...
+      },
+    });
+   * ```
+  */
+  machine?: {
+    /** vCPUs. The default is 0.5.
+     *
+     * Possible values:
+     * - 0.25
+     * - 0.5
+     * - 1
+     * - 2
+     * - 4
+     * @deprecated use preset instead
+     */
+    cpu?: MachineCpu;
+    /** In GBs of RAM. The default is 1.
+     *
+     * Possible values:
+     * - 0.25
+     * - 0.5
+     * - 1
+     * - 2
+     * - 4
+     * - 8
+     * * @deprecated use preset instead
+     */
+    memory?: MachineMemory;
+
+    /** Preset to use for the machine. Defaults to small-1x */
+    preset?:
+      | "micro"
+      | "small-1x"
+      | "small-2x"
+      | "medium-1x"
+      | "medium-2x"
+      | "large-1x"
+      | "large-2x";
+  };
+
+  /**
+   * The maximum duration in compute-time seconds that a task run is allowed to run. If the task run exceeds this duration, it will be stopped.
+   *
+   * Minimum value is 5 seconds
+   */
+  maxDuration?: number;
+
+  /** This gets called when a task is triggered. It's where you put the code you want to execute.
+   *
+   * @param payload - The payload that is passed to your task when it's triggered. This must be JSON serializable.
+   * @param params - Metadata about the run.
+   */
+  run: (payload: TPayload, params: RunFnParams<TInitOutput>) => Promise<TOutput>;
+
+  /**
+   * init is called before the run function is called. It's useful for setting up any global state.
+   */
+  init?: (payload: TPayload, params: InitFnParams) => Promise<TInitOutput>;
+
+  /**
+   * cleanup is called after the run function has completed.
+   */
+  cleanup?: (payload: TPayload, params: RunFnParams<TInitOutput>) => Promise<void>;
+
+  /**
+   * handleError is called when the run function throws an error. It can be used to modify the error or return new retry options.
+   */
+  handleError?: (
+    payload: TPayload,
+    error: unknown,
+    params: HandleErrorFnParams<TInitOutput>
+  ) => HandleErrorResult;
+
+  /**
+   * middleware allows you to run code "around" the run function. This can be useful for logging, metrics, or other cross-cutting concerns.
+   *
+   * When writing middleware, you should always call `next()` to continue the execution of the task:
+   *
+   * ```ts
+   * export const middlewareTask = task({
+   *  id: "middleware-task",
+   *  middleware: async (payload, { ctx, next }) => {
+   *   console.log("Before run");
+   *   await next();
+   *   console.log("After run");
+   *  },
+   *  run: async (payload, { ctx }) => {}
+   * });
+   * ```
+   */
+  middleware?: (payload: TPayload, params: MiddlewareFnParams) => Promise<void>;
+
+  /**
+   * onStart is called the first time a task is executed in a run (not before every retry)
+   */
+  onStart?: (payload: TPayload, params: StartFnParams) => Promise<void>;
+
+  /**
+   * onSuccess is called after the run function has successfully completed.
+   */
+  onSuccess?: (
+    payload: TPayload,
+    output: TOutput,
+    params: SuccessFnParams<TInitOutput>
+  ) => Promise<void>;
+
+  /**
+   * onFailure is called after a task run has failed (meaning the run function threw an error and won't be retried anymore)
+   */
+  onFailure?: (
+    payload: TPayload,
+    error: unknown,
+    params: FailureFnParams<TInitOutput>
+  ) => Promise<void>;
+};
+
+export type TaskOptions<
+  TIdentifier extends string,
+  TPayload = void,
+  TOutput = unknown,
+  TInitOutput extends InitOutput = any,
+> = CommonTaskOptions<TIdentifier, TPayload, TOutput, TInitOutput>;
+
+export type TaskWithSchemaOptions<
+  TIdentifier extends string,
+  TSchema extends TaskSchema | undefined = undefined,
+  TOutput = unknown,
+  TInitOutput extends InitOutput = any,
+> = CommonTaskOptions<TIdentifier, inferSchemaOut<TSchema>, TOutput, TInitOutput> & {
+  schema?: TSchema;
+};
 
 declare const __output: unique symbol;
 declare const __payload: unique symbol;
@@ -311,4 +590,22 @@ export type TaskRunOptions = {
    * Minimum value is 5 seconds
    */
   maxDuration?: number;
+};
+
+export type TaskMetadataWithFunctions = TaskMetadata & {
+  fns: {
+    run: (payload: any, params: RunFnParams<any>) => Promise<any>;
+    init?: (payload: any, params: InitFnParams) => Promise<InitOutput>;
+    cleanup?: (payload: any, params: RunFnParams<any>) => Promise<void>;
+    middleware?: (payload: any, params: MiddlewareFnParams) => Promise<void>;
+    handleError?: (
+      payload: any,
+      error: unknown,
+      params: HandleErrorFnParams<any>
+    ) => HandleErrorResult;
+    onSuccess?: (payload: any, output: any, params: SuccessFnParams<any>) => Promise<void>;
+    onFailure?: (payload: any, error: unknown, params: FailureFnParams<any>) => Promise<void>;
+    onStart?: (payload: any, params: StartFnParams) => Promise<void>;
+    parsePayload?: AnySchemaParseFn;
+  };
 };
