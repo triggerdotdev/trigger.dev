91cbd4b0c7aecee3880f0ae021487d26da6e749b
Eric Allam <eallam@icloud.com>
2024-10-12 21:45:07 +0100

    tweaks

--- references/nextjs-realtime/src/app/page.tsx
+++ references/nextjs-realtime/src/app/page.tsx
@@ -1,13 +1,13 @@
 import RunButton from "@/components/RunButton";
 import BatchRunButton from "@/components/BatchRunButton";
-import { ImageUploadButton, ImageUploadDropzone } from "@/components/ImageUploadButton";
+import { ImageUploadDropzone } from "@/components/ImageUploadButton";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col space-y-4">
-        {/* <RunButton />
-        <BatchRunButton /> */}
+        <RunButton />
+        <BatchRunButton />
         <ImageUploadDropzone />
       </div>
     </main>
--- references/nextjs-realtime/src/trigger/images.ts
+++ references/nextjs-realtime/src/trigger/images.ts
@@ -19,7 +19,6 @@ export const handleUpload = schemaTask({
         high_threshold: 200,
       }),
       runFalModel("fal-ai/aura-sr", file.url, {}),
-      runFalModel("fal-ai/imageutils/depth", file.url, {}),
     ]);
   },
 });
