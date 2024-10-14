e8a57232c2230635eb8f90485f1b19ad3b48fe9b
Matt Aitken <matt@mattaitken.com>
2024-10-14 15:57:24 +0100

    Added the messages in the RunEngine (temporary as they’ll need to move to core so the new Workers can use them too)

---
+++ internal-packages/run-engine/src/engine/messages.ts
@@ -0,0 +1,60 @@
+import { MachinePreset, EnvironmentType } from "@trigger.dev/core/v3";
+import { z } from "zod";
+
+//turn this into a zod schema discriminated union, like the messages we have in core.
+//it will need to move into core
+
+const StartRunMessage = z.object({
+  action: z.literal("START_RUN"),
+  // The payload allows us to a discriminated union with the version
+  payload: z.object({
+    version: z.literal("1"),
+    execution: z.object({
+      id: z.string(),
+      status: z.literal("DEQUEUED_FOR_EXECUTION"),
+    }),
+    image: z.string().optional(),
+    checkpoint: z
+      .object({
+        id: z.string(),
+        type: z.string(),
+        location: z.string(),
+        reason: z.string().optional(),
+      })
+      .optional(),
+    backgroundWorker: z.object({
+      id: z.string(),
+      version: z.string(),
+    }),
+    run: z.object({
+      id: z.string(),
+      friendlyId: z.string(),
+      isTest: z.boolean(),
+      machine: MachinePreset,
+      attemptNumber: z.number(),
+      masterQueue: z.string(),
+    }),
+    environment: z.object({
+      id: z.string(),
+      type: EnvironmentType,
+    }),
+    organization: z.object({
+      id: z.string(),
+    }),
+    project: z.object({
+      id: z.string(),
+    }),
+    traceContext: z.record(z.unknown()),
+  }),
+});
+export type StartRunMessage = z.infer<typeof StartRunMessage>;
+
+export const ContinueRunMessage = z.object({
+  action: z.literal("CONTINUE_RUN"),
+  payload: z.object({
+    version: z.literal("1"),
+  }),
+});
+export type ContinueRunMessage = z.infer<typeof ContinueRunMessage>;
+
+export const Messages = z.discriminatedUnion("action", [StartRunMessage]);
