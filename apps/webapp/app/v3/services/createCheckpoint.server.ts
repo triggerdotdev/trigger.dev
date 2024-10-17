f2b5c2ac42d52d3ab103ccc4b8e1f57c15b4e6e6
nicktrn <55853254+nicktrn@users.noreply.github.com>
2024-10-17 10:38:09 +0100

    prevent creating checkpoints for outdated batch waits

--- apps/webapp/app/v3/services/createCheckpoint.server.ts
+++ apps/webapp/app/v3/services/createCheckpoint.server.ts
@@ -122,6 +122,8 @@ export class CreateCheckpointService extends BaseService {
           logger.error("CreateCheckpointService: Checkpoint not for most recent child run", {
             attemptId: attempt.id,
             runId: attempt.taskRunId,
+            lastChild: lastChildRun.friendlyId,
+            checkpointFor: reason.friendlyId,
             params,
           });
 
@@ -134,6 +136,33 @@ export class CreateCheckpointService extends BaseService {
         break;
       }
       case "WAIT_FOR_BATCH": {
+        const lastChildRun = attempt.taskRun.childRuns[0];
+
+        if (!lastChildRun) {
+          logger.warn("CreateCheckpointService: No child runs, creating checkpoint regardless", {
+            attemptId: attempt.id,
+            runId: attempt.taskRunId,
+            params,
+          });
+
+          break;
+        }
+
+        if (!reason.runFriendlyIds.includes(lastChildRun.friendlyId)) {
+          logger.error("CreateCheckpointService: Checkpoint not for most recent batch", {
+            attemptId: attempt.id,
+            runId: attempt.taskRunId,
+            lastChild: lastChildRun.friendlyId,
+            checkpointFor: reason.runFriendlyIds,
+            params,
+          });
+
+          return {
+            success: false,
+            keepRunAlive: true,
+          };
+        }
+
         break;
       }
       default: {
