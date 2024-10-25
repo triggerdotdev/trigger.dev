9b5bd4ffc6f2a780d189db63400bd09d8146c6d0
Eric Allam <eallam@icloud.com>
2024-10-25 22:47:56 +0100

    More realtime docs

--- references/nextjs-realtime/src/app/page.tsx
+++ references/nextjs-realtime/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="grid grid-rows-[1fr_auto] min-h-screen items-center justify-center w-full bg-gray-900">
       <div className="flex flex-col space-y-8">
         <h1 className="text-gray-200 text-4xl max-w-xl text-center font-bold">
-          Thumbnail Generator Demo
+          Trigger.dev Realtime + UploadThing + fal.ai
         </h1>
         <ImageUploadDropzone />
       </div>
