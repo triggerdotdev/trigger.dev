a659f9d4d52777a7052fe5af2d077631130e026a
nicktrn <55853254+nicktrn@users.noreply.github.com>
2024-11-04 13:43:04 +0000

    add missing consts export

---
+++ packages/core/src/v3/apps/consts.ts
@@ -0,0 +1,1 @@
+export const CURRENT_DEPLOYMENT_LABEL = "current";
--- packages/core/src/v3/apps/index.ts
+++ packages/core/src/v3/apps/index.ts
@@ -9,3 +9,4 @@ export * from "./friendlyId.js";
 export * from "./duration.js";
 export * from "./maxDuration.js";
 export * from "./queueName.js";
+export * from "./consts.js";
