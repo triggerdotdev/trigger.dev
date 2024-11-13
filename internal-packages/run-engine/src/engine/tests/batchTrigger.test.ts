808a6f9bfa21fce77de3b5b4d80fd79183f99a48
Matt Aitken <matt@mattaitken.com>
2024-11-13 18:54:35 +0000

    Organized imports

--- internal-packages/run-engine/src/engine/tests/batchTrigger.test.ts
+++ internal-packages/run-engine/src/engine/tests/batchTrigger.test.ts
@@ -2,13 +2,11 @@ import {
   containerTest,
   setupAuthenticatedEnvironment,
   setupBackgroundWorker,
-  assertNonNullable,
 } from "@internal/testcontainers";
 import { trace } from "@opentelemetry/api";
+import { generateFriendlyId } from "@trigger.dev/core/v3/apps";
 import { expect } from "vitest";
 import { RunEngine } from "../index.js";
-import { EventBusEventArgs } from "../eventBus.js";
-import { generateFriendlyId } from "@trigger.dev/core/v3/apps";
 
 describe("RunEngine batchTrigger", () => {
   containerTest(
