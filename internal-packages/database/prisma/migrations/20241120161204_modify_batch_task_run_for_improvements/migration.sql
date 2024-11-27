4b2932a71df8ba0915e07a21dcd5d4d198370f3b
Eric Allam <eallam@icloud.com>
2024-11-27 17:50:40 +0000

    Fixed the migration that created the new idempotency key index on BatchTaskRun

--- internal-packages/database/prisma/migrations/20241120161204_modify_batch_task_run_for_improvements/migration.sql
+++ internal-packages/database/prisma/migrations/20241120161204_modify_batch_task_run_for_improvements/migration.sql
@@ -1,16 +1,18 @@
 /*
-  Warnings:
-
-  - A unique constraint covering the columns `[runtimeEnvironmentId,idempotencyKey]` on the table `BatchTaskRun` will be added. If there are existing duplicate values, this will fail.
-
-*/
+ Warnings:
+ 
+ - A unique constraint covering the columns `[runtimeEnvironmentId,idempotencyKey]` on the table `BatchTaskRun` will be added. If there are existing duplicate values, this will fail.
+ 
+ */
 -- DropIndex
 DROP INDEX "BatchTaskRun_runtimeEnvironmentId_taskIdentifier_idempotenc_key";
 
 -- AlterTable
-ALTER TABLE "BatchTaskRun" ADD COLUMN     "runCount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "runIds" TEXT[] DEFAULT ARRAY[]::TEXT[],
-ALTER COLUMN "taskIdentifier" DROP NOT NULL;
-
--- CreateIndex
-CREATE UNIQUE INDEX "BatchTaskRun_runtimeEnvironmentId_idempotencyKey_key" ON "BatchTaskRun"("runtimeEnvironmentId", "idempotencyKey");
+ALTER TABLE
+  "BatchTaskRun"
+ADD
+  COLUMN "runCount" INTEGER NOT NULL DEFAULT 0,
+ADD
+  COLUMN "runIds" TEXT [] DEFAULT ARRAY [] :: TEXT [],
+ALTER COLUMN
+  "taskIdentifier" DROP NOT NULL;
