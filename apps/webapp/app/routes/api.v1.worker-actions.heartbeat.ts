a759bf700bcc0a8e0139725f6e2f55d04e7f7e44
nicktrn <55853254+nicktrn@users.noreply.github.com>
2024-12-20 13:22:29 +0000

    fix heartbeat route return type

--- apps/webapp/app/routes/api.v1.worker-actions.heartbeat.ts
+++ apps/webapp/app/routes/api.v1.worker-actions.heartbeat.ts
@@ -1,15 +1,12 @@
 import { json, TypedResponse } from "@remix-run/server-runtime";
-import {
-  WorkerApiConnectResponseBody,
-  WorkerApiHeartbeatRequestBody,
-} from "@trigger.dev/worker";
+import { WorkerApiHeartbeatResponseBody, WorkerApiHeartbeatRequestBody } from "@trigger.dev/worker";
 import { createActionWorkerApiRoute } from "~/services/routeBuilders/apiBuilder.server";
 
 export const action = createActionWorkerApiRoute(
   {
     body: WorkerApiHeartbeatRequestBody,
   },
-  async ({ authenticatedWorker }): Promise<TypedResponse<WorkerApiConnectResponseBody>> => {
+  async ({ authenticatedWorker }): Promise<TypedResponse<WorkerApiHeartbeatResponseBody>> => {
     await authenticatedWorker.heartbeatWorkerInstance();
     return json({ ok: true });
   }
