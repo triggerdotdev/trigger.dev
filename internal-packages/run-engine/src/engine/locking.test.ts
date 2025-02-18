ae6fb3291de4f771ddb642954dc4b2a49cceb647
Matt Aitken <matt@mattaitken.com>
2025-02-18 15:33:07 +0000

    Cleanup redis client in the run lock tests

--- internal-packages/run-engine/src/engine/locking.test.ts
+++ internal-packages/run-engine/src/engine/locking.test.ts
@@ -6,35 +6,43 @@ import Redis from "ioredis";
 describe("RunLocker", () => {
   redisTest("Test acquiring a lock works", { timeout: 15_000 }, async ({ redisOptions }) => {
     const redis = new Redis(redisOptions);
-    const runLock = new RunLocker({ redis });
+    try {
+      const runLock = new RunLocker({ redis });
 
-    expect(runLock.isInsideLock()).toBe(false);
+      expect(runLock.isInsideLock()).toBe(false);
 
-    await runLock.lock(["test-1"], 5000, async (signal) => {
-      expect(signal).toBeDefined();
-      expect(runLock.isInsideLock()).toBe(true);
-    });
-
-    expect(runLock.isInsideLock()).toBe(false);
-  });
-
-  redisTest("Test double locking works", { timeout: 15_000 }, async ({ redisOptions }) => {
-    const redis = new Redis(redisOptions);
-    const runLock = new RunLocker({ redis });
-
-    expect(runLock.isInsideLock()).toBe(false);
-
-    await runLock.lock(["test-1"], 5000, async (signal) => {
-      expect(signal).toBeDefined();
-      expect(runLock.isInsideLock()).toBe(true);
-
-      //should be able to "lock it again"
       await runLock.lock(["test-1"], 5000, async (signal) => {
         expect(signal).toBeDefined();
         expect(runLock.isInsideLock()).toBe(true);
       });
-    });
 
-    expect(runLock.isInsideLock()).toBe(false);
+      expect(runLock.isInsideLock()).toBe(false);
+    } finally {
+      await redis.quit();
+    }
+  });
+
+  redisTest("Test double locking works", { timeout: 15_000 }, async ({ redisOptions }) => {
+    const redis = new Redis(redisOptions);
+    try {
+      const runLock = new RunLocker({ redis });
+
+      expect(runLock.isInsideLock()).toBe(false);
+
+      await runLock.lock(["test-1"], 5000, async (signal) => {
+        expect(signal).toBeDefined();
+        expect(runLock.isInsideLock()).toBe(true);
+
+        //should be able to "lock it again"
+        await runLock.lock(["test-1"], 5000, async (signal) => {
+          expect(signal).toBeDefined();
+          expect(runLock.isInsideLock()).toBe(true);
+        });
+      });
+
+      expect(runLock.isInsideLock()).toBe(false);
+    } finally {
+      await redis.quit();
+    }
   });
 });
