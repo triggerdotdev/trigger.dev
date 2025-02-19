84f5563a3a4eaa82aa010b327fbec716fb25930d
Eric Allam <eallam@icloud.com>
2025-02-19 11:20:21 +0000

    Fix the migrations

--- internal-packages/database/prisma/migrations/20250210164232_add_queue_timestamp_to_run/migration.sql
+++ internal-packages/database/prisma/migrations/20250210164232_add_queue_timestamp_to_run/migration.sql
@@ -1,8 +1,5 @@
--- DropIndex
-DROP INDEX "SecretStore_key_idx";
-
 -- AlterTable
-ALTER TABLE "TaskRun" ADD COLUMN     "queueTimestamp" TIMESTAMP(3);
-
--- CreateIndex
-CREATE INDEX "SecretStore_key_idx" ON "SecretStore"("key" text_pattern_ops);
+ALTER TABLE
+  "TaskRun"
+ADD
+  COLUMN "queueTimestamp" TIMESTAMP(3);
--- internal-packages/database/prisma/migrations/20250211150836_add_aborted_batch_task_run_status/migration.sql
+++ internal-packages/database/prisma/migrations/20250211150836_add_aborted_batch_task_run_status/migration.sql
@@ -1,8 +1,4 @@
 -- AlterEnum
-ALTER TYPE "BatchTaskRunStatus" ADD VALUE 'ABORTED';
-
--- DropIndex
-DROP INDEX "SecretStore_key_idx";
-
--- CreateIndex
-CREATE INDEX "SecretStore_key_idx" ON "SecretStore"("key" text_pattern_ops);
+ALTER TYPE "BatchTaskRunStatus"
+ADD
+  VALUE 'ABORTED';
