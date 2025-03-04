457273e4cf9d34ace5f0a1f3d7cd08caf29befd5
Eric Allam <eric@trigger.dev>
2025-03-04 10:54:39 +0000

    Fix email alert failures because of newlines in error messages (fixes #1745) (#1749)

--- references/v3-catalog/trigger.config.ts
+++ references/v3-catalog/trigger.config.ts
@@ -7,7 +7,6 @@ import { ffmpeg, syncEnvVars } from "@trigger.dev/build/extensions/core";
 import { puppeteer } from "@trigger.dev/build/extensions/puppeteer";
 import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
 import { emitDecoratorMetadata } from "@trigger.dev/build/extensions/typescript";
-import { pythonExtension } from "@trigger.dev/python/extension";
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export { handleError } from "./src/handleError.js";
@@ -87,7 +86,6 @@ export default defineConfig({
           value: secret.secretValue,
         }));
       }),
-      pythonExtension(),
       puppeteer(),
     ],
     external: ["re2"],
