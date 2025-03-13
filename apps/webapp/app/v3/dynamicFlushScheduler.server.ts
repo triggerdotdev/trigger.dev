4f3a8207c3e3f46f29814ae1488dfd014c92bf90
Márton Vető <marton@near.st>
2025-03-13 12:04:26 +0000

    flush takes batch size into consideration when calling callback

--- apps/webapp/app/v3/dynamicFlushScheduler.server.ts
+++ apps/webapp/app/v3/dynamicFlushScheduler.server.ts
@@ -3,22 +3,24 @@ import { nanoid } from "nanoid";
 export type DynamicFlushSchedulerConfig<T> = {
   batchSize: number;
   flushInterval: number;
+  maxConcurrency?: number;
   callback: (flushId: string, batch: T[]) => Promise<void>;
 };
 
 export class DynamicFlushScheduler<T> {
-  private batchQueue: T[][]; // Adjust the type according to your data structure
+  // private batchQueue: T[][]; // Adjust the type according to your data structure
   private currentBatch: T[]; // Adjust the type according to your data structure
   private readonly BATCH_SIZE: number;
   private readonly FLUSH_INTERVAL: number;
+  private readonly MAX_CONCURRENCY: number;
   private flushTimer: NodeJS.Timeout | null;
   private readonly callback: (flushId: string, batch: T[]) => Promise<void>;
 
   constructor(config: DynamicFlushSchedulerConfig<T>) {
-    this.batchQueue = [];
     this.currentBatch = [];
     this.BATCH_SIZE = config.batchSize;
     this.FLUSH_INTERVAL = config.flushInterval;
+    this.MAX_CONCURRENCY = config.maxConcurrency || 1;
     this.callback = config.callback;
     this.flushTimer = null;
     this.startFlushTimer();
@@ -28,8 +30,6 @@ export class DynamicFlushScheduler<T> {
     this.currentBatch.push(...items);
 
     if (this.currentBatch.length >= this.BATCH_SIZE) {
-      this.batchQueue.push(this.currentBatch);
-      this.currentBatch = [];
       this.flushNextBatch();
       this.resetFlushTimer();
     }
@@ -48,21 +48,19 @@ export class DynamicFlushScheduler<T> {
 
   private checkAndFlush(): void {
     if (this.currentBatch.length > 0) {
-      this.batchQueue.push(this.currentBatch);
-      this.currentBatch = [];
+      this.flushNextBatch();
     }
-    this.flushNextBatch();
   }
 
   private async flushNextBatch(): Promise<void> {
-    if (this.batchQueue.length === 0) return;
+    if (this.currentBatch.length === 0) return;
 
-    const batchToFlush = this.batchQueue.shift();
+    const batch = this.currentBatch.splice(0, this.BATCH_SIZE);
+    console.log("flushNextBatch", { batch });
+
     try {
-      await this.callback(nanoid(), batchToFlush!);
-      if (this.batchQueue.length > 0) {
-        this.flushNextBatch();
-      }
+      await this.callback(nanoid(), batch!);
+      this.flushNextBatch();
     } catch (error) {
       console.error("Error inserting batch:", error);
     }
