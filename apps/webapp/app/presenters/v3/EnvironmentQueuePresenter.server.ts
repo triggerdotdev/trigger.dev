99e722c5c9f71266cb863c82a83d590411a16c4b
Matt Aitken <matt@mattaitken.com>
2025-03-19 17:01:26 +0000

    Do all the environment concurrency lookups in parallel

--- apps/webapp/app/presenters/v3/EnvironmentQueuePresenter.server.ts
+++ apps/webapp/app/presenters/v3/EnvironmentQueuePresenter.server.ts
@@ -11,14 +11,15 @@ export type Environment = {
 
 export class EnvironmentQueuePresenter extends BasePresenter {
   async call(environment: AuthenticatedEnvironment): Promise<Environment> {
-    //executing
-    const engineV1Executing = await marqs.currentConcurrencyOfEnvironment(environment);
-    const engineV2Executing = await engine.concurrencyOfEnvQueue(environment);
-    const running = (engineV1Executing ?? 0) + (engineV2Executing ?? 0);
+    const [engineV1Executing, engineV2Executing, engineV1Queued, engineV2Queued] =
+      await Promise.all([
+        marqs.currentConcurrencyOfEnvironment(environment),
+        engine.concurrencyOfEnvQueue(environment),
+        marqs.lengthOfEnvQueue(environment),
+        engine.lengthOfEnvQueue(environment),
+      ]);
 
-    //queued
-    const engineV1Queued = await marqs.lengthOfEnvQueue(environment);
-    const engineV2Queued = await engine.lengthOfEnvQueue(environment);
+    const running = (engineV1Executing ?? 0) + (engineV2Executing ?? 0);
     const queued = (engineV1Queued ?? 0) + (engineV2Queued ?? 0);
 
     return {
