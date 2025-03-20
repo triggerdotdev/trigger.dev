e979dd933707b873f7efe11be3a0a05f580e1156
nicktrn <55853254+nicktrn@users.noreply.github.com>
2025-03-20 11:25:40 +0000

    add trycatch util

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,3 +1,12 @@
 export function assertExhaustive(x: never): never {
   throw new Error("Unexpected object: " + x);
 }
+
+export async function tryCatch<T, E = Error>(promise: Promise<T>): Promise<[T, null] | [null, E]> {
+  try {
+    const data = await promise;
+    return [data, null];
+  } catch (error) {
+    return [null, error as E];
+  }
+}
