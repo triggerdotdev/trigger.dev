f3767f8af7f78be57fd81e610836afffd10f58a4
Eric Allam <eallam@icloud.com>
2025-03-27 14:35:56 +0000

    D3 demo WIP

---
+++ references/d3-demo/trigger.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "@trigger.dev/sdk";
+import { pythonExtension } from "@trigger.dev/python/extension";
+import { installPlaywrightChromium } from "./src/extensions/playwright";
+
+export default defineConfig({
+  project: "proj_wkbbtayxrmeyqhankehb",
+  dirs: ["./src/trigger"],
+  maxDuration: 3600,
+  build: {
+    extensions: [
+      // This is required to use the Python extension
+      pythonExtension({
+        requirementsFile: "./requirements.txt", // Optional: Path to your requirements file
+        devPythonBinaryPath: `.venv/bin/python`, // Optional: Custom Python binary path
+        scripts: ["src/trigger/python/**/*.py"], // List of Python scripts to include
+      }),
+      installPlaywrightChromium(),
+    ],
+  },
+});
