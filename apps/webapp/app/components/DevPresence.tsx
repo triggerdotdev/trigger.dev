a2b66ab1feebce630bcf2575bc7619ac2bc97c5a
James Ritchie <james@trigger.dev>
2025-03-31 14:06:20 +0100

    Makes the DevConnection component reusable and moves components to the DevPresence.tsx file

--- apps/webapp/app/components/DevPresence.tsx
+++ apps/webapp/app/components/DevPresence.tsx
@@ -1,8 +1,23 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { createContext, type ReactNode, useContext, useEffect, useMemo, useState } from "react";
+import {
+  CheckingConnectionIcon,
+  ConnectedIcon,
+  DisconnectedIcon,
+} from "~/assets/icons/ConnectionIcons";
 import { useEnvironment } from "~/hooks/useEnvironment";
 import { useEventSource } from "~/hooks/useEventSource";
 import { useOrganization } from "~/hooks/useOrganizations";
 import { useProject } from "~/hooks/useProject";
+import { docsPath } from "~/utils/pathBuilder";
+import connectedImage from "../assets/images/cli-connected.png";
+import disconnectedImage from "../assets/images/cli-disconnected.png";
+import { InlineCode } from "./code/InlineCode";
+import { Button } from "./primitives/Buttons";
+import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "./primitives/Dialog";
+import { Paragraph } from "./primitives/Paragraph";
+import { TextLink } from "./primitives/TextLink";
+import { PackageManagerProvider, TriggerDevStepV3 } from "./SetupCommands";
 
 // Define Context types
 type DevPresenceContextType = {
@@ -76,4 +91,101 @@ export function useDevPresence() {
     throw new Error("useDevPresence must be used within a DevPresenceProvider");
   }
   return context;
+}
+
+export function ConnectionIcon({ isConnected }: { isConnected: boolean | undefined }) {
+  if (isConnected === undefined) {
+    return <CheckingConnectionIcon className="size-5" />;
+  }
+  return isConnected ? (
+    <ConnectedIcon className="size-5" />
+  ) : (
+    <DisconnectedIcon className="size-5" />
+  );
+}
+
+export function DevConnection({
+  children,
+}: {
+  children: (props: { isConnected: boolean | undefined }) => React.ReactNode;
+}) {
+  const { isConnected } = useDevPresence();
+
+  return (
+    <Dialog>
+      {children({ isConnected })}
+      <DialogContent>
+        <DialogHeader>
+          {isConnected === undefined
+            ? "Checking connection..."
+            : isConnected
+            ? "Your dev server is connected"
+            : "Your dev server is not connected"}
+        </DialogHeader>
+        <div className="mt-2 flex flex-col gap-3 px-2">
+          <div className="flex flex-col items-center justify-center gap-6 px-6 py-10">
+            <img
+              src={isConnected === true ? connectedImage : disconnectedImage}
+              alt={isConnected === true ? "Connected" : "Disconnected"}
+              width={282}
+              height={45}
+            />
+            <Paragraph variant="small" className={isConnected ? "text-success" : "text-error"}>
+              {isConnected === undefined
+                ? "Checking connection..."
+                : isConnected
+                ? "Your local dev server is connected to Trigger.dev"
+                : "Your local dev server is not connected to Trigger.dev"}
+            </Paragraph>
+          </div>
+          {isConnected ? null : (
+            <div className="space-y-3">
+              <PackageManagerProvider>
+                <TriggerDevStepV3 title="Run this command to connect" />
+              </PackageManagerProvider>
+              <Paragraph variant="small">
+                Run this CLI <InlineCode variant="extra-small">dev</InlineCode> command to connect
+                to the Trigger.dev servers to start developing locally. Keep it running while you
+                develop to stay connected. Learn more in the{" "}
+                <TextLink to={docsPath("cli-dev")}>CLI docs</TextLink>.
+              </Paragraph>
+            </div>
+          )}
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export function DevPresenceBanner() {
+  const environment = useEnvironment();
+  const { isConnected } = useDevPresence();
+
+  return (
+    <AnimatePresence>
+      {environment.type === "DEVELOPMENT" && !isConnected && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className="flex"
+        >
+          <DevConnection>
+            {({ isConnected }) => (
+              <DialogTrigger asChild>
+                <Button
+                  variant="minimal/small"
+                  className="py-1 pl-1 pr-2 text-error"
+                  LeadingIcon={<ConnectionIcon isConnected={isConnected} />}
+                >
+                  Your local dev server is not connected to Trigger.dev
+                </Button>
+              </DialogTrigger>
+            )}
+          </DevConnection>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 }
