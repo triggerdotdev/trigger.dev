521d2f9b09cd13d52a72ad84612398876d0d9695
Matt Aitken <matt@mattaitken.com>
2025-04-08 12:11:33 +0100

    Added a warning if you use the same queue twice with different settings

--- packages/core/src/v3/resource-catalog/standardResourceCatalog.ts
+++ packages/core/src/v3/resource-catalog/standardResourceCatalog.ts
@@ -24,6 +24,29 @@ export class StandardResourceCatalog implements ResourceCatalog {
   }
 
   registerQueueMetadata(queue: QueueManifest): void {
+    const existingQueue = this._queueMetadata.get(queue.name);
+
+    //if it exists already AND concurrencyLimit or releaseConcurrencyOnWaitpoint is different, log a warning
+    if (existingQueue) {
+      const isConcurrencyLimitDifferent = existingQueue.concurrencyLimit !== queue.concurrencyLimit;
+      const isReleaseConcurrencyOnWaitpointDifferent =
+        existingQueue.releaseConcurrencyOnWaitpoint !== queue.releaseConcurrencyOnWaitpoint;
+
+      if (isConcurrencyLimitDifferent || isReleaseConcurrencyOnWaitpointDifferent) {
+        let message = `Queue "${queue.name}" is defined twice, with different settings.`;
+        if (isConcurrencyLimitDifferent) {
+          message += `\n        - concurrencyLimit: ${existingQueue.concurrencyLimit} vs ${queue.concurrencyLimit}`;
+        }
+        if (isReleaseConcurrencyOnWaitpointDifferent) {
+          message += `\n        - releaseConcurrencyOnWaitpoint: ${existingQueue.releaseConcurrencyOnWaitpoint} vs ${queue.releaseConcurrencyOnWaitpoint}`;
+        }
+
+        message += "\n       Skipping the second definition.";
+        console.warn(message);
+        return;
+      }
+    }
+
     this._queueMetadata.set(queue.name, queue);
   }
 
