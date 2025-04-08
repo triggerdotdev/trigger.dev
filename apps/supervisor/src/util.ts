72d98778ebe7060a7a6f00b36bda282f52971a4c
nicktrn <55853254+nicktrn@users.noreply.github.com>
2025-04-08 13:07:45 +0100

    Fix node options and OOM retries (#1897)
    * don't hardcode max old space size in deployment images
    
    * flags: treat underscores as hyphens
    
    * append attempt number to runner name if >1
    
    * improve retry spans for oom errors

--- apps/supervisor/src/util.ts
+++ apps/supervisor/src/util.ts
@@ -5,6 +5,12 @@ export function getDockerHostDomain() {
   return isMacOs || isWindows ? "host.docker.internal" : "localhost";
 }
 
-export function getRunnerId(runId: string) {
-  return `runner-${runId.replace("run_", "")}`;
+export function getRunnerId(runId: string, attemptNumber?: number) {
+  const parts = ["runner", runId.replace("run_", "")];
+
+  if (attemptNumber && attemptNumber > 1) {
+    parts.push(`attempt-${attemptNumber}`);
+  }
+
+  return parts.join("-");
 }
