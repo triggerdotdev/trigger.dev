9dc666851fc8fec30b4aa59f731d3dda9dc354fd
Matt Aitken <matt@mattaitken.com>
2025-04-09 12:24:00 +0100

    Delete the old SSE util and updated the test endpoint to use the new one

--- apps/webapp/app/routes/tests.sse.stream.ts
+++ apps/webapp/app/routes/tests.sse.stream.ts
@@ -1,67 +1,33 @@
-import { LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { z } from "zod";
-import { logger } from "~/services/logger.server";
-import { sse } from "~/utils/sse.server";
+import { createSSELoader } from "~/utils/sse";
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const url = new URL(request.url);
-  const searchParams = Object.fromEntries(url.searchParams.entries());
+export const loader = createSSELoader({
+  timeout: 10_000,
+  interval: 1_000,
+  debug: false,
+  handler: async ({ id, controller, debug, request }) => {
+    const url = new URL(request.url);
+    const searchParams = Object.fromEntries(url.searchParams.entries());
 
-  const options = z
-    .object({
-      minDelay: z.coerce.number().int(),
-      maxDelay: z.coerce.number().int(),
-      undefinedProbability: z.coerce.number().min(0).max(1).default(0.1),
-    })
-    .parse(searchParams);
+    const options = z
+      .object({
+        undefinedProbability: z.coerce.number().min(0).max(1).default(0.1),
+      })
+      .parse(searchParams);
 
-  logger.debug("Test SSE stream", { options });
+    return {
+      beforeStream: async () => {},
+      initStream: async ({ send }) => {
+        send({ data: new Date().toISOString() });
+      },
+      iterator: async ({ send, date }) => {
+        if (Math.random() < options.undefinedProbability) {
+          return;
+        }
 
-  let lastSignals = calculateChangeSignals(Date.now());
-
-  return sse({
-    request,
-    run: async (send, stop) => {
-      const result = await dateForUpdates(options);
-
-      if (!result) {
-        return stop();
-      }
-
-      const newSignals = calculateChangeSignals(result);
-
-      if (lastSignals.ts !== newSignals.ts) {
-        send({ data: JSON.stringify(newSignals) });
-      }
-
-      lastSignals = newSignals;
-    },
-  });
-}
-
-async function dateForUpdates(opts: {
-  minDelay: number;
-  maxDelay: number;
-  undefinedProbability: number;
-}): Promise<number | undefined> {
-  // Randomly await between minDelay and maxDelay
-  await new Promise((resolve) => {
-    setTimeout(resolve, Math.random() * (opts.maxDelay - opts.minDelay) + opts.minDelay);
-  });
-
-  // There should be about a x% chance that this returns undefined
-  if (Math.random() < opts.undefinedProbability) {
-    logger.debug("Test SSE dataForUpdates returning undefined");
-
-    return undefined;
-  }
-
-  // Randomly return true or false
-  return Date.now();
-}
-
-function calculateChangeSignals(ts: number) {
-  return {
-    ts,
-  };
-}
+        send({ data: new Date().toISOString() });
+      },
+      cleanup: async () => {},
+    };
+  },
+});
