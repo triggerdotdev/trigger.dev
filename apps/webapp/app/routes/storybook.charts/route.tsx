1ecbb3b962bad311b80f10a7ab9732b34df9a11a
James Ritchie <james@trigger.dev>
2025-04-11 15:43:02 +0100

    Adds mock data for line chart

--- apps/webapp/app/routes/storybook.charts/route.tsx
+++ apps/webapp/app/routes/storybook.charts/route.tsx
@@ -1,25 +1,7 @@
 import { Button } from "~/components/primitives/Buttons";
 import { Card } from "~/components/primitives/charts/Card";
 import { type ChartConfig } from "~/components/primitives/charts/Chart";
-import { BigNumber, ChartBar } from "~/components/primitives/charts/Charts";
-import { Header2 } from "~/components/primitives/Headers";
-
-const barChartData = [
-  { day: "Nov 21", value: 186 },
-  { day: "Nov 22", value: 305 },
-  { day: "Nov 23", value: 237 },
-  { day: "Nov 24", value: 73 },
-  { day: "Nov 25", value: 209 },
-  { day: "Nov 26", value: 214 },
-  { day: "Nov 27", value: 546 },
-];
-
-const chartConfig = {
-  value: {
-    label: "Value",
-    color: "#6366F1",
-  },
-} satisfies ChartConfig;
+import { BigNumber, ChartBar, ChartLine } from "~/components/primitives/charts/Charts";
 
 export default function Story() {
   return (
@@ -27,14 +9,14 @@ export default function Story() {
       <Card>
         <Card.Header>Bar chart</Card.Header>
         <Card.Content>
-          <ChartBar config={chartConfig} data={barChartData} dataKey="value" />
+          <ChartBar config={barChartConfig} data={barChartData} dataKey="value" />
         </Card.Content>
       </Card>
 
       <Card>
         <Card.Header>Line chart</Card.Header>
         <Card.Content>
-          <ChartBar config={chartConfig} data={barChartData} dataKey="value" />
+          <ChartLine config={lineChartConfig} data={lineChartData} dataKey="value" />
         </Card.Content>
       </Card>
       <Card>
@@ -51,15 +33,53 @@ export default function Story() {
       <Card>
         <Card.Header>Stepped chart</Card.Header>
         <Card.Content>
-          <ChartBar config={chartConfig} data={barChartData} dataKey="value" />
+          <ChartBar config={barChartConfig} data={barChartData} dataKey="value" />
         </Card.Content>
       </Card>
       <Card>
         <Card.Header>Stacked chart</Card.Header>
         <Card.Content>
-          <ChartBar config={chartConfig} data={barChartData} dataKey="value" />
+          <ChartBar config={barChartConfig} data={barChartData} dataKey="value" />
         </Card.Content>
       </Card>
     </div>
   );
 }
+
+const barChartConfig = {
+  value: {
+    label: "Value",
+    color: "#6366F1",
+  },
+} satisfies ChartConfig;
+
+const barChartData = [
+  { day: "Nov 21", value: 186 },
+  { day: "Nov 22", value: 305 },
+  { day: "Nov 23", value: 237 },
+  { day: "Nov 24", value: 73 },
+  { day: "Nov 25", value: 209 },
+  { day: "Nov 26", value: 214 },
+  { day: "Nov 27", value: 546 },
+];
+
+const lineChartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#3B82F6",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#28BF5C",
+  },
+} satisfies ChartConfig;
+
+const lineChartData = [
+  { day: "Nov 21", desktop: 186, mobile: 80 },
+  { day: "Nov 22", desktop: 305, mobile: 200 },
+  { day: "Nov 23", desktop: 237, mobile: 120 },
+  { day: "Nov 24", desktop: 73, mobile: 190 },
+  { day: "Nov 25", desktop: 209, mobile: 130 },
+  { day: "Nov 26", desktop: 214, mobile: 140 },
+  { day: "Nov 27", desktop: 546, mobile: 150 },
+];
