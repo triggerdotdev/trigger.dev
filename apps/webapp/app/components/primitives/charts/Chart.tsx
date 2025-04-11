5efe10d7f1e40082977428b9fe8fb85ee07a337f
James Ritchie <james@trigger.dev>
2025-04-11 15:43:40 +0100

    Tooltip invalid style fixes

--- apps/webapp/app/components/primitives/charts/Chart.tsx
+++ apps/webapp/app/components/primitives/charts/Chart.tsx
@@ -158,7 +158,7 @@ const ChartTooltipContent = React.forwardRef<
       <div
         ref={ref}
         className={cn(
-          "border-border/50 bg-background grid min-w-[8rem] items-start gap-1.5 rounded-lg border px-2.5 py-1.5 text-xs shadow-xl",
+          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-grid-bright bg-background-bright px-2.5 py-1.5 text-xs shadow-xl",
           className
         )}
       >
