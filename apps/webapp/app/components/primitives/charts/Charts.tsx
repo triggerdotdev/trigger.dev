6c0b1892f92cc596f6f1fef9ff633cd191506b49
James Ritchie <james@trigger.dev>
2025-04-11 15:43:20 +0100

    Adds loading spinners

--- apps/webapp/app/components/primitives/charts/Charts.tsx
+++ apps/webapp/app/components/primitives/charts/Charts.tsx
@@ -1,56 +1,96 @@
-import { type ReactNode } from "react";
-import { Bar, BarChart, CartesianGrid, Line, XAxis, YAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
 import {
   type ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
 } from "~/components/primitives/charts/Chart";
-import { Spinner } from "../Spinner";
-import { AnimatedNumber } from "../AnimatedNumber";
 import { cn } from "~/utils/cn";
+import { AnimatedNumber } from "../AnimatedNumber";
+import { Spinner } from "../Spinner";
 
 export function ChartBar({
   config,
   data,
   dataKey,
+  loading = false,
 }: {
   config: ChartConfig;
   data: any[];
   dataKey: string;
+  loading?: boolean;
 }) {
   return (
     <ChartContainer config={config} className="min-h-full w-full">
-      <BarChart accessibilityLayer data={data}>
-        <CartesianGrid vertical={false} stroke="#3B3E45" />
-        <XAxis dataKey={dataKey} tickLine={false} tickMargin={8} axisLine={false} />
-        <YAxis axisLine={false} tickLine={false} tickMargin={8} />
-        <ChartTooltip
-          cursor={false}
-          animationDuration={50}
-          content={<ChartTooltipContent hideLabel />}
-        />
-        <Bar dataKey="value" fill="#6366F1" radius={4} />
-      </BarChart>
+      {loading ? (
+        <div className="grid h-full place-items-center">
+          <Spinner className="size-6" />
+        </div>
+      ) : (
+        <BarChart accessibilityLayer data={data}>
+          <CartesianGrid vertical={false} stroke="#272A2E" />
+          <XAxis dataKey={dataKey} tickLine={false} tickMargin={8} axisLine={false} />
+          <YAxis axisLine={false} tickLine={false} tickMargin={8} />
+          <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+          <Bar dataKey="value" fill="#6366F1" radius={4} />
+        </BarChart>
+      )}
     </ChartContainer>
   );
 }
 
-export function LineChart({ config, data }: { config: ChartConfig; data: any[] }) {
+export function ChartLine({
+  config,
+  data,
+  dataKey,
+  loading = false,
+}: {
+  config: ChartConfig;
+  data: any[];
+  dataKey: string;
+  loading?: boolean;
+}) {
   return (
     <ChartContainer config={config} className="min-h-[200px] w-full">
-      <BarChart accessibilityLayer data={data}>
-        <CartesianGrid horizontal={false} />
-        <XAxis
-          dataKey="day"
-          tickLine={false}
-          tickMargin={10}
-          axisLine={false}
-          tickFormatter={(value) => value.slice(0, 3)}
-        />
-        <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-        <Line dataKey="value" stroke="#2563eb" />
-      </BarChart>
+      {loading ? (
+        <div className="grid h-full place-items-center">
+          <Spinner className="size-6" />
+        </div>
+      ) : (
+        <LineChart
+          accessibilityLayer
+          data={data}
+          margin={{
+            left: 12,
+            right: 12,
+          }}
+        >
+          <CartesianGrid vertical={false} stroke="#272A2E" />
+          <XAxis
+            dataKey={dataKey}
+            tickLine={false}
+            axisLine={false}
+            tickMargin={8}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <YAxis axisLine={false} tickLine={false} tickMargin={8} />
+          <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+          <Line
+            dataKey="desktop"
+            type="monotone"
+            stroke="var(--color-desktop)"
+            strokeWidth={2}
+            dot={false}
+          />
+          <Line
+            dataKey="mobile"
+            type="monotone"
+            stroke="var(--color-mobile)"
+            strokeWidth={2}
+            dot={false}
+          />
+        </LineChart>
+      )}
     </ChartContainer>
   );
 }
@@ -78,12 +118,14 @@ export function BigNumber({
   return (
     <div
       className={cn(
-        "h-[3.75rem] text-[3.75rem] font-normal tabular-nums leading-none text-text-bright",
+        "h-full text-[3.75rem] font-normal tabular-nums leading-none text-text-bright",
         valueClassName
       )}
     >
       {loading ? (
-        <Spinner className="size-6" />
+        <div className="grid h-full place-items-center">
+          <Spinner className="size-6" />
+        </div>
       ) : v !== undefined ? (
         <div className="flex items-baseline gap-1">
           {animate ? <AnimatedNumber value={v} /> : v}
