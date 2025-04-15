1023d13d1e623f1d2a6303705b5f2073413cbdb4
nicktrn <55853254+nicktrn@users.noreply.github.com>
2025-04-15 14:17:07 +0100

    restore old heartbeat service but deprecate it

--- packages/core/src/v3/index.ts
+++ packages/core/src/v3/index.ts
@@ -68,6 +68,7 @@ export {
 
 export * from "./utils/imageRef.js";
 export * from "./utils/interval.js";
+export * from "./utils/heartbeat.js";
 
 export * from "./config.js";
 export {
---
+++ packages/core/src/v3/utils/heartbeat.ts
@@ -0,0 +1,96 @@
+type HeartbeatServiceOptions = {
+  heartbeat: () => Promise<void>;
+  intervalMs?: number;
+  leadingEdge?: boolean;
+  onError?: (error: unknown) => Promise<void>;
+};
+
+/**
+ * @deprecated Use IntervalService instead
+ */
+export class HeartbeatService {
+  private _heartbeat: () => Promise<void>;
+  private _intervalMs: number;
+  private _nextHeartbeat: NodeJS.Timeout | undefined;
+  private _leadingEdge: boolean;
+  private _isHeartbeating: boolean;
+  private _onError?: (error: unknown) => Promise<void>;
+
+  constructor(opts: HeartbeatServiceOptions) {
+    this._heartbeat = opts.heartbeat;
+    this._intervalMs = opts.intervalMs ?? 45_000;
+    this._nextHeartbeat = undefined;
+    this._leadingEdge = opts.leadingEdge ?? false;
+    this._isHeartbeating = false;
+    this._onError = opts.onError;
+  }
+
+  start() {
+    if (this._isHeartbeating) {
+      return;
+    }
+
+    this._isHeartbeating = true;
+
+    if (this._leadingEdge) {
+      this.#doHeartbeat();
+    } else {
+      this.#scheduleNextHeartbeat();
+    }
+  }
+
+  stop() {
+    if (!this._isHeartbeating) {
+      return;
+    }
+
+    this._isHeartbeating = false;
+    this.#clearNextHeartbeat();
+  }
+
+  resetCurrentInterval() {
+    if (!this._isHeartbeating) {
+      return;
+    }
+
+    this.#clearNextHeartbeat();
+    this.#scheduleNextHeartbeat();
+  }
+
+  updateInterval(intervalMs: number) {
+    this._intervalMs = intervalMs;
+    this.resetCurrentInterval();
+  }
+
+  #doHeartbeat = async () => {
+    this.#clearNextHeartbeat();
+
+    if (!this._isHeartbeating) {
+      return;
+    }
+
+    try {
+      await this._heartbeat();
+    } catch (error) {
+      if (this._onError) {
+        try {
+          await this._onError(error);
+        } catch (error) {
+          console.error("Error handling heartbeat error", error);
+        }
+      }
+    }
+
+    this.#scheduleNextHeartbeat();
+  };
+
+  #clearNextHeartbeat() {
+    if (this._nextHeartbeat) {
+      clearTimeout(this._nextHeartbeat);
+    }
+  }
+
+  #scheduleNextHeartbeat() {
+    this._nextHeartbeat = setTimeout(this.#doHeartbeat, this._intervalMs);
+  }
+}
