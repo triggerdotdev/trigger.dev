759ad2b78d10a9f95b919cf1b7f116d55c8a3c71
saadi <saadi.myftija@gmail.com>
2025-04-21 17:03:07 -0700

    Allow empty payloads in test runs

--- apps/webapp/app/components/code/codeMirrorSetup.ts
+++ apps/webapp/app/components/code/codeMirrorSetup.ts
@@ -2,18 +2,32 @@ import { closeBrackets } from "@codemirror/autocomplete";
 import { indentWithTab } from "@codemirror/commands";
 import { jsonParseLinter } from "@codemirror/lang-json";
 import { bracketMatching } from "@codemirror/language";
-import { lintGutter, lintKeymap, linter } from "@codemirror/lint";
+import { type Diagnostic, linter, lintGutter, lintKeymap } from "@codemirror/lint";
 import { highlightSelectionMatches } from "@codemirror/search";
 import { Prec, type Extension } from "@codemirror/state";
 import {
   drawSelection,
   dropCursor,
+  type EditorView,
   highlightActiveLine,
   highlightActiveLineGutter,
   highlightSpecialChars,
   keymap,
   lineNumbers,
 } from "@codemirror/view";
+
+function emptyAwareJsonLinter() {
+  return (view: EditorView): Diagnostic[] => {
+    const content = view.state.doc.toString().trim();
+
+    // return no errors if content is empty
+    if (!content) {
+      return [];
+    }
+
+    return jsonParseLinter()(view);
+  };
+}
 
 export function getEditorSetup(showLineNumbers = true, showHighlights = true): Array<Extension> {
   const options = [
@@ -22,7 +36,7 @@ export function getEditorSetup(showLineNumbers = true, showHighlights = true): A
     bracketMatching(),
     closeBrackets(),
     lintGutter(),
-    linter(jsonParseLinter()),
+    linter(emptyAwareJsonLinter()),
     Prec.highest(
       keymap.of([
         {
