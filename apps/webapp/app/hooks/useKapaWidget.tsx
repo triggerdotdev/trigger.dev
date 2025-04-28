284ef26c7c2837a30f1509be25f127f843519ed8
Matt Aitken <matt@mattaitken.com>
2025-04-28 18:01:31 +0100

    Improved Kapa widget colors

--- apps/webapp/app/hooks/useKapaWidget.tsx
+++ apps/webapp/app/hooks/useKapaWidget.tsx
@@ -27,18 +27,13 @@ export function KapaScripts({ websiteId }: { websiteId?: string }) {
         src="https://widget.kapa.ai/kapa-widget.bundle.js"
         data-website-id={websiteId}
         data-project-name={"Trigger.dev"}
-        data-project-color={"#C7D2FE"}
+        data-project-color={"#6366F1"}
         data-project-logo={"https://content.trigger.dev/trigger-logo-circle.png"}
         data-render-on-load={"false"}
         data-button-hide={"true"}
-        data-modal-disclaimer-bg-color={"#1A1B1F"}
-        data-modal-disclaimer-text-color={"#878C99"}
+        data-modal-disclaimer-bg-color={"#D7D9DD"}
         data-modal-header-bg-color={"#2C3034"}
-        data-modal-body-bg-color={"#4D525B"}
-        data-query-input-text-color={"#15171A"}
-        data-query-input-placeholder-text-color={"#878C99"}
         data-modal-title-color={"#D7D9DD"}
-        data-button-text-color={"#D7D9DD"}
       ></script>
       <script
         dangerouslySetInnerHTML={{
