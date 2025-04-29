349adc42fc7a3bd75f47d58e4e6a351b4e698284
Matt Aitken <matt@mattaitken.com>
2025-04-29 16:41:54 +0100

    Added an AI help link to the CLI

--- packages/cli-v3/src/utilities/cliOutput.ts
+++ packages/cli-v3/src/utilities/cliOutput.ts
@@ -116,6 +116,27 @@ export function prettyWarning(header: string, body?: string, footer?: string) {
   );
 }
 
+export function aiHelpLink({
+  dashboardUrl,
+  project,
+  query,
+}: {
+  dashboardUrl: string;
+  project: string;
+  query: string;
+}) {
+  const searchParams = new URLSearchParams();
+
+  //the max length for a URL is 1950 characters
+  const clippedQuery = query.slice(0, 1950);
+
+  searchParams.set("q", clippedQuery);
+  const url = new URL(`/projects/${project}/ai-help`, dashboardUrl);
+  url.search = searchParams.toString();
+
+  log.message(chalkLink(cliLink("💡 Get a fix for this error using AI", url.toString())));
+}
+
 export function cliLink(text: string, url: string, options?: TerminalLinkOptions) {
   return terminalLink(text, url, {
     fallback: (text, url) => `${text} ${url}`,
