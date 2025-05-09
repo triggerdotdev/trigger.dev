b0bf62441bdf1e334ede6c67d12c6ee4ec7c5586
Mendy Landa <landamendy@gmail.com>
2025-05-09 07:53:32 +0300

    full dep list & added some comments

--- packages/build/src/extensions/playwright.ts
+++ packages/build/src/extensions/playwright.ts
@@ -10,11 +10,172 @@ interface PlaywrightExtensionOptions {
   browsers?: PlaywrightBrowser[];
 
   /**
-   * Whether to support non-headless mode.
+   * Run the browsers in headless mode (Recommended)
    * @default true
    */
   headless?: boolean;
 }
+
+/**
+ * This list is from the official playwright registry.
+ *
+ * @see https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/server/registry/nativeDeps.ts
+ */
+const debian12Deps = {
+  tools: [
+    "xvfb",
+    "fonts-noto-color-emoji",
+    "fonts-unifont",
+    "libfontconfig1",
+    "libfreetype6",
+    "xfonts-scalable",
+    "fonts-liberation",
+    "fonts-ipafont-gothic",
+    "fonts-wqy-zenhei",
+    "fonts-tlwg-loma-otf",
+    "fonts-freefont-ttf",
+  ],
+  chromium: [
+    "libasound2",
+    "libatk-bridge2.0-0",
+    "libatk1.0-0",
+    "libatspi2.0-0",
+    "libcairo2",
+    "libcups2",
+    "libdbus-1-3",
+    "libdrm2",
+    "libgbm1",
+    "libglib2.0-0",
+    "libnspr4",
+    "libnss3",
+    "libpango-1.0-0",
+    "libx11-6",
+    "libxcb1",
+    "libxcomposite1",
+    "libxdamage1",
+    "libxext6",
+    "libxfixes3",
+    "libxkbcommon0",
+    "libxrandr2",
+  ],
+  firefox: [
+    "libasound2",
+    "libatk1.0-0",
+    "libcairo-gobject2",
+    "libcairo2",
+    "libdbus-1-3",
+    "libdbus-glib-1-2",
+    "libfontconfig1",
+    "libfreetype6",
+    "libgdk-pixbuf-2.0-0",
+    "libglib2.0-0",
+    "libgtk-3-0",
+    "libharfbuzz0b",
+    "libpango-1.0-0",
+    "libpangocairo-1.0-0",
+    "libx11-6",
+    "libx11-xcb1",
+    "libxcb-shm0",
+    "libxcb1",
+    "libxcomposite1",
+    "libxcursor1",
+    "libxdamage1",
+    "libxext6",
+    "libxfixes3",
+    "libxi6",
+    "libxrandr2",
+    "libxrender1",
+    "libxtst6",
+  ],
+  webkit: [
+    "libsoup-3.0-0",
+    "gstreamer1.0-libav",
+    "gstreamer1.0-plugins-bad",
+    "gstreamer1.0-plugins-base",
+    "gstreamer1.0-plugins-good",
+    "libatk-bridge2.0-0",
+    "libatk1.0-0",
+    "libcairo2",
+    "libdbus-1-3",
+    "libdrm2",
+    "libegl1",
+    "libenchant-2-2",
+    "libepoxy0",
+    "libevdev2",
+    "libfontconfig1",
+    "libfreetype6",
+    "libgbm1",
+    "libgdk-pixbuf-2.0-0",
+    "libgles2",
+    "libglib2.0-0",
+    "libglx0",
+    "libgstreamer-gl1.0-0",
+    "libgstreamer-plugins-base1.0-0",
+    "libgstreamer1.0-0",
+    "libgtk-4-1",
+    "libgudev-1.0-0",
+    "libharfbuzz-icu0",
+    "libharfbuzz0b",
+    "libhyphen0",
+    "libicu72",
+    "libjpeg62-turbo",
+    "liblcms2-2",
+    "libmanette-0.2-0",
+    "libnotify4",
+    "libopengl0",
+    "libopenjp2-7",
+    "libopus0",
+    "libpango-1.0-0",
+    "libpng16-16",
+    "libproxy1v5",
+    "libsecret-1-0",
+    "libwayland-client0",
+    "libwayland-egl1",
+    "libwayland-server0",
+    "libwebp7",
+    "libwebpdemux2",
+    "libwoff1",
+    "libx11-6",
+    "libxcomposite1",
+    "libxdamage1",
+    "libxkbcommon0",
+    "libxml2",
+    "libxslt1.1",
+    "libatomic1",
+    "libevent-2.1-7",
+    "libavif15",
+  ],
+  lib2package: {
+    "libavif.so.15": "libavif15",
+    "libsoup-3.0.so.0": "libsoup-3.0-0",
+    "libasound.so.2": "libasound2",
+    "libatk-1.0.so.0": "libatk1.0-0",
+    "libatk-bridge-2.0.so.0": "libatk-bridge2.0-0",
+    "libatspi.so.0": "libatspi2.0-0",
+    "libcairo.so.2": "libcairo2",
+    "libcups.so.2": "libcups2",
+    "libdbus-1.so.3": "libdbus-1-3",
+    "libdrm.so.2": "libdrm2",
+    "libgbm.so.1": "libgbm1",
+    "libgio-2.0.so.0": "libglib2.0-0",
+    "libglib-2.0.so.0": "libglib2.0-0",
+    "libgobject-2.0.so.0": "libglib2.0-0",
+    "libnspr4.so": "libnspr4",
+    "libnss3.so": "libnss3",
+    "libnssutil3.so": "libnss3",
+    "libpango-1.0.so.0": "libpango-1.0-0",
+    "libsmime3.so": "libnss3",
+    "libX11.so.6": "libx11-6",
+    "libxcb.so.1": "libxcb1",
+    "libXcomposite.so.1": "libxcomposite1",
+    "libXdamage.so.1": "libxdamage1",
+    "libXext.so.6": "libxext6",
+    "libXfixes.so.3": "libxfixes3",
+    "libxkbcommon.so.0": "libxkbcommon0",
+    "libXrandr.so.2": "libxrandr2",
+    "libgtk-4.so.1": "libgtk-4-1",
+  },
+};
 
 /**
  * Creates a Playwright extension for trigger.dev
@@ -24,6 +185,23 @@ export function playwright(options: PlaywrightExtensionOptions = {}) {
   return new PlaywrightExtension(options);
 }
 
+/**
+ * Background:
+ *
+ * Running `npx playwright install --with-deps` normally will install the browsers and the dependencies.
+ * However, this is not possible in a build context, because we don't have sudo access.
+ *
+ * So we need to install the dependencies manually and then download and install the browsers.
+ * This has a few challenges:
+ * 1. We don't want to download all browsers, only the ones we need with it's dependencies
+ *    The less dependencies we have to install, the faster the build, and the smaller the image.
+ * 2. We need to know where to download the browsers from
+ *    while we can hardcode the download url it might change over time (as it has in the past)
+ *    so we need to download the browser info first and then parse the output to get the download url.
+ *
+ * Note: While this looks like we are downloading & installing a lot of stuff, it's actually not that bad
+ *       since running `npx playwright install --with-deps` will result in the same amount of downloads.
+ */
 class PlaywrightExtension implements BuildExtension {
   public readonly name = "PlaywrightExtension";
   private readonly options: Required<PlaywrightExtensionOptions>;
@@ -43,7 +221,7 @@ class PlaywrightExtension implements BuildExtension {
     );
 
     const instructions: string[] = [
-      // Base dependencies
+      // Base dependencies, we need these to download the browsers
       `RUN apt-get update && apt-get install -y --no-install-recommends \
         curl \
         unzip \
@@ -57,90 +235,26 @@ class PlaywrightExtension implements BuildExtension {
       `RUN npm install -g playwright`,
     ];
 
-    // Browser-specific dependencies
-    const chromiumDeps = [
-      "libnspr4",
-      "libatk1.0-0",
-      "libatk-bridge2.0-0",
-      "libatspi2.0-0",
-      "libasound2",
-      "libnss3",
-      "libxcomposite1",
-      "libxdamage1",
-      "libxfixes3",
-      "libxrandr2",
-      "libgbm1",
-      "libxkbcommon0",
-    ];
+    const deps = [...debian12Deps.tools, ...Object.values(debian12Deps.lib2package)];
+    if (this.options.browsers.includes("chromium")) deps.push(...debian12Deps.chromium);
+    if (this.options.browsers.includes("firefox")) deps.push(...debian12Deps.firefox);
+    if (this.options.browsers.includes("webkit")) deps.push(...debian12Deps.webkit);
 
-    const firefoxDeps = [
-      "libgtk-3.0",
-      "libgtk-4-1",
-      "libgtk-4-common",
-      "libgtk-4-dev",
-      "libgtk-4-doc",
-      "libasound2",
-    ];
-
-    const webkitDeps = [
-      "libenchant-2-2",
-      "libgl1",
-      "libgles2",
-      "libgstreamer-gl1.0-0",
-      "libgstreamer-plugins-base1.0-0",
-      "libgstreamer-plugins-bad1.0-0",
-      "libharfbuzz-icu0",
-      "libhyphen0",
-      "libicu72",
-      "libjpeg-dev",
-      "libopenjp2-7",
-      "libopus0",
-      "libpng-dev",
-      "libsecret-1-0",
-      "libvpx7",
-      "libwebp7",
-      "libwoff1",
-      "libx11-6",
-      "libxcomposite1",
-      "libxdamage1",
-      "libxrender1",
-      "libxt6",
-      "libgtk-4-1",
-      "libgraphene-1.0-0",
-      "libxslt1.1",
-      "libevent-2.1-7",
-      "libmanette-0.2-0",
-      "libwebpdemux2",
-      "libwebpmux3",
-      "libatomic1",
-      "libavif15",
-      "libx264-dev",
-      "flite",
-      "libatk1.0-0",
-      "libatk-bridge2.0-0",
-    ];
-
-    const deps = [];
-    if (this.options.browsers.includes("chromium")) deps.push(...chromiumDeps);
-    if (this.options.browsers.includes("firefox")) deps.push(...firefoxDeps);
-    if (this.options.browsers.includes("webkit")) deps.push(...webkitDeps);
-
-    const uniqueDeps = [...new Set(deps)];
-
-    if (uniqueDeps.length > 0) {
-      instructions.push(
-        `RUN apt-get update && apt-get install -y --no-install-recommends ${uniqueDeps.join(" ")} \
+    instructions.push(
+      `RUN apt-get update && apt-get install -y --no-install-recommends ${deps.join(" ")} \
         && apt-get clean && rm -rf /var/lib/apt/lists/*`
-      );
-    }
+    );
 
-    // Setup Playwright browsers
+    // Setup directory for playwright browsers
     instructions.push(`RUN mkdir -p /ms-playwright`);
-    instructions.push(`RUN npx playwright install --dry-run > /tmp/browser-info.txt`);
 
+    /**
+     * `npx playwright install --dry-run` prints the download urls for the browsers.
+     * We save this output to a file and then parse it to get the download urls for the browsers.
+     */
+    instructions.push(`RUN npx playwright install --dry-run > /tmp/browser-info.txt`);
     this.options.browsers.forEach((browser) => {
       const browserType = browser === "chromium" ? "chromium-headless-shell" : browser;
-
       instructions.push(
         `RUN grep -A5 "browser: ${browserType}" /tmp/browser-info.txt > /tmp/${browser}-info.txt`,
 
@@ -160,9 +274,9 @@ class PlaywrightExtension implements BuildExtension {
 
     // Environment variables
     const envVars: Record<string, string> = {
-      PLAYWRIGHT_BROWSERS_PATH: "/ms-playwright",
-      PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD: "1",
-      PLAYWRIGHT_SKIP_BROWSER_VALIDATION: "1",
+      PLAYWRIGHT_BROWSERS_PATH: "/ms-playwright", // where playwright will find the browsers
+      PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD: "1", // we already downloaded the browsers
+      PLAYWRIGHT_SKIP_BROWSER_VALIDATION: "1", // we already downloaded the browsers
     };
 
     if (!this.options.headless) {
@@ -173,7 +287,7 @@ class PlaywrightExtension implements BuildExtension {
         `RUN chmod +x /usr/local/bin/xvfb-exec`
       );
 
-      envVars.DISPLAY = ":99";
+      envVars.DISPLAY = ":99"; // Virtual display for the browsers
     }
 
     context.addLayer({
