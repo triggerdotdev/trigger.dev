9a57df2e208996dead9e276c10ee34cc27aa4e53
Matt Aitken <matt@mattaitken.com>
2025-05-11 16:45:09 +0100

    Staging = yellow Preview = orange

--- apps/webapp/tailwind.config.js
+++ apps/webapp/tailwind.config.js
@@ -149,8 +149,8 @@ const pending = colors.blue[500];
 const warning = colors.amber[500];
 const error = colors.rose[600];
 const devEnv = colors.pink[500];
-const stagingEnv = colors.amber[400];
-const previewEnv = colors.amber[400];
+const stagingEnv = colors.yellow[400];
+const previewEnv = colors.orange[400];
 const prodEnv = mint[500];
 
 /** Icon colors */
