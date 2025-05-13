e7419976e18b42977f396dfadee5cbee9e4bca72
Matt Aitken <matt@mattaitken.com>
2025-05-13 15:11:39 +0100

    Only create the parentEnvironmentId column if it doesn’t exist already

--- internal-packages/database/prisma/migrations/20250509180155_runtime_environment_branching/migration.sql
+++ internal-packages/database/prisma/migrations/20250509180155_runtime_environment_branching/migration.sql
@@ -2,8 +2,21 @@
 ALTER TABLE "RuntimeEnvironment"
 ADD COLUMN "archivedAt" TIMESTAMP(3),
 ADD COLUMN "branchName" TEXT,
-ADD COLUMN "git" JSONB,
-ADD COLUMN "parentEnvironmentId" TEXT;
+ADD COLUMN "git" JSONB;
+
+-- Add the parentEnvironmentId column
+DO $$ 
+BEGIN 
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.columns 
+        WHERE table_name = 'RuntimeEnvironment' 
+        AND column_name = 'parentEnvironmentId'
+    ) THEN 
+        ALTER TABLE "RuntimeEnvironment" 
+        ADD COLUMN "parentEnvironmentId" TEXT;
+    END IF;
+END $$;
 
 -- AddForeignKey
 ALTER TABLE "RuntimeEnvironment" ADD CONSTRAINT "RuntimeEnvironment_parentEnvironmentId_fkey" FOREIGN KEY ("parentEnvironmentId") REFERENCES "RuntimeEnvironment" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
