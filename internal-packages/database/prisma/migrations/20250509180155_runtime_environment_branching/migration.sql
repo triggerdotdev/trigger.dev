0bdd9db2d400d8a5fef9358ac5121a2050ad725e
Matt Aitken <matt@mattaitken.com>
2025-05-27 17:58:53 +0100

    Made the parentEnvironmentId migrations safe

--- internal-packages/database/prisma/migrations/20250509180155_runtime_environment_branching/migration.sql
+++ internal-packages/database/prisma/migrations/20250509180155_runtime_environment_branching/migration.sql
@@ -1,8 +1,8 @@
 -- AlterTable
 ALTER TABLE "RuntimeEnvironment"
-ADD COLUMN "archivedAt" TIMESTAMP(3),
-ADD COLUMN "branchName" TEXT,
-ADD COLUMN "git" JSONB;
+ADD COLUMN IF NOT EXISTS "archivedAt" TIMESTAMP(3),
+ADD COLUMN IF NOT EXISTS "branchName" TEXT,
+ADD COLUMN IF NOT EXISTS "git" JSONB;
 
 -- Add the parentEnvironmentId column
 DO $$ 
@@ -19,4 +19,19 @@ BEGIN
 END $$;
 
 -- AddForeignKey
-ALTER TABLE "RuntimeEnvironment" ADD CONSTRAINT "RuntimeEnvironment_parentEnvironmentId_fkey" FOREIGN KEY ("parentEnvironmentId") REFERENCES "RuntimeEnvironment" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+DO $$ 
+BEGIN 
+    IF NOT EXISTS (
+        SELECT 1 
+        FROM information_schema.table_constraints 
+        WHERE table_name = 'RuntimeEnvironment' 
+        AND constraint_name = 'RuntimeEnvironment_parentEnvironmentId_fkey'
+    ) THEN 
+        ALTER TABLE "RuntimeEnvironment" 
+        ADD CONSTRAINT "RuntimeEnvironment_parentEnvironmentId_fkey" 
+        FOREIGN KEY ("parentEnvironmentId") 
+        REFERENCES "RuntimeEnvironment" ("id") 
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE;
+    END IF;
+END $$;
