0410316e14260250649a6851d357db5548f78e8d
Eric Allam <eric@trigger.dev>
2025-05-27 13:28:45 +0100

    Enable prisma metrics and add them to the /metrics endpoint (#2111)
    * Enable prisma metrics and add them to the /metrics endpoint
    
    * Add support for bearer token auth on metrics endpoint

--- apps/webapp/app/routes/metrics.ts
+++ apps/webapp/app/routes/metrics.ts
@@ -1,8 +1,22 @@
 import { LoaderFunctionArgs } from "@remix-run/server-runtime";
+import { prisma } from "~/db.server";
 import { metricsRegister } from "~/metrics.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  return new Response(await metricsRegister.metrics(), {
+  // If the TRIGGER_METRICS_AUTH_PASSWORD is set, we need to check if the request has the correct password in auth header
+  const authPassword = process.env.TRIGGER_METRICS_AUTH_PASSWORD;
+
+  if (authPassword) {
+    const auth = request.headers.get("Authorization");
+    if (auth !== `Bearer ${authPassword}`) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+  }
+
+  const prismaMetrics = await prisma.$metrics.prometheus();
+  const coreMetrics = await metricsRegister.metrics();
+
+  return new Response(coreMetrics + prismaMetrics, {
     headers: {
       "Content-Type": metricsRegister.contentType,
     },
