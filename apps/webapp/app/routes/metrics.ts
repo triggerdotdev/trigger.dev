6726bf88524f77b86417d3c1b18da1daa5497cc7
Eric Allam <eallam@icloud.com>
2025-05-27 13:20:37 +0100

    Add support for bearer token auth on metrics endpoint

--- apps/webapp/app/routes/metrics.ts
+++ apps/webapp/app/routes/metrics.ts
@@ -3,6 +3,16 @@ import { prisma } from "~/db.server";
 import { metricsRegister } from "~/metrics.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
+  // If the TRIGGER_METRICS_AUTH_PASSWORD is set, we need to check if the request has the correct password in auth header
+  const authPassword = process.env.TRIGGER_METRICS_AUTH_PASSWORD;
+
+  if (authPassword) {
+    const auth = request.headers.get("Authorization");
+    if (auth !== `Bearer ${authPassword}`) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+  }
+
   const prismaMetrics = await prisma.$metrics.prometheus();
   const coreMetrics = await metricsRegister.metrics();
 
