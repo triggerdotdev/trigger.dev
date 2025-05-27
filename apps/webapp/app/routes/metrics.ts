d4813a2f3c9c283fe84340b7b642e0c3dcc9883e
Eric Allam <eallam@icloud.com>
2025-05-27 13:06:26 +0100

    Enable prisma metrics and add them to the /metrics endpoint

--- apps/webapp/app/routes/metrics.ts
+++ apps/webapp/app/routes/metrics.ts
@@ -1,8 +1,12 @@
 import { LoaderFunctionArgs } from "@remix-run/server-runtime";
+import { prisma } from "~/db.server";
 import { metricsRegister } from "~/metrics.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  return new Response(await metricsRegister.metrics(), {
+  const prismaMetrics = await prisma.$metrics.prometheus();
+  const coreMetrics = await metricsRegister.metrics();
+
+  return new Response(coreMetrics + prismaMetrics, {
     headers: {
       "Content-Type": metricsRegister.contentType,
     },
