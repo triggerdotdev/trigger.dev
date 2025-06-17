3bd478eee22b227ad09a8e9ed3f6c49cab9c46c4
Eric Allam <eallam@icloud.com>
2025-06-17 20:18:20 +0100

    Super-warm starts in deployed tasks proof

--- apps/supervisor/src/util.ts
+++ apps/supervisor/src/util.ts
@@ -1,5 +1,15 @@
 import { isMacOS, isWindows } from "std-env";
 
+export function normalizeDockerHostUrl(url: string) {
+  const $url = new URL(url);
+
+  if ($url.host === "localhost") {
+    $url.host = getDockerHostDomain();
+  }
+
+  return $url.toString();
+}
+
 export function getDockerHostDomain() {
   return isMacOS || isWindows ? "host.docker.internal" : "localhost";
 }
