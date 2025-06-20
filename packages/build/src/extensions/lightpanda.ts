29dbf6674568d32cab8d9407c22c7ac805fa37e5
Nicolas Rigaudiere <i.am.a@curious.engineer>
2025-06-20 11:36:26 +0200

    fix: lightpanda extension instructions

--- packages/build/src/extensions/lightpanda.ts
+++ packages/build/src/extensions/lightpanda.ts
@@ -1,26 +1,55 @@
 import type { BuildExtension } from "@trigger.dev/core/v3/build";
 
+const NAME = 'LightpandaExtension'
+
 type LightpandaOpts = {
   arch?: 'aarch64' | 'x86_64'
   version?: 'nightly'
+  disableTelemetry?: boolean
 }
 
-export const lightpanda = ({ arch = 'x86_64', version = 'nightly' }: LightpandaOpts = {}): BuildExtension => ({
-  name: "LightpandaExtension",
+export const lightpanda = ({ arch = 'x86_64', version = 'nightly', disableTelemetry = false }: LightpandaOpts = {}): BuildExtension => ({
+  name: NAME,
   onBuildComplete: async (context) => {
+    context.logger.progress(`Running ${NAME} on ${context.target} env for arch ${arch}`);
+    context.logger.progress(`version: ${version}`);
+
     if (context.target === "dev") {
       return
     }
 
-    context.logger.debug(lightpanda.name);
+    const instructions: string[] = []
+
+    if (disableTelemetry) {
+      instructions.push('RUN export LIGHTPANDA_DISABLE_TELEMETRY=true')
+    }
+
+    /* Update / install required packages */
+    instructions.push(
+      `RUN apt-get update && apt-get install --no-install-recommends -y \
+        curl \
+        ca-certificates \
+        && update-ca-certificates \
+        && apt-get clean && rm -rf /var/lib/apt/lists/*`,
+    )
+
+    /* Install Lightpanda */
+    instructions.push(
+      `RUN curl -L -o lightpanda https://github.com/lightpanda-io/browser/releases/download/${version}/lightpanda-${arch}-linux`,
+      'RUN chmod a+x ./lightpanda',
+      'RUN mv ./lightpanda /usr/bin/lightpanda',
+    )
+
     context.addLayer({
       id: "lightpanda",
       image: {
-        instructions: [
-          `RUN apt-get update && apt-get install curl -y \ &&
-            curl -L -o lightpanda https://github.com/lightpanda-io/browser/releases/download/${version}/lightpanda-${arch}-linux \ &&
-            chmod a+x ./lightpanda`,
-        ],
+        instructions,
+      },
+      deploy: {
+        env: {
+          LIGHTPANDA_BROWSER_PATH: "/usr/bin/lightpanda",
+        },
+        override: true,
       },
     })
   },
