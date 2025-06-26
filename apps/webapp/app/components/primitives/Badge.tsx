1b19697ad3fdd8a667731d5f8eff910c8d317981
Matt Aitken <matt@mattaitken.com>
2025-06-26 17:56:00 +0100

    New badge variant, removed unused ones

--- apps/webapp/app/components/primitives/Badge.tsx
+++ apps/webapp/app/components/primitives/Badge.tsx
@@ -4,14 +4,12 @@ import { cn } from "~/utils/cn";
 const variants = {
   default:
     "grid place-items-center rounded-full px-2 h-5 tracking-wider text-xxs bg-charcoal-750 text-text-bright uppercase whitespace-nowrap",
-  small:
-    "grid place-items-center rounded-full px-[0.4rem] h-4 tracking-wider text-xxs bg-background-dimmed text-text-dimmed uppercase whitespace-nowrap",
   "extra-small":
     "grid place-items-center border border-charcoal-650 rounded-sm px-1 h-4 text-xxs bg-background-bright text-blue-500 whitespace-nowrap",
-  outline:
-    "grid place-items-center rounded-sm px-1.5 h-5 tracking-wider text-xxs border border-dimmed text-text-dimmed uppercase whitespace-nowrap",
   "outline-rounded":
     "grid place-items-center rounded-full px-1 h-4 tracking-wider text-xxs border border-blue-500 text-blue-500 uppercase whitespace-nowrap",
+  rounded:
+    "grid place-items-center rounded-full px-1.5 h-4 text-xxs border bg-blue-600 text-text-bright uppercase whitespace-nowrap",
 };
 
 type BadgeProps = React.HTMLAttributes<HTMLDivElement> & {
