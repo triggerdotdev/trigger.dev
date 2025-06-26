f4d39d7979eef289a8de16f7d89aeadafa89901c
Matt Aitken <matt@mattaitken.com>
2025-06-26 17:56:00 +0100

    New badge variant, removed unused ones

--- apps/webapp/app/routes/storybook.badges/route.tsx
+++ apps/webapp/app/routes/storybook.badges/route.tsx
@@ -4,10 +4,7 @@ export default function Story() {
   return (
     <div className="flex flex-col items-start gap-y-8 p-8">
       <Badge>Default</Badge>
-      <div className="bg-charcoal-1000 p-4">
-        <Badge variant="small">Small</Badge>
-      </div>
-      <Badge variant="outline">Outline</Badge>
+      <Badge variant="rounded">3</Badge>
       <Badge variant="outline-rounded">Outline rounded</Badge>
     </div>
   );
