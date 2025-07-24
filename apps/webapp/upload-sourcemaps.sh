32df4b177398cf40b488591430439874dc843150
Eric Allam <eallam@icloud.com>
2025-07-24 14:03:18 +0100

    Delete source maps after uploading

--- apps/webapp/upload-sourcemaps.sh
+++ apps/webapp/upload-sourcemaps.sh
@@ -5,6 +5,8 @@ if [ -n "$SENTRY_ORG" ] && [ -n "$SENTRY_PROJECT" ] && [ -n "$SENTRY_AUTH_TOKEN"
   sentry-cli releases new $SENTRY_RELEASE
   sentry-cli sourcemaps inject ./build
   sentry-cli sourcemaps upload ./build --release $SENTRY_RELEASE
+  # Now we need to delete the sourcemaps from the build directory
+  rm -rf ./build/*.map
 else
   echo "Skipping sourcemap upload: Missing required environment variables"
   echo "Required: SENTRY_ORG, SENTRY_PROJECT, SENTRY_AUTH_TOKEN, SENTRY_RELEASE"
