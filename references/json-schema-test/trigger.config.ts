5371af620b3b942b96c9ea6a56b5246760f3d5c5
Eric Allam <eallam@icloud.com>
2025-08-06 11:38:21 +0100

    Remove the json-schema-test reference project

--- references/json-schema-test/trigger.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@trigger.dev/sdk/v3";
-
-export default defineConfig({
-  project: "json-schema-test",
-  retries: {
-    enabledInDev: false,
-  },
-  triggerDirectories: ["./src/trigger"],
-});
