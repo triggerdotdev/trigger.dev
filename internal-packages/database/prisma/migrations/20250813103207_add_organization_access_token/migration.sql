1b27cfbd7a9a707e38586e5ddce21cd14f0f5323
myftija <saadi.myftija@gmail.com>
2025-08-14 11:08:58 +0200

    Create schema and migration for organization access tokens

---
+++ internal-packages/database/prisma/migrations/20250813103207_add_organization_access_token/migration.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "OrganizationAccessToken" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "encryptedToken" JSONB NOT NULL,
+    "obfuscatedToken" TEXT NOT NULL,
+    "hashedToken" TEXT NOT NULL,
+    "organizationId" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3),
+    "revokedAt" TIMESTAMP(3),
+    "lastAccessedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OrganizationAccessToken_pkey" PRIMARY KEY ("id")
+);
+
+CREATE UNIQUE INDEX "OrganizationAccessToken_hashedToken_key" ON "OrganizationAccessToken"("hashedToken");
+
+ALTER TABLE "OrganizationAccessToken" ADD CONSTRAINT "OrganizationAccessToken_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
