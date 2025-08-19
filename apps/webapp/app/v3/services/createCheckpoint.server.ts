58df0538c415a9985137355d631070e65117a6c4
Matt Aitken <matt@mattaitken.com>
2025-08-19 16:47:01 +0100

    "CreateCheckpointService: Batch already resumed" is expected

--- apps/webapp/app/v3/services/createCheckpoint.server.ts
+++ apps/webapp/app/v3/services/createCheckpoint.server.ts
@@ -168,7 +168,7 @@ export class CreateCheckpointService extends BaseService {
         }
 
         if (batchRun.resumedAt) {
-          logger.error("CreateCheckpointService: Batch already resumed", {
+          logger.info("CreateCheckpointService: Batch already resumed", {
             batchRun,
             params,
           });
