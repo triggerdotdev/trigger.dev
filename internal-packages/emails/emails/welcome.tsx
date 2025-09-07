a42c7992282f857aa944c37d15dc1a1fb523a5ab
James Ritchie <james@trigger.dev>
2025-09-07 14:57:16 +0100

    Style improvements

--- internal-packages/emails/emails/welcome.tsx
+++ internal-packages/emails/emails/welcome.tsx
@@ -17,14 +17,14 @@ export default function Email({ name }: { name?: string }) {
       <Preview>Welcome to Trigger.dev</Preview>
       <Tailwind>
         <Body className="bg-[#121317] my-auto mx-auto font-sans">
-          <Container className="mb-[40px] mx-auto p-[20px] max-w-[600px]">
-            <Text className="text-[16px] text-[#D7D9DD]">Hey {name ?? "there"},</Text>
-            <Text className="text-[16px] text-[#D7D9DD]">I’m Matt, CEO of Trigger.dev.</Text>
-            <Text className="text-[16px] text-[#D7D9DD]">
+          <Container className="mb-[40px] mx-auto p-[20px] max-w-[600px] text-[#D7D9DD]">
+            <Text className="text-[16px]">Hey {name ?? "there"},</Text>
+            <Text className="text-[16px]">I’m Matt, CEO of Trigger.dev.</Text>
+            <Text className="text-[16px]">
               Our goal is to give developers like you the ability to effortlessly create powerful AI
               agents and workflows in code.
             </Text>
-            <Text className="text-[16px] text-[#D7D9DD]">
+            <Text className="text-[16px]">
               I recommend our{" "}
               <Link
                 className="text-[#6366F1] text-[16px] no-underline"
@@ -43,7 +43,7 @@ export default function Email({ name }: { name?: string }) {
               workflow.
             </Text>
 
-            <Text className="text-[16px] text-[#D7D9DD]">
+            <Text className="text-[16px]">
               Feel free to reply to this email if you have any questions or join our{" "}
               <Link
                 className="text-[#6366F1] text-[16px] no-underline"
@@ -54,12 +54,12 @@ export default function Email({ name }: { name?: string }) {
               to connect with the community and our team.
             </Text>
 
-            <Text className="text-[16px] text-[#D7D9DD]">We hope you enjoy using Trigger.dev!</Text>
+            <Text className="text-[16px]">We hope you enjoy using Trigger.dev!</Text>
 
-            <Text className="text-[16px] text-[#D7D9DD]">Best,</Text>
-            <Text className="text-[16px] text-[#D7D9DD]">Matt</Text>
-            <Text className="text-[16px] text-[#D7D9DD]">CEO, Trigger.dev</Text>
-            <Text className="text-[16px] text-[#D7D9DD]">
+            <Text className="text-[16px]">Best,</Text>
+            <Text className="text-[16px]">Matt</Text>
+            <Text className="text-[16px]">CEO, Trigger.dev</Text>
+            <Text className="text-[16px]">
               If you don’t want me to contact you again, please just let me know and I’ll update
               your preferences.
             </Text>
