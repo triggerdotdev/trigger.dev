b01c92c257b85c778fd54bb2f21b72e26c4f9d50
nicktrn <55853254+nicktrn@users.noreply.github.com>
2025-09-25 22:12:54 +0100

    feat(helm): support topology spread constraints

--- apps/supervisor/src/env.ts
+++ apps/supervisor/src/env.ts
@@ -90,6 +90,7 @@ const Env = z.object({
   KUBERNETES_MEMORY_REQUEST_MIN_GB: z.coerce.number().min(0).default(0),
   KUBERNETES_MEMORY_REQUEST_RATIO: z.coerce.number().min(0).max(1).default(1), // Ratio of memory limit, so 1 = 100% of memory limit
   KUBERNETES_MEMORY_OVERHEAD_GB: z.coerce.number().min(0).optional(), // Optional memory overhead to add to the limit in GB
+  KUBERNETES_TOPOLOGY_SPREAD_CONSTRAINTS: z.string().optional(), // JSON string
 
   // Placement tags settings
   PLACEMENT_TAGS_ENABLED: BoolEnv.default(false),
