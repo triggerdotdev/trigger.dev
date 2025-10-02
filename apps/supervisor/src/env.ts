9b656b8659e986214a70775644af08b6429f8e2f
nicktrn <55853254+nicktrn@users.noreply.github.com>
2025-10-02 09:35:03 +0100

    feat(supervisor): optional custom scheduler

--- apps/supervisor/src/env.ts
+++ apps/supervisor/src/env.ts
@@ -90,6 +90,7 @@ const Env = z.object({
   KUBERNETES_MEMORY_REQUEST_MIN_GB: z.coerce.number().min(0).default(0),
   KUBERNETES_MEMORY_REQUEST_RATIO: z.coerce.number().min(0).max(1).default(1), // Ratio of memory limit, so 1 = 100% of memory limit
   KUBERNETES_MEMORY_OVERHEAD_GB: z.coerce.number().min(0).optional(), // Optional memory overhead to add to the limit in GB
+  KUBERNETES_SCHEDULER_NAME: z.string().optional(), // Custom scheduler name for pods
 
   // Placement tags settings
   PLACEMENT_TAGS_ENABLED: BoolEnv.default(false),
