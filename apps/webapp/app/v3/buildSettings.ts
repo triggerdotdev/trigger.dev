965901c3f0de6c2a02598ec2de9bf35be4f75128
myftija <saadi.myftija@gmail.com>
2025-11-14 20:16:09 +0100

    feat(deployments): expose native build server option in build settings
    Adds a build setting about using our build server for remote builds.

--- apps/webapp/app/v3/buildSettings.ts
+++ apps/webapp/app/v3/buildSettings.ts
@@ -4,6 +4,7 @@ export const BuildSettingsSchema = z.object({
   triggerConfigFilePath: z.string().optional(),
   installCommand: z.string().optional(),
   preBuildCommand: z.string().optional(),
+  useNativeBuildServer: z.boolean().optional(),
 });
 
 export type BuildSettings = z.infer<typeof BuildSettingsSchema>;
