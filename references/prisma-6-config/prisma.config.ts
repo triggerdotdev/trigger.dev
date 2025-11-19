15fef916f6af65bfadddc629c73d1dd18edc667c
Eric Allam <eric@trigger.dev>
2025-11-19 10:48:15 +0000

    feat(build): update prisma extension to work with generated clients and rust-free clients (#2689)
    * prisma extension fixes WIP
    
    * More prisma stuff
    
    * more prisma stuff
    
    * remove changelog
    
    * upgrade github workflows to use node 20.19 because installing prisma@7 breaks with lower versions
    
    * Don't use generate for the prisma reference projects
    
    * make sure it works if no mode is passed in

---
+++ references/prisma-6-config/prisma.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, env } from "prisma/config";
+import "dotenv/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+    directUrl: env("DATABASE_URL_UNPOOLED"),
+  },
+});
---
+++ references/prisma-6/trigger.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "@trigger.dev/sdk";
+import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
+
+export default defineConfig({
+  project: process.env.TRIGGER_PROJECT_REF!,
+  logLevel: "debug",
+  maxDuration: 3600,
+  retries: {
+    enabledInDev: true,
+    default: {
+      maxAttempts: 3,
+      minTimeoutInMs: 1000,
+      maxTimeoutInMs: 10000,
+      factor: 2,
+      randomize: true,
+    },
+  },
+  machine: "small-1x",
+  build: {
+    extensions: [
+      prismaExtension({
+        mode: "legacy",
+        schema: "prisma/schema.prisma",
+        migrate: true,
+        typedSql: true,
+        directUrlEnvVarName: "DATABASE_URL_UNPOOLED",
+      }),
+    ],
+  },
+});
