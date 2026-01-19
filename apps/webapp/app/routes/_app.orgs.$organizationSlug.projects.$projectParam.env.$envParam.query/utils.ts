95ae6033e6b22ab67402a66a8e24ddfd6754fd98
Matt Aitken <matt@mattaitken.com>
2026-01-19 09:38:57 +0000

    formatBytes protection from divide by zero

--- apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam.env.$envParam.query/utils.ts
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam.env.$envParam.query/utils.ts
@@ -24,7 +24,10 @@ export function formatBytes(bytes: number): string {
   if (bytes < 0) return "-" + formatBytes(-bytes);
   const k = 1024;
   const sizes = ["B", "KB", "MB", "GB"];
-  const i = Math.min(Math.floor(Math.log(bytes) / Math.log(k)), sizes.length - 1);
+  const i = Math.max(
+    0,
+    Math.min(Math.floor(Math.log(bytes) / Math.log(k)), sizes.length - 1)
+  );
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(1))} ${sizes[i]}`;
 }
 
