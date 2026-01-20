84b05d6f3deec96264dd795b1728bc179d2acda8
Matt Aitken <matt@mattaitken.com>
2026-01-20 17:21:32 +0000

    Better AppliedFilter label hiding

--- apps/webapp/app/components/primitives/AppliedFilter.tsx
+++ apps/webapp/app/components/primitives/AppliedFilter.tsx
@@ -51,7 +51,7 @@ export function AppliedFilter({
       <div className={cn("flex items-start leading-4", label === undefined ? "gap-1.5" : "gap-0.5")}>
         <div className="-mt-[0.5px] flex items-center gap-1">
           {icon}
-          {label !== undefined && <div className="text-text-bright">
+          {label && <div className="text-text-bright">
             <span>{label}</span>:
           </div>}
         </div>
