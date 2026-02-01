6b659a31fa42fd848ece33db7e78b0ef4fff042c
Matt Aitken <matt@mattaitken.com>
2026-02-01 20:31:39 +0000

    Time filtering and auto-reloading

--- apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam.env.$envParam.metrics.$dashboardKey/route.tsx
+++ apps/webapp/app/routes/_app.orgs.$organizationSlug.projects.$projectParam.env.$envParam.metrics.$dashboardKey/route.tsx
@@ -18,6 +18,10 @@ import { MetricWidget } from "../resources.metric";
 import { useOrganization } from "~/hooks/useOrganizations";
 import { useProject } from "~/hooks/useProject";
 import { useEnvironment } from "~/hooks/useEnvironment";
+import { TimeFilter, timeFilterFromTo, timeFilters } from "~/components/runs/v3/SharedFilters";
+import { useCurrentPlan } from "../_app.orgs.$organizationSlug/route";
+import { useSearchParams } from "~/hooks/useSearchParam";
+import parse from "parse-duration";
 
 const ParamSchema = EnvironmentParamSchema.extend({
   dashboardKey: z.string(),
@@ -44,13 +48,16 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   }
 
   const presenter = new MetricDashboardPresenter();
-  const dashboard = await presenter.builtInDashboard(dashboardKey);
+  const dashboard = await presenter.builtInDashboard({
+    organizationId: project.organizationId,
+    key: dashboardKey,
+  });
 
   return typedjson(dashboard);
 };
 
 export default function Page() {
-  const { title, layout } = useTypedLoaderData<typeof loader>();
+  const { title, layout, defaultPeriod } = useTypedLoaderData<typeof loader>();
 
   return (
     <PageContainer>
@@ -59,46 +66,86 @@ export default function Page() {
       </NavBar>
       <PageBody scrollable={false}>
         <div className="h-full">
-          <MetricDashboard layout={layout} />
+          <MetricDashboard layout={layout} defaultPeriod={defaultPeriod} />
         </div>
       </PageBody>
     </PageContainer>
   );
 }
 
-function MetricDashboard({ layout }: { layout: DashboardLayout }) {
+function determineRefreshIntervalMs(props: {
+  period?: string;
+  from?: string;
+  to?: string;
+}): number {
+  const { from, to } = timeFilterFromTo({ ...props, defaultPeriod: "7d" });
+  const intervalMs = to.getTime() - from.getTime();
+
+  //Refresh 4 times in the period
+  return intervalMs / 4;
+}
+
+function MetricDashboard({
+  layout,
+  defaultPeriod,
+}: {
+  layout: DashboardLayout;
+  defaultPeriod: string;
+}) {
+  const { value } = useSearchParams();
   const { width, containerRef, mounted } = useContainerWidth({ measureBeforeMount: true });
+
   const organization = useOrganization();
   const project = useProject();
   const environment = useEnvironment();
 
+  const plan = useCurrentPlan();
+  const maxPeriodDays = plan?.v3Subscription?.plan?.limits?.queryPeriodDays?.number;
+
+  const period = value("period");
+  const from = value("from");
+  const to = value("to");
+
+  const refreshIntervalMs = determineRefreshIntervalMs({ period, from, to });
+
   return (
-    //@ts-expect-error TODO fix this legacy ref
-    <div ref={containerRef} className="h-full">
-      {mounted && (
-        <ReactGridLayout
-          layout={layout.layout}
-          width={width}
-          gridConfig={{ cols: 12, rowHeight: 30 }}
-        >
-          {Object.entries(layout.widgets).map(([key, widget]) => (
-            <div key={key}>
-              <MetricWidget
-                title={widget.title}
-                query={widget.query}
-                scope="environment"
-                period={null}
-                from={null}
-                to={null}
-                config={widget.display}
-                organizationId={organization.id}
-                projectId={project.id}
-                environmentId={environment.id}
-              />
-            </div>
-          ))}
-        </ReactGridLayout>
-      )}
+    <div className="grid grid-rows-[auto_1fr]">
+      <div className="flex items-center">
+        <TimeFilter
+          defaultPeriod={defaultPeriod}
+          labelName="Period"
+          hideLabel
+          maxPeriodDays={maxPeriodDays}
+        />
+      </div>
+      {/* @ts-expect-error TODO fix this legacy ref */}
+      <div ref={containerRef}>
+        {mounted && (
+          <ReactGridLayout
+            layout={layout.layout}
+            width={width}
+            gridConfig={{ cols: 12, rowHeight: 30 }}
+          >
+            {Object.entries(layout.widgets).map(([key, widget]) => (
+              <div key={key}>
+                <MetricWidget
+                  title={widget.title}
+                  query={widget.query}
+                  scope="environment"
+                  period={period ?? null}
+                  from={from ?? null}
+                  to={to ?? null}
+                  config={widget.display}
+                  organizationId={organization.id}
+                  projectId={project.id}
+                  environmentId={environment.id}
+                  refreshIntervalMs={refreshIntervalMs}
+                />
+              </div>
+            ))}
+          </ReactGridLayout>
+        )}
+      </div>
     </div>
   );
 }
