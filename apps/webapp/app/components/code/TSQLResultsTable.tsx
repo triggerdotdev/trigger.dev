eb47ff67ef186334e7fe1b8a5f18ec94009ec706
James Ritchie <james@trigger.dev>
2026-02-11 09:51:32 +0000

    Table header tweak

--- apps/webapp/app/components/code/TSQLResultsTable.tsx
+++ apps/webapp/app/components/code/TSQLResultsTable.tsx
@@ -1128,7 +1128,7 @@ export const TSQLResultsTable = memo(function TSQLResultsTable({
                       onMouseDown={header.getResizeHandler()}
                       onTouchStart={header.getResizeHandler()}
                       className={cn(
-                        "absolute right-0 top-0 h-full w-1 cursor-col-resize touch-none select-none",
+                        "absolute right-0 top-0 h-full w-0.5 cursor-col-resize touch-none select-none",
                         "opacity-0 group-hover/header:opacity-100",
                         "bg-charcoal-600 hover:bg-indigo-500",
                         header.column.getIsResizing() && "bg-indigo-500 opacity-100"
