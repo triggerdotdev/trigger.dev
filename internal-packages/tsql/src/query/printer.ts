27611efb607ade5e6b4ca78948766846c16f4d6e
Matt Aitken <matt@mattaitken.com>
2026-02-17 13:33:56 +0000

    Added millisecond, microsecond and nanosecond support to date fns

--- internal-packages/tsql/src/query/printer.ts
+++ internal-packages/tsql/src/query/printer.ts
@@ -2869,6 +2869,9 @@ export class ClickHousePrinter {
    * Valid ClickHouse interval unit keywords used by date functions like dateAdd, dateDiff, etc.
    */
   private static readonly INTERVAL_UNITS = new Set([
+    "nanosecond",
+    "microsecond",
+    "millisecond",
     "second",
     "minute",
     "hour",
