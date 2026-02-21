e963271289ef2ad74012b55bcd7772690e27f725
Eric Allam <eallam@icloud.com>
2026-02-21 15:18:22 +0000

    expose a useTriggerChatTransport hook

--- packages/trigger-sdk/src/v3/chat.ts
+++ packages/trigger-sdk/src/v3/chat.ts
@@ -391,3 +391,4 @@ export class TriggerChatTransport implements ChatTransport<UIMessage> {
 export function createChatTransport(options: TriggerChatTransportOptions): TriggerChatTransport {
   return new TriggerChatTransport(options);
 }
+
