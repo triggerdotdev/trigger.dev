a0c598a967c0444a1f72625ce500770119851f75
James Ritchie <james@trigger.dev>
2026-03-24 14:58:44 +0000

    Fixes hover behaviour and z-index issues of Resizable

--- apps/webapp/app/components/primitives/Resizable.tsx
+++ apps/webapp/app/components/primitives/Resizable.tsx
@@ -4,19 +4,16 @@ import React from "react";
 import { PanelGroup, Panel, PanelResizer } from "react-window-splitter";
 import { cn } from "~/utils/cn";
 
-const ResizablePanelGroup = ({ className, snapshot, ...props }: React.ComponentProps<typeof PanelGroup>) => {
-  return (
-    <PanelGroup
-      className={cn(
-        "flex w-full overflow-hidden data-[panel-group-direction=vertical]:flex-col",
-        className
-      )}
-      autosaveStrategy={props.autosaveId ? "cookie" : undefined}
-      snapshot={snapshot}
-      {...props}
-    />
-  );
-};
+const ResizablePanelGroup = ({ className, ...props }: React.ComponentProps<typeof PanelGroup>) => (
+  <PanelGroup
+    className={cn(
+      "flex w-full overflow-hidden data-[panel-group-direction=vertical]:flex-col",
+      className
+    )}
+    autosaveStrategy={props.autosaveId ? "cookie" : undefined}
+    {...props}
+  />
+);
 
 const ResizablePanel = Panel;
 
@@ -30,7 +27,7 @@ const ResizableHandle = ({
   <PanelResizer
     className={cn(
       // Base styles
-      "group relative isolate flex items-center justify-center focus-custom",
+      "group relative flex items-center justify-center focus-custom",
       // Horizontal orientation (default)
       "w-0.75 after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2",
       // Vertical orientation
@@ -45,19 +42,19 @@ const ResizableHandle = ({
     {...props}
   >
     {/* Horizontal orientation line indicator */}
-    <div className="absolute left-[0.0625rem] top-0 h-full w-px bg-grid-bright transition group-hover:left-0 group-hover:w-0.75 group-hover:bg-indigo-500 group-data-[handle-orientation=vertical]:hidden" />
+    <div className="absolute left-[0.0625rem] top-0 z-20 h-full w-px bg-grid-bright transition group-hover:left-0 group-hover:w-0.75 group-hover:bg-indigo-500 group-data-[handle-orientation=vertical]:hidden" />
     {/* Vertical orientation line indicator */}
-    <div className="absolute left-0 top-[0.0625rem] hidden h-px w-full bg-grid-bright transition group-hover:top-0 group-hover:h-0.75 group-hover:bg-indigo-500 group-data-[handle-orientation=vertical]:block" />
+    <div className="absolute left-0 top-[0.0625rem] z-20 hidden h-px w-full bg-grid-bright transition group-hover:top-0 group-hover:h-0.75 group-hover:bg-indigo-500 group-data-[handle-orientation=vertical]:block" />
     {withHandle && (
       <>
         {/* Horizontal orientation dots (vertical arrangement) */}
-        <div className="relative flex h-5 w-0.75 flex-col items-center justify-center gap-[0.1875rem] bg-background-dimmed transition-opacity group-hover:opacity-0 group-data-[handle-orientation=vertical]:hidden">
+        <div className="z-10 flex h-5 w-0.75 flex-col items-center justify-center gap-[0.1875rem] bg-background-dimmed group-hover:hidden group-data-[handle-orientation=vertical]:hidden">
           {Array.from({ length: 3 }).map((_, index) => (
             <div key={index} className="h-[0.1875rem] w-0.75 rounded-full bg-charcoal-600" />
           ))}
         </div>
         {/* Vertical orientation dots (horizontal arrangement) */}
-        <div className="relative hidden h-0.75 w-5 flex-row items-center justify-center gap-[0.1875rem] bg-background-dimmed transition-opacity group-hover:opacity-0 group-data-[handle-orientation=vertical]:flex">
+        <div className="z-10 hidden h-0.75 w-5 flex-row items-center justify-center gap-[0.1875rem] bg-background-dimmed group-hover:hidden group-data-[handle-orientation=vertical]:flex">
           {Array.from({ length: 3 }).map((_, index) => (
             <div key={index} className="h-0.75 w-[0.1875rem] rounded-full bg-charcoal-600" />
           ))}
