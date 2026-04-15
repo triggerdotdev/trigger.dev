98c1d5535c44506512356ca5c66ec5bb7c0a1bd4
James Ritchie <james@trigger.dev>
2026-04-15 18:00:04 +0100

    Scheduled filters improvements

--- apps/webapp/app/components/runs/v3/ScheduleFilters.tsx
+++ apps/webapp/app/components/runs/v3/ScheduleFilters.tsx
@@ -1,12 +1,10 @@
 import * as Ariakit from "@ariakit/react";
-import { ClockIcon, MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/20/solid";
+import { ClockIcon, XMarkIcon } from "@heroicons/react/20/solid";
 import { useNavigate } from "@remix-run/react";
-import { AnimatePresence, motion } from "framer-motion";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useRef } from "react";
 import { z } from "zod";
-import { TaskIcon } from "~/assets/icons/TaskIcon";
 import { AppliedFilter } from "~/components/primitives/AppliedFilter";
-import { Input } from "~/components/primitives/Input";
+import { SearchInput } from "~/components/primitives/SearchInput";
 import {
   SelectItem,
   SelectList,
@@ -15,14 +13,10 @@ import {
 } from "~/components/primitives/Select";
 import { ShortcutKey } from "~/components/primitives/ShortcutKey";
 import { useOptimisticLocation } from "~/hooks/useOptimisticLocation";
-import { useSearchParams } from "~/hooks/useSearchParam";
 import { useShortcutKeys } from "~/hooks/useShortcutKeys";
-import { useThrottle } from "~/hooks/useThrottle";
-import { cn } from "~/utils/cn";
 import { Button } from "../../primitives/Buttons";
-import { ScheduleTypeCombo, ScheduleTypeIcon, scheduleTypeName } from "./ScheduleType";
+import { ScheduleTypeIcon, scheduleTypeName } from "./ScheduleType";
 import { FilterMenuProvider } from "./SharedFilters";
-import { ScheduleIcon } from "~/assets/icons/ScheduleIcon";
 
 export const ScheduleListFilters = z.object({
   page: z.coerce.number().default(1),
@@ -57,67 +51,7 @@ export function ScheduleFilters({ possibleTasks }: ScheduleFiltersProps) {
 }
 
 function ScheduleSearchInput() {
-  const navigate = useNavigate();
-  const location = useOptimisticLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const initialSearch = searchParams.get("search") ?? "";
-  const [text, setText] = useState(initialSearch);
-  const [isFocused, setIsFocused] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const throttledSearch = useThrottle((value: string) => {
-    const params = new URLSearchParams(location.search);
-    if (value.length > 0) {
-      params.set("search", value);
-    } else {
-      params.delete("search");
-    }
-    params.delete("page");
-    navigate(`${location.pathname}?${params.toString()}`);
-  }, 300);
-
-  return (
-    <motion.div
-      initial={{ width: "auto" }}
-      animate={{ width: isFocused && text.length > 0 ? "24rem" : "auto" }}
-      transition={{
-        type: "spring",
-        stiffness: 300,
-        damping: 30,
-      }}
-      className="relative h-6 min-w-44"
-    >
-      <AnimatePresence>
-        {isFocused && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 0.5 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.2, ease: "linear" }}
-            className="pointer-events-none absolute inset-0 h-6 rounded border border-charcoal-500"
-          />
-        )}
-      </AnimatePresence>
-      <div className="absolute inset-0 left-0 top-0 h-6">
-        <Input
-          type="text"
-          variant="secondary-small"
-          placeholder="Search schedules…"
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-            throttledSearch(e.target.value);
-          }}
-          fullWidth
-          ref={inputRef}
-          className={cn(isFocused && "placeholder:text-text-dimmed/70")}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          icon={<MagnifyingGlassIcon className="size-4" />}
-        />
-      </div>
-    </motion.div>
-  );
+  return <SearchInput placeholder="Search schedules…" resetParams={["page"]} />;
 }
 
 const typeShortcut = { key: "y" };
