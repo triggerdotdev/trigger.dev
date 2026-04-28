05095c53028bd25d9ef30383265935a925d34103
James Ritchie <james@trigger.dev>
2026-04-28 19:09:24 +0100

    Merge the 2 db migrations

--- internal-packages/database/prisma/migrations/20260428140746_add_session_duration_columns/migration.sql
+++ internal-packages/database/prisma/migrations/20260428140746_add_session_duration_columns/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "public"."User" ADD COLUMN "sessionDuration" INTEGER NOT NULL DEFAULT 31536000;
+ALTER TABLE "public"."User" ADD COLUMN "sessionDuration" INTEGER NOT NULL DEFAULT 31556952;
 
 -- AlterTable
 ALTER TABLE "public"."Organization" ADD COLUMN "maxSessionDuration" INTEGER;
--- internal-packages/database/prisma/migrations/20260428152919_use_gregorian_year_for_user_session_duration_default/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."User" ALTER COLUMN "sessionDuration" SET DEFAULT 31556952;
