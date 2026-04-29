187cea36464a6395ffeaa30bf13073b25621dd8e
isshaddad <issamhaddad47@gmail.com>
2026-04-29 09:13:59 -0400

    feat(webapp): add Coupon Deals sub-tab to Back office

--- apps/webapp/app/routes/admin.back-office.tsx
+++ apps/webapp/app/routes/admin.back-office.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "@remix-run/react";
 import type { LoaderFunctionArgs } from "@remix-run/server-runtime";
 import { redirect, typedjson } from "remix-typedjson";
+import { Tabs } from "~/components/primitives/Tabs";
 import { requireUser } from "~/services/session.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -17,6 +18,17 @@ export default function BackOfficeLayout() {
       aria-labelledby="primary-heading"
       className="flex h-full min-w-0 flex-1 flex-col overflow-y-auto px-4 pb-4 lg:order-last"
     >
+      <div className="flex items-center pt-2">
+        <Tabs
+          tabs={[
+            {
+              label: "Coupon Deals",
+              to: "/admin/back-office/coupons",
+            },
+          ]}
+          layoutId="admin-back-office"
+        />
+      </div>
       <Outlet />
     </main>
   );
