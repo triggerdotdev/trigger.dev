706a898ea448a4b9cd5a2f544c3f3523eca20e87
deepshekhardas <deepshekharadas1234@gmail.com>
2026-06-02 12:04:06 +0530

    fix: prevent nanosecond overflow in OTLP timestamps
    Multiply after BigInt conversion to avoid IEEE 754 precision loss.
    Affects 4 locations where epoch-ms * 1e6 exceeds Number.MAX_SAFE_INTEGER.
    
    Fixes #3292

--- apps/webapp/app/v3/runEngineHandlers.server.ts
+++ apps/webapp/app/v3/runEngineHandlers.server.ts
@@ -464,7 +464,7 @@ export function registerRunEngineEventBusHandlers() {
         );
 
         await eventRepository.recordEvent(retryMessage, {
-          startTime: BigInt(time.getTime() * 1000000),
+          startTime: BigInt(time.getTime()) * BigInt(1_000_000),
           taskSlug: run.taskIdentifier,
           environment,
           attributes: {
