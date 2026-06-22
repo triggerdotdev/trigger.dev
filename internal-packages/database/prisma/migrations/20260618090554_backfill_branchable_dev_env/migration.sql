0841d7ea8829ca96f39b0d007d97380a1e28d277
Chris Arderne <chris@trigger.dev>
2026-06-22 14:39:10 +0100

    Revert "chore(db): backfill isBranchableEnvironment for existing dev environments"
    This reverts commit 7e92114464a694bb323adf67f818d9040aee8747.

--- internal-packages/database/prisma/migrations/20260618090554_backfill_branchable_dev_env/migration.sql
+++
@@ -1,6 +0,0 @@
--- Dev environments are now branchable, backfill all existing
-UPDATE "RuntimeEnvironment"
-SET "isBranchableEnvironment" = true
-WHERE "type" = 'DEVELOPMENT'
-  AND "isBranchableEnvironment" = false
-  AND "archivedAt" IS NULL;
