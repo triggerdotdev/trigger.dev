c9a0284e6d6e1b269e7b4b86e6fa206a78dfc705
Chris Arderne <chris@trigger.dev>
2026-08-17 14:13:13 +0100

    refactor(observability-map): remove unused entry point

--- internal-packages/observability-map/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { scanDirectory, scanFile } from "./scan.js";
-export { buildReport, scoreEntry } from "./score.js";
-export { renderTerminal } from "./report/terminal.js";
-export { renderJson } from "./report/json.js";
-export type { MapReport, ScoredEntry } from "./score.js";
-export type { EntryPoint, CheckResult, CheckStatus } from "./types.js";
